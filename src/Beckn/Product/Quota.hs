9f9d683cac4220355d3ff70c5e015f8bb6476c23
Shawn Rebello <shawn.rebello@juspay.in>
2020-04-29 17:29:38 +0530

    quota apis impl

--- src/Beckn/Product/Quota.hs
+++ src/Beckn/Product/Quota.hs
@@ -1,27 +1,74 @@
 module Beckn.Product.Quota where
 
-import qualified Beckn.Data.Accessor       as Accessor
+import qualified Beckn.Storage.Queries.Quota as DB
 import           Beckn.Types.API.Quota
 import           Beckn.Types.App
 import           Beckn.Types.Common
-import           Beckn.Types.Storage.Quota as Storage
+import           Beckn.Types.Storage.Quota   as Storage
+import           Beckn.Utils.Common
+import           Beckn.Utils.Routes
+import           Beckn.Utils.Storage
 import           Data.Aeson
 import           Data.Default
+import           Data.Time
+import qualified Database.Beam.Schema.Tables as B
+import qualified EulerHS.Language            as L
 import           EulerHS.Prelude
-
+import           Servant
 
 create :: Maybe RegistrationToken -> CreateReq -> FlowHandler CreateRes
-create mRegToken CreateReq {..} =  pure $ def CreateRes
+create mRegToken CreateReq {..} =  withFlowHandler $ do
+   verifyToken mRegToken
+   id <- generateGUID
+   quota <-  quotaRec id
+   DB.create quota
+   eres <- DB.findById id
+   case eres of
+     Right (Just quotaDb) -> return $ CreateRes quotaDb
+     _                 -> L.throwException $ err500 {errBody = "Could not create Quota"}
+    where
+      quotaRec id = do
+        now  <- getCurrTime
+        return Storage.Quota
+          { _id         = id
+          , _createdAt  = now
+          , _updatedAt  = now
+          , _info       = Nothing
+          ,..
+          }
 
 update :: Maybe RegistrationToken -> QuotaId -> UpdateReq -> FlowHandler UpdateRes
-update mRegToken id UpdateReq {..} =  pure $ def UpdateRes
+update mRegToken id UpdateReq {..} =  withFlowHandler $ do
+  verifyToken mRegToken
+  eres <- DB.update id _maxAllowed _startTime _endTime
+  case eres of
+    Left err -> L.throwException $ err500 {errBody = ("DBError: " <> show err)}
+    Right _ ->
+      DB.findById id
+      >>= \case
+        Right (Just v) -> return $ UpdateRes v
+        Right Nothing -> L.throwException $ err400 {errBody = "Quota not found"}
+        Left err -> L.throwException $ err500 {errBody = ("DBError: " <> show err)}
 
 list ::
   Maybe RegistrationToken
-  -> Maybe Text
   -> Maybe Limit
-  -> Maybe Offset -> FlowHandler ListRes
-list mRegToken mtype mlimit moffset = pure $ ListRes {_quotas = [def Storage.Quota]}
+  -> Maybe Offset
+  -> EntityType
+  -> Text
+  -> FlowHandler ListRes
+list mRegToken mlimit moffset entityType entityId = withFlowHandler $ do
+  verifyToken mRegToken
+  DB.findAllWithLimitOffset mlimit moffset entityType entityId
+  >>= \case
+      Left err -> L.throwException $ err500 {errBody = ("DBError: " <> show err)}
+      Right v -> return $ ListRes v
 
 get :: Maybe Text -> QuotaId -> FlowHandler GetRes
-get regToken userId = pure $ def Quota
+get mRegToken quotaId = withFlowHandler $ do
+  verifyToken mRegToken
+  DB.findById quotaId
+  >>= \case
+    Right (Just user) -> return user
+    Right Nothing -> L.throwException $ err400 {errBody = "Quota not found"}
+    Left err -> L.throwException $ err500 {errBody = ("DBError: " <> show err)}
