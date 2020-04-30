73835d16b6c06cdbf46dbb7fe85d31c9c965009f
Nikith Shetty <shettynikith3@gmail.com>
2020-04-30 13:03:31 +0530

    OTP login flow for User

--- src/Beckn/Product/Registration.hs
+++ src/Beckn/Product/Registration.hs
@@ -5,11 +5,13 @@ import qualified Beckn.External.MyValuesFirst.Flow       as Sms
 import qualified Beckn.External.MyValuesFirst.Types      as Sms
 import qualified Beckn.Storage.Queries.Customer          as QC
 import qualified Beckn.Storage.Queries.RegistrationToken as QR
+import qualified Beckn.Storage.Queries.User              as User
 import           Beckn.Types.API.Registration
 import           Beckn.Types.App
 import           Beckn.Types.Common
 import qualified Beckn.Types.Storage.Customer            as SC
 import qualified Beckn.Types.Storage.RegistrationToken   as SR
+import qualified Beckn.Types.Storage.User                as SU
 import           Beckn.Utils.Common
 import           Beckn.Utils.Extra
 import           Beckn.Utils.Routes
@@ -17,6 +19,7 @@ import           Beckn.Utils.Storage
 import qualified Crypto.Number.Generate                  as Cryptonite
 import           Data.Aeson
 import qualified Data.Text                               as T
+import           Data.Time.LocalTime
 import qualified EulerHS.Language                        as L
 import           EulerHS.Prelude
 import           Servant
@@ -26,68 +29,86 @@ initiateLogin :: InitiateLoginReq -> FlowHandler InitiateLoginRes
 initiateLogin req =
   withFlowHandler $ do
     case (req ^. Lens.medium, req ^. Lens._type) of
-      (SR.SMS, SR.OTP) -> initiateSms req
+      (SR.SMS, SR.OTP) -> initiateFlow req
       _ -> L.throwException $ err400 {errBody = "UNSUPPORTED_MEDIUM_TYPE"}
 
-initiateSms :: InitiateLoginReq -> L.Flow InitiateLoginRes
-initiateSms req = do
-  otp <- generateOTPCode
-  submitOtpCode (req ^. Lens._value) otp
-  uuid <- L.generateGUID
-  uuidRt <- L.generateGUID
-  uuidR <- L.generateGUID
+initiateFlow :: InitiateLoginReq -> L.Flow InitiateLoginRes
+initiateFlow req = do
+  let entityType = req ^. Lens.entityType
+  let mobileNumber = req ^. Lens.identifier
+  entityId <-
+    case entityType of
+      SR.CUSTOMER -> do
+        cust <- makeCustomer req
+        QC.create cust
+        return $ _getCustomerId $ SC._id cust
+      SR.USER -> do
+        user <- fromMaybeM400 "User not found" =<< User.findByMobileNumber mobileNumber
+        return $ _getUserId $ SU._id user
+  regToken <- makeSession req entityId entityType
+  QR.create regToken
+  sendOTP mobileNumber (SR._authValueHash regToken)
+  let
+    attempts = SR._attempts regToken
+    tokenId = SR._id regToken
+  return $ InitiateLoginRes {attempts, tokenId}
+
+makeCustomer :: InitiateLoginReq -> L.Flow SC.Customer
+makeCustomer req = do
+  role <- fromMaybeM400 "CUSTOMER_ROLE required" (req ^. Lens.role)
+  id <- generateGUID
   now <- getCurrentTimeUTC
-  let cust = makeCustomer uuid now
-  QC.create cust
+  return $ SC.Customer
+    { _id = id
+    , _name = Nothing
+    , _OrganizationId = Nothing
+    , _TenantOrganizationId = Nothing
+    , _verified = False
+    , _role = role
+    , _info = Nothing
+    , _createdAt = now
+    , _updatedAt = now
+    }
+
+makeSession :: InitiateLoginReq -> Text -> SR.RTEntityType -> L.Flow SR.RegistrationToken
+makeSession req entityId entityType = do
+  otp <- generateOTPCode
+  id <- L.generateGUID
+  token <- L.generateGUID
+  now <- getCurrentTimeUTC
   attempts <-
     L.runIO $ fromMaybe 3 . (>>= readMaybe) <$> lookupEnv "SMS_ATTEMPTS"
   authExpiry <-
     L.runIO $ fromMaybe 3 . (>>= readMaybe) <$> lookupEnv "AUTH_EXPIRY"
   tokenExpiry <-
     L.runIO $ fromMaybe 365 . (>>= readMaybe) <$> lookupEnv "TOKEN_EXPIRY"
-  let regToken = makeSession uuidR uuidRt uuid now otp attempts authExpiry tokenExpiry
-  QR.create regToken
-  return $ InitiateLoginRes {attempts = attempts, tokenId = uuidR}
+  return $ SR.RegistrationToken
+    { _id = id
+    , _token = token
+    , _attempts = attempts
+    , _authMedium = (req ^. Lens.medium)
+    , _authType = (req ^. Lens._type)
+    , _authValueHash = otp
+    , _verified = False
+    , _authExpiry = authExpiry
+    , _tokenExpiry = tokenExpiry
+    , _EntityId = entityId
+    , _entityType = entityType
+    , _createdAt = now
+    , _updatedAt = now
+    , _info = Nothing
+    }
+
+generateOTPCode :: L.Flow Text
+generateOTPCode =
+  L.runIO $ padLeft 4 '0' . show <$> Cryptonite.generateBetween 1 9999
   where
-    makeCustomer uuid now =
-      SC.Customer
-        (CustomerId uuid)
-        Nothing
-        Nothing
-        Nothing
-        False
-        (req ^. Lens.role)
-        Nothing
-        now
-        now
-
-    makeSession uuidR uuidRt uuid now otp a ae te =
-      SR.RegistrationToken
-        uuidR
-        uuidRt
-        a
-        (req ^. Lens.medium)
-        (req ^. Lens._type)
-        otp
-        False
-        ae
-        te
-        uuid
-        SR.CUSTOMER
-        now
-        now
-        Nothing
-
-    generateOTPCode :: L.Flow Text
-    generateOTPCode =
-      L.runIO $ padLeft 4 '0' . show <$> Cryptonite.generateBetween 1 9999
-
     padLeft n c txt =
       let prefix = replicate (max 0 $ n - length txt) c
        in T.pack prefix <> txt
 
-submitOtpCode :: Text -> Text -> L.Flow ()
-submitOtpCode phoneNumber otpCode = do
+sendOTP :: Text -> Text -> L.Flow ()
+sendOTP phoneNumber otpCode = do
   username <- L.runIO $ getEnv "SMS_GATEWAY_USERNAME"
   password <- L.runIO $ getEnv "SMS_GATEWAY_PASSWORD"
   res <-
@@ -103,36 +124,58 @@ submitOtpCode phoneNumber otpCode = do
   whenLeft res $ \err -> L.throwException err503 {errBody = encode err}
 
 login :: Text -> LoginReq -> FlowHandler LoginRes
-login tokenId req =
-  withFlowHandler $ do
-    SR.RegistrationToken {..} <-
-      QR.findRegistrationToken tokenId >>= fromMaybeM400 "INVALID_TOKEN"
-    cust <-
-      QC.findCustomerById (CustomerId _EntityId) >>=
-      fromMaybeM400 "INVALID_DATA"
-    unlessM (isExpired (realToFrac (_authExpiry * 60)) _updatedAt) $
-      L.throwException $ err400 { errBody = "AUTH_EXPIRED" }
-    when _verified $ L.throwException $ err400 {errBody = "ALREADY_VERIFIED"}
-    let verify =
-          _authMedium == req ^. Lens.medium && _authType == req ^. Lens._type &&
-          _authValueHash == req ^. Lens.hash
-    if verify
-      then do
-        QC.updateStatus True (SC._id cust)
-        return $ LoginRes _token cust
-      else L.throwException $ err400 {errBody = "VALUE_MISMATCH"}
+login tokenId req = withFlowHandler $ do
+  SR.RegistrationToken {..} <- checkRegistrationTokenExists tokenId
+  when _verified $ L.throwException $ err400 {errBody = "ALREADY_VERIFIED"}
+  checkForExpiry _authExpiry _updatedAt
+
+  let isValid =
+        _authMedium == req ^. Lens.medium && _authType == req ^. Lens._type &&
+        _authValueHash == req ^. Lens.hash
+  if isValid
+    then do
+      case _entityType of
+        SR.CUSTOMER -> do
+          cust <- checkCustomerExists _EntityId
+          QC.updateStatus True (SC._id cust)
+          return $ LoginRes _token (Just cust) Nothing
+
+        SR.USER -> do
+          user <- checkUserExists _EntityId
+          User.update (SU._id user) SU.ACTIVE Nothing Nothing Nothing
+          return $ LoginRes _token Nothing (Just user)
+
+    else L.throwException $ err400 {errBody = "VALUE_MISMATCH"}
+  where
+    checkForExpiry authExpiry updatedAt =
+      unlessM (isExpired (realToFrac (authExpiry * 60)) updatedAt) $
+        L.throwException $ err400 { errBody = "AUTH_EXPIRED" }
+
+checkRegistrationTokenExists :: Text -> L.Flow SR.RegistrationToken
+checkRegistrationTokenExists tokenId =
+  QR.findRegistrationToken tokenId
+    >>= fromMaybeM400 "INVALID_TOKEN"
+
+checkCustomerExists :: Text -> L.Flow SC.Customer
+checkCustomerExists _EntityId =
+  QC.findCustomerById (CustomerId _EntityId)
+    >>= fromMaybeM400 "INVALID_DATA"
+
+checkUserExists :: Text -> L.Flow SU.User
+checkUserExists _EntityId =
+  User.findById (UserId _EntityId)
+    >>= fromMaybeM400 "INVALID_DATA"
 
 reInitiateLogin :: Text -> ReInitiateLoginReq -> FlowHandler InitiateLoginRes
-reInitiateLogin tokenId req =
-  withFlowHandler $ do
-    SR.RegistrationToken {..} <-
-      QR.findRegistrationToken tokenId >>= fromMaybeM400 "INVALID_TOKEN"
-    cust <-
-      QC.findCustomerById (CustomerId _EntityId) >>=
-      fromMaybeM400 "INVALID_DATA"
-    if _attempts > 0
-      then do
-        submitOtpCode (req ^. Lens._value) _authValueHash
-        QR.updateAttempts (_attempts - 1) _id
-        return $ InitiateLoginRes tokenId (_attempts - 1)
-      else L.throwException $ err400 {errBody = "LIMIT_EXCEEDED"}
+reInitiateLogin tokenId req = withFlowHandler $ do
+  SR.RegistrationToken {..} <- checkRegistrationTokenExists tokenId
+  case _entityType of
+    SR.CUSTOMER -> void $ checkCustomerExists _EntityId
+    SR.USER     -> void $ checkUserExists _EntityId
+
+  if _attempts > 0
+    then do
+      sendOTP (req ^. Lens.identifier) _authValueHash
+      QR.updateAttempts (_attempts - 1) _id
+      return $ InitiateLoginRes tokenId (_attempts - 1)
+    else L.throwException $ err400 {errBody = "LIMIT_EXCEEDED"}
--- src/Beckn/Types/API/Registration.hs
+++ src/Beckn/Types/API/Registration.hs
@@ -3,16 +3,18 @@ module Beckn.Types.API.Registration where
 import           Beckn.Types.Common
 import           Beckn.Types.Storage.Customer
 import           Beckn.Types.Storage.RegistrationToken
+import           Beckn.Types.Storage.User
 import           Data.Swagger
 import           EulerHS.Prelude
 import           Servant.Swagger
 
 data InitiateLoginReq =
   InitiateLoginReq
-    { _medium :: Medium
-    , __type  :: LoginType
-    , __value :: Text
-    , _role   :: CustomerRole
+    { _medium     :: Medium
+    , __type      :: LoginType
+    , _identifier :: Text
+    , _role       :: Maybe CustomerRole
+    , _entityType :: RTEntityType
     }
   deriving (Generic, ToSchema)
 
@@ -21,9 +23,10 @@ instance FromJSON InitiateLoginReq where
 
 data ReInitiateLoginReq =
   ReInitiateLoginReq
-    { _medium :: Medium
-    , __type  :: LoginType
-    , __value :: Text
+    { _medium     :: Medium
+    , __type      :: LoginType
+    , _identifier :: Text
+    , _entityType :: RTEntityType
     }
   deriving (Generic, ToSchema)
 
@@ -42,7 +45,6 @@ data LoginReq =
   LoginReq
     { _medium :: Medium
     , __type  :: LoginType
-    , __value :: Text
     , _hash   :: Text
     }
   deriving (Generic, ToSchema)
@@ -53,6 +55,7 @@ instance FromJSON LoginReq where
 data LoginRes =
   LoginRes
     { registrationToken :: Text
-    , customer          :: Customer
+    , customer          :: Maybe Customer
+    , user              :: Maybe User
     }
   deriving (Generic, ToJSON, ToSchema)
