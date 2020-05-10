87d990b97dce74cda2d51bd49a21bfba862c9815
venkatesh_juspay <venkatesh.devendran@juspay.in>
2020-05-10 22:09:17 +0530

    added another method in transform

--- lib/beckn-core/src/Beckn/TypeClass/Transform.hs
+++ lib/beckn-core/src/Beckn/TypeClass/Transform.hs
@@ -7,3 +7,4 @@ import qualified EulerHS.Language                        as L
 class Transform f g  | f -> g, g ->f where
   transform :: f -> g -> g
   transformFlow :: f -> L.Flow g
+  tramsformFlow2 :: f -> g -> L.Flow g
