728ff7f7f8aa0ba46dcaa2f4dd7b1a554c51eeef
Dilip Jain <dilip.jain@juspay.in>
2020-05-11 21:26:15 +0530

    Changing CaseProduct and Case APIs to take regToken as Authorization header

--- app/beckn-transport/src/Product/CaseProduct.hs
+++ app/beckn-transport/src/Product/CaseProduct.hs
@@ -23,9 +23,9 @@ import qualified Storage.Queries.Products as PQ
 import System.Environment
 import Types.API.CaseProduct
 
-list :: Text -> CaseProdReq -> FlowHandler CaseProductList
+list :: Maybe Text -> CaseProdReq -> FlowHandler CaseProductList
 list regToken CaseProdReq {..} = withFlowHandler $ do
-  SR.RegistrationToken {..} <- QR.findRegistrationTokenByToken $ Just regToken
+  SR.RegistrationToken {..} <- QR.findRegistrationTokenByToken regToken
   prodList <- PQ.findAllByTypeOrgId _organisationId _type
   caseProdList <- DB.findAllByIds _limit _offset (Product._id <$> prodList)
   caseList <- CQ.findAllByIds (Storage._caseId <$> caseProdList)
