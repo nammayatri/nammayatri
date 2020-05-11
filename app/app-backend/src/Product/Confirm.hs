3b791f16edc4b1bf1bb1e9857e7b04a3e509fec3
Aravind Virat <aravind.mallapureddy@juspay.in>
2020-05-11 16:48:19 +0530

    added few params

--- app/app-backend/src/Product/Confirm.hs
+++ app/app-backend/src/Product/Confirm.hs
@@ -1,17 +1,31 @@
 module Product.Confirm where
 
-import           Beckn.Types.API.Confirm
-import           Beckn.Types.Core.Ack
-import qualified EulerHS.Language        as L
-import           EulerHS.Prelude
-import qualified EulerHS.Types           as ET
-import qualified External.Gateway.Types  as Gateway
-import           Types.App
-import           Utils.Routes
+import Beckn.Types.API.Confirm
+import Beckn.Types.App
+import Beckn.Types.Core.Ack
+import Beckn.Types.Core.Catalog
+import Beckn.Types.Core.Category
+import Beckn.Types.Core.Service
+import Epass.Utils.Extra
+import qualified EulerHS.Language as L
+import EulerHS.Prelude
+import qualified EulerHS.Types as ET
+import qualified External.Gateway.Types as Gateway
+import qualified Storage.Queries.Case as QCase
+import qualified Storage.Queries.Products as QProducts
+import Types.App
+import Utils.Routes
 
 confirm :: Text -> Text -> FlowHandler Ack
 confirm caseId productId = withFlowHandler $ do
-  -- call transporter
+  lt <- getCurrentTimeUTC
+  currentCase <- QCase.findById $ CaseId caseId
+  product <- QProducts.findById $ ProductsId productId
+  transactionId <- L.generateGUID
+  context <- buildContext "confirm" transactionId
+  let provider = defaultProvider lt
+      catalog = Catalog (Category "" []) []
+      service = Service caseId catalog provider
   return $ Ack "Confirm" "Confirm invoked to transporter"
 
 on_confirm :: OnConfirmReq -> FlowHandler OnConfirmRes
--- app/app-backend/src/Utils/Routes.hs
+++ app/app-backend/src/Utils/Routes.hs
@@ -1,5 +1,10 @@
 module Utils.Routes where
 
+import Beckn.Types.Core.Api
+import Beckn.Types.Core.Contact
+import Beckn.Types.Core.Context
+import Beckn.Types.Core.Provider
+import Epass.Utils.Extra
 import qualified EulerHS.Interpreters as I
 import qualified EulerHS.Language as L
 import EulerHS.Prelude
@@ -9,3 +14,48 @@ withFlowHandler :: L.Flow a -> FlowHandler a
 withFlowHandler flow = do
   (Env flowRt) <- ask
   lift $ ExceptT $ try $ I.runFlow flowRt $ flow
+
+buildContext :: Text -> Text -> L.Flow Context
+buildContext action tId = do
+  now <- getCurrentTimeUTC
+  return $
+    Context
+      { domain = "MOBILITY",
+        action = action,
+        version = Nothing,
+        transaction_id = tId,
+        message_id = Nothing,
+        timestamp = now,
+        dummy = "DUMMY"
+      }
+
+defaultProvider lt =
+  Provider
+    { _id = "",
+      _name = "",
+      _website = "",
+      _contact = contact,
+      _api = Api "" lt
+    }
+
+contact =
+  Contact
+    { _email = "",
+      _mobile = mobile,
+      _landline = landLine,
+      _ivr = []
+    }
+
+mobile =
+  Mobile
+    { _country_code = "",
+      _number = ""
+    }
+
+landLine =
+  LandLine
+    { _country_code = "",
+      _std_code = "",
+      _number = "",
+      _extension = ""
+    }
