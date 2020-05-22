5724fca095e89a16f49f68e9f50621837da62701
therewillbecode <tomw08@gmail.com>
2020-05-22 15:00:30 +0100

    Add Initial Error Types Scaffolding

--- lib/beckn-core/src/Beckn/Types/Error.hs
+++ lib/beckn-core/src/Beckn/Types/Error.hs
@@ -4,17 +4,68 @@ module Beckn.Types.Error where
 
 import EulerHS.Prelude
 
+newtype ErrorCode = ErrorCode Int deriving (Generic, Eq, Show, FromJSON, ToJSON)
 
-data JsonError
-  = JsonError
-      { _errorCode :: Text,
-        _errorMessage :: Text,
-        _action :: Text
+newtype ErrorMsg = ErrorMsg Text deriving (Generic, Eq, Show, Read, FromJSON, ToJSON)
+
+data DomainError = 
+   EpassErr EpassError
+ | QuotaErr QuotaError
+ | CommentErr CommentError
+ | HealthCheckErr HealthCheckError
+ | BlacklistErr BlacklistError
+ | TagErr TagError
+ | OrganisationErr OrganisationError
+ | UnknownDomainError
+  deriving (Generic, Eq, Show, FromJSON, ToJSON)
+
+data EpassError = 
+    EpassNotFound
+  deriving (Generic, Eq, Show, FromJSON, ToJSON)
+
+data QuotaError = 
+    EpassNotFound
+  deriving (Generic, Eq, Show, FromJSON, ToJSON)
+
+
+data CommentError = 
+   CommentNotFound
+  deriving (Generic, Eq, Show, FromJSON, ToJSON)
+
+
+data HealthCheckError = 
+   CommentNotFound
+   deriving (Generic, Eq, Show, FromJSON, ToJSON)
+
+
+data BlacklistError = 
+   CommentNotFound
+  deriving (Generic, Eq, Show, FromJSON, ToJSON)
+
+
+data TagError = 
+   CommentNotFound
+   deriving (Generic, Eq, Show, FromJSON, ToJSON)
+
+
+data OrganisationError = 
+   CommentNotFound
+  deriving (Generic, Eq, Show, FromJSON, ToJSON)
+
+
+data SystemError = 
+     MethodNotAllowed
+ deriving (Generic, Eq, Show, FromJSON, ToJSON)
+
+data BecknError
+  = BecknError
+      { _errorCode :: ErrorCode,
+        _errorMessage :: ErrorMsg
       }
   deriving (Generic)
 
-instance FromJSON JsonError where
+instance FromJSON BecknError where
   parseJSON = genericParseJSON stripLensPrefixOptions
 
-instance ToJSON JsonError where
+instance ToJSON BecknError where
   toJSON = genericToJSON stripLensPrefixOptions
