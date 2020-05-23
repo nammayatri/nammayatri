8d4a634c982441fb1542f21738657f156c2182d1
Kiran Kumar <kiran.kumar@juspay.in>
2020-05-23 20:19:58 +0530

    making type field mandatory in list caseproduct api

--- app/beckn-transport/src/Product/CaseProduct.hs
+++ app/beckn-transport/src/Product/CaseProduct.hs
@@ -4,8 +4,10 @@ import Beckn.Types.App
 import Beckn.Types.Common as BC
 import qualified Beckn.Types.Storage.Case as Case
 import qualified Beckn.Types.Storage.CaseProduct as Storage
-import qualified Beckn.Types.Storage.Products as Product
 import qualified Beckn.Types.Storage.Location as Loc
+import qualified Beckn.Types.Storage.Person as SP
+import qualified Beckn.Types.Storage.Products as Product
+import qualified Beckn.Types.Storage.RegistrationToken as SR
 import Beckn.Utils.Common (withFlowHandler)
 import qualified Data.Accessor as Lens
 import Data.Aeson
@@ -14,14 +16,12 @@ import Data.Time.LocalTime
 import qualified EulerHS.Language as L
 import EulerHS.Prelude
 import Servant
-import qualified Beckn.Types.Storage.RegistrationToken as SR
-import qualified Beckn.Types.Storage.Person as SP
-import qualified Storage.Queries.Person as QP
-import qualified Storage.Queries.RegistrationToken as QR
 import qualified Storage.Queries.Case as CQ
 import qualified Storage.Queries.CaseProduct as DB
-import qualified Storage.Queries.Products as PQ
 import Storage.Queries.Location as LQ
+import qualified Storage.Queries.Person as QP
+import qualified Storage.Queries.Products as PQ
+import qualified Storage.Queries.RegistrationToken as QR
 import System.Environment
 import Types.API.CaseProduct
 
@@ -31,9 +31,7 @@ list regToken CaseProdReq {..} = withFlowHandler $ do
   person <- QP.findPersonById (PersonId _EntityId)
   case SP._organizationId person of
     Just orgId -> do
-      prodList <- case _type of
-        Just statusList -> PQ.findAllByTypeOrgId orgId statusList
-        Nothing -> PQ.findAllByOrgId orgId
+      prodList <- PQ.findAllByTypeOrgId orgId _type --TODO: need to apply limit/offset here
       caseProdList <- DB.findAllByIds _limit _offset (Product._id <$> prodList)
       caseList <- CQ.findAllByIds (Storage._caseId <$> caseProdList)
       locList <- LQ.findAllByLocIds (Case._fromLocationId <$> caseList) (Case._toLocationId <$> caseList)
