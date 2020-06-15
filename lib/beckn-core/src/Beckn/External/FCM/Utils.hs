0f7ce6dabe59610b78e1d57801dd9af663e42715
Evgeny Panfilov <evgeny.p@juspay.in>
2020-06-15 14:40:31 +0300

    Use where block in FCM utils function

--- lib/beckn-core/src/Beckn/External/FCM/Utils.hs
+++ lib/beckn-core/src/Beckn/External/FCM/Utils.hs
@@ -15,18 +15,21 @@ createFCMTokenRefreshThread = do
     Just _ -> L.forkFlow forkDesc $ do
       _ <- forever $ do
         token <- FCM.checkAndGetToken
-        L.runIO $ case token of
-          Left _ -> threadDelay $ 5 * 1000000 -- bad token? retry
-          Right t -> do
-            validityStatus <- JWT.isValid t
-            threadDelay $
-              1000000 * case validityStatus of
-                JWT.JWTValid x ->
-                  -- seconds before token expiration
-                  if x > 300
-                    then (fromInteger x) - 300
-                    else 10
-                _ -> 10 -- just a caution, it should be valid by this moment
+        L.runIO $ delay token
       pure ()
   where
     forkDesc = "Forever loop that checks and refreshes FCM token"
+    -- wait for some time depeding on a token status
+    delay token =
+      case token of
+        Left _ -> threadDelay $ 5 * 1000000 -- bad token? retry
+        Right t -> do
+          validityStatus <- JWT.isValid t
+          threadDelay $
+            1000000 * case validityStatus of
+              JWT.JWTValid x ->
+                -- seconds before token expiration
+                if x > 300
+                  then (fromInteger x) - 300
+                  else 10
+              _ -> 10 -- just a caution, it should be valid by this moment
