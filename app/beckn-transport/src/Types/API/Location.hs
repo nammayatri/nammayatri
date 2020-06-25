8521d845457389eeb3d26e936f4edfec14b555de
Nikith Shetty <shettynikith3@gmail.com>
2020-06-25 21:11:42 +0530

    updated get Location api to send route, eta info

--- app/beckn-transport/src/Types/API/Location.hs
+++ app/beckn-transport/src/Types/API/Location.hs
@@ -5,6 +5,7 @@ module Types.API.Location where
 import Beckn.Types.App
 import Beckn.Types.Common as BC
 import Beckn.Types.Common
+import Beckn.Types.MapSearch
 import qualified Beckn.Types.Storage.Location as SL
 import Beckn.Utils.Extra
 import Data.Generics.Labels
@@ -23,8 +24,7 @@ data UpdateLocationReq = UpdateLocationReq
     state :: Maybe Text,
     country :: Maybe Text,
     pincode :: Maybe Text,
-    address :: Maybe Text,
-    bound :: Maybe Text
+    address :: Maybe Text
   }
   deriving (Generic, ToSchema, ToJSON, Show, FromJSON)
 
@@ -33,7 +33,26 @@ data UpdateLocationRes = UpdateLocationRes
   }
   deriving (Generic, ToJSON, ToSchema)
 
-data GetLocationRes = GetLocationRes
-  { location :: Maybe UpdateLocationReq
+data LocationInfo = LocationInfo
+  { locationType :: Maybe SL.LocationType,
+    lat :: Maybe Double,
+    long :: Maybe Double,
+    ward :: Maybe Text,
+    district :: Maybe Text,
+    city :: Maybe Text,
+    state :: Maybe Text,
+    country :: Maybe Text,
+    pincode :: Maybe Text,
+    address :: Maybe Text,
+    duration :: Maybe Integer,
+    distance :: Maybe Float,
+    bbox :: Maybe BoundingBoxWithoutCRS,
+    waypoints :: Maybe GeospatialGeometry,
+    snapped_waypoints :: Maybe GeospatialGeometry
   }
-  deriving (Generic, ToJSON, ToSchema, FromJSON)
+  deriving (Generic, ToJSON, Show, FromJSON)
+
+data GetLocationRes = GetLocationRes
+  { location :: Maybe LocationInfo
+  }
+  deriving (Generic, ToJSON, FromJSON)
