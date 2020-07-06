d74e59440322531431189c1058c80fe8a3672a9f
Arun Raghavan <arun.raghavan@juspay.in>
2020-07-06 15:08:47 -0400

    gateway: Trivial proxy implementation for search

---
+++ app/gateway/src/Product/ProviderRegistry.hs
@@ -0,0 +1,17 @@
+module Product.ProviderRegistry
+  ( lookup,
+  )
+where
+
+import qualified Beckn.Types.Core.Context as B
+import qualified EulerHS.Language as EL
+import EulerHS.Prelude
+import qualified System.Environment as SE
+
+-- TODO: All of it
+lookup :: B.Context -> EL.Flow (String, Int)
+lookup _ = do
+  EL.runIO $
+    (,)
+      <$> (fromMaybe "localhost" <$> SE.lookupEnv "PROVIDER_HOST")
+      <*> (fromMaybe 8014 . (>>= readMaybe) <$> SE.lookupEnv "PROVIDER_PORT")
--- app/gateway/src/Product/Search.hs
+++ app/gateway/src/Product/Search.hs
@@ -1,17 +1,41 @@
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Product.Search
   ( search,
     searchCb,
   )
 where
 
-import Beckn.Types.API.Search (OnSearchReq, OnSearchRes, SearchReq, SearchRes)
+import Beckn.Types.API.Search (OnSearchReq, OnSearchRes, SearchReq, SearchRes, searchAPI)
 import Beckn.Types.App (FlowHandler)
+import Beckn.Types.Common (AckResponse (..))
+import Beckn.Types.Core.Ack
+import Beckn.Utils.Common (withFlowHandler)
+import qualified EulerHS.Language as EL
 import EulerHS.Prelude
+import qualified EulerHS.Types as ET
+import qualified Product.ProviderRegistry as Provider
+import Servant.Client (BaseUrl (..), Scheme (..))
 
 -- TODO: auth
 
 search :: SearchReq -> FlowHandler SearchRes
-search = error "Not implemented"
+search req = withFlowHandler $ do
+  (url, port) <- Provider.lookup $ req ^. #context
+  let search' = ET.client searchAPI
+      baseUrl =
+        BaseUrl
+          { baseUrlScheme = Http,
+            baseUrlHost = url,
+            baseUrlPort = port,
+            baseUrlPath = "/v1"
+          }
+  eRes <- EL.callAPI baseUrl $ search' req
+  let ack = either (Ack "Error" . show) (^. #_message) eRes
+      resp = AckResponse (req ^. #context) ack
+  EL.logDebug @Text "gateway" $ "req: " <> show req <> ", resp: " <> show resp
+  return resp
 
 searchCb :: OnSearchReq -> FlowHandler OnSearchRes
 searchCb = error "Not implemented"
