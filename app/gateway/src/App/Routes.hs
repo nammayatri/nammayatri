552c1cda764a12ece815f6878b0743ae5dcf8a64
Arun Raghavan <arun.raghavan@juspay.in>
2020-07-15 16:50:45 -0400

    gateway: Only decode context while passing through requests
    We leave the "message" part of search and on_search requests un-decoded,
    so that we don't have to implement domain-specific encoding/decoding yet.

--- app/gateway/src/App/Routes.hs
+++ app/gateway/src/App/Routes.hs
@@ -1,20 +1,18 @@
 module App.Routes where
 
 import App.Types
-import qualified Beckn.Types.API.Search as Search
 import Beckn.Types.App (FlowServerR)
 import qualified Data.Vault.Lazy as V
 import EulerHS.Prelude
-import EulerHS.Types (EulerClient, client)
 import qualified Product.Search as P
 import Servant
-import Servant.Client (Client)
-import Utils.Auth
+import Types.API.Search
 
 type GatewayAPI =
   "v1"
     :> ( Get '[JSON] Text
            :<|> SearchAPI
+           :<|> OnSearchAPI
        )
 
 gatewayAPI :: Proxy GatewayAPI
@@ -23,18 +21,5 @@ gatewayAPI = Proxy
 gatewayServer :: V.Key (HashMap Text Text) -> FlowServerR AppEnv GatewayAPI
 gatewayServer _key =
   pure "Gateway is UP"
-    :<|> searchFlow
-
-type SearchAPI =
-  Search.SearchAPI VerifyAPIKey
-    :<|> Search.OnSearchAPI VerifyAPIKey
-
-searchFlow :: FlowServerR AppEnv SearchAPI
-searchFlow =
-  P.search
+    :<|> P.search
     :<|> P.searchCb
-
-cliHealthCheck :: EulerClient Text
-cliSearch :: Client EulerClient (Search.SearchAPI VerifyAPIKey)
-cliOnSearch :: Client EulerClient (Search.OnSearchAPI VerifyAPIKey)
-cliHealthCheck :<|> (cliSearch :<|> cliOnSearch) = client gatewayAPI
