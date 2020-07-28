ed0cda750271fc74c7808ae078ceec995d95632c
Nikith Shetty <shettynikith3@gmail.com>
2020-07-28 19:10:34 +0530

    added Dunzo types
    Added custom JSON instances
    
    appended Dunzo PackageContent types
    
    typed Dunzo id fields
    
    generalized Dunzo flow env

---
+++ app/fmd-wrapper/src/External/Dunzo/Flow.hs
@@ -0,0 +1,119 @@
+{-# LANGUAGE OverloadedLabels #-}
+
+module External.Dunzo.Flow where
+
+import Beckn.Types.Common (FlowR)
+import qualified EulerHS.Language as L
+import EulerHS.Prelude
+import qualified EulerHS.Types as T
+import External.Dunzo.Types
+import Servant (Capture, Get, Header, JSON, Post, QueryParam, ReqBody, (:>))
+import Servant.Client (BaseUrl, ClientError)
+
+type GetTokenAPI =
+  "api" :> "v1" :> "token"
+    :> QueryParam "client-id" Text
+    :> QueryParam "client-secret" Text
+    :> Get '[JSON] TokenRes
+
+getTokenAPI :: Proxy GetTokenAPI
+getTokenAPI = Proxy
+
+getToken :: BaseUrl -> TokenReq -> FlowR e (Either ClientError TokenRes)
+getToken url req = L.callAPI url tokenReq
+  where
+    clientId = Just $ getClientId $ req ^. #client_id
+    clientSecret = Just $ getClientSecret $ req ^. #client_secret
+    tokenReq = T.client getTokenAPI clientId clientSecret
+
+type QouteAPI =
+  "api" :> "v1" :> "quote"
+    :> Header "Authorization" Token
+    :> Header "client-id" ClientId
+    :> QueryParam "pickup_lat" Double
+    :> QueryParam "pickup_lng" Double
+    :> QueryParam "drop_lat" Double
+    :> QueryParam "drop_lng" Double
+    :> QueryParam "category_id" Text
+    :> Get '[JSON] QuoteRes
+
+quoteAPI :: Proxy QouteAPI
+quoteAPI = Proxy
+
+getQuote :: ClientId -> Token -> BaseUrl -> QuoteReq -> FlowR e (Either ClientError QuoteRes)
+getQuote clientId token url req = L.callAPI url quoteReq
+  where
+    quoteReq =
+      T.client
+        quoteAPI
+        (Just token)
+        (Just clientId)
+        (Just $ req ^. #pickup_lat)
+        (Just $ req ^. #pickup_lng)
+        (Just $ req ^. #drop_lat)
+        (Just $ req ^. #drop_lng)
+        (Just $ req ^. #category_id)
+
+type CreateTaskAPI =
+  "api" :> "v1" :> "tasks"
+    :> Header "Authorization" Token
+    :> Header "client-id" ClientId
+    :> ReqBody '[JSON] CreateTaskReq
+    :> Post '[JSON] CreateTaskRes
+
+createTaskAPI :: Proxy CreateTaskAPI
+createTaskAPI = Proxy
+
+createTask :: ClientId -> Token -> BaseUrl -> CreateTaskReq -> FlowR e (Either ClientError CreateTaskRes)
+createTask clientId token url req = L.callAPI url task
+  where
+    task =
+      T.client
+        createTaskAPI
+        (Just token)
+        (Just clientId)
+        req
+
+type TaskStatusAPI =
+  "api" :> "v1" :> "tasks"
+    :> Capture "task_id" TaskId
+    :> "status"
+    :> Header "Authorization" Token
+    :> Header "client-id" ClientId
+    :> Get '[JSON] TaskStatus
+
+taskStatusAPI :: Proxy TaskStatusAPI
+taskStatusAPI = Proxy
+
+taskStatus :: ClientId -> Token -> BaseUrl -> TaskId -> FlowR e (Either ClientError TaskStatus)
+taskStatus clientId token url taskId = L.callAPI url status
+  where
+    status =
+      T.client
+        taskStatusAPI
+        taskId
+        (Just token)
+        (Just clientId)
+
+type CancelTaskAPI =
+  "api" :> "v1" :> "tasks"
+    :> Capture "task_id" TaskId
+    :> "_cancel"
+    :> Header "Authorization" Token
+    :> Header "client-id" ClientId
+    :> ReqBody '[JSON] CancelTaskReq
+    :> Post '[JSON] ()
+
+cancelTaskAPI :: Proxy CancelTaskAPI
+cancelTaskAPI = Proxy
+
+cancelTask :: ClientId -> Token -> BaseUrl -> TaskId -> Text -> FlowR e (Either ClientError ())
+cancelTask clientId token url taskId cancellationReason = L.callAPI url cancel
+  where
+    cancel =
+      T.client
+        cancelTaskAPI
+        taskId
+        (Just token)
+        (Just clientId)
+        (CancelTaskReq cancellationReason)
---
+++ app/fmd-wrapper/src/External/Dunzo/Types.hs
@@ -0,0 +1,199 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
+module External.Dunzo.Types where
+
+import Data.Aeson
+import Data.Char (toLower)
+import qualified Data.Text as T
+import EulerHS.Prelude
+import Servant (FromHttpApiData, ToHttpApiData)
+
+newtype ClientId = ClientId {getClientId :: Text}
+  deriving (Generic, Show)
+
+deriving newtype instance ToJSON ClientId
+
+deriving newtype instance FromJSON ClientId
+
+deriving newtype instance ToHttpApiData ClientId
+
+deriving newtype instance FromHttpApiData ClientId
+
+newtype ClientSecret = ClientSecret {getClientSecret :: Text}
+  deriving (Generic, Show)
+
+deriving newtype instance ToJSON ClientSecret
+
+deriving newtype instance FromJSON ClientSecret
+
+deriving newtype instance ToHttpApiData ClientSecret
+
+deriving newtype instance FromHttpApiData ClientSecret
+
+newtype TaskId = TaskId {getTaskId :: Text}
+  deriving (Generic, Show)
+
+deriving newtype instance ToJSON TaskId
+
+deriving newtype instance FromJSON TaskId
+
+deriving newtype instance ToHttpApiData TaskId
+
+deriving newtype instance FromHttpApiData TaskId
+
+newtype Token = Token {getToken :: Text}
+  deriving (Generic, Show)
+
+deriving newtype instance ToJSON Token
+
+deriving newtype instance FromJSON Token
+
+deriving newtype instance ToHttpApiData Token
+
+deriving newtype instance FromHttpApiData Token
+
+data TokenReq = TokenReq
+  { client_id :: ClientId,
+    client_secret :: ClientSecret
+  }
+  deriving (Generic, Show)
+
+newtype TokenRes = TokenRes
+  {token :: Text}
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+data QuoteReq = QuoteReq
+  { pickup_lat :: Double,
+    pickup_lng :: Double,
+    drop_lat :: Double,
+    drop_lng :: Double,
+    category_id :: Text
+  }
+  deriving (Generic, Show)
+
+data QuoteRes = QuoteRes
+  { category_id :: Text,
+    distance :: Float,
+    eta :: Eta,
+    estimated_price :: Float
+  }
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+data Eta = Eta
+  { pickup :: Integer, -- minutes
+    drop :: Integer -- minutes
+  }
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+data LocationDetails = LocationDetails
+  { lat :: Double,
+    lng :: Double,
+    address :: Address
+  }
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+data Address = Address
+  { apartment_address :: Maybe Text,
+    street_address_1 :: Text,
+    street_address_2 :: Maybe Text,
+    landmark :: Maybe Text,
+    city :: Maybe Text,
+    state :: Maybe Text,
+    pincode :: Maybe Text,
+    country :: Maybe Text
+  }
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+data PersonDetails = PersonDetails
+  { name :: Text,
+    phone_number :: Text
+  }
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+data LatLng = LatLng {lat :: Double, lng :: Double}
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+data RunnerDetails = RunnerDetails
+  { name :: Text,
+    phone_number :: Text,
+    location :: LatLng
+  }
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+data PackageContent
+  = Documents_or_Books
+  | Clothes_or_Accessories
+  | Food_or_Flowers
+  | Household_Items
+  | Sports_and_Other_Equipment
+  | Electronic_Items
+  deriving (Show, Generic)
+
+data TaskState
+  = CREATED
+  | QUEUED
+  | RUNNER_ACCEPTED
+  | REACHED_FOR_PICKUP
+  | PICKUP_COMPLETE
+  | STARTED_FOR_DELIVERY
+  | REACHED_FOR_DELIVERY
+  | DELIVERED
+  | CANCELLED
+  | RUNNER_CANCELLED
+  deriving (Show, Generic)
+
+data TaskStatus = TaskStatus
+  { task_id :: TaskId,
+    state :: TaskState,
+    estimated_price :: Float,
+    eta :: Eta,
+    event_timestamp :: Maybe Integer,
+    request_timestamp :: Maybe Integer,
+    runner :: Maybe RunnerDetails,
+    price :: Maybe Float,
+    total_time :: Maybe Integer, -- minutes
+    cancelled_by :: Maybe Text,
+    cancellation_reason :: Maybe Text
+  }
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+data CreateTaskReq = CreateTaskReq
+  { request_id :: Text,
+    pickup_details :: LocationDetails,
+    drop_details :: LocationDetails,
+    sender_details :: PersonDetails,
+    receiver_details :: PersonDetails,
+    special_instructions :: Text,
+    package_approx_value :: Float,
+    package_content :: PackageContent,
+    reference_id :: Text
+  }
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+type CreateTaskRes = TaskStatus
+
+newtype CancelTaskReq = CancelTaskReq {cancellation_reason :: Text}
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+packageContentOptions :: Options
+packageContentOptions =
+  defaultOptions
+    { constructorTagModifier = T.unpack . T.replace "_" " " . T.replace "_and_" " & " . T.replace "_or_" " | " . T.pack
+    }
+
+taskStateOptions :: Options
+taskStateOptions = defaultOptions {constructorTagModifier = map toLower}
+
+instance ToJSON PackageContent where
+  toJSON = genericToJSON packageContentOptions
+
+instance FromJSON PackageContent where
+  parseJSON = genericParseJSON packageContentOptions
+
+instance ToJSON TaskState where
+  toJSON = genericToJSON taskStateOptions
+
+instance FromJSON TaskState where
+  parseJSON = genericParseJSON taskStateOptions
