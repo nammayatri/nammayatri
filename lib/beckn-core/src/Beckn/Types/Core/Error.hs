37cf659e71d54cc5bee3ca45c0d6bcbeb769907f
Arun Raghavan <arun.raghavan@juspay.in>
2020-07-31 17:38:09 -0400

    core: Update Error type to spec version 0.8.0-rc2

--- lib/beckn-core/src/Beckn/Types/Core/Error.hs
+++ lib/beckn-core/src/Beckn/Types/Core/Error.hs
@@ -1,59 +1,20 @@
 module Beckn.Types.Core.Error where
 
-import Data.Text
 import EulerHS.Prelude
 
 data Error = Error
-  { _t_error :: Maybe TError, --"ISO 3166-1 ALPHA-2", "ISO 3166-1 ALPHA-3", "ISO 3166-1 NUMERIC)"
-    _r_error :: Maybe RError,
-    _f_error :: Maybe [FError],
-    _d_error :: Maybe Text -- it may changes
+  { _type :: Text, -- "CONTEXT-ERROR", "CORE-ERROR", "DOMAIN-ERROR", "POLICY-ERROR", "JSON-SCHEMA-ERROR"
+    _code :: Text,
+    _path :: Maybe Text,
+    _message :: Maybe Text
   }
   deriving (Generic, Show)
 
 domainError :: Text -> Error
-domainError = Error Nothing Nothing Nothing . Just
+domainError err = Error "DOMAIN-ERROR" err Nothing Nothing
 
 instance FromJSON Error where
   parseJSON = genericParseJSON stripAllLensPrefixOptions
 
 instance ToJSON Error where
-  toJSON = genericToJSON stripAllLensPrefixOptions
-
-data TError = TError
-  { _t_protocol :: Text,
-    _t_version :: Text,
-    _t_version :: Text
-  }
-  deriving (Generic, Show)
-
-instance FromJSON TError where
-  parseJSON = genericParseJSON stripAllLensPrefixOptions
-
-instance ToJSON TError where
-  toJSON = genericToJSON stripAllLensPrefixOptions
-
-data RError = RError
-  { _rcode :: Text,
-    _rmessage :: Text
-  }
-  deriving (Generic, Show)
-
-instance FromJSON RError where
-  parseJSON = genericParseJSON stripAllLensPrefixOptions
-
-instance ToJSON RError where
-  toJSON = genericToJSON stripAllLensPrefixOptions
-
-data FError = FError
-  { _fpath :: Text,
-    _fcode :: Text,
-    _fmessage :: Text
-  }
-  deriving (Generic, Show)
-
-instance FromJSON FError where
-  parseJSON = genericParseJSON stripAllLensPrefixOptions
-
-instance ToJSON FError where
   toJSON = genericToJSON stripAllLensPrefixOptions
