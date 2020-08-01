449c7904e6fdf6ffab914a17fb79ff932974e7e9
Shawn Rebello <shawn.rebello@juspay.in>
2020-08-01 15:47:14 +0530

    fixed city & country format context

--- lib/beckn-core/src/Beckn/Types/Core/Context.hs
+++ lib/beckn-core/src/Beckn/Types/Core/Context.hs
@@ -2,16 +2,14 @@
 
 module Beckn.Types.Core.Context where
 
-import Beckn.Types.Core.City
-import Beckn.Types.Core.Country
 import Data.Text
 import Data.Time.LocalTime
 import EulerHS.Prelude
 
 data Context = Context
   { _domain :: Text, -- "MOBILITY", "FINAL-MILE-DELIVERY", "FOOD-AND-BEVERAGE", "HEALTHCARE"
-    _country :: Maybe Country,
-    _city :: Maybe City,
+    _country :: Maybe Text, -- Country code as per ISO 3166-1 and ISO 3166-2 format"
+    _city :: Maybe Text, -- City code
     _action :: Text, -- "search", "select", "init", "confirm", "update", "status", "track", "cancel", "feedback", "support", "on_search", "on_select", "on_init", "on_confirm", "on_update", "on_status", "on_track", "on_cancel", "on_feedback", "on_support"
     _core_version :: Maybe Text,
     _domain_version :: Maybe Text,
