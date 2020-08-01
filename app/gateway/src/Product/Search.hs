3ac87dd25a2a3b24579b23bd6d545d02d8a7a210
Vivek Shukla <vivek.shukla@juspay.in>
2020-08-01 15:21:05 +0530

    update bg_id, bg_new_addr

--- app/gateway/src/Product/Search.hs
+++ app/gateway/src/Product/Search.hs
@@ -9,6 +9,7 @@ where
 
 import App.Types
 import Beckn.Types.Common (AckResponse (..), ack)
+import Beckn.Types.Core.Context
 import Beckn.Types.Core.Error
 import qualified Beckn.Types.Storage.Organization as Org
 import Beckn.Utils.Common (fromMaybeM400, withFlowHandler)
@@ -19,6 +20,7 @@ import qualified EulerHS.Types as ET
 import qualified Product.AppLookup as BA
 import qualified Product.ProviderRegistry as BP
 import Servant.Client (BaseUrl, parseBaseUrl)
+import System.Environment (lookupEnv)
 import Types.API.Search (OnSearchReq, SearchReq, onSearchAPI, searchAPI)
 import Utils.Common
 
@@ -34,9 +36,16 @@ search org req = withFlowHandler $ do
       messageId = req ^. #context . #_request_transaction_id
   appUrl <- Org._callbackUrl org & fromMaybeM400 "INVALID_ORG"
   providerUrls <- BP.lookup $ req ^. #context
+  bgId <- L.runIO $ lookupEnv "GATEWAY_ID"
+  bgNwAddr <- L.runIO $ lookupEnv "GATEWAY_NW_ADDR"
+  let context =
+        (req ^. #context)
+          { _bg_id = fromString <$> bgId,
+            _bg_nw_address = fromString <$> bgNwAddr
+          }
   resps <- forM providerUrls $ \providerUrl -> do
     baseUrl <- parseOrgUrl providerUrl
-    eRes <- callAPI baseUrl (search' "" req) "search"
+    eRes <- callAPI baseUrl (search' "" (req & #context .~ context)) "search"
     L.logDebug @Text "gateway" $
       "request_transaction_id: " <> messageId
         <> ", search: req: "
@@ -47,8 +56,8 @@ search org req = withFlowHandler $ do
   if or resps
     then do
       BA.insert messageId appUrl
-      return $ AckResponse (req ^. #context) (ack "ACK") Nothing
-    else return $ AckResponse (req ^. #context) (ack "NACK") (Just $ domainError "No providers")
+      return $ AckResponse context (ack "ACK") Nothing
+    else return $ AckResponse context (ack "NACK") (Just $ domainError "No providers")
 
 searchCb :: Org.Organization -> OnSearchReq -> FlowHandler AckResponse
 searchCb _org req = withFlowHandler $ do
