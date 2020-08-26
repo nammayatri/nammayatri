0217b827118e79fedd6d3327ad9afe7373e4ba9d
Sanchayan Maity <sanchayan.maity@juspay.in>
2020-08-26 18:56:52 +0530

    app: gateway: Add Log API for gateway
    The logs API would be a part of the gateway for now. All other
    consumers like BAP/BPP/Wrapper would be sending logs to this.
    
    Signed-off-by: Sanchayan Maity <sanchayan.maity@juspay.in>

--- app/gateway/src/App/Routes.hs
+++ app/gateway/src/App/Routes.hs
@@ -1,18 +1,22 @@
 module App.Routes where
 
 import App.Types
+import Beckn.Types.API.Log
 import Beckn.Types.App (FlowServerR)
 import qualified Data.Vault.Lazy as V
 import EulerHS.Prelude
+import qualified Product.Log as P
 import qualified Product.Search as P
 import Servant
 import Types.API.Search
+import Utils.Auth (VerifyAPIKey)
 
 type GatewayAPI =
   "v1"
     :> ( Get '[JSON] Text
            :<|> SearchAPI
            :<|> OnSearchAPI
+           :<|> LogAPI VerifyAPIKey
        )
 
 gatewayAPI :: Proxy GatewayAPI
@@ -23,3 +27,4 @@ gatewayServer _key =
   pure "Gateway is UP"
     :<|> P.search
     :<|> P.searchCb
+    :<|> P.log
---
+++ app/gateway/src/Product/Log.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Product.Log where
+
+import App.Types
+import Beckn.Types.API.Log (LogReq)
+import Beckn.Types.Common (AckResponse (..), ack)
+import Beckn.Types.Core.Error
+import qualified Beckn.Types.Storage.Organization as Org
+import Beckn.Utils.Common (withFlowHandler)
+import Data.Aeson (encode)
+import qualified EulerHS.Language as L
+import EulerHS.Prelude
+
+log :: Org.Organization -> LogReq -> FlowHandler AckResponse
+log _org req = withFlowHandler $ do
+  let context = req ^. #_message . #_context
+      messageId = context ^. #_message_id
+      transactionId = context ^. #_transaction_id
+      action = req ^. #_context . #_action
+  if action == "log"
+    then do
+      L.logDebug @Text "gateway" $
+        "transaction_id: " <> transactionId
+          <> "message_id: "
+          <> messageId
+          <> ", log: req: "
+          <> decodeUtf8 (encode req)
+          <> ", context: "
+          <> decodeUtf8 (encode context)
+      return $ AckResponse (req ^. #_message . #_context) (ack "ACK") Nothing
+    else do
+      let err = Just $ Error "CONTEXT-ERROR" "" Nothing $ Just $ "Invalid action: " <> action
+      return $ AckResponse (req ^. #_message . #_context) (ack "NACK") err
