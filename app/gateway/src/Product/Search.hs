5ec8cac879aee1a9d9cf9573ce9be073758b5e18
Paul Kapustin <paul.kapustin@juspay.in>
2020-08-26 21:41:19 +0200

    gateway: Use callback_api_key of the callee to deliver the callback

--- app/gateway/src/Product/Search.hs
+++ app/gateway/src/Product/Search.hs
@@ -11,9 +11,9 @@ import App.Types
 import Beckn.Types.Common (AckResponse (..), ack)
 import Beckn.Types.Core.Error
 import qualified Beckn.Types.Storage.Organization as Org
-import Beckn.Utils.Common (fork, fromMaybeM400, fromMaybeM500, withFlowHandler)
+import Beckn.Utils.Common
 import Beckn.Utils.Servant.Trail.Client (callAPIWithTrail, withClientTracing)
-import Data.Aeson (encode)
+import Data.Aeson (decode, encode)
 import qualified EulerHS.Language as L
 import EulerHS.Prelude
 import qualified EulerHS.Types as ET
@@ -32,31 +32,35 @@ search :: Org.Organization -> SearchReq -> FlowHandler AckResponse
 search org req = withFlowHandler $ do
   let search' = ET.client $ withClientTracing searchAPI
       messageId = req ^. #context . #_transaction_id
-  appUrl <- Org._callbackUrl org & fromMaybeM400 "INVALID_ORG"
-  providers <- BP.lookup $ req ^. #context
-  BA.insert messageId appUrl
-  forM_ providers $ \provider -> fork "Provider search" $ do
-    providerUrl <- provider ^. #_callbackUrl & fromMaybeM500 "PROVIDER_URL_NOT_FOUND" -- Already checked for existance
-    let providerApiKey = fromMaybe "" $ provider ^. #_callbackApiKey
-    baseUrl <- parseOrgUrl providerUrl
-    eRes <- callAPIWithTrail baseUrl (search' providerApiKey req) "search"
-    L.logDebug @Text "gateway" $
-      "request_transaction_id: " <> messageId
-        <> ", search: req: "
-        <> decodeUtf8 (encode req)
-        <> ", resp: "
-        <> show eRes
-  if null providers
-    then return $ AckResponse (req ^. #context) (ack "NACK") (Just $ domainError "No providers")
-    else return $ AckResponse (req ^. #context) (ack "ACK") Nothing
+  case (Org._callbackUrl org, Org._callbackApiKey org) of
+    (Nothing, _) -> throwJsonError400 "Search.search" "CALLBACK_URL_NOT_CONFIGURED"
+    (_, Nothing) -> throwJsonError400 "Search.search" "CB_API_KEY_NOT_CONFIGURED"
+    (cbUrl, cbApiKey) -> do
+      providers <- BP.lookup $ req ^. #context
+      BA.insert messageId $ decodeUtf8 $ encode (cbUrl, cbApiKey)
+      forM_ providers $ \provider -> fork "Provider search" $ do
+        providerUrl <- provider ^. #_callbackUrl & fromMaybeM500 "PROVIDER_URL_NOT_FOUND" -- Already checked for existance
+        let providerApiKey = fromMaybe "" $ provider ^. #_callbackApiKey
+        baseUrl <- parseOrgUrl providerUrl
+        eRes <- callAPIWithTrail baseUrl (search' providerApiKey req) "search"
+        L.logDebug @Text "gateway" $
+          "request_transaction_id: " <> messageId
+            <> ", search: req: "
+            <> decodeUtf8 (encode req)
+            <> ", resp: "
+            <> show eRes
+      if null providers
+        then return $ AckResponse (req ^. #context) (ack "NACK") (Just $ domainError "No providers")
+        else return $ AckResponse (req ^. #context) (ack "ACK") Nothing
 
 searchCb :: Org.Organization -> OnSearchReq -> FlowHandler AckResponse
-searchCb org req = withFlowHandler $ do
+searchCb _ req = withFlowHandler $ do
   let onSearch = ET.client $ withClientTracing onSearchAPI
       messageId = req ^. #context . #_transaction_id
-  appUrl <- BA.lookup messageId >>= fromMaybeM400 "INVALID_MESSAGE"
-  baseUrl <- parseOrgUrl appUrl
-  cbApiKey <- org ^. #_callbackApiKey & fromMaybeM500 "CB_API_KEY_NOT_CONFIGURED"
+  message <- BA.lookup messageId
+  let parsedMessage = message >>= decode . encodeUtf8
+  (cbUrl, cbApiKey) <- fromMaybeM400 "INVALID_MESSAGE" parsedMessage
+  baseUrl <- parseOrgUrl cbUrl
   eRes <- callAPIWithTrail baseUrl (onSearch cbApiKey req) "on_search"
   let resp = case eRes of
         Left err -> AckResponse (req ^. #context) (ack "NACK") (Just $ domainError $ show err)
