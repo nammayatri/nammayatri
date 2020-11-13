8aea99be1d803b318f6a3a954afdc3054182afac
Arun Raghavan <arun.raghavan@juspay.in>
2020-11-13 18:00:58 -0500

    beckn-core: Add a hard-coded registry implementation
    This should be replaced by a lookup + cache mechanism soon, and possibly
    a text config-based approach sooner.

---
+++ lib/beckn-core/src/Beckn/Types/Storage/Credential.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Beckn.Types.Storage.Credential where
+
+import Beckn.Types.Storage.Organization (OrganizationDomain)
+import Data.Time (UTCTime)
+import qualified Database.Beam as B
+import EulerHS.Prelude
+
+data CredentialT f = Credential
+  { _keyId :: B.C f Text,
+    _orgId :: B.C f Text,
+    _domain :: B.C f (Maybe OrganizationDomain),
+    _city :: B.C f (Maybe Text),
+    _country :: B.C f (Maybe Text),
+    _signPubKey :: B.C f Text,
+    _signPrivKey :: B.C f (Maybe Text),
+    _encPubKey :: B.C f Text,
+    _encPrivKey :: B.C f (Maybe Text),
+    _validFrom :: B.C f UTCTime,
+    _validTill :: B.C f UTCTime
+  }
+  deriving (Generic, B.Beamable)
+
+type Credential = CredentialT Identity
+
+type CredentialPrimaryKey = B.PrimaryKey CredentialT Identity
+
+instance B.Table CredentialT where
+  data PrimaryKey CredentialT f = CredentialPrimaryKey (B.C f Text)
+    deriving (Generic, B.Beamable)
+  primaryKey = CredentialPrimaryKey . _keyId
+
+deriving instance Show Credential
+
+deriving instance Eq Credential
+
+-- FIXME: Are these needed? Beam defaults seem to cover everything we have
+fieldEMod ::
+  B.EntityModification (B.DatabaseEntity be db) be (B.TableEntity CredentialT)
+fieldEMod =
+  B.modifyTableFields
+    B.tableModification
+      { _keyId = "key_id",
+        _orgId = "org_id",
+        _signPubKey = "sign_pub_key",
+        _signPrivKey = "sign_priv_key",
+        _encPubKey = "enc_pub_key",
+        _encPrivKey = "enc_priv_key",
+        _validFrom = "valid_from",
+        _validTill = "valid_till"
+      }
---
+++ lib/beckn-core/src/Beckn/Utils/Registry.hs
@@ -0,0 +1,32 @@
+module Beckn.Utils.Registry
+  ( lookup,
+  )
+where
+
+import Beckn.Types.Storage.Credential
+import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
+import qualified EulerHS.Language as L
+import EulerHS.Prelude
+
+registry :: [Credential]
+registry =
+  [ mkCredential "test-app-2-key" "test-app",
+    mkCredential "fmd-test-app-key" "fmd-test-app",
+    mkCredential "juspay-bg-1-key" "JUSPAY.BG.1",
+    mkCredential "juspay-mobility-bap-1-key" "JUSPAY.MOBILITY.APP.UAT.1",
+    mkCredential "juspay-mobility-bpp-1-key" "JUSPAY.MOBILITY.PROVIDER.UAT.1",
+    mkCredential "juspay-bg-1-key" "JUSPAY.BG.1",
+    mkCredential "juspay-mock-bap-1-key" "JUSPAY.BAP.MOCK.1",
+    mkCredential "juspay-mock-bpp-1-key" "JUSPAY.BPP.MOCK.1"
+  ]
+  where
+    mkCredential keyId orgId =
+      Credential keyId orgId Nothing Nothing Nothing examplePubKey examplePrivKey examplePubKey examplePrivKey exampleValidFrom exampleValidTill
+
+    examplePubKey = "kCa4OlmRVfCPcvzjPPGik0Ljei5dRYuuj/2K6upaf1E="
+    examplePrivKey = Just "ftjLZNZ6+QG8KAcNqax3NiX6Cg1bKVVdnbygReTwpFw="
+    exampleValidFrom = posixSecondsToUTCTime 1605232000
+    exampleValidTill = posixSecondsToUTCTime 1920592000
+
+lookup :: L.MonadFlow m => Text -> m (Maybe Credential)
+lookup k = return $ find (\c -> c ^. #_keyId == k) registry
