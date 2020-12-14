33196aba2b95f3531c737c923fbbaad4cb715fb0
Oleg Romashin <oleg.romashin@juspay.in>
2020-12-14 20:09:20 +0700

    removed fromJust from Time fixtures

--- app/beckn-transport/test/src/Fixtures/Time.hs
+++ app/beckn-transport/test/src/Fixtures/Time.hs
@@ -1,12 +1,13 @@
 module Fixtures.Time (defaultTime) where
 
 import qualified Data.Time as Time
+import qualified Data.Time.Calendar.OrdinalDate as Time
+import qualified Data.Time.Clock as Time
 import EulerHS.Prelude
 
-parseTime :: String -> Time.UTCTime
-parseTime time =
-  let fmt = Time.iso8601DateFormat (Just "%H:%M:%S%QZ")
-   in fromJust . Time.parseTimeM True Time.defaultTimeLocale fmt $ time
-
 defaultTime :: Time.UTCTime
-defaultTime = parseTime "2019-11-28T07:30:54.471Z"
+defaultTime =
+  Time.UTCTime
+    { utctDay = Time.fromOrdinalDate 2020 120,
+      utctDayTime = Time.secondsToDiffTime 40000
+    }
