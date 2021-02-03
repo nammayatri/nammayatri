497147b1a4fb733d483d20f6d0a862ec8042decc
Oleg Romashin <oleg.romashin@juspay.in>
2021-02-03 14:50:32 +0700

    referencing on allocation_request table in notification_status table

--- dev/sql-seed/transporter-backend-seed.sql
+++ dev/sql-seed/transporter-backend-seed.sql
@@ -704,7 +704,7 @@ CREATE TABLE atlas_transporter.notification_status (
     id character(36) PRIMARY KEY NOT NULL,
     ride_id character(36) NOT NULL REFERENCES atlas_transporter.product_instance (id),
     driver_id character(36) NOT NULL REFERENCES atlas_transporter.person (id),
-    allocation_request_id character(36) NOT NULL,
+    allocation_request_id character(36) NOT NULL REFERENCES atlas_transporter.allocation_request (id),
     status character varying(20) NOT NULL,
     notified_at timestamp with time zone,
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
