7c471984f7260bdb39934f326ef9e8e85ab5fa62
Alexander Korovin <alexander.korovin@metalamp.io>
2021-02-04 14:59:40 +0700

    Changed JSON stored in Redis

--- app/beckn-transport/src/Product/Ride.hs
+++ app/beckn-transport/src/Product/Ride.hs
@@ -9,13 +9,17 @@ import App.Types (FlowHandler)
 import qualified Beckn.Storage.Redis.Queries as Redis
 import qualified Beckn.Types.APIResult as APIResult
 import qualified Beckn.Types.Storage.RegistrationToken as SR
-import Beckn.Utils.Common (withFlowHandler)
+import Beckn.Utils.Common
 import EulerHS.Prelude
 import Types.API.Ride
+import Types.App
 
 setDriverAcceptance :: SR.RegistrationToken -> SetDriverAcceptanceReq -> FlowHandler SetDriverAcceptanceRes
-setDriverAcceptance _ SetDriverAcceptanceReq {..} = withFlowHandler $ do
-  Redis.setExRedis redisKey _response 600
+setDriverAcceptance SR.RegistrationToken {..} SetDriverAcceptanceReq {..} = withFlowHandler $ do
+  now <- getCurrTime
+  Redis.setExRedis redisKey (redisValue now) 600
   return APIResult.Success
   where
+    driverId = DriverId _EntityId
     redisKey = "beckn:" <> _productInstanceId ^. #_getProductInstanceId <> ":response"
+    redisValue now = DriverResponse {_driverId = driverId, _productInstanceId = _productInstanceId, _status = _response, _respondedAt = now}
--- app/beckn-transport/src/Types/API/Ride.hs
+++ app/beckn-transport/src/Types/API/Ride.hs
@@ -2,7 +2,9 @@ module Types.API.Ride where
 
 import qualified Beckn.Types.APIResult as APIResult
 import Beckn.Types.App
+import Data.Time
 import EulerHS.Prelude
+import Types.App
 
 data SetDriverAcceptanceReq = SetDriverAcceptanceReq
   { _productInstanceId :: ProductInstanceId,
@@ -15,4 +17,12 @@ type SetDriverAcceptanceRes = APIResult.APIResult
 data NotificationStatus
   = ACCEPT
   | REJECT
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+data DriverResponse = DriverResponse
+  { _driverId :: DriverId,
+    _productInstanceId :: ProductInstanceId,
+    _status :: NotificationStatus,
+    _respondedAt :: UTCTime
+  }
   deriving (Show, Generic, ToJSON, FromJSON)
