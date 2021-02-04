9fa45b54a0a01a3a40bcae3b57c3b4a4cf5f4162
Alexander Korovin <alexander.korovin@metalamp.io>
2021-02-04 16:13:55 +0700

    HealthCheck is done

--- app/beckn-transport/src/Product/HealthCheck.hs
+++ app/beckn-transport/src/Product/HealthCheck.hs
@@ -3,13 +3,18 @@ module Product.HealthCheck (healthCheck) where
 import qualified App.Types as App
 import qualified Beckn.Storage.Redis.Queries as Redis
 import Beckn.Utils.Common
+import Data.Time
 import EulerHS.Prelude
 
 healthCheck :: Text -> App.FlowHandler Text
 healthCheck _ = withFlowHandler $ do
   mbTime <- Redis.getKeyRedis "beckn:allocation:service"
-  case mbTime of
-    Just a -> checkLastUpdateTime a
-    Nothing -> throwError500 ""
+  maybe markAsDead checkLastUpdateTime mbTime
   where
-    checkLastUpdateTime a = return a
+    markAsDead = throwError500 "Service is dead."
+    checkLastUpdateTime lastUpdateTime = do
+      now <- getCurrTime
+      let diffTime = diffUTCTime now lastUpdateTime
+      if diffTime > 10
+        then markAsDead
+        else return "Service is up!"
