98013fcd2eafb86affb7693d558ffb48b9c1504a
Oleg Romashin <oleg.romashin@juspay.in>
2021-02-16 18:25:39 +0700

    creating functions for calling Google maps API

--- app/beckn-transport/src/Product/Services/GoogleMaps.hs
+++ app/beckn-transport/src/Product/Services/GoogleMaps.hs
@@ -1,49 +1,28 @@
-{-# LANGUAGE OverloadedLabels #-}
-{-# LANGUAGE TypeApplications #-}
-
 module Product.Services.GoogleMaps where
 
 import App.Types (AppEnv (..), FlowHandler)
-import qualified Beckn.External.GoogleMaps.API as API
+import qualified Beckn.External.GoogleMaps.Client as ClientGoogleMaps
 import qualified Beckn.External.GoogleMaps.Types as GoogleMaps
 import qualified Beckn.Types.Storage.RegistrationToken as RegToken
-import Beckn.Utils.Common (fromClientError, throwError500, withFlowHandler)
-import Beckn.Utils.Servant.Trail.Client (callAPIWithTrail)
-import qualified EulerHS.Language as L
+import Beckn.Utils.Common (withFlowHandler)
 import EulerHS.Prelude
 
 autoComplete :: RegToken.RegistrationToken -> Text -> Text -> Integer -> FlowHandler GoogleMaps.SearchLocationResp
 autoComplete _auth input location radius = withFlowHandler $ do
   url <- googleMapsUrl <$> ask
   apiKey <- googleMapsKey <$> ask
-  res <- callAPIWithTrail url (API.autoComplete apiKey input location radius "country:in") "autoComplete"
-  case res of
-    Right x -> return x
-    Left cliErr -> do
-      let err = fromClientError cliErr
-      L.logError @Text "client Google maps API autoComplete call error" $ (err ^. #_message) ?: "Some error"
-      throwError500 "Google maps API autoComplete error"
+  let components = "country:in"
+  ClientGoogleMaps.autoComplete url apiKey input location radius components
 
 placeDetails :: RegToken.RegistrationToken -> Text -> FlowHandler GoogleMaps.PlaceDetailsResp
 placeDetails _auth placeId = withFlowHandler $ do
   url <- googleMapsUrl <$> ask
   apiKey <- googleMapsKey <$> ask
-  res <- callAPIWithTrail url (API.placeDetails apiKey placeId "geometry") "placeDetails"
-  case res of
-    Right x -> return x
-    Left cliErr -> do
-      let err = fromClientError cliErr
-      L.logError @Text "client Google maps API placeDetails call error" $ (err ^. #_message) ?: "Some error"
-      throwError500 "Google maps API placeDetails error"
+  let fields = "geometry"
+  ClientGoogleMaps.placeDetails url apiKey placeId fields
 
 getPlaceName :: RegToken.RegistrationToken -> Text -> FlowHandler GoogleMaps.GetPlaceNameResp
 getPlaceName _auth latLng = withFlowHandler $ do
   url <- googleMapsUrl <$> ask
   apiKey <- googleMapsKey <$> ask
-  res <- callAPIWithTrail url (API.getPlaceName latLng apiKey) "getPlaceName"
-  case res of
-    Right x -> return x
-    Left cliErr -> do
-      let err = fromClientError cliErr
-      L.logError @Text "client Google maps API getPlaceName call error" $ (err ^. #_message) ?: "Some error"
-      throwError500 "Google maps API getPlaceName error"
+  ClientGoogleMaps.getPlaceName url latLng apiKey
---
+++ lib/beckn-core/src/Beckn/External/GoogleMaps/Client.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE TypeApplications #-}
+
+module Beckn.External.GoogleMaps.Client where
+
+import qualified Beckn.External.GoogleMaps.API as API
+import qualified Beckn.External.GoogleMaps.Types as GoogleMaps
+import Beckn.Storage.DB.Config (DBConfig)
+import Beckn.Types.App (TraceFlag)
+import Beckn.Types.Common
+import Beckn.Utils.Common (fromClientError, throwError500)
+import Beckn.Utils.Servant.Trail.Client (callAPIWithTrail)
+import qualified EulerHS.Language as L
+import EulerHS.Prelude
+import GHC.Records (HasField)
+import Servant.Client.Core (BaseUrl)
+
+autoComplete ::
+  (HasField "dbCfg" r DBConfig, HasField "traceFlag" r TraceFlag) =>
+  BaseUrl ->
+  Text ->
+  Text ->
+  Text ->
+  Integer ->
+  Text ->
+  FlowR r GoogleMaps.SearchLocationResp
+autoComplete url apiKey input location radius components = do
+  res <- callAPIWithTrail url (API.autoComplete apiKey input location radius components) "autoComplete"
+  case res of
+    Right x -> return x
+    Left cliErr -> do
+      let err = fromClientError cliErr
+      L.logError @Text "client Google maps API autoComplete call error" $ (err ^. #_message) ?: "Some error"
+      throwError500 "Google maps API autoComplete error"
+
+placeDetails ::
+  (HasField "dbCfg" r DBConfig, HasField "traceFlag" r TraceFlag) =>
+  BaseUrl ->
+  Text ->
+  Text ->
+  Text ->
+  FlowR r GoogleMaps.PlaceDetailsResp
+placeDetails url apiKey placeId fields = do
+  res <- callAPIWithTrail url (API.placeDetails apiKey placeId fields) "placeDetails"
+  case res of
+    Right x -> return x
+    Left cliErr -> do
+      let err = fromClientError cliErr
+      L.logError @Text "client Google maps API placeDetails call error" $ (err ^. #_message) ?: "Some error"
+      throwError500 "Google maps API placeDetails error"
+
+getPlaceName ::
+  (HasField "dbCfg" r DBConfig, HasField "traceFlag" r TraceFlag) =>
+  BaseUrl ->
+  Text ->
+  Text ->
+  FlowR r GoogleMaps.GetPlaceNameResp
+getPlaceName url latLng apiKey = do
+  res <- callAPIWithTrail url (API.getPlaceName latLng apiKey) "getPlaceName"
+  case res of
+    Right x -> return x
+    Left cliErr -> do
+      let err = fromClientError cliErr
+      L.logError @Text "client Google maps API getPlaceName call error" $ (err ^. #_message) ?: "Some error"
+      throwError500 "Google maps API getPlaceName error"
