7904722f3d9eded5a7ae6f752e9f01dc97296be4
Paul Kapustin <paul.kapustin@juspay.in>
2021-02-26 23:37:52 +0100

    Fixed allocation unit tests

--- app/beckn-transport/test/src/Flow/Allocation.hs
+++ app/beckn-transport/test/src/Flow/Allocation.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedLabels #-}
+
 module Flow.Allocation where
 
 import qualified Control.Concurrent.MVar as MVar
@@ -5,11 +7,21 @@ import qualified Data.List.NonEmpty as NonEmpty
 import qualified Data.Map as Map
 import qualified Data.Time as Time
 import EulerHS.Prelude
-import qualified Services.Allocation as Allocation
+import qualified Services.Allocation.Allocation as Allocation
 import Test.Tasty
 import Test.Tasty.HUnit
+import Types.API.Ride
 import Types.App
 import Utils.Time
+
+numRequestsToProcess :: Integer
+numRequestsToProcess = 10
+
+request01Id :: RideRequestId
+request01Id = RideRequestId "request01"
+
+request02Id :: RideRequestId
+request02Id = RideRequestId "request02"
 
 ride01Id :: RideId
 ride01Id = RideId "ride01"
@@ -17,17 +29,95 @@ ride01Id = RideId "ride01"
 ride02Id :: RideId
 ride02Id = RideId "ride02"
 
-availableRides :: [Allocation.Ride]
-availableRides =
-  [ Allocation.Ride
-      { id = ride01Id,
-        orderedAt = parseTime "2018-12-06T11:39:57.153Z"
-      },
-    Allocation.Ride
-      { id = ride02Id,
-        orderedAt = parseTime "2018-12-06T11:39:57.153Z"
-      }
-  ]
+rideRequestMap :: Map RideRequestId Allocation.RideRequest
+rideRequestMap =
+  Map.fromList
+    [ ( request01Id,
+        Allocation.RideRequest
+          { requestHeader =
+              Allocation.RequestHeader
+                { requestId = request01Id,
+                  rideId = ride01Id,
+                  requestTime = parseTime "2018-12-06T11:39:57.153Z"
+                },
+            requestData =
+              Allocation.Allocation $ Allocation.OrderTime $ parseTime "2018-12-06T11:39:57.153Z"
+          }
+      ),
+      ( request02Id,
+        Allocation.RideRequest
+          { requestHeader =
+              Allocation.RequestHeader
+                { requestId = request02Id,
+                  rideId = ride02Id,
+                  requestTime = parseTime "2018-12-06T11:39:57.153Z"
+                },
+            requestData =
+              Allocation.Allocation $ Allocation.OrderTime $ parseTime "2018-12-06T11:39:57.153Z"
+          }
+      )
+    ]
+
+modifyTVarIO :: TVar a -> (a -> a) -> IO ()
+modifyTVarIO tvar f = atomically $ modifyTVar tvar f
+
+handle :: Repository -> Allocation.ServiceHandle IO
+handle Repository {..} =
+  Allocation.ServiceHandle
+    { getCurrentTime = pure $ parseTime "2018-12-06T11:39:57.153Z",
+      getDriverSortMode = pure ETA,
+      getConfiguredNotificationTime = pure 120,
+      getConfiguredAllocationTime = pure 15,
+      getRequests = \numRides -> do
+        rideMap <- readTVarIO rideRequests
+        let rides = Map.elems rideMap
+        pure $ take (fromIntegral numRides) rides,
+      getDriverPool = \rideId -> do
+        poolMap <- readTVarIO driverPool
+        let pool = fromMaybe [] $ Map.lookup rideId poolMap
+        pure pool,
+      sendNewRideNotification = \rideId driverId -> do
+        pure (),
+      getCurrentNotification = \rideId -> do
+        ns <- readTVarIO notificationStatus
+        let filtered = fmap snd $ Map.keys $ Map.filterWithKey (\(r, _) s -> isNotified s && r == rideId) ns
+        case filtered of
+          [a] -> pure $ Just $ Allocation.CurrentNotification a $ parseTime "2018-12-06T11:39:57.153Z"
+          _ -> pure Nothing,
+      addNotificationStatus = \rideId driverId nStatus -> do
+        now <- Time.getCurrentTime
+        modifyTVarIO notificationStatus $ Map.insert (rideId, driverId) (Allocation.Notified now),
+      updateNotificationStatus = \rideId driverId nStatus -> do
+        now <- Time.getCurrentTime
+        modifyTVarIO notificationStatus $ Map.insert (rideId, driverId) nStatus,
+      resetLastRejectionTime = \rideId -> pure (),
+      getAttemptedDrivers = \rideId -> do
+        ns <- readTVarIO notificationStatus
+        let filtered = fmap snd $ Map.keys $ Map.filterWithKey (\(r, _) _ -> r == rideId) ns
+        pure filtered,
+      getDriversWithNotification = do
+        ns <- readTVarIO notificationStatus
+        let filtered = fmap snd $ Map.keys $ Map.filter isNotified ns
+        pure filtered,
+      getDriverResponse = \rideId driverId -> do
+        ar <- readTVarIO acceptReject
+        pure $ Map.lookup (rideId, driverId) ar,
+      assignDriver = \rideId driverId -> do
+        _ <- modifyTVarIO acceptReject $ Map.delete (rideId, driverId)
+        modifyTVarIO hasFound $ (:) (rideId, driverId),
+      cancelRide = \_ -> pure (),
+      cleanupRide = \rideId -> do
+        _ <- modifyTVarIO rideRequests $ Map.filter (\request -> request ^. #requestHeader . #rideId == rideId)
+        pure (),
+      getFirstDriverInTheQueue = pure . NonEmpty.head,
+      checkAvailability = pure . NonEmpty.toList,
+      sendRideNotAssignedNotification = \rideId driverId -> pure (),
+      resetRequestTime = \_ -> pure (),
+      runSafely = (Right <$>),
+      logInfo = \_ _ -> pure (),
+      logError = \_ _ -> pure (),
+      logEvent = \_ _ -> pure ()
+    }
 
 driverPool1 :: [DriverId]
 driverPool1 = [DriverId "driver01", DriverId "driver02", DriverId "driver03"]
@@ -44,16 +134,16 @@ isNotified _ = False
 
 data Repository = Repository
   { driverPool :: TVar (Map RideId [DriverId]),
-    rideRequest :: TVar (Map RideId Allocation.Ride),
+    rideRequests :: TVar (Map RideRequestId Allocation.RideRequest),
     notificationStatus :: TVar (Map (RideId, DriverId) Allocation.NotificationStatus),
     hasFound :: TVar [(RideId, DriverId)],
-    acceptReject :: TVar (Map (RideId, DriverId) Allocation.DriverResponse)
+    acceptReject :: TVar (Map (RideId, DriverId) DriverResponse)
   }
 
-initRepository :: Map RideId Allocation.Ride -> IO Repository
-initRepository rides = do
+initRepository :: Map RideRequestId Allocation.RideRequest -> IO Repository
+initRepository rideRequests = do
   initDriverPool <- newTVarIO driverPoolPerRide
-  initRideRequest <- newTVarIO rides
+  initRideRequest <- newTVarIO rideRequests
   initNotificationStatus <- newTVarIO Map.empty
   initHasFound <- newTVarIO []
   initAcceptReject <- newTVarIO Map.empty
@@ -61,88 +151,24 @@ initRepository rides = do
 
 allocateDriverForRide :: TestTree
 allocateDriverForRide = testCase "Find a driver for ride" $ do
-  r@Repository {..} <-
-    initRepository $
-      Map.fromList
-        [ ( ride01Id,
-            Allocation.Ride
-              { id = ride01Id,
-                orderedAt = parseTime "2018-12-06T11:39:57.153Z"
-              }
-          ),
-          ( ride02Id,
-            Allocation.Ride
-              { id = ride02Id,
-                orderedAt = parseTime "2018-12-06T11:39:57.153Z"
-              }
-          )
-        ]
+  r@Repository {..} <- initRepository rideRequestMap
 
-  _ <- Allocation.process (handle r) >>= (@?=) 2
-  _ <- atomically $ modifyTVar acceptReject $ Map.insert (ride01Id, DriverId "driver01") Allocation.Reject
-  _ <- atomically $ modifyTVar acceptReject $ Map.insert (ride02Id, DriverId "driver05") Allocation.Reject
-  _ <- Allocation.process (handle r) >>= (@?=) 2
-  _ <- atomically $ modifyTVar acceptReject $ Map.insert (ride01Id, DriverId "driver02") Allocation.Reject
-  _ <- atomically $ modifyTVar acceptReject $ Map.insert (ride02Id, DriverId "driver07") Allocation.Reject
-  _ <- atomically $ modifyTVar acceptReject $ Map.insert (ride01Id, DriverId "driver03") Allocation.Accept
-  _ <- Allocation.process (handle r) >>= (@?=) 2
-  _ <- atomically $ modifyTVar acceptReject $ Map.insert (ride02Id, DriverId "driver08") Allocation.Accept
-  _ <- Allocation.process (handle r) >>= (@?=) 2
+  Allocation.process (handle r) numRequestsToProcess >>= \numProcessed -> numProcessed @?= 2
+  modifyTVarIO acceptReject $ Map.insert (ride01Id, DriverId "driver01") rejectResponse
+  modifyTVarIO acceptReject $ Map.insert (ride02Id, DriverId "driver05") rejectResponse
+  Allocation.process (handle r) numRequestsToProcess >>= \numProcessed -> numProcessed @?= 2
+  modifyTVarIO acceptReject $ Map.insert (ride01Id, DriverId "driver02") rejectResponse
+  modifyTVarIO acceptReject $ Map.insert (ride02Id, DriverId "driver07") rejectResponse
+  modifyTVarIO acceptReject $ Map.insert (ride01Id, DriverId "driver03") acceptResponse
+  Allocation.process (handle r) numRequestsToProcess >>= \numProcessed -> numProcessed @?= 2
+  modifyTVarIO acceptReject $ Map.insert (ride02Id, DriverId "driver08") acceptResponse
+  Allocation.process (handle r) numRequestsToProcess >>= \numProcessed -> numProcessed @?= 2
 
   found <- readTVarIO hasFound
   found @?= [(ride02Id, DriverId "driver08"), (ride01Id, DriverId "driver03")]
   where
-    handle :: Repository -> Allocation.ServiceHandle IO
-    handle Repository {..} =
-      Allocation.ServiceHandle
-        { getCurrentTime = pure $ parseTime "2018-12-06T11:39:57.153Z",
-          getDriverSortMode = pure ETA,
-          getConfiguredNotificationTime = pure 120,
-          getConfiguredAllocationTime = pure 15,
-          getTopRidesToAllocate = \nmbOfRides -> do
-            rideMap <- readTVarIO rideRequest
-            let rides = Map.elems rideMap
-            pure $ take (fromIntegral nmbOfRides) rides,
-          getDriverPool = \rideId -> do
-            poolMap <- readTVarIO driverPool
-            let pool = fromMaybe [] $ Map.lookup rideId poolMap
-            pure pool,
-          sendNewRideNotification = \rideId driverId -> do
-            pure (),
-          getCurrentNotification = \rideId -> do
-            ns <- readTVarIO notificationStatus
-            let filtered = fmap snd $ Map.keys $ Map.filterWithKey (\(r, _) s -> isNotified s && r == rideId) ns
-            case filtered of
-              [a] -> pure $ Just $ Allocation.CurrentNotification a $ parseTime "2018-12-06T11:39:57.153Z"
-              _ -> pure Nothing,
-          addNotificationStatus = \rideId driverId nStatus -> do
-            now <- Time.getCurrentTime
-            atomically $ modifyTVar notificationStatus $ Map.insert (rideId, driverId) (Allocation.Notified now),
-          updateNotificationStatus = \rideId driverId nStatus -> do
-            now <- Time.getCurrentTime
-            atomically $ modifyTVar notificationStatus $ Map.insert (rideId, driverId) nStatus,
-          resetLastRejectionTime = \rideId -> pure (),
-          getAttemptedDrivers = \rideId -> do
-            ns <- readTVarIO notificationStatus
-            let filtered = fmap snd $ Map.keys $ Map.filterWithKey (\(r, _) _ -> r == rideId) ns
-            pure filtered,
-          getDriversWithNotification = do
-            ns <- readTVarIO notificationStatus
-            let filtered = fmap snd $ Map.keys $ Map.filter isNotified ns
-            pure filtered,
-          getDriverResponse = \rideId driverId -> do
-            ar <- readTVarIO acceptReject
-            pure $ Map.lookup (rideId, driverId) ar,
-          assignDriver = \rideId driverId -> do
-            _ <- atomically $ modifyTVar acceptReject $ Map.delete (rideId, driverId)
-            atomically $ modifyTVar hasFound $ (:) (rideId, driverId),
-          cancelRide = atomically . modifyTVar rideRequest . Map.delete,
-          cleanupRide = \rideId -> do
-            _ <- atomically $ modifyTVar rideRequest $ Map.delete rideId
-            pure (),
-          getFirstDriverInTheQueue = pure . NonEmpty.head,
-          checkAvailability = pure . NonEmpty.toList
-        }
+    rejectResponse = DriverResponse REJECT $ parseTime "2021-26-02T12:40:00.000Z"
+    acceptResponse = DriverResponse ACCEPT $ parseTime "2021-26-02T12:40:00.000Z"
 
 allocation :: TestTree
 allocation =
