ca38712b11e277ce98a1e200a85e0e09a7876dd0
Paul Kapustin <paul.kapustin@juspay.in>
2021-03-05 12:30:00 +0100

    Terminate background task thread on shutdown

--- app/beckn-transport/src/BackgroundTaskManager.hs
+++ app/beckn-transport/src/BackgroundTaskManager.hs
@@ -6,6 +6,7 @@ module BackgroundTaskManager where
 import App.Routes
 import App.Types
 import Beckn.External.FCM.Utils (createFCMTokenRefreshThread)
+import Beckn.Storage.DB.Config
 import Beckn.Storage.Redis.Config
 import qualified Beckn.Types.App as App
 import qualified Beckn.Types.Storage.Organization as Organization
@@ -13,7 +14,7 @@ import Beckn.Utils.Common
 import Beckn.Utils.Dhall (readDhallConfigDefault)
 import Beckn.Utils.Logging
 import qualified Beckn.Utils.Servant.Server as Server
-import Beckn.Utils.Servant.SignatureAuth (prepareAuthManagers)
+import Beckn.Utils.Servant.SignatureAuth
 import EulerHS.Prelude
 import qualified EulerHS.Runtime as R
 import Network.Wai.Handler.Warp
@@ -27,7 +28,9 @@ runBackgroundTaskManager configModifier = do
   appEnv <- configModifier <$> readDhallConfigDefault "beckn-transport"
   let loggerCfg = getEulerLoggerConfig $ appEnv ^. #loggerConfig
   let redisCfg = appEnv ^. #redisCfg
-  let checkConnections = prepareRedisConnections redisCfg
+  let checkConnections = prepareRedisConnections redisCfg >> prepareDBConnections
+  let port = appEnv ^. #bgtmPort
+  putStrLn @Text $ "Starting Background Task Manager on port " <> show port
 
   shutdown <- newEmptyTMVarIO
   activeTask <- newEmptyTMVarIO
@@ -35,35 +38,33 @@ runBackgroundTaskManager configModifier = do
   void $ installHandler sigTERM (Catch $ handleShutdown shutdown) Nothing
   void $ installHandler sigINT (Catch $ handleShutdown shutdown) Nothing
 
-  appThreadId <- forkIO $
-    R.withFlowRuntime (Just loggerCfg) $ \flowRt -> do
-      putStrLn @String "Setting up for signature auth..."
-      try (runFlowR flowRt appEnv Storage.loadAllProviders) >>= \case
-        Left (e :: SomeException) -> putStrLn @String ("Exception thrown: " <> show e)
-        Right allProviders -> do
-          let allShortIds = map (App._getShortOrganizationId . Organization._shortId) allProviders
-          case prepareAuthManagers flowRt appEnv allShortIds of
-            Left err -> putStrLn @String ("Could not prepare authentication managers: " <> show err)
-            Right getManagers -> do
-              managerMap <- getManagers
-              putStrLn @Text $ "Loaded http managers - " <> show (keys managerMap)
-              let flowRt' = flowRt {R._httpClientManagers = managerMap}
-              putStrLn @String "Initializing Redis Connections..."
-              try (runFlowR flowRt' appEnv checkConnections) >>= \case
-                Left (e :: SomeException) -> putStrLn @Text ("Connections check failed. Exception thrown: " <> show e)
-                Right _ ->
-                  try (runFlowR flowRt' appEnv createFCMTokenRefreshThread) >>= \case
-                    Left (e :: SomeException) -> putStrLn @String ("Exception thrown: " <> show e)
-                    Right _ -> do
-                      let settings = setPort (bgtmPort appEnv) defaultSettings
-                      _bgtmThreadId <- forkIO $ runFlowR flowRt' appEnv $ Runner.run shutdown activeTask
-                      runSettings settings $ Server.run healthCheckAPI (healthCheckServer shutdown) EmptyContext (App.EnvR flowRt' appEnv)
-
-  atomically $ readTMVar shutdown
-  putStrLn @Text "Shutting down..."
-  waitForTaskToComplete activeTask
-  killThread appThreadId
-  exitSuccess
+  R.withFlowRuntime (Just loggerCfg) $ \flowRt -> do
+    try (runFlowR flowRt appEnv checkConnections) >>= \case
+      Left (e :: SomeException) -> putStrLn @Text ("Connections check failed. Exception thrown: " <> show e) >> handleShutdown shutdown
+      Right _ -> do
+        putStrLn @Text "Setting up for signature auth..."
+        try (runFlowR flowRt appEnv Storage.loadAllProviders) >>= \case
+          Left (e :: SomeException) -> putStrLn @Text ("Exception thrown: " <> show e) >> handleShutdown shutdown
+          Right allProviders -> do
+            let allShortIds = map (App._getShortOrganizationId . Organization._shortId) allProviders
+            prepareAuthManagers flowRt appEnv allShortIds & \case
+              Left err -> putStrLn @Text ("Could not prepare authentication managers: " <> show err) >> handleShutdown shutdown
+              Right getManagers -> do
+                managerMap <- getManagers
+                putStrLn @Text $ "Loaded http managers - " <> show (keys managerMap)
+                let flowRt' = flowRt {R._httpClientManagers = managerMap}
+                let settings = setPort port defaultSettings
+                mError <- try (runFlowR flowRt appEnv createFCMTokenRefreshThread)
+                whenLeft mError $ \(e :: SomeException) -> putStrLn @Text ("FCM check failed: " <> show e)
+                appThreadId <- forkIO $ runFlowR flowRt' appEnv $ Runner.run shutdown activeTask
+                apiThreadId <- forkIO $ runSettings settings $ Server.run healthCheckAPI (healthCheckServer shutdown) EmptyContext (App.EnvR flowRt' appEnv)
+                putStrLn @Text "Background Task Manager is ready."
+                atomically $ readTMVar shutdown
+                putStrLn @Text "Shutting down..."
+                waitForTaskToComplete activeTask
+                killThread appThreadId
+                killThread apiThreadId
+                exitSuccess
   where
     handleShutdown shutdown = do
       putStrLn @Text "Received shutdown signal. Handling..."
