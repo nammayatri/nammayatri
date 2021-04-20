b308a79f55103f3b5a19c62d1ce8ae186e5cf5f1
Alexander Korovin <alexander.korovin@metalamp.io>
2021-04-20 17:17:21 +0700

    Separate AppEnv in mock-provider-backend

--- app/mock-provider-backend/src/App.hs
+++ app/mock-provider-backend/src/App.hs
@@ -31,26 +31,27 @@ import Network.Wai.Handler.Warp
   )
 import System.Environment
 
-runMockProvider :: (AppEnv -> AppEnv) -> IO ()
+runMockProvider :: (AppCfg -> AppCfg) -> IO ()
 runMockProvider configModifier = do
-  appEnv <- configModifier <$> readDhallConfigDefault "mock-provider-backend"
-  Metrics.serve (metricsPort appEnv)
+  appCfg <- configModifier <$> readDhallConfigDefault "mock-provider-backend"
+  Metrics.serve (appCfg ^. #metricsPort)
   hostname <- (T.pack <$>) <$> lookupEnv "POD_NAME"
-  let loggerRt = getEulerLoggerRuntime hostname $ appEnv ^. #loggerConfig
+  let loggerRt = getEulerLoggerRuntime hostname $ appCfg ^. #loggerConfig
   let settings =
         setOnExceptionResponse mockProviderExceptionResponse $
-          setPort (port appEnv) defaultSettings
+          setPort (appCfg ^. #port) defaultSettings
+      appEnv = mkAppEnv appCfg
   E.withFlowRuntime (Just loggerRt) $ \flowRt -> do
     flowRt' <- runFlowR flowRt appEnv $ do
       withLogContext "Server startup" $ do
         logInfo "Setting up for signature auth..."
-        let selfId = appEnv ^. #selfId
+        let selfId = appCfg ^. #selfId
         getManager <-
           prepareAuthManager flowRt appEnv "Authorization" selfId
             & handleLeft exitAuthManagerPrepFailure "Could not prepare authentication manager: "
         authManager <- L.runIO getManager
-        logInfo ("Runtime created. Starting server at port " <> show (port appEnv))
-        migrateIfNeeded (migrationPath appEnv) (dbCfg appEnv) (autoMigrate appEnv)
+        logInfo ("Runtime created. Starting server at port " <> show (appCfg ^. #port))
+        migrateIfNeeded (appCfg ^. #migrationPath) (appCfg ^. #dbCfg) (appCfg ^. #autoMigrate)
           >>= handleLeft exitDBMigrationFailure "Couldn't migrate database: "
         return $ flowRt {R._httpClientManagers = Map.singleton signatureAuthManagerKey authManager}
     runSettings settings $ run $ App.EnvR flowRt' appEnv
