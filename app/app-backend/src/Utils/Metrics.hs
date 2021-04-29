1f67ab274e5e4e8e8905db54a4d9ce17954e061d
Alexander Korovin <alexander.korovin@metalamp.io>
2021-04-29 17:03:59 +0700

    Another approach

--- app/app-backend/src/Utils/Metrics.hs
+++ app/app-backend/src/Utils/Metrics.hs
@@ -11,6 +11,7 @@ import Beckn.Types.Common
 import qualified Beckn.Types.Storage.Case as Case
 import Beckn.Utils.Common
 import Data.Time (diffUTCTime)
+import Data.Time.Clock (UTCTime)
 import qualified EulerHS.Language as L
 import EulerHS.Prelude
 import Prometheus as P
@@ -23,34 +24,40 @@ incrementCaseCount caseStatus caseType =
   L.runIO $ P.withLabel caseCounter (show caseStatus, show caseType) P.incCounter
 
 searchDurationHistogram :: P.Histogram
-searchDurationHistogram = P.unsafeRegister . P.histogram (P.Info "beckn_search_round_trip" "") $ P.linearBuckets 0 0.5 30
+searchDurationHistogram = P.unsafeRegister . P.histogram (P.Info "beckn_search_round_trip" "") $ P.linearBuckets 0 0.5 32
 
 putSearchDuration :: Double -> Flow ()
 putSearchDuration duration =
   L.runIO $ P.observe searchDurationHistogram duration
 
 searchDurationKey :: Text -> Text
-searchDurationKey txnId = "beckn:" <> txnId <> ":on_seach:received"
+searchDurationKey txnId = "beckn:" <> txnId <> ":on_search:received"
+
+searchDurationLockKey :: Text -> Text
+searchDurationLockKey txnId = txnId <> ":on_search"
+
+searchRedisExTime :: (Num a) => a
+searchRedisExTime = 15
 
 startSearchMetrics :: Text -> Flow ()
 startSearchMetrics txnId = do
   startTime <- getCurrentTime
+  Redis.setExRedis (searchDurationKey txnId) startTime searchRedisExTime
   fork "Gateway Search Metrics" $ do
-    searchDuration <- checkForKey startTime
-    putSearchDuration $ realToFrac searchDuration
-  where
-    checkForKey startTime = do
-      L.runIO $ threadDelay 200000
-      Redis.getKeyRedis (searchDurationKey txnId) >>= \case
-        Nothing -> do
-          currTime <- getCurrentTime
-          let diffTime = diffUTCTime currTime startTime
-          if diffTime > 10
-            then return diffTime
-            else checkForKey startTime
-        Just endTime -> return $ diffUTCTime endTime startTime
+    L.runIO $ threadDelay $ searchRedisExTime * 1000000
+    tryToPutSearchDuration txnId (const searchRedisExTime)
 
 finishSearchMetrics :: Text -> Flow ()
 finishSearchMetrics txnId = do
   endTime <- getCurrentTime
-  Redis.setExRedis (searchDurationKey txnId) endTime 10
+  tryToPutSearchDuration txnId (realToFrac . diffUTCTime endTime)
+
+tryToPutSearchDuration :: Text -> (UTCTime -> Double) -> Flow ()
+tryToPutSearchDuration txnId countDurr =
+  whenM (Redis.tryLockRedis (searchDurationLockKey txnId) searchRedisExTime) $ do
+    Redis.getKeyRedis (searchDurationKey txnId) >>= \case
+      Just startTime -> do
+        void $ Redis.deleteKeyRedis (searchDurationKey txnId)
+        putSearchDuration $ countDurr startTime
+      Nothing -> return ()
+    Redis.unlockRedis $ searchDurationLockKey txnId
