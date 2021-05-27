966df03cf13cf1acf7d73ab91225e8a3733137b5
Oleg Romashin <oleg.romashin@juspay.in>
2021-05-27 13:53:40 +0700

    unusued query functions deletion

--- app/app-backend/src/Storage/Queries/Person.hs
+++ app/app-backend/src/Storage/Queries/Person.hs
@@ -58,17 +58,6 @@ complementVal l
   | null l = B.val_ True
   | otherwise = B.val_ False
 
-findByIdentifier ::
-  Storage.IdentifierType -> Text -> Flow (Maybe Storage.Person)
-findByIdentifier idType mb = do
-  dbTable <- getDbTable
-  DB.findOne dbTable predicate
-    >>= decrypt
-  where
-    predicate Storage.Person {..} =
-      _identifierType ==. B.val_ idType
-        &&. (_mobileNumber ^. #_hash) ==. B.val_ (Just $ evalDbHash mb)
-
 findByUsernameAndPassword ::
   Text -> Text -> Flow (Maybe Storage.Person)
 findByUsernameAndPassword email password = do
@@ -160,16 +149,6 @@ update id statusM nameM emailM roleM identTypeM identM = do
         )
     predicate pid Storage.Person {..} = _id ==. B.val_ pid
 
-updatePersonOrgId :: Text -> Id Storage.Person -> Flow ()
-updatePersonOrgId orgId personId = do
-  dbTable <- getDbTable
-  now <- getCurrentTime
-  DB.update dbTable (setClause orgId now) (predicate personId)
-  where
-    setClause a n Storage.Person {..} =
-      mconcat [_organizationId <-. B.val_ (Just a), _updatedAt <-. B.val_ n]
-    predicate i Storage.Person {..} = _id ==. B.val_ i
-
 updatePersonalInfo :: Id Storage.Person -> Maybe Text -> Maybe Text -> Maybe Text -> Maybe Text -> Maybe Storage.Gender -> Maybe Text -> Maybe FCM.FCMRecipientToken -> Flow ()
 updatePersonalInfo personId mbFirstName mbMiddleName mbLastName mbFullName mbGender mbEmail mbDeviceToken = do
   dbTable <- getDbTable
@@ -188,19 +167,6 @@ updatePersonalInfo personId mbFirstName mbMiddleName mbLastName mbFullName mbGen
           maybe mempty (\x -> _deviceToken <-. B.val_ (Just x)) mbDToken
         ]
     predicate id Storage.Person {..} = _id ==. B.val_ id
-
-findAllWithLimitOffsetByRole :: Maybe Int -> Maybe Int -> [Storage.Role] -> Flow [Storage.Person]
-findAllWithLimitOffsetByRole mlimit moffset roles = do
-  dbTable <- getDbTable
-  DB.findAll dbTable (B.limit_ limit . B.offset_ offset . B.orderBy_ orderByDesc) (predicate roles)
-    >>= decrypt
-  where
-    limit = toInteger $ fromMaybe 10 mlimit
-    offset = toInteger $ fromMaybe 0 moffset
-    predicate [] Storage.Person {..} = B.val_ True
-    predicate r Storage.Person {..} =
-      _role `B.in_` (B.val_ <$> r)
-    orderByDesc Storage.Person {..} = B.desc_ _createdAt
 
 findAllWithLimitOffsetBy :: Maybe Int -> Maybe Int -> [Storage.Role] -> [Id Organization] -> Flow [Storage.Person]
 findAllWithLimitOffsetBy mlimit moffset roles orgIds = do
