781e9315de9f74a4e3cc6a7e153a7152f530751d
Nikith Shetty <shettynikith3@gmail.com>
2021-08-04 20:30:21 +0530

    remove alter query for adding dead_distance field in migrations

--- dev/migrations/beckn-transport/0005-add-distance-to-PI.sql
+++ dev/migrations/beckn-transport/0005-add-distance-to-PI.sql
@@ -1,4 +1,2 @@
 ALTER TABLE ONLY atlas_transporter.product_instance
 ADD COLUMN IF NOT EXISTS distance double precision NOT NULL DEFAULT 0;
-ALTER TABLE ONLY atlas_transporter.product_instance
-ADD COLUMN IF NOT EXISTS dead_distance double precision NOT NULL DEFAULT 0;
