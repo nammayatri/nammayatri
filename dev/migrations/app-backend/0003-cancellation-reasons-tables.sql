89b6016431984badd7d3df582bc45681c3c50eac
Alexander Korovin <alexander.korovin@metalamp.io>
2021-08-17 00:00:40 +0700

    Add some reasons

--- dev/migrations/app-backend/0003-cancellation-reasons-tables.sql
+++ dev/migrations/app-backend/0003-cancellation-reasons-tables.sql
@@ -1,10 +1,22 @@
 CREATE TABLE atlas_app.cancellation_reason (
-    reason_code character varying(255) NOT NULL,
+    reason_code character varying(255) PRIMARY KEY NOT NULL,
     description character varying(255) NOT NULL
 );
 
 CREATE TABLE atlas_app.ride_cancellation_reason (
-    id character(36) PRIMARY KEY REFERENCES atlas_transporter.product_instance (id) on delete cascade,
-    reason_code character varying(255) NOT NULL REFERENCES atlas_transporter.cancellation_reasons (reason_code) on delete cascade,
+    id character(36) PRIMARY KEY REFERENCES atlas_app.product_instance (id) on delete cascade,
+    reason_code character varying(255) NOT NULL REFERENCES atlas_app.cancellation_reason (reason_code) on delete cascade,
     description character varying(255)
 );
+
+INSERT INTO atlas_app.cancellation_reason (reason_code, description) VALUES
+  ('URGENT_BUSINESS', 'Urgent business came up.');
+
+INSERT INTO atlas_app.cancellation_reason (reason_code, description) VALUES
+  ('BAD_DRIVER', 'Something wrong with driver.');
+
+INSERT INTO atlas_app.cancellation_reason (reason_code, description) VALUES
+  ('DRIVER_IS_ABSENT', 'My driver is late.');
+
+INSERT INTO atlas_app.cancellation_reason (reason_code, description) VALUES
+  ('OTHER', 'Some other reason.');
--- dev/migrations/beckn-transport/0008-cancellation-reasons-tables.sql
+++ dev/migrations/beckn-transport/0008-cancellation-reasons-tables.sql
@@ -1,10 +1,22 @@
 CREATE TABLE atlas_transporter.cancellation_reason (
-    reason_code character varying(255) NOT NULL,
+    reason_code character varying(255) PRIMARY KEY NOT NULL,
     description character varying(255) NOT NULL
 );
 
 CREATE TABLE atlas_transporter.ride_cancellation_reason (
     id character(36) PRIMARY KEY REFERENCES atlas_transporter.product_instance (id) on delete cascade,
-    reason_code character varying(255) NOT NULL REFERENCES atlas_transporter.cancellation_reasons (reason_code) on delete cascade,
+    reason_code character varying(255) NOT NULL REFERENCES atlas_transporter.cancellation_reason (reason_code) on delete cascade,
     description character varying(255)
 );
+
+INSERT INTO atlas_transporter.cancellation_reason (reason_code, description) VALUES
+  ('CRASH', 'I had an accident.');
+
+INSERT INTO atlas_transporter.cancellation_reason (reason_code, description) VALUES
+  ('CAR_IS_MALFUNCTIONING', 'Car is malfunctioning');
+
+INSERT INTO atlas_transporter.cancellation_reason (reason_code, description) VALUES
+  ('REQUESTOR_IS_ABSENT', 'I can not find my rider.');
+
+INSERT INTO atlas_transporter.cancellation_reason (reason_code, description) VALUES
+  ('OTHER', 'Some other reason.');
