e69cea7026a5c0320cd5fda8f2c0eb820b57ea77
Vlad Zagvozdkin <vladislav.zagvozdkin@juspay.in>
2021-10-27 17:32:54 +0400

    fix mobility domain variant in spec

--- lib/beckn-core/src/Beckn/Types/Registry/Domain.hs
+++ lib/beckn-core/src/Beckn/Types/Registry/Domain.hs
@@ -12,20 +12,24 @@ data Domain
   | LOCAL_RETAIL
   | FOOD_AND_BEVERAGE
   | HEALTHCARE
+  | METRO
   | UNKNOWN_DOMAIN Text
   deriving (Eq, Generic, Show, ToSchema)
 
 instance ToJSON Domain where
+  toJSON MOBILITY = String "nic2004:60221"
+  toJSON METRO = String "nic2004:60212"
   toJSON LOCAL_RETAIL = String "nic2004:52110"
   toJSON FINAL_MILE_DELIVERY = String "nic2004:55204"
   toJSON (UNKNOWN_DOMAIN domain) = String domain
   toJSON val = genericToJSON constructorsWithHyphensUntagged val -- TODO: update remaining domains with codes
 
 instance FromJSON Domain where
+  parseJSON (String "nic2004:60221") = pure MOBILITY
   parseJSON (String "nic2004:52110") = pure LOCAL_RETAIL
   parseJSON (String "nic2004:55204") = pure FINAL_MILE_DELIVERY
-  parseJSON (String "MOBILITY") = pure MOBILITY
   parseJSON (String "FOOD-AND-BEVERAGE") = pure FOOD_AND_BEVERAGE
   parseJSON (String "HEALTHCARE") = pure HEALTHCARE
+  parseJSON (String "nic2004:60212") = pure METRO
   parseJSON (String domain) = pure $ UNKNOWN_DOMAIN domain
   parseJSON e = typeMismatch "Registry Domain" e
