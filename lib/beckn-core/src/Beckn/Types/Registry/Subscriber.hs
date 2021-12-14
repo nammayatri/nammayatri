501bbee03ea4cfd25b8977e48275973f4c5231da
Nikith Shetty <shettynikith3@gmail.com>
2021-12-14 15:56:17 +0530

    fix subscriber parsing issue

--- lib/beckn-core/src/Beckn/Types/Registry/Subscriber.hs
+++ lib/beckn-core/src/Beckn/Types/Registry/Subscriber.hs
@@ -1,8 +1,8 @@
 module Beckn.Types.Registry.Subscriber where
 
-import Beckn.Utils.JSON (stripPrefixUnderscoreIfAny)
+import Data.Aeson (object, withObject, (.:), (.:?), (.=))
 import Data.Time (UTCTime)
-import EulerHS.Prelude
+import EulerHS.Prelude hiding ((.=))
 import Servant.Client (BaseUrl)
 
 data Subscriber = Subscriber
@@ -24,10 +24,41 @@ data Subscriber = Subscriber
   deriving (Show, Generic)
 
 instance FromJSON Subscriber where
-  parseJSON = genericParseJSON stripPrefixUnderscoreIfAny
+  parseJSON = withObject "Subscriber" $ \o ->
+    Subscriber
+      <$> o .: "ukId"
+      <*> o .: "subscriber_id"
+      <*> o .:? "subscriber_url"
+      <*> o .:? "type"
+      <*> o .:? "domain"
+      <*> o .:? "city"
+      <*> o .:? "country"
+      <*> o .: "signing_public_key"
+      <*> o .:? "encr_public_key"
+      <*> o .:? "valid_from"
+      <*> o .:? "valid_until"
+      <*> o .:? "status"
+      <*> o .:? "created"
+      <*> o .:? "updated"
 
 instance ToJSON Subscriber where
-  toJSON = genericToJSON stripPrefixUnderscoreIfAny
+  toJSON subscriber =
+    object
+      [ "ukId" .= subscriber.unique_key_id,
+        "subscriber_id" .= subscriber.subscriber_id,
+        "subscriber_url" .= subscriber.subscriber_url,
+        "type" .= subscriber._type,
+        "domain" .= subscriber.domain,
+        "city" .= subscriber.city,
+        "country" .= subscriber.country,
+        "signing_public_key" .= subscriber.signing_public_key,
+        "encr_public_key" .= subscriber.encr_public_key,
+        "valid_from" .= subscriber.valid_from,
+        "valid_until" .= subscriber.valid_until,
+        "status" .= subscriber.status,
+        "created" .= subscriber.created,
+        "updated" .= subscriber.updated
+      ]
 
 data SubscriberType
   = BAP
