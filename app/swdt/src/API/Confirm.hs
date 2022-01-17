ab004048b2956fbd90c441ecc7ed0629a54732ce
Yuri Romanowski <yuri.romanowski@juspay.in>
2022-01-17 15:28:52 +0300

    removed unused modules

--- app/swdt/src/API/Confirm.hs
+++ app/swdt/src/API/Confirm.hs
@@ -4,39 +4,10 @@ import Beckn.Prelude
 import Beckn.Types.Core.Ack
 import Beckn.Types.Core.ReqTypes
 import Beckn.Utils.Logging
-import Core.Confirm (ConfirmMessage)
+import Data.Aeson
 import Types.App
 
-confirmServer :: BecknReq ConfirmMessage -> MockM AckResponse
+confirmServer :: BecknReq Value -> MockM AckResponse
 confirmServer _ = do
-  mockLog INFO $ "got confirm request"
+  mockLog INFO "got confirm request"
   pure Ack
-
-{-
-confirmServer :: BecknReq ConfirmMessage -> MockM AckResponse
-confirmServer confirmReq@(BecknReq ctx req) = do
-  mockLog DEBUG $ "request body: " <> show confirmReq
-  _ <- mockFork $ do
-    mockLog DEBUG "debug message inside fork"
-    liftIO $ threadDelay $ 1000000 * 2
-    context' <- buildOnConfirmContext ctx
-    let onConfirmCallbackData = buildOnConfirmCallbackData req
-    let bapUri = ctx.bap_uri
-    ack <- callBapOnConfirmS bapUri $ BecknCallbackReq context' $ Right onConfirmCallbackData
-    mockLog DEBUG $ "got ack" <> show ack
-  pure Ack
-
-buildOnConfirmContext :: Context -> MockM Context
-buildOnConfirmContext ctx = do
-  now <- getCurrentTime
-  bppId <- asks (.selfId)
-  bppUri <- asks (.selfUri)
-  let ctx' =
-        ctx{action = ON_CONFIRM,
-            bpp_id = Just bppId,
-            bpp_uri = Just bppUri,
-            timestamp = now
-           }
-
-  pure ctx'
--}
