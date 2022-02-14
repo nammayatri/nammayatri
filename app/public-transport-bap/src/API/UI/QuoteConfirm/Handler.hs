92559c91fc6c69d172441336cf8a569e68055177
Yuri Romanowski <yuri.romanowski@metalamp.io>
2022-02-14 17:10:07 +0300

    added askConfig

--- app/public-transport-bap/src/API/UI/QuoteConfirm/Handler.hs
+++ app/public-transport-bap/src/API/UI/QuoteConfirm/Handler.hs
@@ -9,12 +9,7 @@ import Beckn.Types.Id
 import Beckn.Utils.Common
 import Core.ACL.Confirm
 import Core.Spec.Confirm (ConfirmMessage (ConfirmMessage))
-<<<<<<< HEAD
 import Domain.Endpoints.UI.QuoteConfirm
-=======
-import Domain.Confirm (ConfirmMessageD)
-import Domain.Endpoints.UI.QuoteConfirm.Handler
->>>>>>> Added confirm/on_confirm for public transport bap
 import qualified Domain.Types.Quote as D
 import qualified ExternalAPI.Flow as ExternalAPI
 import Tools.Auth (PersonId)
@@ -28,14 +23,8 @@ handler personId quoteId confirmReq = withFlowHandlerAPI $ do
 
 callConfirm :: ConfirmMessageD -> Flow ()
 callConfirm msg = do
-<<<<<<< HEAD
   selfUrl <- askConfig (.selfURI)
   selfId <- askConfig (.selfId)
   context <- buildContext CONFIRM msg.txnId selfUrl Nothing selfId
-=======
-  selfUrl <- asks (.config.selfURI)
-  selfId <- asks (.config.selfId)
-  context <- buildContext CONFIRM msg.txnId selfId selfUrl Nothing
->>>>>>> Added confirm/on_confirm for public transport bap
   let confirmOrder = mkConfirmMessage msg
   ExternalAPI.confirm msg.booking.bppUrl (BecknReq context $ ConfirmMessage confirmOrder)
