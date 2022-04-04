f1c517df46e731838302e1e2be4c2c76170f70de
Andrey Kitavtsev <AKitavtsev@mail.ru>
2022-04-04 17:10:35 +0300

    [BKN-1203]use the hlint suggestion, formated

--- app/beckn-transport/src/Domain/Types/Person.hs
+++ app/beckn-transport/src/Domain/Types/Person.hs
@@ -6,7 +6,7 @@ module Domain.Types.Person where
 import Beckn.External.Encryption
 import qualified Beckn.External.FCM.Types as FCM
 import Beckn.Types.Id
-import Beckn.Utils.Common (maskText, EsqDBFlow)
+import Beckn.Utils.Common (EsqDBFlow, maskText)
 import Data.Aeson
 import qualified Data.ByteString.Lazy as BSL
 import Data.OpenApi (ToSchema)
@@ -123,7 +123,6 @@ getPersonNumber person = do
   decMobileNumber <- mapM decrypt person.mobileNumber
   return $ person.mobileCountryCode <> decMobileNumber
 
-getPersonFullName :: (EsqDBFlow m r, EncFlow m r)  => Person -> m (Maybe Text)
+getPersonFullName :: (EsqDBFlow m r, EncFlow m r) => Person -> m (Maybe Text)
 getPersonFullName person = do
   return ((\fN -> fN <> maybe "" (" " <>) person.lastName) <$> person.firstName)
-
