2cd77282c4862394e7f3c21181ae5415a90ed819
Ritika Jindal <ritika.j@juspay.in>
2022-05-06 14:56:53 +0530

    BKN-1290-added-fix-for-cancelling-ongoing-ride-from-customer-side

--- app/app-backend/src/Product/Cancel.hs
+++ app/app-backend/src/Product/Cancel.hs
@@ -9,9 +9,11 @@ import qualified Beckn.Types.Core.Taxi.Cancel.Req as ReqCancel
 import qualified Beckn.Types.Core.Taxi.Common.Context as Context
 import Beckn.Types.Id
 import qualified Domain.Types.Person as Person
+import qualified Domain.Types.Ride as Ride
 import qualified Domain.Types.RideBooking as SRB
 import qualified Domain.Types.RideBookingCancellationReason as SBCR
 import qualified ExternalAPI.Flow as ExternalAPI
+import qualified Storage.Queries.Ride as QR
 import qualified Storage.Queries.RideBooking as QRB
 import qualified Storage.Queries.RideBookingCancellationReason as QBCR
 import qualified Storage.Queries.SearchRequest as MC
@@ -24,7 +26,8 @@ cancel bookingId personId req = withFlowHandlerAPI . withPersonIdLogTag personId
   let bookingCancellationReasonAPI = req.bookingCancellationReason
   rideBooking <- QRB.findById bookingId >>= fromMaybeM RideBookingDoesNotExist
   searchRequest <- MC.findByPersonId personId (rideBooking.requestId) >>= fromMaybeM SearchRequestNotFound
-  unless (isRideBookingCancellable rideBooking) $
+  canCancelRideBooking <- isRideBookingCancellable rideBooking
+  unless canCancelRideBooking $
     throwError $ RideInvalidStatus "Cannot cancel this ride"
   let txnId = getId $ searchRequest.id
   bapURIs <- asks (.bapSelfURIs)
@@ -55,6 +58,10 @@ cancel bookingId personId req = withFlowHandlerAPI . withPersonIdLogTag personId
             ..
           }
 
-isRideBookingCancellable :: SRB.RideBooking -> Bool
-isRideBookingCancellable ride =
-  ride.status `elem` [SRB.CONFIRMED, SRB.AWAITING_REASSIGNMENT, SRB.TRIP_ASSIGNED]
+isRideBookingCancellable :: EsqDBFlow m r => SRB.RideBooking -> m Bool
+isRideBookingCancellable rideBooking
+  | rideBooking.status `elem` [SRB.CONFIRMED, SRB.AWAITING_REASSIGNMENT] = pure True
+  | rideBooking.status == SRB.TRIP_ASSIGNED = do
+    ride <- QR.findActiveByRBId rideBooking.id >>= fromMaybeM RideDoesNotExist
+    pure (ride.status == Ride.NEW)
+  | otherwise = pure False
