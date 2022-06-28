ec5eee4df46b3ff91b3bf808f0bc81bf23a4db5d
Alexander Korovin <alexander.korovin@metalamp.io>
2022-06-28 23:24:45 +0700

    Fix sql files

--- dev/migrations/beckn-transport/1026-add-bussines-event-table.sql
+++ dev/migrations/beckn-transport/1026-add-bussines-event-table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE atlas_transporter.business_event (
     id character(36) PRIMARY KEY NOT NULL,
-    driver_id character(36), 
+    driver_id character(36),
     event_type varchar (255) NOT NULL,
     time_stamp timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    rideBooking_id character(36),
+    ride_booking_id character(36),
     when_pool_was_computed varchar (255),
     vehicle_variant varchar (255),
     distance double precision,
