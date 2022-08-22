ee5607f8abead87b3fe9dbc9c1dbbb645d3f27bf
Alexander Korovin <alexander.korovin@metalamp.io>
2022-08-22 17:21:25 +0700

    fix ormolu

--- app/mock-idfy/src/App.hs
+++ app/mock-idfy/src/App.hs
@@ -4,18 +4,18 @@ import App.Routes (mockIdfyAPI, mockIdfyServer)
 import App.Types
 import Beckn.Prelude
 import Beckn.Types.Logging
+import Beckn.Utils.Servant.Client
 import Beckn.Utils.Servant.Server
+import qualified EulerHS.Runtime as R
 import Servant
 import Servant.Client.Core
-import qualified EulerHS.Runtime as R
-import Beckn.Utils.Servant.Client
 
 runMockIdfy :: (AppCfg -> AppCfg) -> IO ()
 runMockIdfy configModifier = do
   appEnv <- buildAppEnv $ configModifier defaultConfig
   runServerWithHealthCheck appEnv mockIdfyAPI mockIdfyServer identity identity EmptyContext releaseAppEnv \flowRt -> do
-      managers <- createManagers mempty -- default manager is created
-      pure $ flowRt {R._httpClientManagers = managers}
+    managers <- createManagers mempty -- default manager is created
+    pure $ flowRt {R._httpClientManagers = managers}
 
 defaultConfig :: AppCfg
 defaultConfig =
@@ -27,11 +27,11 @@ defaultConfig =
       accountId = "xxxxxxx",
       apiKey = "xxxxxxx",
       secret = "xxxxxxx",
-      httpClientOptions = 
-        HttpClientOptions 
+      httpClientOptions =
+        HttpClientOptions
           { timeoutMs = 2000,
             maxRetries = 3
-          }, 
+          },
       loggerConfig =
         LoggerConfig
           { level = DEBUG,
@@ -41,4 +41,4 @@ defaultConfig =
             logRawSql = True,
             prettyPrinting = True
           }
-    } 
+    }
