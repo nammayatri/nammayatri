a38f8e1ff42fcee8a7340f39ba1505a81ffb0409
Umang Joshi <umang.joshi@juspay.com>
2023-01-16 16:47:40 +0530

    setting myvaluefirst in migrations

--- dev/migrations/app-backend/1071-merchant-sms-config.sql
+++ dev/migrations/app-backend/1071-merchant-sms-config.sql
@@ -1,8 +1,8 @@
 INSERT INTO atlas_app.merchant_service_config (merchant_id, service_name, config_json)
 SELECT m.id, 'Sms_MyValueFirst',
   json_build_object(
-      'username','0.1.0|2|nP/5EXjeoVjgH1s9LbAy/VdVByNBlTI6ZfqT5rcXDAo5MNwKeejnLy7CeP5+Hm8vVPWM3qUfmqdsjrjfJQ=='
-    , 'password','0.1.0|2|gKVPR0m1qe/6NSNz2EBx6IjxagSagfsL+kbL36htuDYrpQ9F3dFGZ6d6zOuPY1Tk4uYMSMZRJHq6yF4='
+      'username','0.1.0|2|aH69syF+qmjP8wpjdwy5KdrHqhsTd1s7lH6TupSUYwMAS5rpi4jGDsA6Nt1uqGPWZxdMshc18cDhmQ=='
+    , 'password','0.1.0|0|MbGCmY0OMu39bi7dEokkZ4kvgN17S+whz29QJa+XXUy+mue72jMsAHfVGd4lM9AEWbCqRywCu2RTpA=='
     , 'url','http://localhost:4343'
   )
 FROM atlas_app.merchant m;
@@ -10,8 +10,8 @@ FROM atlas_app.merchant m;
 INSERT INTO atlas_app.merchant_service_config (merchant_id, service_name, config_json)
 SELECT m.id, 'Sms_ExotelSms',
   json_build_object(
-      'apiKey','0.1.0|1|DoT32ljUozGmqzLcIPoInLPqrw6rXRquuAuMINhTDV+wU9gI0rtBkCnDh8zz0yuzcFZerBOWPigP2RmTB05ilSybL7MpuA/uPlm6yBYKMEVt8z76nrbx5+a9P81VVLSoR3Bl'
-    , 'apiToken','0.1.0|1|VX8BmY9cwsvErnmXjjfrwcOePFK1yzBxTov/9mwnnBzuGIfA4kXVt5MZ7N/q7YEaU9u3w2le6aQthwrFBZjtle8isRS3y7CoEsRJEd3yll3n+oTcGRJr4n1FRzYvqPcU2zlv'
+      'apiKey','0.1.0|2|aH69syF+qmjP8wpjdwy5KdrHqhsTd1s7lH6TupSUYwMAS5rpi4jGDsA6Nt1uqGPWZxdMshc18cDhmQ=='
+    , 'apiToken','0.1.0|0|MbGCmY0OMu39bi7dEokkZ4kvgN17S+whz29QJa+XXUy+mue72jMsAHfVGd4lM9AEWbCqRywCu2RTpA=='
     , 'sid','juspay2m'
     , 'url','api.in.exotel.com'
   )
@@ -20,4 +20,4 @@ FROM atlas_app.merchant m;
 ALTER TABLE atlas_app.merchant_service_usage_config ADD COLUMN send_s_m_s character varying(30);
 
 UPDATE atlas_app.merchant_service_usage_config
-SET send_s_m_s = 'ExotelSms';
+SET send_s_m_s = 'MyValueFirst';
