78ad9fe7fdb78557acfdfb291f23aee50d2a64d7
Roman Bodavskiy <roman.bodavsky@juspay.in>
2023-01-28 09:04:02 +0300

    [BKN-2767] run mock google in location update tests

--- lib/location-updates/test/app/Main.hs
+++ lib/location-updates/test/app/Main.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE OverloadedLabels #-}
+
 module Main where
 
 import API
+import qualified "mock-google" App as MockGoogle
+import Beckn.Utils.Common
 import EulerHS.Prelude
 import qualified "mock-google" Lib.IntegrationTests.Environment as Environment
 import RedisAlgorithm
+import System.Environment as Env (setEnv)
 import Test.Tasty
 import Test.Tasty.Hspec
 import Utils
@@ -15,6 +20,12 @@ main = do
 specs :: Environment.AppCfg -> AppEnv -> IO TestTree
 specs appCfg appEnv = do
   googleCfgEncrypted <- Environment.buildGoogleConfig appCfg.encTools appCfg.googleCfg
+  -- Try to run mock-google. We already run it in integration tests, but we don't know which tests will start faster
+  Env.setEnv "MOCK_GOOGLE_CONFIG_PATH" "../../dhall-configs/dev/mock-google.dhall"
+  _ <- forkIO $
+    MockGoogle.runService $ \cfg ->
+      cfg & hideLogging
+        & #mockDataPath .~ "../../app/mocks/google/mock-data/"
   apiTreeSnap <- testSpec "Testing API using Snap-to-road" $ apiSpec appEnv googleCfgEncrypted
   apiTreeOsrm <- testSpec "Testing API using Osrm" $ apiSpec appEnv osrmConfig
   return $
@@ -24,3 +35,12 @@ specs appCfg appEnv = do
         apiTreeSnap,
         apiTreeOsrm
       ]
+
+hideLogging :: HasField "loggerConfig" cfg LoggerConfig => cfg -> cfg
+hideLogging cfg =
+  cfg{loggerConfig =
+        cfg.loggerConfig
+          & #logToConsole .~ False
+          & #logRawSql .~ False
+          & #logToFile .~ True
+     }
