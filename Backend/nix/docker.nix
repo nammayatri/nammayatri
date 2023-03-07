2e3135f5f664f1219b2fce96647ca8ead55733ff
Sridhar Ratnakumar <srid@srid.ca>
2023-03-07 16:28:07 -0500

    ci: Build & push docker image
    - Push when on 'main' or 'nixify'

--- Backend/nix/docker.nix
+++ Backend/nix/docker.nix
@@ -1,17 +1,22 @@
 { self, ... }:
 
+let
+  imageName = "ghcr.io/nammayatri/nammayatri";
+  # self.rev will be non-null only when the working tree is clean
+  # This is equivalent to `git rev-parse --short HEAD`
+  imageTag = builtins.substring 0 9 (self.rev or "dev");
+in
 {
   config = {
+    flake.dockerImageName = imageName + ":" + imageTag;
     perSystem = { self', pkgs, lib, ... }: {
       packages = lib.optionalAttrs pkgs.stdenv.isLinux {
         dockerImage =
           # TODO: Build a layered image, separating tools and packages
           pkgs.dockerTools.buildImage {
-            name = "beckn";
+            name = imageName;
             created = "now";
-            # self.rev will be non-null only when the working tree is clean
-            # This is equivalent to `git rev-parse --short HEAD`
-            tag = builtins.substring 0 9 (self.rev or "dev");
+            tag = imageTag;
             copyToRoot = pkgs.buildEnv {
               paths = with pkgs; [
                 cacert
