334c44ed6e4477ea5e8e4481ccd818bf30c81040
Piyush <piyush.kumar@juspay.in>
2023-03-14 19:44:55 +0530

    backend/fix/#152/migration_index_failing_fix

--- Backend/dev/migrations/dynamic-offer-driver-app/0113-add-merchantid-in-constraints.sql
+++ Backend/dev/migrations/dynamic-offer-driver-app/0113-add-merchantid-in-constraints.sql
@@ -1,7 +1,7 @@
 ALTER TABLE atlas_driver_offer_bpp.person drop CONSTRAINT person_unique_mobile_number_country_code;
 ALTER TABLE atlas_driver_offer_bpp.person drop CONSTRAINT unique_email;
 ALTER TABLE atlas_driver_offer_bpp.person drop CONSTRAINT unique_identifier;
-DROP INDEX idx_person_mobile_num;
+DROP INDEX atlas_driver_offer_bpp.idx_person_mobile_num;
 
 ALTER TABLE atlas_driver_offer_bpp.person ADD CONSTRAINT person_unique_mobile_number_country_code UNIQUE (merchant_id, mobile_country_code, mobile_number_hash);
 ALTER TABLE atlas_driver_offer_bpp.person ADD CONSTRAINT unique_email UNIQUE (merchant_id, email);
@@ -10,7 +10,7 @@ CREATE INDEX idx_person_mobile_num ON atlas_driver_offer_bpp.person USING btree 
 
 -- RIDER DETAILS
 ALTER TABLE atlas_driver_offer_bpp.rider_details drop CONSTRAINT ride_details_unique_mobile_number;
-DROP INDEX idx_rider_details_mobile_number;
+DROP INDEX atlas_driver_offer_bpp.idx_rider_details_mobile_number;
 
 ALTER TABLE atlas_driver_offer_bpp.rider_details ADD CONSTRAINT ride_details_unique_mobile_number UNIQUE (merchant_id, mobile_number_hash, mobile_country_code);
 CREATE INDEX idx_rider_details_mobile_number ON atlas_driver_offer_bpp.rider_details USING btree (merchant_id, mobile_number_hash);
