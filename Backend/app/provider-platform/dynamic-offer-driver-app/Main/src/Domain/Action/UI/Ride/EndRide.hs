7b445143060e7f0fdeb523fcf6dbb8c0a944a09f
Alexander Korovin <alexander.korovin@juspay.in>
2023-03-16 18:22:39 +0400

    Fix end ride fare calculation logic

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/Ride/EndRide.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/Ride/EndRide.hs
@@ -295,11 +295,14 @@ calculateFinalValuesForCorrectDistanceCalculations handle booking ride now = do
 
   if distanceDiff < 0
     then do
-      let recalculationDistance =
-            min
-              (roundToIntegral ride.traveledDistance + roundToIntegral (abs (distanceDiff * 0.5)))
-              (roundToIntegral ride.traveledDistance + (50 - fromIntegral (fromMaybe 0 booking.fareParams.driverSelectedFare)))
-      recalculateFareForDistance handle booking ride recalculationDistance
+      fare1@(_, money1, _) <- recalculateFareForDistance handle booking ride (roundToIntegral $ ride.traveledDistance + abs (distanceDiff * 0.5))
+      (dist2, money2', fp2) <- recalculateFareForDistance handle booking ride $ roundToIntegral ride.traveledDistance
+      let money2 = money2' + 50 - (fromMaybe 0 booking.fareParams.driverSelectedFare)
+      let fare2 = (dist2, money2, fp2)
+      return $
+        if money1 < money2
+          then fare1
+          else fare2
     else
       if distanceDiff < 1200
         then do
@@ -320,11 +323,14 @@ calculateFinalValuesForFailedDistanceCalculations handle@ServiceHandle {..} book
 
   if distanceDiff < 0
     then do
-      let recalculationDistance =
-            min
-              (approxTraveledDistance + roundToIntegral (abs (distanceDiff * 0.5)))
-              (approxTraveledDistance + (50 - fromIntegral (fromMaybe 0 booking.fareParams.driverSelectedFare)))
-      recalculateFareForDistance handle booking ride recalculationDistance
+      fare1@(_, money1, _) <- recalculateFareForDistance handle booking ride (approxTraveledDistance + roundToIntegral (abs (distanceDiff * 0.5)))
+      (dist2, money2', fp2) <- recalculateFareForDistance handle booking ride approxTraveledDistance
+      let money2 = money2' + 50 - (fromMaybe 0 booking.fareParams.driverSelectedFare)
+      let fare2 = (dist2, money2, fp2)
+      return $
+        if money1 < money2
+          then fare1
+          else fare2
     else
       if distanceDiff < 1200
         then do
