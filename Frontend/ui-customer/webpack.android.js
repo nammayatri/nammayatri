a2f8fb1b7367842983b8838f7abd5d8b4921f61c
Kavyashree <kavyashree.s@juspay.in>
2023-03-17 22:02:36 +0530

    Updated dependency

--- Frontend/ui-customer/webpack.android.js
+++ Frontend/ui-customer/webpack.android.js
@@ -3,7 +3,6 @@ const {merge} = require('webpack-merge');
 const common = require('./webpack.config.js');
 const webpack = require('webpack');
 const packageJSON = require("./package.json");
-const JusPayJSAZipPlugin = require("jsa-tools").JusPayJSAZipPlugin;
 var path = require('path');
 
 const getOutputFileDir = (mode) => mode ==  "development" ? "" :"android/";
@@ -18,15 +17,6 @@ module.exports = (env, argv) => {
         __VERSION__: JSON.stringify(packageJSON.version),
         "window.configEnv": JSON.stringify(env)
       })]
-  if (argv.mode != "development"){
-    plugins.push(new JusPayJSAZipPlugin({
-          bundleInputFile: "./dist/" + getOutputFileLocation(argv.mode),
-          jsaOutputFile: "./dist/" + getOutputFileDir(argv.mode) + "v1-index_bundle.jsa",
-          zipOutputFile: "./dist/" + getOutputFileDir(argv.mode) + "v1-index_bundle.zip",
-          keyFile: (typeof (argv.mode) == "string" && (argv.mode == "development")) ?
-            "./dev.key" : "/home/jenkins/.juspay/remote_asset_private.key"
-    }))
-  }
   return merge(common(env), {
     output: {
       filename: getOutputFileLocation(argv.mode),
