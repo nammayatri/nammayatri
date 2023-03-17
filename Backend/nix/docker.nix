628c82b8b01b1dbd5c66a7d46431b7b5e3078f6d
Sridhar Ratnakumar <srid@srid.ca>
2023-03-17 08:30:50 -0400

    Run nixpkgs-fmt

--- Backend/nix/docker.nix
+++ Backend/nix/docker.nix
@@ -10,50 +10,53 @@ in
   config = {
     flake.dockerImageName = imageName + ":" + imageTag;
     flake.dockerImageTag = imageTag;
-    perSystem = { self', pkgs, lib, ... }: {
-      packages = lib.optionalAttrs pkgs.stdenv.isLinux {
-        dockerImage =
-          # TODO: Build a layered image, separating tools and packages
-          pkgs.dockerTools.buildImage {
-            name = imageName;
-            created = "now";
-            tag = imageTag;
-            copyToRoot = pkgs.buildEnv {
-              paths = with pkgs; [
-                cacert
-                awscli
-                coreutils
-                bash
-                # Add project root to paths to copy dhall-configs and swagger dirs
-                self
-              ] ++ (with self'.packages;
-                let inherit (pkgs.haskell.lib) justStaticExecutables;
-                # TODO: Refactor
-                in builtins.map justStaticExecutables [
-                  rider-app
-                  dynamic-offer-driver-app
-                  static-offer-driver-app
-                  static-offer-driver-app-allocator
-                  static-offer-driver-app-scheduler
-                  driver-offer-allocator
-                  rider-dashboard
-                  provider-dashboard
-                  driver-tracking-healthcheck
-                ]);
-              name = "beckn-root";
-              pathsToLink = [ "/dhall-configs" "/swagger" "/bin" ];
-              postBuild = ''
-                # Legacy k8s deployment expects the app to be in /opt/app
-                mkdir $out/opt
-                cp -r $out/bin $out/opt/app
-              '';
+    perSystem = { self', pkgs, lib, ... }:
+      let
+        exes = builtins.map pkgs.haskell.lib.justStaticExecutables (with self'.packages; [
+          rider-app
+          dynamic-offer-driver-app
+          static-offer-driver-app
+          static-offer-driver-app-allocator
+          static-offer-driver-app-scheduler
+          driver-offer-allocator
+          rider-dashboard
+          provider-dashboard
+          driver-tracking-healthcheck
+        ]);
+      in
+      {
+        packages = lib.optionalAttrs pkgs.stdenv.isLinux {
+          dockerImage =
+            # TODO: Build a layered image, separating tools and packages
+            pkgs.dockerTools.buildImage {
+              name = imageName;
+              created = "now";
+              tag = imageTag;
+              copyToRoot = pkgs.buildEnv {
+                paths = with pkgs; [
+                  cacert
+                  awscli
+                  coreutils
+                  bash
+                  # Add project root to paths to copy dhall-configs and swagger dirs
+                  self
+                ] ++ exes;
+                name = "beckn-root";
+                pathsToLink = [ "/Backend/dhall-configs" "/Backend/swagger" "/bin" ];
+                postBuild = ''
+                  # Legacy k8s deployment expects the app to be in /opt/app
+                  mkdir $out/opt
+                  cp -r $out/bin $out/opt/app
+                '';
+              };
+              config = {
+                Env = [
+                  "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+                ];
+                Cmd = [ "${pkgs.lib.getExe self'.packages.default}" ];
+              };
             };
-            config = {
-              Env = [ "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" ];
-              Cmd = [ "${pkgs.lib.getExe self'.packages.default}" ];
-            };
-          };
+        };
       };
-    };
   };
 }
