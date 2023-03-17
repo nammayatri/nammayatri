20f4bf8bcd8c584929050e01b4bfdb4f2e249aae
Pavel Zarubin <pavel.zarubin.ext@juspay.in>
2023-03-17 07:58:55 +0300

    fixed bug in registration and verifying alternate number, added requests for manual testing in rest-api

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Storage/Queries/Person.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Storage/Queries/Person.hs
@@ -262,7 +262,9 @@ findByMobileNumberAndMerchant countryCode mobileNumberHash merchantId = do
     person <- from $ table @PersonT
     where_ $
       person ^. PersonMobileCountryCode ==. val (Just countryCode)
-        &&. person ^. PersonMobileNumberHash ==. val (Just mobileNumberHash)
+        &&. ( person ^. PersonMobileNumberHash ==. val (Just mobileNumberHash)
+                ||. person ^. PersonAlternateMobileNumberHash ==. val (Just mobileNumberHash)
+            )
         &&. person ^. PersonMerchantId ==. val (toKey merchantId)
     return person
 
