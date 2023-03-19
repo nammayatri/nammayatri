2062ed3215db64dede3bd1c3a3eeb52b177cc506
Shailesh Gahlawat <shailesh.gahlawat@juspay.in>
2023-03-19 16:28:35 +0530

    adding github url for enabling semantic release

--- Frontend/android-native/bundling.sh
+++ Frontend/android-native/bundling.sh
@@ -7,30 +7,26 @@ cd ./../android-native
 
 echo " ---------- Copy index_bundle.js Customer :- --------------"
 
-mkdir -p "app/src/user/js/juspay"
-rm -rf app/src/user/js/juspay/index_bundle.js
-cp ./../ui-customer/dist/android/index_bundle.js app/src/user/js/juspay
+mkdir -p "app/src/user/assets/juspay"
+rm -rf app/src/user/assets/juspay/index_bundle.js
+cp ./../ui-customer/dist/android/index_bundle.js app/src/user/assets/juspay
 rm -rf app/src/user/assets/juspay/juspay_assets.json
+cd ..
 sh userJuspayAssets.sh
 
 echo " ---------- Driver prod:android :- --------------"
 
-cd ./../ui-driver
+cd ui-driver
 bower i
 npm run prod:android
 cd ./../android-native
 
 echo " ---------- Copy index_bundle.js Driver :- --------------"
 
-mkdir -p "app/src/driver/js/juspay"
-rm -rf app/src/driver/js/juspay/index_bundle.js
-cp ./../ui-driver/dist/android/index_bundle.js app/src/driver/js/juspay
+mkdir -p "app/src/driver/assets/juspay"
+rm -rf app/src/driver/assets/juspay/index_bundle.js
+cp ./../ui-driver/dist/android/index_bundle.js app/src/driver/assets/juspay
 rm -rf app/src/driver/assets/juspay/juspay_assets.json
+cd ..
 sh driverJuspayAssets.sh
 
-echo " ---------- CreateJSAFiles :- --------------"
-
-rm -rf app/src/driver/assets/juspay/v1-index_bundle.jsa
-rm -rf app/src/user/assets/juspay/v1-index_bundle.jsa
-rm -rf app/src/main/assets/juspay/v1-config.jsa
-./gradlew createJSAFiles
