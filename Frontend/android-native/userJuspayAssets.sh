7df5c1c05dbb5719e40ef0c3c92c17026842f4b3
dakshana.j@juspay.in <dakshanajayakumar04@gmail.com>
2023-03-21 15:50:20 +0530

    frontend/fix: #214 font issue fix script

--- Frontend/android-native/userJuspayAssets.sh
+++ Frontend/android-native/userJuspayAssets.sh
@@ -5,11 +5,15 @@ find app/src/main/res/drawable | grep ".png" | cut -d "/" -f 6 | sed 's/.png//' 
 find app/src/user/res/drawable | grep ".png" | cut -d "/" -f 6 | sed 's/.png//' | awk '{print "\"" $1 "\" : true," }' >> assests.json
 find app/src/user/res/drawable | grep ".xml" | cut -d "/" -f 6 | sed 's/.xml//' | awk '{print "\"" $1 "\" : true," }' >> assests.json
 find app/src/main/res/drawable | grep ".xml" | cut -d "/" -f 6 | sed 's/.xml//' | awk '{print "\"" $1 "\" : true," }' >> assests.json
-
 sed '$ s/.$//' assests.json > juspay_assets.json
-echo "}}" >> juspay_assets.json
-cat juspay_assets.json | json_pp | tee ./app/src/user/assets/juspay/juspay_assets.json
 
-rm juspay_assets.json assests.json
+echo "},\"fonts\":{" >> juspay_assets.json
+find app/src/main/assets/fonts | grep ".ttf" | cut -d "/" -f 6 | sed 's/.ttf//' | awk '{print "\"" $1 "\" : true," }' >> juspay_assets.json
+sed '$ s/.$//' juspay_assets.json > juspay_assets_fonts.json
+echo "}}" >> juspay_assets_fonts.json
+
+cat juspay_assets_fonts.json | json_pp | tee ./app/src/user/assets/juspay/juspay_assets.json
+
+rm juspay_assets_fonts.json juspay_assets.json assests.json
 
 echo " ---------- End assests Customer :- --------------"
