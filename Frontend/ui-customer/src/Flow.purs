4cef0699af4a240010cf86aacb82167f54690fdc
ManavGuptaJuspay <manav.gupta@juspay.in>
2023-03-24 20:39:18 +0530

    dummy changes

--- Frontend/ui-customer/src/Flow.purs
+++ Frontend/ui-customer/src/Flow.purs
@@ -10,7 +10,7 @@
  
   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of
  
-  the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+  the   GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 -}
 
 module Flow where
