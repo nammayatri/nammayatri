ba8b09399ae717adbadfe27d37675de4dfd2436b
Utkarsh <utkarsh.pandey1@juspay.in>
2023-03-28 19:21:15 +0530

    Backend/feat/special_zone_server

---
+++ Backend/dev/sql-seed/special-zone-seed.sql
@@ -0,0 +1,13 @@
+CREATE USER atlas_special_zone_user WITH PASSWORD 'atlas';
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+CREATE SCHEMA atlas_special_zone;
+ALTER SCHEMA atlas_special_zone OWNER TO atlas_special_zone_user;
