2538132f0935134e7719de3ab59b194b055d71cc
Arjun Kathuria <arjun.kathuria8@gmail.com>
2023-03-30 19:39:48 +0530

    Make "incomplete-uni-patterns" a warning for source files
    * "incomplete-uni-patterns" was made a warning instead of
      a build error for source files.
    
    * This was done with a OPTIONS_GHC pragma on a per-file bases,
      to minimize scope of this and to make similar future errors,
      errors, instead of a warning, since they come after
      this point in time.

--- Backend/test/src/Mobility/ARDU/Utils.hs
+++ Backend/test/src/Mobility/ARDU/Utils.hs
@@ -12,6 +12,8 @@
  the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 -}
 
+{-# OPTIONS_GHC -Wwarn=incomplete-uni-patterns #-}
+
 module Mobility.ARDU.Utils (module Mobility.ARDU.Utils) where
 
 import qualified "dynamic-offer-driver-app" API.UI.Driver as TDriver
