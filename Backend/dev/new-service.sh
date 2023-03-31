bbea4f3bbb544eb938c2641d1ad6d4813943b431
Sridhar Ratnakumar <srid@srid.ca>
2023-03-31 14:43:56 -0400

    Remove now unused files

--- Backend/dev/new-service.sh
+++
@@ -1,6 +0,0 @@
-echo 'Type in name of a new service in kebab case:'
-read name
-cp -r ./app/example-service ./app/${name}
-echo ${name} | sed -i "s/example-service/${name}/g" ./app/${name}/package.yaml
-rm ./app/${name}/example-service.cabal
-tree ./app/${name}
