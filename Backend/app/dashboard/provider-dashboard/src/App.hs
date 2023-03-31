45de29393254f6ac471bd90b4e6d9f8d1b75d7c5
Arjun Kathuria <arjun.kathuria8@gmail.com>
2023-03-31 15:01:43 +0530

    Fix breaking type-change of "_httpClientManagers" from Euler.Runtime
    * in now-old version of Euler-hs the type representation of
      _httpClientManagers       :: Map String Manager
    
      but, now in the Newer version of Euler, it is
      _httpClientManagers       :: HashMap Text Manager
    
      This breaks a bunch of stuff in the code
    
    * The changes made here fix the above type representation change issue
    
    * Adds "unordered-containers" as dependencies for packages where it was
      missing in their package.yaml files
    
    * Adds updated cabal files with the above package.yaml change reflected
    
    * Deletes some now un-needed imports

--- Backend/app/dashboard/provider-dashboard/src/App.hs
+++ Backend/app/dashboard/provider-dashboard/src/App.hs
@@ -18,7 +18,7 @@ module App
 where
 
 import API
-import qualified Data.HashMap.Internal as HMap
+import qualified Data.HashMap.Strict as HMS
 import "lib-dashboard" Environment
 import qualified EulerHS.Runtime as R
 import Kernel.Exit
@@ -39,7 +39,7 @@ runService configModifier = do
   runServerWithHealthCheck appEnv (Proxy @API) handler identity identity context releaseAppEnv \flowRt -> do
     migrateIfNeeded appCfg.migrationPath appCfg.autoMigrate appCfg.esqDBCfg
       >>= handleLeft exitDBMigrationFailure "Couldn't migrate database: "
-    let flowRt' = flowRt {R._httpClientManagers = HMap.singleton "default" (R._defaultHttpClientManager flowRt)}
+    let flowRt' = flowRt {R._httpClientManagers = HMS.singleton "default" (R._defaultHttpClientManager flowRt)}
     pure flowRt'
   where
     context =
