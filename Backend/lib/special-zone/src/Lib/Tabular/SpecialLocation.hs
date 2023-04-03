63e5f7e528d68de3fb4347ca74c4b232d2a5fa20
Arjun Kathuria <arjun.kathuria8@gmail.com>
2023-04-03 20:14:25 +0530

    Fix a TemplateHaskell "derivePersistField" function error
    * "derivePersistField" needs a valid "Read" instance for the datatype
      its working on
    
    * The "Read" instance for "LatLong" was placed below the template-haskell
      function
    
    * Since TH is source position dependent and where the code appears lexically
      in a file, the function fails to compile and asks for a valid "Read" instance
      for "LatLong", since it cannot yet see the instance that is declared later
      , lower in the source file.
    
    * The solution was to move the "Read" instance declaration up
      in the source file. The instance is now visible TH function.

--- Backend/lib/special-zone/src/Lib/Tabular/SpecialLocation.hs
+++ Backend/lib/special-zone/src/Lib/Tabular/SpecialLocation.hs
@@ -25,10 +25,11 @@ import Kernel.Storage.Esqueleto
 import Kernel.Types.Id
 import qualified Lib.Types.SpecialLocation as Domain
 
+deriving instance Read LatLong
+deriving instance Read Domain.GatesInfo
+
 derivePersistField "LatLong"
 derivePersistField "Domain.GatesInfo"
-
-deriving instance Read Domain.GatesInfo
 
 mkPersist
   defaultSqlSettings
