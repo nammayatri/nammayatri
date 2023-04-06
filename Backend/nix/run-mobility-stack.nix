8496defe0a49bd9eabdd81bdfddf67a362f30d87
Sridhar Ratnakumar <srid@srid.ca>
2023-04-06 15:54:25 -0400

    run-mobility-stack: Simplify further using https://github.com/srid/haskell-flake/pull/137

--- Backend/nix/run-mobility-stack.nix
+++ Backend/nix/run-mobility-stack.nix
@@ -23,41 +23,34 @@
             mock-registry = {
               nixExe = lib.getExe config.haskellProjects.default.outputs.finalPackages.mock-registry;
             };
-
-            # TODO: Refactor this after https://github.com/srid/haskell-flake/pull/137
-            scheduler-example-app-exe = {
-              nixExe = config.haskellProjects.default.outputs.localApps.scheduler-example-app-exe.program;
-              cabalExe = "scheduler-example:exe:scheduler-example-app-exe";
-            };
-            scheduler-example-scheduler-exe = {
-              nixExe = config.haskellProjects.default.outputs.localApps.scheduler-example-scheduler-exe.program;
-              cabalExe = "scheduler-example:exe:scheduler-example-scheduler-exe";
-            };
-
-          } // lib.listToAttrs (builtins.map
-            (p: lib.nameValuePair p.pname {
-              nixExe = lib.getExe p;
-              cabalExe = p.pname;
-            })
-            (with config.localPackagesStatic; [
-              driver-offer-allocator
-              driver-tracking-healthcheck
-              dynamic-offer-driver-app
-              image-api-helper
-              kafka-consumers
-              mock-fcm
-              mock-idfy
-              mock-sms
-              provider-dashboard
-              public-transport-rider-platform
-              public-transport-search-consumer
-              rider-app
-              rider-dashboard
-              search-result-aggregator
-              static-offer-driver-app
-              static-offer-driver-app-allocator
-              static-offer-driver-app-scheduler
-            ]));
+          } // lib.listToAttrs
+            (lib.concatMap
+              (p: lib.attrValues (lib.mapAttrs
+                (name: exe: lib.nameValuePair name {
+                  nixExe = exe.program;
+                  cabalExe = "${p.package.pname}:exe:${name}";
+                })
+                p.exes))
+              (with config.haskellProjects.default.outputs.packages; [
+                driver-offer-allocator
+                driver-tracking-healthcheck
+                dynamic-offer-driver-app
+                image-api-helper
+                kafka-consumers
+                mock-fcm
+                mock-idfy
+                mock-sms
+                provider-dashboard
+                public-transport-rider-platform
+                public-transport-search-consumer
+                rider-app
+                rider-dashboard
+                scheduler-example
+                search-result-aggregator
+                static-offer-driver-app
+                static-offer-driver-app-allocator
+                static-offer-driver-app-scheduler
+              ]));
 
           getNixExe = _: v: { command = "set -x; ${v.nixExe}"; };
           # Like getNixExe, but use `cabal run` for fast iteration. But fall
