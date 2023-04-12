81a52b62c7b4d50a2cdec833b107b8ecce479450
jaiprasath.m <jaiprasath.m@juspay.in>
2023-04-12 17:48:16 +0530

    Frontend/refactor: #269 Created TestIdType and keys file

---
+++ Frontend/ui-common/src/Constant/Keys.purs
@@ -0,0 +1,399 @@
+{-
+ 
+  Copyright 2022-23, Juspay India Pvt Ltd
+ 
+  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License
+ 
+  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program
+ 
+  is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ 
+  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of
+ 
+  the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+-}
+
+module Constant.Keys where
+import Prelude
+
+-------------------OtherNames----------------------
+
+underScore :: String
+underScore = "_"
+
+backIcon :: String
+backIcon = "backIcon"
+
+closeIcon :: String
+closeIcon = "closeIcon"
+
+infoIcon :: String
+infoIcon = "infoIcon"
+
+favIcon :: String
+favIcon = "favIcon"
+
+link :: String
+link = "link"
+
+logout :: String
+logout = "logout"
+
+call :: String
+call = "call"
+
+cancel :: String
+cancel = "cancel"
+
+toggle :: String
+toggle = "toggle"
+
+copy :: String
+copy = "copy"
+
+upload :: String
+upload = "upload"
+
+clear :: String
+clear = "clear"
+
+recenter :: String
+recenter = "recenter"
+
+backSpace :: String
+backSpace = "backSpace"
+
+done :: String
+done = "done"
+
+decrease :: String
+decrease = "decrease"
+
+increase :: String
+increase = "increase"
+
+decline :: String
+decline = "decline"
+
+request :: String
+request = "request"
+
+cancelRide :: String
+cancelRide = "cancelride"
+
+preview :: String
+preview = "preview"
+
+forwardIcon :: String
+forwardIcon = "forwardIcon"
+
+front :: String
+front = "front"
+
+back :: String
+back = "back"
+
+click :: String
+click = "click"
+
+tab :: String
+tab = "tab"
+
+date :: String
+date = "date"
+
+reason :: String
+reason = "reason"
+
+close :: String
+close = "close"
+
+image :: String
+image = "image"
+
+errorModel :: String
+errorModel = "errorModel"
+
+genericHeader :: String
+genericHeader = "genericHeader"
+
+genericMessage :: String
+genericMessage = "genericMessage"
+
+menuButton :: String
+menuButton = "menuButton"
+
+popUpModal :: String
+popUpModal = "popUpModal"
+
+primaryedittext :: String
+primaryedittext = "primaryedittext"
+
+updateErrorModel :: String
+updateErrorModel = "updateErrorModel"
+
+driverInfoCard :: String
+driverInfoCard = "driverInfoCard"
+
+sos :: String
+sos = "Sos"
+
+supportview :: String
+supportview = "supportview"
+
+individualRideCard :: String
+individualRideCard = "individualRideCard"
+
+locationListItem :: String
+locationListItem = "locationListItem"
+
+pricingtutorial :: String
+pricingtutorial = "pricingtutorial"
+
+quoteListItem :: String
+quoteListItem = "quoteListItem"
+
+quoteListModel :: String
+quoteListModel = "quoteListModel"
+
+savedLocationCard :: String
+savedLocationCard = "savedLocationCard"
+
+rateCard :: String
+rateCard = "rateCard"
+
+editLocation :: String
+editLocation = "editLocation"
+
+trackLocation :: String
+trackLocation = "trackLocation"
+
+searchLocModel :: String
+searchLocModel = "searchLocModel"
+
+settingSideBar :: String
+settingSideBar = "settingSideBar"
+
+profile :: String
+profile = "profile"
+
+hamburger :: String
+hamburger = "hamburger"
+
+rideStarRating :: String
+rideStarRating = "rideStarRating"
+
+changeAddress :: String
+changeAddress = "changeAddress"
+
+calendar :: String
+calendar = "calendar"
+
+update :: String
+update = "update"
+
+dropDown :: String
+dropDown = "dropDown"
+
+source :: String
+source = "source"
+
+destination :: String
+destination = "destination"
+
+fareBreakup :: String
+fareBreakup = "fareBreakup"
+
+bottomNavBar :: String
+bottomNavBar = "bottomNavBar"
+
+inAppOtpModal :: String
+inAppOtpModal = "inAppOtpModal"
+
+onboardingHeader :: String
+onboardingHeader = "onboardingHeader"
+
+primarySelectItem :: String
+primarySelectItem = "primarySelectItem"
+
+referralMobileNumber :: String
+referralMobileNumber = "referralMobileNumber"
+
+registrationModal :: String
+registrationModal = "registrationModal"
+
+rideActionModal :: String
+rideActionModal = "rideActionModal"
+
+rideAllocationModal :: String
+rideAllocationModal = "rideAllocationModal"
+
+selectVehicleTypeModal :: String
+selectVehicleTypeModal = "selectVehicleTypeModal"
+
+tutorialModal :: String
+tutorialModal = "tutorialModal"
+
+showAllOptions :: String
+showAllOptions = "showAllOptions"
+
+locationTagBar :: String
+locationTagBar = "locationTagBar"
+
+feedback :: String
+feedback = "feedback"
+
+requestInfoCard :: String
+requestInfoCard = "requestInfoCard"
+
+saveFavouriteCard :: String
+saveFavouriteCard = "saveFavouriteCard"
+
+notificationDetailModel :: String
+notificationDetailModel = "notificationDetailModel"
+
+tag :: String
+tag = "tag"
+
+continue :: String
+continue = "continue"
+
+next :: String
+next = "next"
+
+contactList :: String
+contactList = "contactList"
+
+emergencyHelp :: String
+emergencyHelp = "emergencyHelp"
+
+driverStatus :: String
+driverStatus = "driverStatus"
+
+noAction :: String
+noAction = "noAction"
+
+enabled :: String
+enabled = "enabled"
+
+disabled :: String
+disabled = "disabled"
+
+-------------------ScreenNames----------------------
+
+------- Driver Screen names -----
+
+addVehicleDetailsScreen :: String
+addVehicleDetailsScreen = "addVehicleDetails"
+
+applicationStatusScreen :: String
+applicationStatusScreen = "applicationStatus"
+
+bankDetailsScreen :: String
+bankDetailsScreen = "bankDetails"
+
+driverDetailsScreen :: String
+driverDetailsScreen = "driverDetails"
+
+driverProfileScreen :: String
+driverProfileScreen = "driverProfile"
+
+driverRideRatingScreen :: String
+driverRideRatingScreen = "driverRideRating"
+
+editAadhaarDetailsScreen :: String
+editAadhaarDetailsScreen = "editAadhaarDetails"
+
+editBankDetailsScreen :: String
+editBankDetailsScreen = "editBankDetails"
+
+notificationsScreen :: String
+notificationsScreen = "notifications"
+
+permissionsScreen :: String
+permissionsScreen = "permissions"
+
+registrationScreen :: String
+registrationScreen = "registration"
+
+rideDetailScreen :: String
+rideDetailScreen = "rideDetail"
+
+rideHistoryScreen :: String
+rideHistoryScreen = "rideHistory"
+
+uploadAdhaarScreen :: String
+uploadAdhaarScreen = "uploadAdhaar"
+
+uploadDrivingLicenseScreen :: String
+uploadDrivingLicenseScreen = "uploadDrivingLicense"
+
+vehicleDetailsScreen :: String
+vehicleDetailsScreen = "vehicleDetails"
+
+writeToUsScreen :: String
+writeToUsScreen = "writeToUs"
+
+----- customer screen names -----
+
+accountSetUpScreen :: String
+accountSetUpScreen = "accountSetUp"
+
+addNewAddressScreen :: String
+addNewAddressScreen = "addNewAddress"
+
+contactUsScreen :: String
+contactUsScreen = "contactUs"
+
+emergencyContactsScreen :: String
+emergencyContactsScreen = "emergencyContacts"
+
+invoiceScreen :: String
+invoiceScreen = "invoice"
+
+myProfileScreen :: String
+myProfileScreen = "myProfile"
+
+myRidesScreen :: String
+myRidesScreen = "myRides"
+
+permissionScreen :: String
+permissionScreen = "permission"
+
+savedLocationScreen :: String
+savedLocationScreen = "savedLocation"
+
+successScreen :: String
+successScreen = "successScreen"
+
+----- common screen names -----
+
+aboutUsScreen :: String
+aboutUsScreen = "aboutUs"
+
+appUpdatePopUpScreen :: String
+appUpdatePopUpScreen = "appUpdatePopUp"
+
+chooseLanguageScreen :: String
+chooseLanguageScreen = "chooselanguage"
+
+enterMobileNumberScreen :: String
+enterMobileNumberScreen = "enterMobileNumber"
+
+enterOTPScreen :: String
+enterOTPScreen = "enterOTP"
+
+helpAndSupportScreen :: String
+helpAndSupportScreen = "helpAndSupport"
+
+homeScreen :: String
+homeScreen = "home"
+
+referralScreen :: String
+referralScreen = "referral"
+
+selectLanguageScreen :: String
+selectLanguageScreen = "selectLanguage"
+
+tripDetailsScreen :: String
+tripDetailsScreen = "tripDetails"
---
+++ Frontend/ui-common/src/Constant/TestIdType.purs
@@ -0,0 +1,78 @@
+{-
+ 
+  Copyright 2022-23, Juspay India Pvt Ltd
+ 
+  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License
+ 
+  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program
+ 
+  is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ 
+  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of
+ 
+  the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+-}
+
+module Constant.TestIdType where
+
+import Prelude
+
+import Data.Generic.Rep (class Generic)
+import Foreign.Class (class Decode)
+import Halogen.VDom.DOM.Prop (Prop(..))
+import Presto.Core.Utils.Encoding (defaultDecode)
+import PrestoDOM as PD
+
+data TestID
+    = Button Check
+    | Screen String
+    | Container String
+    | ToolBar String
+    | Component String
+    | List String
+    | Element String
+    | Select String
+    | DropDown String
+    | Bar String
+    | Object String
+    | Text String
+    | Option String
+    | TextField String
+    | Status String
+    | Toggle String
+    | RadioButton String
+    | Hamburger String
+
+data Check
+  = BtnConfig String
+
+instance showTestID :: Show TestID where
+    show (Screen a) = "screen_" <> a
+    show (Component a) = "component_" <> a
+    show (Button a) = "button_" <> show a
+    show (Container a) = "container_" <> a
+    show (ToolBar a) = "toolbar_" <> a
+    show (Click a) = "click_" <> a
+    show (List a) = "list_" <> a
+    show (Element a) = "element_" <> a
+    show (Select a) = "select_" <> a
+    show (DropDown a) = "dropdown_" <> a
+    show (Bar a) = "bar_" <> a
+    show (Object a) = "object_" <> a
+    show (Text a) = "text_" <> a
+    show (Option a) = "option_" <> a
+    show (TextField a) = "textfield_" <> a
+    show (Status a) = "status_" <> a
+    show (Toggle a) = "toggle_" <> a
+    show (RadioButton a) = "radiobutton_" <> a
+    show (Hamburger a) = "hamburger_" <> a
+
+
+
+derive instance genericCheck :: Generic Check _
+instance decodeCheck :: Decode Check where decode = defaultDecode
+instance showCheck :: Show Check where
+  show (BtnConfig a) = a
+
+testId :: forall a. TestID -> Prop a
+testId = PD.testID <<< show
