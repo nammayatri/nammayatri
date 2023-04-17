d31310e8aeaab852e7c4fa0ea1db558c29b20eeb
Ben Levy <ben.levy@juspay.in>
2023-04-17 17:39:13 +0000

    Change packages.dhall to refer to forked libs on github instead local repos

--- Frontend/default.nix
+++ Frontend/default.nix
@@ -3,14 +3,18 @@
   imports = [
   ];
   perSystem = { config, self', pkgs, lib, ... }:
-      let easy-ps = import inputs.easy-purescript-nix { inherit pkgs; };
-      in {
-        devShells.frontend = pkgs.mkShell {
-          name = "ps-dev-shell";
-          packages = [
-              easy-ps.purs-0_15_4
-              easy-ps.spago
-          ];
-        };
-  };
+    let easy-ps = import inputs.easy-purescript-nix { inherit pkgs; };
+    in {
+      devShells.frontend = pkgs.mkShell {
+        name = "ps-dev-shell";
+        packages = [
+          easy-ps.purs-0_15_4
+          easy-ps.spago
+          easy-ps.psa
+          pkgs.dhall
+          pkgs.nodejs-14_x
+        ];
+
+      };
+    };
 }
