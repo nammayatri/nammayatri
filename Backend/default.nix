0ece71f63152a25d21665835cd420110d2c39b78
Sridhar Ratnakumar <srid@srid.ca>
2023-04-17 12:19:00 -0400

    docker: Include /swagger and /dhall-configs in right location

--- Backend/default.nix
+++ Backend/default.nix
@@ -33,16 +33,32 @@
         };
     };
 
-    # The final nammayatri package containing the various executables.
-    packages.nammayatri =
-      let
-        localCabalPackages = builtins.map
-          (p: pkgs.haskell.lib.justStaticExecutables p.package)
-          (lib.attrValues config.haskellProjects.default.outputs.packages);
-      in
-      pkgs.symlinkJoin {
-        name = "nammayatri-exes";
-        paths = localCabalPackages;
+    packages = {
+      # The final nammayatri package containing the various executables.
+      nammayatri =
+        let
+          localCabalPackages = builtins.map
+            (p: pkgs.haskell.lib.justStaticExecutables p.package)
+            (lib.attrValues config.haskellProjects.default.outputs.packages);
+        in
+        pkgs.symlinkJoin {
+          name = "nammayatri-exes";
+          paths = localCabalPackages;
+        };
+      # The nammayatri dist to be deployed in Docker image
+      nammayatri-dist = pkgs.symlinkJoin {
+        name = "nammayatri-dist";
+        paths = [ self'.packages.nammayatri ];
+        postBuild = ''
+          mkdir $out/opt
+          # Rationale: Our k8s deployment config is hardcoded to look for exes
+          # under /opt/app.
+          mv $out/bin $out/opt/app
+          cp -r ${./dhall-configs} $out/opt/app/dhall-configs
+          cp -r ${./swagger} $out/opt/app/swagger
+        '';
       };
+    };
+
   };
 }
