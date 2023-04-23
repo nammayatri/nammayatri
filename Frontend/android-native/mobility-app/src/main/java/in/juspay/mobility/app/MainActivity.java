7c65c31419c1f251bc04a5d3ed04df444a78abec
khuzema786 <khomosikhuzema894@gmail.com>
2023-04-24 02:42:08 +0530

    frontend/feature: Signature auth SDK

--- Frontend/android-native/mobility-app/src/main/java/in/juspay/mobility/app/MainActivity.java
+++ Frontend/android-native/mobility-app/src/main/java/in/juspay/mobility/app/MainActivity.java
@@ -380,6 +380,11 @@ public class MainActivity extends AppCompatActivity {
             payload.put("service", key);
             payload.put(PaymentConstants.ENV, "prod");
 
+            // JSONObject signatureAuthData = new JSONObject();
+            // signatureAuthData.put("signature", "Tz8ew9MYewcXBKIkjT7U+Tu3bPN06RZHBIKKbaJjMQ+e5uTaI4Hz0Ktu2KAXITR+7xBhBaLkMZ4Fb6HyaEOUjZES/qid/cVghyi1rJn3A0mI4VmMGt50IOep0b+5Ae2N1yCz58SwWvIRunv345amE0URHD6uca71rk2Rijva5XGjwgNrqOWXpzrHT0y0FRrvEr4u3du8QS2q0Wu4fZ2Ps9RSh04iPVNNuuMcUgkGSktxFP5vJLVYllYJDUzrOxi7nq3R11utNlSQMu18+ATSO5HyMTLxpndjhFlUJqn4QGxYovpp7amztJYjvoEnG9itPp2WdYamVeRAEcJnqRon2w==");
+            // signatureAuthData.put("authData", "{\"mobileNumber\":\"9642429378\",\"mobileCountryCode\":\"+91\",\"merchantId\":\"NAMMA_YATRI\",\"timestamp\":\"2023-04-13T07:28:40+00:00\"}");
+            // payload.put("signatureAuthData", signatureAuthData);
+
             json.put(PaymentConstants.PAYLOAD, payload);
         } catch (JSONException e) {
             e.printStackTrace();
