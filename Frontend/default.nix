70d27e1cc6121cf1fe0448e585e1d1465825bf79
Ben Levy <ben.levy@juspay.in>
2023-04-26 20:30:30 +0000

    Add purifix watch and webpack watch for ui-customer; TODO: Use a single watch for both

--- Frontend/default.nix
+++ Frontend/default.nix
@@ -11,6 +11,72 @@
       localPackages = pkgsWithPurifix.purifix {
         src = ./.;
       };
+      nodeDeps = (import ./nix/node-composition.nix {
+        inherit pkgs;
+        nodejs = pkgs.nodejs-14_x;
+      }).nodeDependencies;
+      webpack = pkgs.writeShellApplication {
+        name = "webpack-wrapped";
+        runtimeInputs = [ nodeDeps ];
+        text = ''
+          ln -sf ${nodeDeps}/lib/node_modules node_modules
+          trap 'unlink node_modules' EXIT
+          webpack "$@"
+        '';
+      };
+      webpack-dev-server = pkgs.writeShellApplication {
+        name = "webpack-dev-server-wrapped";
+        runtimeInputs = [ nodeDeps ];
+        text = ''
+          ln -sf ${nodeDeps}/lib/node_modules node_modules
+          trap 'unlink node_modules' EXIT
+          webpack-dev-server "$@"
+        '';
+      };
+      run-all = pkgs.writeShellApplication {
+        name = "run-all";
+        text = ''
+          trap 'kill 0' EXIT
+          for program in "''${@}"; do
+            sh -c "$program" &
+          done
+          wait >/dev/null
+        '';
+      };
+      purifix-watch =
+        pkgs.writeShellApplication {
+          name = "purifix-watch";
+          runtimeInputs = [
+            pkgs.watchexec
+            webpack
+            pkgs.nodejs-14_x
+          ] ++ localPackages.beckn-common.develop.buildInputs;
+          text = ''
+            SRC_FOLDERS=()
+            SRC_GLOBS=()
+            for SRC_FOLDER in "$@"; do
+              if [ -d "$SRC_FOLDER" ]; then
+                SRC_FOLDERS+=(-w "$SRC_FOLDER")
+                SRC_GLOBS+=("$SRC_FOLDER/src/**/*.purs")
+              fi
+            done
+            watchexec "''${SRC_FOLDERS[@]}" -rn -f "**/*.purs" -- purifix "''${SRC_GLOBS[@]}"
+          '';
+        };
+      watch-customer = pkgs.writeShellApplication {
+        name = "watch-customer";
+        runtimeInputs = [ run-all purifix-watch webpack-dev-server ];
+        text = ''
+          if [ -d ./ui-customer ]; then
+            cd ./ui-customer
+          fi
+          if [ "$(basename "$PWD")" != "ui-customer" ]; then
+            echo "Not in ui-customer directory...exiting"
+            exit 1
+          fi
+          run-all "purifix-watch . ../ui-common" "webpack-dev-server-wrapped --watch-files-reset --progress --config webpack.android.js --watch-files ./output/Main/index.js ./index.js"
+        '';
+      };
     in
     {
       treefmt.config = {
@@ -18,6 +84,24 @@
         settings.formatter.dhall.excludes = [
           "Frontend/packages.dhall"
         ];
+      };
+      apps = {
+        webpack = {
+          type = "app";
+          program = "${webpack}/bin/webpack-wrapped";
+        };
+        webpack-dev-server = {
+          type = "app";
+          program = "${webpack-dev-server}/bin/webpack-dev-server-wrapped";
+        };
+        purifix-watch = {
+          type = "app";
+          program = purifix-watch;
+        };
+        watch-customer = {
+          type = "app";
+          program = watch-customer;
+        };
       };
       devShells.frontend = pkgs.mkShell {
         name = "ps-dev-shell";
@@ -29,11 +113,11 @@
         packages = [
           pkgs.dhall
           pkgs.nodejs-14_x
+          purifix-watch
         ];
       };
       packages = {
-        ui-customer = localPackages.ui-customer;
-        ui-driver = localPackages.ui-driver;
+        inherit (localPackages) ui-customer ui-driver;
         ui-common = localPackages.beckn-common;
       };
     };
