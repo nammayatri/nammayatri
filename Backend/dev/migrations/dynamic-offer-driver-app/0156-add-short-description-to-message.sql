992e3672773d97daede89688616fa055c6b93357
ashish saini <ashish.saini@ashish.saini-MacBookPro>
2023-05-08 17:57:39 +0530

    backend/bugfix #695:added default for short description

--- Backend/dev/migrations/dynamic-offer-driver-app/0156-add-short-description-to-message.sql
+++ Backend/dev/migrations/dynamic-offer-driver-app/0156-add-short-description-to-message.sql
@@ -1,5 +1,5 @@
 ALTER TABLE atlas_driver_offer_bpp.message
-ADD COLUMN short_description Text;
+ADD COLUMN short_description Text DEFAULT '';
 
 ALTER TABLE atlas_driver_offer_bpp.message_translation
-ADD COLUMN short_description Text;
+ADD COLUMN short_description Text DEFAULT '';
