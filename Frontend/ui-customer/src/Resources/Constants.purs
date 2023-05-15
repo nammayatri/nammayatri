46758cdc5a1cd0debe4b2849d79dde5d2b7d96e9
Chakradhar <chakradhar.k@juspay.in>
2023-05-15 23:13:29 +0530

    frontend/fix: #672 added strict bounds

--- Frontend/ui-customer/src/Resources/Constants.purs
+++ Frontend/ui-customer/src/Resources/Constants.purs
@@ -35,6 +35,9 @@ getDelayForAutoComplete = 500
 getDelayForLocateOnMap :: Int
 getDelayForLocateOnMap = 1000
 
+getSearchRadius :: Int
+getSearchRadius = 100000
+
 getLanguages :: Array Language
 getLanguages =
   [ { name: "English", value: "EN_US" }
