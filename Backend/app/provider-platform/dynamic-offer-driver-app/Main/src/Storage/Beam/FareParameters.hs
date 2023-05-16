bd4af96d75facdf3a0716b24f059c1ef789e72d2
aashok3 <arjun.ashok@juspay.in>
2023-05-16 16:00:41 +0530

    Adding default values for beam types and Serialize instances

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Storage/Beam/FareParameters.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Storage/Beam/FareParameters.hs
@@ -97,6 +97,9 @@ data FareParametersT f = FareParametersT
 instance IsString Money where
   fromString = show
 
+instance IsString Domain.FarePolicyType where
+  fromString = show
+
 instance B.Table FareParametersT where
   data PrimaryKey FareParametersT f
     = Id (B.C f Text)
@@ -164,6 +167,7 @@ defaultFareParameters =
       waitingChargePerMin = Nothing,
       waitingOrPickupCharges = Nothing,
       serviceCharge = Nothing,
+      farePolicyType = "",
       govtChargesPerc = Nothing
     }
 
