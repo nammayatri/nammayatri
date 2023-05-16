51217b51d04dce769658093d275bcb2bf0d0185c
Arjun Kathuria <arjun.kathuria8@gmail.com>
2023-05-16 17:08:57 +0530

    backend/refactor: Adds missing "Lens" library imports to SignatureAuth.hs
    * Adds missing "Lens" library imports to the file
      "app/rider-platform/rider-app/Main/src/Tools/SignatureAuth.hs"

--- Backend/app/rider-platform/rider-app/Main/src/Tools/SignatureAuth.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Tools/SignatureAuth.hs
@@ -15,8 +15,7 @@
 module Tools.SignatureAuth where
 
 import Control.Arrow
-import Control.Lens ((.=), (?=))
-import Control.Lens.At (at)
+import Control.Lens (at, (.=), (.~), (?=))
 import qualified Crypto.Hash as Hash
 import qualified Crypto.PubKey.RSA.PKCS15 as RSA
 import qualified Crypto.Store.X509 as CryptoStore
