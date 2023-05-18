e9a48b6a93aa36e1728c63048b31e5e3d3c71482
Ben Levy <ben.levy@juspay.in>
2023-05-18 20:47:26 +0000

    Remove old-style spago files, only support building via purifix; Cleanup package.json files to remove the scripts and dependencies which are now managed via nix; Reset juspay_assets to match master.

--- Frontend/ui-driver/cleanSpago.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo " ---------- cleaning project --------------"
-rm -rf node_modules .pulp-cache output .spago
-echo " ---------- cleaning done --------------"
-
-echo " ---------- started installing --------------"
-npm install
-npm run start:android:master
-echo " ---------- cleaning done --------------"
