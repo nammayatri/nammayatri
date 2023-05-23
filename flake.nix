5559a28aa9720761ca0b15e70cf7ae475970695c
Sridhar Ratnakumar <srid@srid.ca>
2023-05-23 17:09:48 -0400

    backend/nix: Fully share common's inputs

--- flake.nix
+++ flake.nix
@@ -1,21 +1,21 @@
 {
   inputs = {
     common.url = "github:nammayatri/common";
-    nixpkgs.follows = "common/nixpkgs";
-    flake-parts.follows = "common/flake-parts";
-    systems.url = "github:nix-systems/default";
 
     # Backend inputs
-    shared-kernel.url = "github:nammayatri/shared-kernel";
-    shared-kernel.inputs.nixpkgs.follows = "nixpkgs";
-    beckn-gateway.url = "github:nammayatri/beckn-gateway";
+    shared-kernel.url = "github:nammayatri/shared-kernel/srid/share-common";
+    shared-kernel.inputs.common.follows = "common";
+    beckn-gateway.url = "github:nammayatri/beckn-gateway/srid/share-common";
+    beckn-gateway.inputs.common.follows = "common";
     beckn-gateway.inputs.shared-kernel.follows = "shared-kernel";
+
     easy-purescript-nix.url = "github:justinwoo/easy-purescript-nix";
     easy-purescript-nix.flake = false;
   };
-  outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
+  outputs = inputs: inputs.common.inputs.flake-parts.lib.mkFlake
+    { inputs = inputs // { inherit (inputs.common.inputs) nixpkgs; }; }
+    {
+      systems = import inputs.common.inputs.systems;
       imports = [
         inputs.common.flakeModules.default
         ./Backend/default.nix
