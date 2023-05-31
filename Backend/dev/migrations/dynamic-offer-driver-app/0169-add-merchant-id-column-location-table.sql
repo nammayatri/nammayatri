8fd2eb4451eb33eadc37c67e0619afdda786fe9d
Shailesh Gahlawat <shailesh.techcourse@gmail.com>
2023-05-31 12:40:03 +0530

    backend/fix: migration queries for copying merchantID from person table

--- Backend/dev/migrations/dynamic-offer-driver-app/0169-add-merchant-id-column-location-table.sql
+++ Backend/dev/migrations/dynamic-offer-driver-app/0169-add-merchant-id-column-location-table.sql
@@ -1,12 +1,8 @@
 ALTER TABLE atlas_driver_offer_bpp.driver_location ADD COLUMN merchant_id character(36);
 
-WITH PersonAndMerchantIDs AS (
-  SELECT p.id, p.merchant_id
-  FROM atlas_driver_offer_bpp.person AS p
-)
-UPDATE atlas_driver_offer_bpp.driver_location
-SET merchant_id = PersonAndMerchantIDs.merchant_id
-WHERE atlas_driver_offer_bpp.driver_location.driver_id = PersonAndMerchantIDs.id;
+UPDATE atlas_driver_offer_bpp.driver_location SET merchant_id = (SELECT merchant_id FROM atlas_driver_offer_bpp.person WHERE atlas_driver_offer_bpp.driver_location.driver_id = atlas_driver_offer_bpp.person.id);
 
 ALTER TABLE atlas_driver_offer_bpp.driver_location
-ALTER COLUMN merchant_id character(36) NOT NULL DEFAULT "favorit0-0000-0000-0000-00000favorit";
+ALTER COLUMN merchant_id set default '7f7896dd-787e-4a0b-8675-e9e6fe93bb8f';
+ALTER TABLE atlas_driver_offer_bpp.driver_location
+ALTER COLUMN merchant_id set NOT NULL;
