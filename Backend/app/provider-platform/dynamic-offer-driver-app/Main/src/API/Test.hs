c6662549a29a7c94a1d83ddd33c07d738743be99
Anuragini Paunikar <aapaunikar2018@gmail.com>
2023-06-16 10:51:39 +0530

    tmp/load-test-hedis

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/API/Test.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/API/Test.hs
@@ -41,24 +41,47 @@ setNx iter = withFlowHandlerAPI $ do
   pure "test complete"
 
 get :: Maybe Integer -> FlowHandler Text
-get _iter = withFlowHandlerAPI $ do
-  pure "test"
+get iter = withFlowHandlerAPI $ do
+  void $
+    replicateM (fromMaybe 1 $ fromInteger <$> iter) $ do
+      num <- getRandomInRange (10000000, 99999999 :: Int)
+      let key = "test" <> show num
+      let value :: Text = "test"
+      res :: Maybe Text <- (Hedis.get (keyPrefix <> key))
+      -- res <- fromMaybe False <$> Hedis.get (keyPrefix <> key)
+      logDebug $ "get " <> "key: " <> key <> " value: " <> value <> " res: " <> show res
+  pure "test complete"
 
 -- let key = "test"
 -- res <- redis $ \conn -> liftIO $ runRedis conn $ get key
 -- pure $ show res
 
 del :: Maybe Integer -> FlowHandler Text
-del _iter = withFlowHandlerAPI $ do
+del iter = withFlowHandlerAPI $ do
+  void $
+    replicateM (fromMaybe 1 $ fromInteger <$> iter) $ do
+      num <- getRandomInRange (10000000, 99999999 :: Int)
+      let key = "test" <> show num
+      let value :: Text = "test"
+      res <- Hedis.del (keyPrefix <> key)
+      logDebug $ "del " <> "key: " <> key <> " value: " <> value <> " res: " <> show res
+
   -- let key = "test"
   -- res <- redis $ \conn -> liftIO $ runRedis conn $ del [key]
   -- pure $ show res
-  pure "test"
+  pure "test complete"
 
 setNxEx :: Maybe Integer -> FlowHandler Text
-setNxEx _iter = withFlowHandlerAPI $ do
+setNxEx iter = withFlowHandlerAPI $ do
+  void $
+    replicateM (fromMaybe 1 $ fromInteger <$> iter) $ do
+      num <- getRandomInRange (10000000, 99999999 :: Int)
+      let key = "test" <> show num
+      let value :: Text = "test"
+      res <- Hedis.setNxExpire (keyPrefix <> key) 10 value
+      logDebug $ "setNxEx " <> "key: " <> key <> " value: " <> value <> " res: " <> show res
   -- let key = "test"
   -- let value = "test"
   -- res <- redis $ \conn -> liftIO $ runRedis conn $ setnxex key value 10
   -- pure $ show res
-  pure "test"
+  pure "test complete"
