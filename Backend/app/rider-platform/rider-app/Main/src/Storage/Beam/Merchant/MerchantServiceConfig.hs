3ee97c01e2e7d07d9b8be3dd6873d6d69e4066dc
Vijay Gupta <vijay.gupta@juspay.in>
2023-06-20 16:01:00 +0530

    added beam folder for Maps farePolicy and Merchant

---
+++ Backend/app/rider-platform/rider-app/Main/src/Storage/Beam/Merchant/MerchantServiceConfig.hs
@@ -0,0 +1,119 @@
+{-
+  Copyright 2022-23, Juspay India Pvt Ltd
+
+  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License
+
+  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program
+
+  is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+
+  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of
+
+  the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Storage.Beam.Merchant.MerchantServiceConfig where
+
+import qualified Data.Aeson as A
+import qualified Data.HashMap.Internal as HM
+import qualified Data.Map.Strict as M
+import Data.Serialize
+import qualified Data.Time as Time
+import qualified Database.Beam as B
+import Database.Beam.Backend
+import Database.Beam.MySQL ()
+import Database.Beam.Postgres
+  ( Postgres,
+  )
+import Database.PostgreSQL.Simple.FromField (FromField, fromField)
+import qualified Domain.Types.Merchant.MerchantServiceConfig as Domain
+import EulerHS.KVConnector.Types (KVConnector (..), MeshMeta (..), primaryKey, secondaryKeys, tableName)
+import GHC.Generics (Generic)
+import Kernel.Prelude hiding (Generic)
+import Lib.Utils
+import Lib.UtilsTH
+import Sequelize
+
+instance FromField Domain.ServiceName where
+  fromField = fromFieldEnum
+
+instance HasSqlValueSyntax be String => HasSqlValueSyntax be Domain.ServiceName where
+  sqlValueSyntax = autoSqlValueSyntax
+
+instance BeamSqlBackend be => B.HasSqlEqualityCheck be Domain.ServiceName
+
+instance FromBackendRow Postgres Domain.ServiceName
+
+instance IsString Domain.ServiceName where
+  fromString = show
+
+data MerchantServiceConfigT f = MerchantServiceConfigT
+  { merchantId :: B.C f Text,
+    serviceName :: B.C f Domain.ServiceName,
+    configJSON :: B.C f Text,
+    updatedAt :: B.C f Time.UTCTime,
+    createdAt :: B.C f Time.UTCTime
+  }
+  deriving (Generic, B.Beamable)
+
+instance B.Table MerchantServiceConfigT where
+  data PrimaryKey MerchantServiceConfigT f
+    = Id (B.C f Text)
+    deriving (Generic, B.Beamable)
+  primaryKey = Id . merchantId
+
+instance ModelMeta MerchantServiceConfigT where
+  modelFieldModification = merchantServiceConfigTMod
+  modelTableName = "merchant_service_config"
+  modelSchemaName = Just "atlas_app"
+
+type MerchantServiceConfig = MerchantServiceConfigT Identity
+
+instance FromJSON MerchantServiceConfig where
+  parseJSON = A.genericParseJSON A.defaultOptions
+
+instance ToJSON MerchantServiceConfig where
+  toJSON = A.genericToJSON A.defaultOptions
+
+deriving stock instance Show MerchantServiceConfig
+
+merchantServiceConfigTMod :: MerchantServiceConfigT (B.FieldModification (B.TableField MerchantServiceConfigT))
+merchantServiceConfigTMod =
+  B.tableModification
+    { merchantId = B.fieldNamed "merchant_id",
+      serviceName = B.fieldNamed "service_name",
+      configJSON = B.fieldNamed "config_j_s_o_n",
+      updatedAt = B.fieldNamed "updated_at",
+      createdAt = B.fieldNamed "created_at"
+    }
+
+defaultMerchantServiceConfig :: MerchantServiceConfig
+defaultMerchantServiceConfig =
+  MerchantServiceConfigT
+    { merchantId = "",
+      serviceName = "",
+      configJSON = "",
+      updatedAt = defaultUTCDate,
+      createdAt = defaultUTCDate
+    }
+
+instance Serialize MerchantServiceConfig where
+  put = error "undefined"
+  get = error "undefined"
+
+psToHs :: HM.HashMap Text Text
+psToHs = HM.empty
+
+merchantServiceConfigToHSModifiers :: M.Map Text (A.Value -> A.Value)
+merchantServiceConfigToHSModifiers =
+  M.empty
+
+merchantServiceConfigToPSModifiers :: M.Map Text (A.Value -> A.Value)
+merchantServiceConfigToPSModifiers =
+  M.empty
+
+$(enableKVPG ''MerchantServiceConfigT ['serviceName] [])
---
+++ Backend/app/rider-platform/rider-app/Main/src/Storage/Beam/Merchant/MerchantServiceUsageConfig.hs
@@ -0,0 +1,205 @@
+{-
+  Copyright 2022-23, Juspay India Pvt Ltd
+
+  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License
+
+  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program
+
+  is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+
+  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of
+
+  the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Storage.Beam.Merchant.MerchantServiceUsageConfig where
+
+import qualified Data.Aeson as A
+import qualified Data.HashMap.Internal as HM
+import qualified Data.Map.Strict as M
+import Data.Serialize
+import qualified Data.Time as Time
+import qualified Database.Beam as B
+import Database.Beam.Backend
+import Database.Beam.MySQL ()
+import Database.Beam.Postgres
+  ( Postgres,
+  )
+import Database.PostgreSQL.Simple.FromField (FromField, fromField)
+import EulerHS.KVConnector.Types (KVConnector (..), MeshMeta (..), primaryKey, secondaryKeys, tableName)
+import GHC.Generics (Generic)
+import Kernel.External.Call.Types (CallService)
+import Kernel.External.Maps.Types
+import Kernel.External.Notification.Types (NotificationService)
+import Kernel.External.SMS (SmsService)
+import Kernel.External.Whatsapp.Types (WhatsappService)
+import Kernel.Prelude hiding (Generic)
+import Lib.Utils
+import Lib.UtilsTH
+import Sequelize
+
+instance FromField NotificationService where
+  fromField = fromFieldEnum
+
+instance HasSqlValueSyntax be String => HasSqlValueSyntax be NotificationService where
+  sqlValueSyntax = autoSqlValueSyntax
+
+instance BeamSqlBackend be => B.HasSqlEqualityCheck be NotificationService
+
+instance FromBackendRow Postgres NotificationService
+
+instance IsString NotificationService where
+  fromString = show
+
+instance FromField CallService where
+  fromField = fromFieldEnum
+
+instance HasSqlValueSyntax be String => HasSqlValueSyntax be CallService where
+  sqlValueSyntax = autoSqlValueSyntax
+
+instance BeamSqlBackend be => B.HasSqlEqualityCheck be CallService
+
+instance FromBackendRow Postgres CallService
+
+instance IsString CallService where
+  fromString = show
+
+instance FromField MapsService where
+  fromField = fromFieldEnum
+
+instance HasSqlValueSyntax be String => HasSqlValueSyntax be MapsService where
+  sqlValueSyntax = autoSqlValueSyntax
+
+instance BeamSqlBackend be => B.HasSqlEqualityCheck be MapsService
+
+instance FromBackendRow Postgres MapsService
+
+instance IsString MapsService where
+  fromString = show
+
+instance FromField [WhatsappService] where
+  fromField = fromFieldEnum
+
+instance HasSqlValueSyntax be String => HasSqlValueSyntax be [WhatsappService] where
+  sqlValueSyntax = autoSqlValueSyntax
+
+instance BeamSqlBackend be => B.HasSqlEqualityCheck be [WhatsappService]
+
+instance FromBackendRow Postgres [WhatsappService]
+
+instance FromField [SmsService] where
+  fromField = fromFieldEnum
+
+instance HasSqlValueSyntax be String => HasSqlValueSyntax be [SmsService] where
+  sqlValueSyntax = autoSqlValueSyntax
+
+instance BeamSqlBackend be => B.HasSqlEqualityCheck be [SmsService]
+
+instance FromBackendRow Postgres [SmsService]
+
+data MerchantServiceUsageConfigT f = MerchantServiceUsageConfigT
+  { merchantId :: B.C f Text,
+    initiateCall :: B.C f CallService,
+    getDistances :: B.C f MapsService,
+    getRoutes :: B.C f MapsService,
+    snapToRoad :: B.C f MapsService,
+    getPlaceName :: B.C f MapsService,
+    getPickupRoutes :: B.C f MapsService,
+    getTripRoutes :: B.C f MapsService,
+    getPlaceDetails :: B.C f MapsService,
+    autoComplete :: B.C f MapsService,
+    getDistancesForCancelRide :: B.C f MapsService,
+    notifyPerson :: B.C f NotificationService,
+    useFraudDetection :: B.C f Bool,
+    smsProvidersPriorityList :: B.C f [SmsService],
+    whatsappProvidersPriorityList :: B.C f [WhatsappService],
+    updatedAt :: B.C f Time.UTCTime,
+    createdAt :: B.C f Time.UTCTime
+  }
+  deriving (Generic, B.Beamable)
+
+instance B.Table MerchantServiceUsageConfigT where
+  data PrimaryKey MerchantServiceUsageConfigT f
+    = Id (B.C f Text)
+    deriving (Generic, B.Beamable)
+  primaryKey = Id . merchantId
+
+instance ModelMeta MerchantServiceUsageConfigT where
+  modelFieldModification = merchantServiceUsageConfigTMod
+  modelTableName = "merchant_service_usage_config"
+  modelSchemaName = Just "atlas_app"
+
+type MerchantServiceUsageConfig = MerchantServiceUsageConfigT Identity
+
+instance FromJSON MerchantServiceUsageConfig where
+  parseJSON = A.genericParseJSON A.defaultOptions
+
+instance ToJSON MerchantServiceUsageConfig where
+  toJSON = A.genericToJSON A.defaultOptions
+
+deriving stock instance Show MerchantServiceUsageConfig
+
+merchantServiceUsageConfigTMod :: MerchantServiceUsageConfigT (B.FieldModification (B.TableField MerchantServiceUsageConfigT))
+merchantServiceUsageConfigTMod =
+  B.tableModification
+    { merchantId = B.fieldNamed "merchant_id",
+      initiateCall = B.fieldNamed "initiate_call",
+      getDistances = B.fieldNamed "get_distances",
+      getRoutes = B.fieldNamed "get_routes",
+      snapToRoad = B.fieldNamed "snap_to_road",
+      getPlaceName = B.fieldNamed "get_place_name",
+      getPickupRoutes = B.fieldNamed "get_pickup_routes",
+      getTripRoutes = B.fieldNamed "get_trip_routes",
+      getPlaceDetails = B.fieldNamed "get_place_details",
+      autoComplete = B.fieldNamed "auto_complete",
+      getDistancesForCancelRide = B.fieldNamed "get_distances_for_cancel_ride",
+      notifyPerson = B.fieldNamed "notify_person",
+      useFraudDetection = B.fieldNamed "use_fraud_detection",
+      smsProvidersPriorityList = B.fieldNamed "sms_providers_priority_list",
+      whatsappProvidersPriorityList = B.fieldNamed "whatsapp_providers_priority_list",
+      updatedAt = B.fieldNamed "updated_at",
+      createdAt = B.fieldNamed "created_at"
+    }
+
+defaultMerchantServiceUsageConfig :: MerchantServiceUsageConfig
+defaultMerchantServiceUsageConfig =
+  MerchantServiceUsageConfigT
+    { merchantId = "",
+      initiateCall = "",
+      getDistances = "",
+      getRoutes = "",
+      snapToRoad = "",
+      getPlaceName = "",
+      getPickupRoutes = "",
+      getTripRoutes = "",
+      getPlaceDetails = "",
+      autoComplete = "",
+      getDistancesForCancelRide = "",
+      notifyPerson = "",
+      useFraudDetection = False,
+      smsProvidersPriorityList = [],
+      whatsappProvidersPriorityList = [],
+      updatedAt = defaultUTCDate,
+      createdAt = defaultUTCDate
+    }
+
+instance Serialize MerchantServiceUsageConfig where
+  put = error "undefined"
+  get = error "undefined"
+
+psToHs :: HM.HashMap Text Text
+psToHs = HM.empty
+
+merchantServiceUsageConfigToHSModifiers :: M.Map Text (A.Value -> A.Value)
+merchantServiceUsageConfigToHSModifiers =
+  M.empty
+
+merchantServiceUsageConfigToPSModifiers :: M.Map Text (A.Value -> A.Value)
+merchantServiceUsageConfigToPSModifiers =
+  M.empty
+
+$(enableKVPG ''MerchantServiceUsageConfigT ['merchantId] [])
