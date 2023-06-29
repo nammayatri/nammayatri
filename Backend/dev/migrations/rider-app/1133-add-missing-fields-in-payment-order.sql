2da0c979eca2a4def7ca2bc2e56b5cee55b54de5
Roman Bodavskiy <roman.bodavskiy@metalamp.io>
2023-06-29 12:57:17 +0300

    backend/fix: order.request_id migration

--- Backend/dev/migrations/rider-app/1133-add-missing-fields-in-payment-order.sql
+++ Backend/dev/migrations/rider-app/1133-add-missing-fields-in-payment-order.sql
@@ -3,3 +3,4 @@ ALTER TABLE atlas_app.payment_order ADD COLUMN client_id character varying(255);
 ALTER TABLE atlas_app.payment_order ADD COLUMN description character varying(1024);
 ALTER TABLE atlas_app.payment_order ADD COLUMN return_url character varying(255);
 ALTER TABLE atlas_app.payment_order ADD COLUMN action character varying(255);
+ALTER TABLE atlas_app.payment_order ADD COLUMN request_id character varying(255);
