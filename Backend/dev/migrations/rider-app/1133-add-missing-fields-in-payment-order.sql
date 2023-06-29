f4470258c22dff4c00f1d0ea599ee921b012e33c
hkmangla <hemantmangla78@gmail.com>
2023-06-29 14:59:39 +0530

    backend/fix/added missed db migrations for payment

---
+++ Backend/dev/migrations/rider-app/1133-add-missing-fields-in-payment-order.sql
@@ -0,0 +1,5 @@
+ALTER TABLE atlas_app.payment_order ADD COLUMN service character varying(255);
+ALTER TABLE atlas_app.payment_order ADD COLUMN client_id character varying(255);
+ALTER TABLE atlas_app.payment_order ADD COLUMN description character varying(1024);
+ALTER TABLE atlas_app.payment_order ADD COLUMN return_url character varying(255);
+ALTER TABLE atlas_app.payment_order ADD COLUMN action character varying(255);
