dbb3a1160a29feeea6cddcd25bb39f3fa76c509c
Ben Levy <ben.levy@juspay.in>
2023-06-30 11:19:16 -0500

    Remove useless treefmt config entry

--- Frontend/default.nix
+++ Frontend/default.nix
@@ -20,29 +20,6 @@
 
     in
     {
-      treefmt.config = {
-        # Suppress autoformatting of frontend dhall files.
-        settings.formatter.dhall.excludes = [
-          "Frontend/packages.dhall"
-        ];
-      };
-      devShells = lib.optionalAttrs (system != "aarch64-linux") {
-        frontend = pkgs.mkShell {
-          name = "ps-dev-shell";
-          inputsFrom = [
-            config.pre-commit.devShell
-          ];
-          packages = [
-            easy-ps.purs-0_15_4
-            easy-ps.spago
-            easy-ps.psa
-            pkgs.dhall
-            pkgs.nodejs-14_x
-          ];
-        };
-      };
-    };
-
       devShells.frontend = pkgs.mkShell {
         name = "ps-dev-shell";
         inputsFrom = [
