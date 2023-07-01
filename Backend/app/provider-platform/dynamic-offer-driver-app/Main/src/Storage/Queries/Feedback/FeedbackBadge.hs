dbd5784e16ee8d381675098254115b3026703fe5
Prashant Singh <prashantsingh061201@gmail.com>
2023-07-01 11:12:56 +0530

    Backend/feat: #1235/added-feedback-badges-and-added-feedback-form-for-bap-and-bpp

---
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Storage/Queries/Feedback/FeedbackBadge.hs
@@ -0,0 +1,46 @@
+{-
+ Copyright 2022-23, Juspay India Pvt Ltd
+
+ This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License
+
+ as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program
+
+ is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+
+ or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of
+
+ the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+-}
+
+module Storage.Queries.Feedback.FeedbackBadge where
+
+import Data.Text
+import Domain.Types.Feedback.Feedback (FeedbackBadge)
+import Domain.Types.Person (Person)
+import Kernel.Storage.Esqueleto as Esq
+import Kernel.Types.Id
+import Kernel.Types.Time
+import Storage.Tabular.Feedback.FeedbackBadge
+import Prelude
+
+createFeedbackBadge :: FeedbackBadge -> SqlDB ()
+createFeedbackBadge = Esq.create
+
+findFeedbackBadgeForDriver :: Transactionable m => Id Person -> Text -> m (Maybe FeedbackBadge)
+findFeedbackBadgeForDriver driverId badge = findOne $ do
+  feedbackBadge <- from $ table @FeedbackBadgeT
+  where_ $ feedbackBadge ^. FeedbackBadgeDriverId ==. val (toKey driverId) &&. feedbackBadge ^. FeedbackBadgeBadge ==. val badge
+  pure feedbackBadge
+
+updateFeedbackBadge :: FeedbackBadge -> Int -> SqlDB ()
+updateFeedbackBadge feedbackBadge newBadgeCount = do
+  now <- getCurrentTime
+  Esq.update $ \tbl -> do
+    set
+      tbl
+      [ FeedbackBadgeBadgeCount =. val newBadgeCount,
+        FeedbackBadgeUpdatedAt =. val now
+      ]
+    where_ $
+      tbl ^. FeedbackBadgeTId ==. val (toKey feedbackBadge.id)
+        &&. tbl ^. FeedbackBadgeDriverId ==. val (toKey feedbackBadge.driverId)
---
+++ Backend/app/rider-platform/rider-app/Main/src/API/UI/FeedbackForm.hs
@@ -0,0 +1,53 @@
+{-
+ Copyright 2022-23, Juspay India Pvt Ltd
+
+ This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License
+
+ as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program
+
+ is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+
+ or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of
+
+ the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use newtype instead of data" #-}
+{-# OPTIONS_GHC -Wno-unused-do-bind #-}
+
+module API.UI.FeedbackForm where
+
+import qualified Domain.Action.UI.FeedbackForm as DF
+import Domain.Types.FeedbackForm (FeedbackFormList, FeedbackFormReq)
+import qualified Domain.Types.Merchant as Merchant
+import qualified Domain.Types.Person as Person
+import Environment
+import Kernel.Prelude
+import Kernel.Types.APISuccess
+import Kernel.Types.Id
+import Kernel.Utils.Common
+import Servant
+import Tools.Auth
+
+type API =
+  "feedback"
+    :> ( "form"
+           :> TokenAuth
+           :> QueryParam "rating" Int
+           :> Get '[JSON] FeedbackFormList
+           :<|> "submit"
+             :> TokenAuth
+             :> ReqBody '[JSON] FeedbackFormReq
+             :> Post '[JSON] APISuccess
+       )
+
+handler :: FlowServer API
+handler =
+  getFeedbackForm
+    :<|> submitFeedbackForm
+
+getFeedbackForm :: (Id Person.Person, Id Merchant.Merchant) -> Maybe Int -> FlowHandler FeedbackFormList
+getFeedbackForm _ rating = withFlowHandlerAPI $ DF.feedbackForm rating
+
+submitFeedbackForm :: (Id Person.Person, Id Merchant.Merchant) -> FeedbackFormReq -> FlowHandler APISuccess
+submitFeedbackForm _ feedbackFormReq = withFlowHandlerAPI $ DF.submitFeedback feedbackFormReq
