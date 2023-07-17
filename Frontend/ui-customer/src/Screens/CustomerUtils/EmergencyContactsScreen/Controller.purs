1344609f28d89579111162f8b30f33349fc8997d
ManavGuptaJuspay <manav.gupta@juspay.in>
2023-07-17 19:03:52 +0530

    Frontend/Fix for Emergency Contact Screen

--- Frontend/ui-customer/src/Screens/CustomerUtils/EmergencyContactsScreen/Controller.purs
+++ Frontend/ui-customer/src/Screens/CustomerUtils/EmergencyContactsScreen/Controller.purs
@@ -195,7 +195,7 @@ eval BackPressed state =
           pure LoadMoreContacts
         ]
   else if (state.props.showInfoPopUp == true) then
-    continue state{props{showInfoPopUp = false}}
+    continue state{props{showInfoPopUp = false, showContactList = false}}
   else
     exit $ GoToHomeScreen
 
@@ -212,6 +212,8 @@ eval (ContactListGenericHeaderActionController GenericHeader.PrefixImgOnClick) s
               pure unit
           pure LoadMoreContacts
         ]
+  else if state.props.showInfoPopUp then
+    continue state{props{showInfoPopUp = false, showContactList = false}}
   else
     exit $ GoToHomeScreen
 
@@ -334,7 +336,9 @@ contactListTransformerProp contactList =(map (\(contact) -> {
   name: toPropValue (contact.name),
   number: toPropValue (contact.number),
   isSelected: toPropValue (contact.isSelected),
-  contactBackgroundColor: toPropValue(if contact.isSelected then Color.grey900 else Color.white900),
+  contactBackgroundColor: toPropValue (if contact.isSelected then Color.grey900 else Color.white900),
+  visibilitySelectedImage: toPropValue (if contact.isSelected then "visible" else "gone"),
+  visibilityUnSelectedImage: toPropValue (if contact.isSelected then "gone" else "visible"),
   isSelectImage: toPropValue(if contact.isSelected then "ny_ic_selected_icon,https://assets.juspay.in/nammayatri/images/user/ny_ic_selected_icon.png" else "ny_ic_outer_circle,https://assets.juspay.in/nammayatri/images/user/ny_ic_outer_circle.png") 
 })(contactList))
 
@@ -343,7 +347,9 @@ contactTransformerProp contact = {
   name: toPropValue (contact.name),
   number: toPropValue (contact.number),
   isSelected: toPropValue (contact.isSelected),
-  contactBackgroundColor : toPropValue(if contact.isSelected then Color.grey900 else Color.white900),
+  contactBackgroundColor : toPropValue (if contact.isSelected then Color.grey900 else Color.white900),
+  visibilitySelectedImage: toPropValue (if contact.isSelected then "visible" else "gone"),
+  visibilityUnSelectedImage: toPropValue (if contact.isSelected then "gone" else "visible"),
   isSelectImage: toPropValue(if contact.isSelected then "ny_ic_selected_icon,https://assets.juspay.in/nammayatri/images/user/ny_ic_selected_icon.png" else "ny_ic_outer_circle,https://assets.juspay.in/nammayatri/images/user/ny_ic_outer_circle.png") 
 }
 
