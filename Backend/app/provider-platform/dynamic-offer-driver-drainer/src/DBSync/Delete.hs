acd79635a695aa58137b9a6d7da684f67ff9d989
Mechanic-98 <parasherabhi98@gmail.com>
2023-07-17 14:39:48 +0530

    DBSync changes

--- Backend/app/provider-platform/dynamic-offer-driver-drainer/src/DBSync/Delete.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-drainer/src/DBSync/Delete.hs
@@ -9,7 +9,6 @@ import EulerHS.KVConnector.Types
 import qualified EulerHS.Language as EL
 import EulerHS.Prelude hiding (id)
 -- import           Utils.Logging
-
 -- import System.CPUTime
 import Types.DBSync
 import Types.Event as Event
@@ -18,6 +17,77 @@ import Utils.Utils
 runDeleteCommands :: (DeleteDBCommand, ByteString) -> Flow (Either (MeshError, EL.KVDBStreamEntryID) EL.KVDBStreamEntryID)
 runDeleteCommands (cmd, val) = do
   case cmd of
+    DeleteDBCommand id _ _ _ _ (RegistrationTokenDeleteOptions _ whereClause) -> runDelete id val whereClause ("RegistrationToken" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (BookingDeleteOptions _ whereClause) -> runDelete id val whereClause ("Booking" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (BookingLocationDeleteOptions _ whereClause) -> runDelete id val whereClause ("BookingLocation" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (BookingCancellationReasonDeleteOptions _ whereClause) -> runDelete id val whereClause ("BookingCancellationReason" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (BusinessEventDeleteOptions _ whereClause) -> runDelete id val whereClause ("BusinessEvent" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (CallStatusDeleteOptions _ whereClause) -> runDelete id val whereClause ("CallStatus" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (CancellationReasonDeleteOptions _ whereClause) -> runDelete id val whereClause ("CancellationReason" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (DriverFlowStatusDeleteOptions _ whereClause) -> runDelete id val whereClause ("DriverFlowStatus" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (DriverFeeDeleteOptions _ whereClause) -> runDelete id val whereClause ("DriverFee" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (DriverInformationDeleteOptions _ whereClause) -> runDelete id val whereClause ("DriverInformation" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (DriverLocationDeleteOptions _ whereClause) -> runDelete id val whereClause ("DriverLocation" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (AadhaarOtpReqDeleteOptions _ whereClause) -> runDelete id val whereClause ("AadhaarOtpReq" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (AadhaarOtpVerifyDeleteOptions _ whereClause) -> runDelete id val whereClause ("AadhaarOtpVerify" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (AadhaarVerificationDeleteOptions _ whereClause) -> runDelete id val whereClause ("AadhaarVerification" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (DriverLicenseDeleteOptions _ whereClause) -> runDelete id val whereClause ("DriverLicense" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (DriverRCAssociationDeleteOptions _ whereClause) -> runDelete id val whereClause ("DriverRCAssociation" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (IdfyVerificationDeleteOptions _ whereClause) -> runDelete id val whereClause ("IdfyVerification" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (ImageDeleteOptions _ whereClause) -> runDelete id val whereClause ("Image" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (OperatingCityDeleteOptions _ whereClause) -> runDelete id val whereClause ("OperatingCity" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (VehicleRegistrationCertificateDeleteOptions _ whereClause) -> runDelete id val whereClause ("VehicleRegistrationCertificate" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (DriverQuoteDeleteOptions _ whereClause) -> runDelete id val whereClause ("DriverQuote" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (DriverReferralDeleteOptions _ whereClause) -> runDelete id val whereClause ("DriverReferral" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (DriverStatsDeleteOptions _ whereClause) -> runDelete id val whereClause ("DriverStats" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (EstimateDeleteOptions _ whereClause) -> runDelete id val whereClause ("Estimate" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (ExophoneDeleteOptions _ whereClause) -> runDelete id val whereClause ("Exophone" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (FareParametersDeleteOptions _ whereClause) -> runDelete id val whereClause ("FareParameters" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (FareParametersProgressiveDetailsDeleteOptions _ whereClause) -> runDelete id val whereClause ("FareParametersProgressiveDetails" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (FareParametersSlabDetailsDeleteOptions _ whereClause) -> runDelete id val whereClause ("FareParametersSlabDetails" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (FarePolicyDeleteOptions _ whereClause) -> runDelete id val whereClause ("FarePolicy" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (DriverExtraFeeBoundsDeleteOptions _ whereClause) -> runDelete id val whereClause ("DriverExtraFeeBounds" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (FarePolicyProgressiveDetailsDeleteOptions _ whereClause) -> runDelete id val whereClause ("FarePolicyProgressiveDetails" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (FarePolicyProgressiveDetailsPerExtraKmRateSectionDeleteOptions _ whereClause) -> runDelete id val whereClause ("FarePolicyProgressiveDetailsPerExtraKmRateSection" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (FarePolicySlabDetailsSlabDeleteOptions _ whereClause) -> runDelete id val whereClause ("FarePolicySlabDetailsSlab" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (RestrictedExtraFareDeleteOptions _ whereClause) -> runDelete id val whereClause ("RestrictedExtraFare" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (FareProductDeleteOptions _ whereClause) -> runDelete id val whereClause ("FareProduct" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (GeometryDeleteOptions _ whereClause) -> runDelete id val whereClause ("Geometry" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (CommentDeleteOptions _ whereClause) -> runDelete id val whereClause ("Comment" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (IssueCategoryDeleteOptions _ whereClause) -> runDelete id val whereClause ("IssueCategory" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (IssueOptionDeleteOptions _ whereClause) -> runDelete id val whereClause ("IssueOption" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (IssueReportDeleteOptions _ whereClause) -> runDelete id val whereClause ("IssueReport" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (IssueTranslationDeleteOptions _ whereClause) -> runDelete id val whereClause ("IssueTranslation" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (LeaderBoardConfigDeleteOptions _ whereClause) -> runDelete id val whereClause ("LeaderBoardConfig" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (PlaceNameCacheDeleteOptions _ whereClause) -> runDelete id val whereClause ("PlaceNameCache" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (MediaFileDeleteOptions _ whereClause) -> runDelete id val whereClause ("MediaFile" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (MerchantDeleteOptions _ whereClause) -> runDelete id val whereClause ("Merchant" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (DriverIntelligentPoolConfigDeleteOptions _ whereClause) -> runDelete id val whereClause ("DriverIntelligentPoolConfig" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (DriverPoolConfigDeleteOptions _ whereClause) -> runDelete id val whereClause ("DriverPoolConfig" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (MerchantLeaderBoardConfigDeleteOptions _ whereClause) -> runDelete id val whereClause ("MerchantLeaderBoardConfig" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (MerchantMessageDeleteOptions _ whereClause) -> runDelete id val whereClause ("MerchantMessage" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (MerchantPaymentMethodDeleteOptions _ whereClause) -> runDelete id val whereClause ("MerchantPaymentMethod" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (MerchantServiceConfigDeleteOptions _ whereClause) -> runDelete id val whereClause ("MerchantServiceConfig" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (MerchantServiceUsageConfigDeleteOptions _ whereClause) -> runDelete id val whereClause ("MerchantServiceUsageConfig" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (MerchantOnboardingDocumentConfigDeleteOptions _ whereClause) -> runDelete id val whereClause ("MerchantOnboardingDocumentConfig" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (TransporterConfigDeleteOptions _ whereClause) -> runDelete id val whereClause ("TransporterConfig" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (MessageDeleteOptions _ whereClause) -> runDelete id val whereClause ("Message" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (MessageReportDeleteOptions _ whereClause) -> runDelete id val whereClause ("MessageReport" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (MessageTranslationDeleteOptions _ whereClause) -> runDelete id val whereClause ("MessageTranslation" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (OnboardingDocumentConfigDeleteOptions _ whereClause) -> runDelete id val whereClause ("OnboardingDocumentConfig" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (PersonDeleteOptions _ whereClause) -> runDelete id val whereClause ("Person" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (QuoteSpecialZoneDeleteOptions _ whereClause) -> runDelete id val whereClause ("QuoteSpecialZone" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (RatingDeleteOptions _ whereClause) -> runDelete id val whereClause ("Rating" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (RideDeleteOptions _ whereClause) -> runDelete id val whereClause ("Ride" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (RideDetailsDeleteOptions _ whereClause) -> runDelete id val whereClause ("RideDetails" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (RiderDetailsDeleteOptions _ whereClause) -> runDelete id val whereClause ("RiderDetails" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (SearchRequestDeleteOptions _ whereClause) -> runDelete id val whereClause ("SearchRequest" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (SearchReqLocationDeleteOptions _ whereClause) -> runDelete id val whereClause ("SearchReqLocation" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (SearchRequestForDriverDeleteOptions _ whereClause) -> runDelete id val whereClause ("SearchRequestForDriver" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (SearchRequestSpecialZoneDeleteOptions _ whereClause) -> runDelete id val whereClause ("SearchRequestSpecialZone" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (SearchTryDeleteOptions _ whereClause) -> runDelete id val whereClause ("SearchTry" :: Text) =<< Config.getEulerPgDbConf
+    DeleteDBCommand id _ _ _ _ (VehicleDeleteOptions _ whereClause) -> runDelete id val whereClause ("Vehicle" :: Text) =<< Config.getEulerPgDbConf
+  where
     -- DeleteDBCommand id _ _ _ _ (TxnOfferInfoDeleteOptions                  _ whereClause) -> runDelete id val whereClause ("TxnOfferInfo"                  :: Text) =<< Config.getEulerDbConf
     -- DeleteDBCommand id _ _ _ _ (HdfcHashedNumDeleteOptions                 _ whereClause) -> runDelete id val whereClause ("HdfcHashedNum"                 :: Text) =<< Config.getEulerDbConf
     -- DeleteDBCommand id _ _ _ _ (MerchantGatewayAccountDeleteOptions        _ whereClause) -> runDelete id val whereClause ("MerchantGatewayAccount"        :: Text) =<< Config.getEulerDbConf
@@ -92,8 +162,6 @@ runDeleteCommands (cmd, val) = do
     -- DeleteDBCommand id _ _ _ _ (PaymentLinksDeleteOptions                  _ whereClause) -> runDelete id val whereClause ("PaymentLinks"                  :: Text) =<< Config.getEulerDbConf
     -- DeleteDBCommand id _ _ _ _ (CustomerAccountDeleteOptions               _ whereClause) -> runDelete id val whereClause ("CustomerAccount"               :: Text) =<< Config.getEulerDbConf
     -- DeleteDBCommand id _ _ _ _ (AuthMappingDeleteOptions                   _ whereClause) -> runDelete id val whereClause ("AuthMapping"                   :: Text) =<< Config.getEulerDbConf
-    DeleteDBCommand id _ _ _ _ (RegistrationTokenDeleteOptions _ whereClause) -> runDelete id val whereClause ("RegistrationToken" :: Text) =<< Config.getEulerPgDbConf
-  where
     -- DeleteDBCommand id _ _ _ _ (OfferRedemptionDeleteOptions               _ whereClause) -> runDelete id val whereClause ("OfferRedemption"               :: Text) =<< Config.getEulerPgDbConf
     -- DeleteDBCommand id _ _ _ _ (ExternalMerchantCustomerDeleteOptions      _ whereClause) -> runDelete id val whereClause ("ExternalMerchantCustomer"      :: Text) =<< Config.getEulerPgDbConf
     -- DeleteDBCommand id _ _ _ _ (AgencyDeleteOptions                        _ whereClause) -> runDelete id val whereClause ("Agency"                        :: Text) =<< Config.getEulerPgDbConf
