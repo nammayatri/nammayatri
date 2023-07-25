3dfa43d118b78cc4f64963e6d56a1ec6d3b754f5
DwivediAman-exe <19dwivediaman@gmail.com>
2023-07-25 19:28:35 +0530

    backend/feat/producer

---
+++ Backend/lib/producer/src/Environment.hs
@@ -0,0 +1,101 @@
+{-
+ Copyright 2022-23, Juspay India Pvt Ltd
+
+ This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License
+
+ as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program
+
+ is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+
+ or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of
+
+ the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+-}
+
+module Environment where
+
+import qualified Data.Text as T
+import EulerHS.Prelude hiding (maybe, show)
+import Kernel.Storage.Hedis
+import Kernel.Tools.Metrics.CoreMetrics
+import Kernel.Types.Common hiding (id)
+import Kernel.Types.Flow (FlowR)
+import Kernel.Utils.App (lookupDeploymentVersion)
+import Kernel.Utils.Dhall (FromDhall)
+import Kernel.Utils.IOLogging
+import System.Environment (lookupEnv)
+
+data AppCfg = AppCfg
+  { hedisCfg :: HedisCfg,
+    hedisClusterCfg :: HedisCfg,
+    hedisNonCriticalCfg :: HedisCfg,
+    hedisNonCriticalClusterCfg :: HedisCfg,
+    hedisMigrationStage :: Bool,
+    cutOffHedisCluster :: Bool,
+    loggerConfig :: LoggerConfig,
+    enableRedisLatencyLogging :: Bool,
+    batchSize :: Int,
+    waitTimeMilliSec :: Milliseconds,
+    enablePrometheusMetricLogging :: Bool,
+    streamName :: Text,
+    producerTimestampKey :: Text,
+    cacheConfig :: CacheConfig,
+    setName :: Text,
+    entryId :: Text
+  }
+  deriving (Generic, FromDhall)
+
+data AppEnv = AppEnv
+  { hedisEnv :: HedisEnv,
+    hedisNonCriticalEnv :: HedisEnv,
+    hedisNonCriticalClusterEnv :: HedisEnv,
+    hedisClusterEnv :: HedisEnv,
+    cutOffHedisCluster :: Bool,
+    hedisMigrationStage :: Bool,
+    hostname :: Maybe Text,
+    coreMetrics :: CoreMetricsContainer,
+    loggerEnv :: LoggerEnv,
+    enableRedisLatencyLogging :: Bool,
+    enablePrometheusMetricLogging :: Bool,
+    waitTimeMilliSec :: Milliseconds,
+    loggerConfig :: LoggerConfig,
+    batchSize :: Int,
+    version :: DeploymentVersion,
+    streamName :: Text,
+    producerTimestampKey :: Text,
+    cacheConfig :: CacheConfig,
+    setName :: Text,
+    entryId :: Text
+  }
+  deriving (Generic)
+
+buildAppEnv :: AppCfg -> IO AppEnv
+buildAppEnv AppCfg {..} = do
+  hedisEnv <- connectHedis hedisCfg id
+  version <- lookupDeploymentVersion
+  hostname <- map T.pack <$> lookupEnv "POD_NAME"
+  coreMetrics <- registerCoreMetricsContainer
+  hedisNonCriticalEnv <- connectHedis hedisNonCriticalCfg id
+  hedisClusterEnv <-
+    if cutOffHedisCluster
+      then pure hedisEnv
+      else connectHedisCluster hedisClusterCfg id
+  hedisNonCriticalClusterEnv <-
+    if cutOffHedisCluster
+      then pure hedisNonCriticalEnv
+      else connectHedisCluster hedisNonCriticalClusterCfg id
+  loggerEnv <- prepareLoggerEnv loggerConfig hostname
+  pure $ AppEnv {..}
+
+type FlowHandler = FlowHandlerR AppEnv
+
+type Flow = FlowR AppEnv
+
+newtype CacheConfig = CacheConfig
+  { configsExpTime :: Seconds
+  }
+  deriving (Generic, FromDhall)
+
+type HasCacheConfig r = HasField "cacheConfig" r CacheConfig
+
+type CacheFlow m r = (HasCacheConfig r, HedisFlow m r)
---
+++ Backend/lib/producer/src/Producer/Flow.hs
@@ -0,0 +1,80 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+
+module Producer.Flow where
+
+import qualified Data.Aeson as Ae
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
+import Data.Text (pack)
+import qualified Data.Text.Encoding as TE
+import Environment
+import Kernel.Prelude
+import qualified Kernel.Storage.Hedis.Queries as Hedis
+import Kernel.Tools.Metrics.CoreMetrics (CoreMetrics (addGenericLatency))
+import Kernel.Types.Flow ()
+import Kernel.Utils.Common
+import Kernel.Utils.Time ()
+
+getCurrentTimestamp :: (CacheFlow m r) => m Milliseconds
+getCurrentTimestamp = liftIO getClockTimeInMs
+
+splitIntoBatches :: Int -> [a] -> [[a]]
+splitIntoBatches _ [] = []
+splitIntoBatches batchSize xs =
+  let (batch, rest) = splitAt batchSize xs
+   in batch : splitIntoBatches batchSize rest
+
+concatenateChunks :: [BS.ByteString] -> BS.ByteString
+concatenateChunks = BS.concat
+
+encodeToJSON :: BS.ByteString -> BSL.ByteString
+encodeToJSON = Ae.encode . TE.decodeUtf8
+
+arrayToByteString :: [BS.ByteString] -> BS.ByteString
+arrayToByteString chunks = BSL.toStrict (encodeToJSON (concatenateChunks chunks))
+
+getTime :: (CacheFlow m r) => Text -> m Milliseconds
+getTime producerTimestampKey = do
+  Hedis.safeGet producerTimestampKey >>= \case
+    Just currentTime -> return currentTime
+    Nothing -> getCurrentTimestamp
+
+runProducer :: Flow ()
+runProducer = do
+  begTime <- getCurrentTimestamp
+  producerTimestampKey <- asks (.producerTimestampKey)
+  startTime <- getTime producerTimestampKey
+  endTime <- getCurrentTimestamp
+
+  Hedis.set producerTimestampKey endTime
+
+  print $ pack "Producer is running ..."
+
+  setName <- asks (.setName)
+  currentJobs <- Hedis.zrangebyscore setName (millisToSecondsDouble startTime) (millisToSecondsDouble endTime)
+  logDebug $ "Jobs taken out of sortedset" <> show currentJobs
+
+  batchSize <- asks (.batchSize)
+  let jobChunks = splitIntoBatches batchSize currentJobs
+  logDebug $ "Job chunks producer" <> show jobChunks
+
+  forM_ jobChunks $ \chunk -> do
+    streamName <- asks (.streamName)
+    entryId <- asks (.entryId)
+    eqId <- generateGUID
+    let eqIdByteString = TE.encodeUtf8 eqId
+    let chunk_ = arrayToByteString chunk
+    let fieldValue = [(eqIdByteString, chunk_)]
+    result <- Hedis.xadd streamName entryId fieldValue
+    logDebug $ "Jobs inserted out of stream" <> show result
+
+  endTime <- getCurrentTimestamp
+  let diff = endTime - begTime
+  waitTimeMilliSec <- asks (.waitTimeMilliSec)
+  threadDelayMilliSec $ max 0 (waitTimeMilliSec - diff)
+
+  fork "" $ addGenericLatency "producer" (millisToNominalDiffTime diff)
+
+  runProducer
