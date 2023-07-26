d2c2653ac58d5d5976552d7f8e5ff88f8e7e9646
Shailesh Gahlawat <shailesh.techcourse@gmail.com>
2023-07-26 17:28:07 +0530

    frontend/feat: add location update limited and mid vt in headers

--- Frontend/ui-driver/src/Services/ConfigJBridge.js
+++ Frontend/ui-driver/src/Services/ConfigJBridge.js
@@ -5,3 +5,9 @@ export const getKeyInSharedPrefKeysConfig = function (key) {
 export const getValueToLocalNativeStoreConfig = function (key) {
   return window.JBridge.getKeyInNativeSharedPrefKeys(key);
 }
+
+export const setKeyInSharedPrefKeysImpl = function (key) {
+  return function (value) {
+    window.JBridge.setKeysInSharedPrefs(key, value);
+  };
+};
