5f7d95f5488d6a8f627f6f3e81c5d32b4eec3bcb
Ritika <ritikahanish@gmail.com>
2023-07-26 21:31:10 +0530

    backend/tracking issue fix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     common.url = "github:nammayatri/common";
 
     # Backend inputs
-    shared-kernel.url = "github:nammayatri/shared-kernel/18431eae960830b1b326767a4caba1b68c5f214d";
+    shared-kernel.url = "github:nammayatri/shared-kernel";
     beckn-gateway.url = "github:nammayatri/beckn-gateway";
     beckn-gateway.inputs.common.follows = "common";
     beckn-gateway.inputs.shared-kernel.follows = "shared-kernel";
