044ee1ed7a298bd89c840058f31f187f8d18c4bd
S S Mercy Queen <mercy.queen@juspay.in>
2023-07-29 20:54:47 +0530

    frontend/relase android ys 1.8.49

--- Frontend/ui-customer/src/Components/DriverInfoCard/DriverInfoCard.purs
+++ Frontend/ui-customer/src/Components/DriverInfoCard/DriverInfoCard.purs
@@ -16,4 +16,4 @@
 module Components.DriverInfoCard (module Reexport) where
 
 import Components.DriverInfoCard.View as Reexport
-import Components.DriverInfoCard.Controller as Reexport
+import Components.DriverInfoCard.Controller as Reexport 
