1f0aac4baf355d8ee8f5eebc579faeeacdae843e
Shantanu Mukherjee <shantanu.mukherjee@juspay.in>
2023-08-01 20:04:29 +0530

    backend/fix/gupshup

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Types/Merchant/MerchantServiceConfig.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Types/Merchant/MerchantServiceConfig.hs
@@ -131,6 +131,7 @@ getServiceName osc = case osc.serviceConfig of
   SmsServiceConfig smsCfg -> case smsCfg of
     Sms.ExotelSmsConfig _ -> SmsService Sms.ExotelSms
     Sms.MyValueFirstConfig _ -> SmsService Sms.MyValueFirst
+    Sms.GupShupConfig _ -> SmsService Sms.GupShup
   WhatsappServiceConfig whatsappCfg -> case whatsappCfg of
     Whatsapp.GupShupConfig _ -> WhatsappService Whatsapp.GupShup
   VerificationServiceConfig verifictaionCfg -> case verifictaionCfg of
