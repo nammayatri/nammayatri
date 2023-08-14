4667bb22dc7fc40161095a1bdf21a77a5ca0651c
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2023-08-14 15:43:01 -0400

    Jenkins: Linux ARM builds (#1952)
    Configure Nix and Jenkins to build for aarch64-linux. 
    
    Devshell is unavailable, but nammayatri packages and the docker image
    build.
    
    The CI doesn't push the arm docker image, yet.

--- Backend/nix/docker.nix
+++ Backend/nix/docker.nix
@@ -1,14 +1,19 @@
 { self, ... }:
 
-let
-  imageName = "ghcr.io/nammayatri/nammayatri";
-  # self.rev will be non-null only when the working tree is clean
-  # This is equivalent to `git rev-parse --short HEAD`
-  imageTag = builtins.substring 0 6 (self.rev or "dev");
-in
+
 {
-  config = {
-    perSystem = { self', pkgs, lib, ... }: {
+  perSystem = { self', pkgs, lib, system, ... }:
+    let
+      imageNameSuffix = {
+        "aarch64-linux" = "-arm64";
+        "aarch64-darwin" = "-arm64";
+      };
+      imageName = "ghcr.io/nammayatri/nammayatri" + (imageNameSuffix.${system} or "");
+      # self.rev will be non-null only when the working tree is clean
+      # This is equivalent to `git rev-parse --short HEAD`
+      imageTag = builtins.substring 0 6 (self.rev or "dev");
+    in
+    {
       packages = {
         dockerImage = pkgs.dockerTools.buildImage {
           name = imageName;
@@ -48,5 +53,4 @@ in
         };
       };
     };
-  };
 }
