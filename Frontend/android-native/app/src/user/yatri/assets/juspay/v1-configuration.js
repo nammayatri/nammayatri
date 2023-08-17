c76acf8104886818c878332f26c9c0ffb3e1c667
Akash Sharma <akash.sharma@juspay.in>
2023-08-17 15:41:43 +0530

    frontend/fix/made dashboard link configurable

--- Frontend/android-native/app/src/user/yatri/assets/juspay/v1-configuration.js
+++ Frontend/android-native/app/src/user/yatri/assets/juspay/v1-configuration.js
@@ -49,6 +49,7 @@ window.getMerchantConfig = function () {
     "logs": ["JUSPAY","FIREBASE"],
     "primaryButtonCornerRadius" : 8.0,
     "cancelSearchTextColor": "#E55454",
+    "dashboardUrl" : "https://www.getyatri.com/open/?source=in-app",
     "quoteListItemConfig": {
       "primaryButtonCorner": 8.0,
       "expiresColor" : "#E55454",
