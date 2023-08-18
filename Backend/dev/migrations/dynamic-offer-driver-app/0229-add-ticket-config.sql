edd1718c7c995ed18d1aad576e79afd8ca0899b7
Hemant Mangla <hemant.mangla@juspay.in>
2023-08-18 15:55:18 +0530

    [Backend/fix] Add ticket configuration SQL scripts
    - Inserts ticket configuration for merchant services

--- Backend/dev/migrations/dynamic-offer-driver-app/0229-add-ticket-config.sql
+++ Backend/dev/migrations/dynamic-offer-driver-app/0229-add-ticket-config.sql
@@ -1,11 +1,12 @@
+-- Please update auth key with prod credentials before deploying
 INSERT INTO atlas_driver_offer_bpp.merchant_service_config (merchant_id, service_name, config_json)
-VALUES ('favorit0-0000-0000-0000-00000favorit', 'Ticket_Kapture',
-    json_build_object
-        ('auth', '0.1.0|2|JMM1g9qYbwWtoscHG4qTUql80FhbCz8dMpB0ZMWaUhbSa9RBqO3bUsggftvSo2kCHjJTYgGykwXJzU1xVjDu',
-         'version', 'v.2.0',
-         'url', 'nammayatri.kapturecrm.com'
-        )
-    );
+SELECT m.id, 'Ticket_Kapture',
+  json_build_object(
+    'auth', '0.1.0|2|JMM1g9qYbwWtoscHG4qTUql80FhbCz8dMpB0ZMWaUhbSa9RBqO3bUsggftvSo2kCHjJTYgGykwXJzU1xVjDu',
+    'version', 'v.2.0',
+    'url', 'nammayatri.kapturecrm.com'
+    )
+FROM atlas_driver_offer_bpp.merchant m;
 
 ALTER TABLE atlas_driver_offer_bpp.merchant_service_usage_config ADD COLUMN issue_ticket_service character varying(30) DEFAULT 'Kapture' NOT NULL;
 
--- Backend/dev/migrations/rider-app/1155-add-ticket-config.sql
+++ Backend/dev/migrations/rider-app/1155-add-ticket-config.sql
@@ -1,19 +1,13 @@
+-- Please update auth key with prod credentials before deploying
 INSERT INTO atlas_app.merchant_service_config (merchant_id, service_name, config_json)
-VALUES ('da4e23a5-3ce6-4c37-8b9b-41377c3c1a51', 'Ticket_Kapture',
-    json_build_object
-        ('auth', '0.1.0|2|JMM1g9qYbwWtoscHG4qTUql80FhbCz8dMpB0ZMWaUhbSa9RBqO3bUsggftvSo2kCHjJTYgGykwXJzU1xVjDu',
-         'version', 'v.2.0',
-         'url', 'nammayatri.kapturecrm.com'
-        )
-    );
-INSERT INTO atlas_app.merchant_service_config (merchant_id, service_name, config_json)
-VALUES ('da4e23a5-3ce6-4c37-8b9b-41377c3c1a52', 'Ticket_Kapture',
-    json_build_object
-        ('auth', '0.1.0|2|JMM1g9qYbwWtoscHG4qTUql80FhbCz8dMpB0ZMWaUhbSa9RBqO3bUsggftvSo2kCHjJTYgGykwXJzU1xVjDu',
-         'version', 'v.2.0',
-         'url', 'nammayatri.kapturecrm.com'
-        )
-    );
+SELECT m.id, 'Ticket_Kapture',
+  json_build_object(
+    'auth', '0.1.0|2|JMM1g9qYbwWtoscHG4qTUql80FhbCz8dMpB0ZMWaUhbSa9RBqO3bUsggftvSo2kCHjJTYgGykwXJzU1xVjDu',
+    'version', 'v.2.0',
+    'url', 'nammayatri.kapturecrm.com'
+    )
+FROM atlas_app.merchant m;
+
 ALTER TABLE atlas_app.merchant_service_usage_config ADD COLUMN issue_ticket_service character varying(30) DEFAULT 'Kapture' NOT NULL;
 
 ALTER TABLE atlas_app.issue RENAME COLUMN ride_booking_id to booking_id;
