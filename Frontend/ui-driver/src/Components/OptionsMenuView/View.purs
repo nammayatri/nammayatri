ab0092f4db1326e53a388c544a2bfa9ac64828c1
prakhar.saxena <prakhar.saxena@juspay.in>
2023-09-02 18:00:28 +0530

    frontend/fix/#2319 : Added a check on options view.

--- Frontend/ui-driver/src/Components/OptionsMenuView/View.purs
+++ Frontend/ui-driver/src/Components/OptionsMenuView/View.purs
@@ -15,76 +15,83 @@
 
 module Components.OptionsMenu.View where
 
-import Prelude (Unit, bind, const, map, pure, unit, ($), (/=), (<>))
-import PrestoDOM (Gravity(..), Length(..), Margin(..), Orientation(..), Padding(..), PrestoDOM, Screen, Visibility(..), background, color, fontStyle, gravity, height, imageUrl, imageView, linearLayout, margin, orientation, padding, text, textSize, textView, weight, width, onClick, frameLayout, layoutGravity, alpha, scrollView, cornerRadius, onBackPressed, afterRender, id, visibility, imageWithFallback, clickable, relativeLayout, stroke)
+import Common.Types.App
+
+import Animation as Anim
+import Components.OptionsMenu.Controller (Action(..), Config, MenuItemData)
+import Data.Maybe (Maybe(..), fromMaybe, isJust)
+import Data.Show (show)
+import Data.String (length)
+import Debug (spy)
 import Effect (Effect)
+import Effect.Class (liftEffect)
+import Engineering.Helpers.Commons as EHC
+import Font.Size as FontSize
+import Font.Style as FontStyle
+import JBridge as JB
+import Language.Strings (getString)
+import Language.Types (STR(..))
+import Prelude (Unit, bind, const, map, pure, unit, ($), (/=), (<>))
+import PrestoDOM (Gravity(..), Length(..), Margin(..), Orientation(..), Padding(..), PrestoDOM, Screen, Visibility(..), afterRender, alpha, background, clickable, color, cornerRadius, fontStyle, frameLayout, gravity, height, id, imageUrl, imageView, imageWithFallback, layoutGravity, linearLayout, margin, onBackPressed, onClick, orientation, padding, relativeLayout, scrollView, stroke, text, textSize, textView, visibility, weight, width)
+import PrestoDOM.Types.DomAttributes (Corners(..))
 import Screens.Types as ST
 import Styles.Colors as Color
-import Font.Style as FontStyle
-import Font.Size as FontSize
-import Engineering.Helpers.Commons as EHC
-import Animation as Anim
-import Language.Strings (getString)
-import Language.Types(STR(..))
-import JBridge as JB
-import Effect.Class (liftEffect)
-import Common.Types.App
-import Data.Maybe (Maybe(..), fromMaybe, isJust)
-import Debug (spy)
-import Data.String (length)
-import Components.OptionsMenu.Controller (Action(..), Config, MenuItemData)
-import Data.Show (show)
-import PrestoDOM.Types.DomAttributes (Corners(..))
 
 view :: forall w . (Action -> Effect Unit) -> Config -> PrestoDOM (Effect Unit) w
 view push config = 
   linearLayout[
-    background config.backgroundColor
+    width MATCH_PARENT
     , height MATCH_PARENT
-    , width MATCH_PARENT
-    , gravity config.gravity
-    , visibility if config.menuExpanded then VISIBLE else GONE
-    , onClick push $ const BackgroundClick
+    , background Color.transparent
   ][
     linearLayout[
-      background config.menuBackgroundColor
-    , orientation VERTICAL
-    , height WRAP_CONTENT
-    , width $ V config.width
-    , margin $ MarginRight config.marginRight
-    , cornerRadius config.cornerRadius
-    ](map (\item -> 
-              linearLayout[
-                height WRAP_CONTENT
-                , width MATCH_PARENT
-                , gravity CENTER_VERTICAL
-                , orientation VERTICAL
-                , onClick push $ const $ ItemClick item.action
-                , visibility if item.isVisible then VISIBLE else GONE
-              ][
+      background config.backgroundColor
+    , visibility if config.menuExpanded then VISIBLE else GONE
+      , height MATCH_PARENT
+      , width MATCH_PARENT
+      , gravity config.gravity
+      , onClick push $ const BackgroundClick
+    ][
+      linearLayout[
+        background config.menuBackgroundColor
+      , orientation VERTICAL
+      , height WRAP_CONTENT
+      , width $ V config.width
+      , margin $ MarginRight config.marginRight
+      , cornerRadius config.cornerRadius
+      ](map (\item -> 
                 linearLayout[
-                  gravity CENTER_VERTICAL
-                  , padding $ Padding config.itemPadding config.itemPadding config.itemPadding config.itemPadding
-                  , height WRAP_CONTENT
+                  height WRAP_CONTENT
+                  , width MATCH_PARENT
+                  , gravity CENTER_VERTICAL
+                  , orientation VERTICAL
+                  , onClick push $ const $ ItemClick item.action
+                  , visibility if item.isVisible then VISIBLE else GONE
                 ][
-                  imageView [
-                    imageWithFallback item.image
-                    , margin $ MarginRight 8
-                    , visibility if length item.image /= 0 then VISIBLE else GONE
-                    , height $ V 16
-                    , width $ V 16
+                  linearLayout[
+                    gravity CENTER_VERTICAL
+                    , padding $ Padding config.itemPadding config.itemPadding config.itemPadding config.itemPadding
+                    , height WRAP_CONTENT
+                  ][
+                    imageView [
+                      imageWithFallback item.image
+                      , margin $ MarginRight 8
+                      , visibility if length item.image /= 0 then VISIBLE else GONE
+                      , height $ V 16
+                      , width $ V 16
+                    ]
+                    , textView $ [
+                      text item.textdata
+                      , margin $ MarginBottom 1
+                      , color Color.black800
+                    ] <> FontStyle.paragraphText TypoGraphy
                   ]
-                  , textView $ [
-                    text item.textdata
-                    , margin $ MarginBottom 1
-                    , color Color.black800
-                  ] <> FontStyle.paragraphText TypoGraphy
-                ]
-                , linearLayout [
-                    width MATCH_PARENT
-                    , height $ V 2
-                    , background Color.grey700
-                    , margin $ MarginHorizontal 16 16
-                  ][]
-              ]) config.menuItems)
+                  , linearLayout [
+                      width MATCH_PARENT
+                      , height $ V 2
+                      , background Color.grey700
+                      , margin $ MarginHorizontal 16 16
+                    ][]
+                ]) config.menuItems)
+    ]
   ]
