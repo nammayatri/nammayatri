ba5bcab82d854759eeedb9406f554c839b82922e
Shailesh Gahlawat <shailesh.techcourse@gmail.com>
2023-09-06 02:05:31 +0530

    frontend/fix: disable force update banner for nyp

--- Frontend/ui-driver/src/Flow.purs
+++ Frontend/ui-driver/src/Flow.purs
@@ -197,7 +197,7 @@ checkDateAndTime = do
 getLatestAndroidVersion :: Merchant -> Int
 getLatestAndroidVersion merchant =
   case merchant of
-    NAMMAYATRI -> 20000
+    NAMMAYATRI -> 1
     YATRI -> 48
     YATRISATHI -> 16
     MOBILITY_PM -> 1
