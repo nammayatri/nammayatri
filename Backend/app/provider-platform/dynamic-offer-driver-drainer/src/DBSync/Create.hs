a338f2283548b3426e38cf339f20912d1f0772d9
Vijay Gupta <vijayrauniyar1818@gmail.com>
2023-09-11 17:52:27 +0530

    backend/fix/stopping the drainer in case kafka service fails

--- Backend/app/provider-platform/dynamic-offer-driver-drainer/src/DBSync/Create.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-drainer/src/DBSync/Create.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-type-defaults #-}
 {-# OPTIONS_GHC -Wno-unused-local-binds #-}
 
 module DBSync.Create where
@@ -15,6 +16,7 @@ import EulerHS.Types as ET
 import Kafka.Producer as KafkaProd
 import Kafka.Producer as Producer
 import qualified Kernel.Beam.Types as KBT
+import System.Timeout (timeout)
 import Types.DBSync
 import Types.Event as Event
 import Utils.Utils
@@ -127,7 +129,6 @@ runCreateCommands cmds streamKey = do
               Env {..} <- ask
               res <- EL.runIO $ streamDriverDrainerCreates _kafkaConnection dataObjects streamKey'
               either (\_ -> pure [Left entryIds]) (\_ -> pure [Right entryIds]) res
-
     runCreateInKafkaAndDb dbConf streamKey' model object = do
       isPushToKafka' <- EL.runIO isPushToKafka
       if not isPushToKafka'
@@ -170,9 +171,15 @@ runCreateCommands cmds streamKey = do
 streamDriverDrainerCreates :: ToJSON a => Producer.KafkaProducer -> [a] -> Text -> IO (Either Text ())
 streamDriverDrainerCreates producer dbObject streamKey = do
   let topicName = "driver-drainer"
-  result' <- mapM (KafkaProd.produceMessage producer . message topicName) dbObject
-  if any isJust result' then pure $ Left ("Kafka Error: " <> show result') else pure $ Right ()
+  mapM_ (KafkaProd.produceMessage producer . message topicName) dbObject
+  flushResult <- timeout (5 * 60 * 1000000) $ prodPush producer
+  case flushResult of
+    Just _ -> do
+      pure $ Right ()
+    Nothing -> pure $ Left "KafkaProd.flushProducer timed out after 5 minutes"
   where
+    prodPush producer' = KafkaProd.flushProducer producer' >> pure True
+
     message topicName event =
       ProducerRecord
         { prTopic = TopicName topicName,
