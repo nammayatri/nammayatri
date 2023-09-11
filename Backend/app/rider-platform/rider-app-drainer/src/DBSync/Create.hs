6ef555e6dddcd4176846b954e16fc51682fa9a5e
Vijay Gupta <vijayrauniyar1818@gmail.com>
2023-09-11 17:52:27 +0530

    backend/fix/stopping the drainer in case kafka service fails

--- Backend/app/rider-platform/rider-app-drainer/src/DBSync/Create.hs
+++ Backend/app/rider-platform/rider-app-drainer/src/DBSync/Create.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-type-defaults #-}
 {-# OPTIONS_GHC -Wno-unused-local-binds #-}
 
 module DBSync.Create where
@@ -15,6 +16,7 @@ import EulerHS.Types as ET
 import Kafka.Producer as KafkaProd
 import Kafka.Producer as Producer
 import qualified Kernel.Beam.Types as KBT
+import System.Timeout (timeout)
 import Types.DBSync
 import Types.Event as Event
 import Utils.Utils
@@ -129,9 +131,15 @@ runCreateCommands cmds streamKey = do
 streamDriverDrainerCreates :: ToJSON a => Producer.KafkaProducer -> [a] -> Text -> IO (Either Text ())
 streamDriverDrainerCreates producer dbObject streamKey = do
   let topicName = "rider-drainer"
-  result' <- mapM (KafkaProd.produceMessage producer . message topicName) dbObject
-  if any isJust result' then pure $ Left ("Kafka Error: " <> show result') else pure $ Right ()
+  mapM_ (KafkaProd.produceMessage producer . message topicName) dbObject
+  flushResult <- timeout (5 * 60 * 1000000) $ prodPush producer
+  case flushResult of
+    Just _ -> do
+      pure $ Right ()
+    Nothing -> pure $ Left "KafkaProd.flushProducer timed out after 5 minutes"
   where
+    prodPush producer' = KafkaProd.flushProducer producer' >> pure True
+
     message topicName event =
       ProducerRecord
         { prTopic = TopicName topicName,
