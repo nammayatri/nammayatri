6762cae7b33641f67f3e2a7a209a69a174a8804c
S S Mercy Queen <mercy.queen@juspay.in>
2023-09-16 18:05:42 +0530

    frontend/android/nyp release- 1.8.101

--- Frontend/ui-driver/src/Screens/HomeScreen/View.purs
+++ Frontend/ui-driver/src/Screens/HomeScreen/View.purs
@@ -11,7 +11,7 @@
   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of
 
   the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
--}
+-} 
 
 module Screens.HomeScreen.View where
 
