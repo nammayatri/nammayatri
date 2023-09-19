da4991da118d20eb2dc5f50e7a5ef6c3efdeb272
Hemant Mangla <hemant.mangla@juspay.in>
2023-09-20 01:09:06 +0530

    backend/fix: add query param in payment history api for payment mode filter

--- Backend/dev/migrations/dynamic-offer-driver-app/0254-driver-id-in-invoice.sql
+++ Backend/dev/migrations/dynamic-offer-driver-app/0254-driver-id-in-invoice.sql
@@ -1,7 +1,7 @@
-ALTER TABLE atlas_driver_offer_bpp.invoice ADD COLUMN driver_id text;
+ALTER TABLE atlas_driver_offer_bpp.invoice ADD COLUMN driver_id text; -- refill it after release
 
 UPDATE atlas_driver_offer_bpp.invoice as inv
-SET inv.driver_id = df.driver_id
+SET driver_id = df.driver_id
 FROM atlas_driver_offer_bpp.driver_fee as df  --- pls validate this query ---
 WHERE inv.driver_fee_id = df.id;
 
