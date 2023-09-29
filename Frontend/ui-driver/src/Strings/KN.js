2fba6c457ce610617760ee852937323aebbbdd91
prakhar.saxena <prakhar.saxena@juspay.in>
2023-09-29 21:36:40 +0530

    frontend/fix/#2319 : Added video link to go online blocker.

--- Frontend/ui-driver/src/Strings/KN.js
+++ Frontend/ui-driver/src/Strings/KN.js
@@ -887,5 +887,6 @@ const kannadaStrings = {
   CLEARANCE_AND_REGISTERATION : "ತೆರವು + ನೋಂದಣಿ",
   UPI_AUTOPAY_SETUP : "UPI ಸ್ವಯಂ ಪಾವತಿ ಸೆಟಪ್",
   WATCH_VIDEO_FOR_HELP : "ಸಹಾಯಕ್ಕಾಗಿ ವೀಡಿಯೊ ನೋಡಿ",
-  PAYMENT_PENDING_SOFT_NUDGE : "ಮಿತಿಯನ್ನು ಶೀಘ್ರದಲ್ಲೇ ತಲುಪಲಾಗುವುದು.ತಡೆರಹಿತ ಸವಾರಿಗಳನ್ನು ಆನಂದಿಸಲು ಬಾಕಿ ತೆರವುಗೊಳಿಸಿ"
+  PAYMENT_PENDING_SOFT_NUDGE : "ಮಿತಿಯನ್ನು ಶೀಘ್ರದಲ್ಲೇ ತಲುಪಲಾಗುವುದು.ತಡೆರಹಿತ ಸವಾರಿಗಳನ್ನು ಆನಂದಿಸಲು ಬಾಕಿ ತೆರವುಗೊಳಿಸಿ",
+  CLEAR_YOUR_DUES_EARLY : "ನಿಮ್ಮ ಬಾಕಿಗಳನ್ನು ಮುಂಚಿತವಾಗಿ ತೆರವುಗೊಳಿಸಿ"
 }
