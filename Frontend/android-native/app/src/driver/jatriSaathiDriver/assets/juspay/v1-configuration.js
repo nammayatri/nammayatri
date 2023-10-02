599f2e7cd0ebdc96c799cc3aa010c143b794d9ef
S S Mercy Queen <mercy.queen@juspay.in>
2023-10-02 12:43:33 +0530

    frontend/fix/#2467: accessibility Issue fixes

--- Frontend/android-native/app/src/driver/jatriSaathiDriver/assets/juspay/v1-configuration.js
+++ Frontend/android-native/app/src/driver/jatriSaathiDriver/assets/juspay/v1-configuration.js
@@ -47,7 +47,7 @@ window.getMerchantConfig = function () {
       "ABOUT_TEXT": "Yatri Sathi partner is an open platform to connect drivers with riders. The app makes it convenient for drivers to find riders with proposed desired rates. No ride based commission, just pay small amount in the form of monthly subscription",
       "NEED_IT_TO_ENABLE_LOCATION": "Yatri Sathi Driver collect location data to enable share your location to monitor driver current location, even when the app is closed or not in use.",
       "CURRENTLY_WE_ALLOW_ONLY_KARNATAKA_REGISTERED_NUMBER": "Currently,We allow only West Bengal registered number",
-      "YOU_ARE_ABOUT_TO_CALL_NAMMA_YATRI_SUPPORT": "You are about to place a call to the Jatri Sathi Support Team. Do you want to proceed?",
+      "YOU_ARE_ABOUT_TO_CALL_NAMMA_YATRI_SUPPORT": "You are about to place a call to the Yatri Sathi Support Team. Do you want to proceed?",
       "YOUR_LOCATION_HELPS_OUR_SYSTEM": "Your location helps our system to map down all the near by taxis and get you the quickest ride possible.",
       "EARNED_ON_APP" : "Earned on YS",
       "TRAVELLED_ON_APP" : "Travelled On Yatri Sathi",
