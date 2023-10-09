2914fae0d00a79dbef197fa450b727a4ba784a30
Utkarsh Pandey1 <utkarshpandey911@gmail.com>
2023-10-09 20:36:18 +0530

    backend/migrations/station_volunteer_mapping

---
+++ Backend/dev/migrations/dynamic-offer-driver-app/0273-volunteer-station-mapping.sql
@@ -0,0 +1,30 @@
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('753ba278-d760-457e-9c2d-caad2b55b2da','Howrah Station',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('4c996631-3c50-4c7d-b12e-8f666fb1c498','Howrah Station',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('e242e9a8-bbd7-44a3-b41a-033af3c30ef9','Howrah Station',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('c86087b5-c79e-4e69-9e2c-bf762524c435','Howrah Station',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('c8d11fc7-923a-4940-b201-06f425c820cc','Howrah Station',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('8ee620c9-a99d-47ba-97aa-2a300a9986a2','Howrah Station',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('3b6185f4-d76f-4b75-8791-0c27e8d7fddd','Howrah Station',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('af6fefb8-fd41-4a3e-8ced-da0929e26c01','Howrah Station',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('00dbe48f-0b06-4bae-8542-cab118c8c668','Howrah Station',now(),now());
+
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('94322270-6547-4766-aa89-17a6231a02b0','Sealdah Station',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('6c9909ce-2182-4695-8c52-0a18d3025e5d','Sealdah Station',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('beb168db-a824-49f8-a957-2c3a473049b0','Sealdah Station',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('d7401c00-bb36-4f12-94e0-111b46f19941','Sealdah Station',now(),now());
+
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('1e6e1222-2efe-4cb2-8590-2f317814e9ed','Kolkata Station',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('5ddc667a-25f6-4832-b47e-ea5032faa88a','Kolkata Station',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('ddef0be1-d0ea-4d62-930f-43dcf6bd5804','Kolkata Station',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('8755ab3c-f904-4876-bda0-d75516d67efd','Kolkata Station',now(),now());
+
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('289ad0dc-b344-4eec-ac4d-bc35d0ca5070','Santragachi Station',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('4c2a3947-52af-43f4-a63c-90e15de19f9a','Santragachi Station',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('edd5b161-17e8-4f09-8a2e-8fb795112778','Santragachi Station',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('a26c1bce-6982-4143-9e66-46e8c54e5b0d','Santragachi Station',now(),now());
+
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('660088e2-a9bc-400d-a8d4-c04f02a5a741','Airport',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('afc69896-4825-467f-b247-6c9d51830c30','Airport',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('bf712937-7e8f-4842-8e03-09aa9ca3593d','Airport',now(),now());
+INSERT INTO atlas_driver_offer_bpp.volunteer VALUES('b5ae3e17-5fd4-4dce-8c10-dc707b2fb211','Airport',now(),now());
+
