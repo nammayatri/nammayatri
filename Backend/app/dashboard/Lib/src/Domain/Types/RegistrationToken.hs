7f0a6269a3f946c111f786be6281d515db448b90
Arjun Kathuria <arjun.kathuria8@gmail.com>
2023-10-11 15:36:09 +0530

    backend/refactor: Enable `TemplateHaskell` Language Extension in files where necessary
    * Enables `TemplateHaskell` language extension in codebase using
      the LANGUAGE pragma in files that require it.
    
    * The pre-commit hooks machinery seem to require the `TemplateHaskell` extension
      to be in the list of default-enabled extensions in "dynamic-offer-driver-app.cabal"
      file for it to accept this commit for some reason, therefore adding that
      in along here otherwise it doesn't accept commit.
    
    * Explicit TemplateHaskell declarations are less confusing, esp. for new people.

--- Backend/app/dashboard/Lib/src/Domain/Types/RegistrationToken.hs
+++ Backend/app/dashboard/Lib/src/Domain/Types/RegistrationToken.hs
@@ -11,6 +11,7 @@
 
  the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 -}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Domain.Types.RegistrationToken where
 
