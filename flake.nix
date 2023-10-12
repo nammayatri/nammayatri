8c6ac3ca19514fb62444db5c0402bbba26240c97
Arjun Kathuria <arjun.kathuria8@gmail.com>
2023-10-12 17:18:49 +0530

    backend/update: Update nix flake files for GHC 9.2.7
    * Updates flake.nix and flake.lock files for GHC 9.2.7 upgrade
    
    * flake.nix changes:
      1. Use GHC 9.2.7 package set as base packages now from updated common.
      2. Update the "common" flake url to one that builds with & supports
         GHC 9.2.7. It provides new common GHC927 package-set.
      3. Update "shared-kernel" url to one that builds with and supports
         GHC 9.2.7 and related updated package-set.
      4. Update "beckn-gateway" url to one that builds with and supports
         GHC 9.2.7 and related updated package-set.
      5. Adds required amazonka-2 and streamly-083r1 non-flake sources.
      6. nixpkgs now follow the new updated package-set from common.
    
    * flake.lock changes capture the above flake.nix changes in the lock file.

--- flake.nix
+++ flake.nix
@@ -1,12 +1,27 @@
 {
   inputs = {
-    common.url = "github:nammayatri/common";
+    # Note: replace with upstream when GHC 9.2 changes get merged there
+    # common.url = "github:nammayatri/common";
+    common.url = "github:arjunkathuria/common/Mobility-GHC927-rebased-03";
+    nixpkgs.follows = "common/nixpkgs";
+    haskell-flake.follows = "common/haskell-flake";
 
     # Backend inputs
-    shared-kernel.url = "github:nammayatri/shared-kernel";
-    beckn-gateway.url = "github:nammayatri/beckn-gateway";
-    beckn-gateway.inputs.common.follows = "common";
-    beckn-gateway.inputs.shared-kernel.follows = "shared-kernel";
+    # Note: replace with upstream when GHC 9.2 changes get merged there
+    # shared-kernel.url = "github:nammayatri/shared-kernel";
+    shared-kernel.url = "github:arjunkathuria/shared-kernel/Mobility-GHC927-rebased-03";
+    shared-kernel.inputs.nixpkgs.follows = "nixpkgs";
+
+    # beckn-gateway.url = "github:nammayatri/beckn-gateway";
+    beckn-gateway = {
+      url = "github:arjunkathuria/beckn-gateway/Mobility-GHC927-rebased-03";
+      inputs = {
+        common.follows = "common";
+        haskell-flake.follows = "haskell-flake";
+        nixpkgs.follows = "nixpkgs";
+        shared-kernel.follows = "shared-kernel";
+      };
+    };
 
     # We cannot use southern-zone-latest here, because the sha256 will change
     # over time.  NOTE: This file is not permanent, find the available one at
@@ -17,6 +32,15 @@
 
     easy-purescript-nix.url = "github:justinwoo/easy-purescript-nix/a90bd941297497c83205f0a64f30c5188a2a4fda";
     easy-purescript-nix.flake = false;
+
+    # Amazonka 2.0 tagged release
+    # amazonka-2.0 flake seems broken still.
+    amazonka-git.url = "github:brendanhay/amazonka?ref=2.0.0";
+    amazonka-git.flake = false;
+
+    # Streamly version 0.8.3r1 tagged release
+    streamly-083r1-git.url = "github:composewell/streamly?ref=v0.8.3r1";
+    streamly-083r1-git.flake = false;
   };
 
   outputs = inputs:
