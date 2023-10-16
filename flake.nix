ac65a462ddfc667db301626b920ac1a8bf9d948e
shivaraj-bh <sbh69840@gmail.com>
2023-10-16 16:15:01 +0530

    services-flake, process-compose-flake and passetto in flake inputs

--- flake.nix
+++ flake.nix
@@ -1,12 +1,24 @@
 {
   inputs = {
     common.url = "github:nammayatri/common";
+    common.inputs.process-compose-flake.follows = "process-compose-flake";
 
     # Backend inputs
     shared-kernel.url = "github:nammayatri/shared-kernel";
     beckn-gateway.url = "github:nammayatri/beckn-gateway";
     beckn-gateway.inputs.common.follows = "common";
     beckn-gateway.inputs.shared-kernel.follows = "shared-kernel";
+    passetto.url = "github:nammayatri/passetto/nixify";
+    passetto.inputs.nixpkgs.follows = "common/nixpkgs";
+    passetto.inputs.flake-parts.follows = "common/flake-parts";
+    passetto.inputs.haskell-flake.follows = "common/haskell-flake";
+    # TODO: update the commit in upstream and follow it
+    # passetto.inputs.process-compose-flake.follows = "common/process-compose-flake";
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+    passetto.inputs.services-flake.follows = "services-flake";
+    # Question: move this to common?
+    services-flake.url = "github:juspay/services-flake";
+
 
     # We cannot use southern-zone-latest here, because the sha256 will change
     # over time.  NOTE: This file is not permanent, find the available one at
