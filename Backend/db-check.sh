79381bf411c46d0620fd63edc1852eccf5e002b5
hkmangla <hemantmangla78@gmail.com>
2023-10-21 02:40:34 +0530

    backend/enhance: add suggestion for db backward compatible change

--- Backend/db-check.sh
+++ Backend/db-check.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+# Fetch all branches
 git fetch origin
 
 # Using git to get the changed SQL files
 changed_files=$(git diff --name-only origin/$GITHUB_BASE_REF..origin/$GITHUB_HEAD_REF | grep ".sql$")
 
 for file in $changed_files; do
-    if grep -q "DROP TABLE\|DROP COLUMN\|NOT NULL\|ALTER COLUMN\|DROP PRIMARY KEY" "$file"; then
-        echo "Potential backward incompatible DB change detected in $file"
-        exit 1
-    fi
+    # This will print filename:linenumber:detected issue
+    grep -v -- '--' "$file" | grep -n "DROP TABLE\|DROP COLUMN\|ALTER COLUMN\|DROP PRIMARY KEY" | while read -r line; do
+        echo "$file:$line:Potential backward incompatible DB change detected"
+    done
 done
