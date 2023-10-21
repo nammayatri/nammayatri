780c7aabd6589c04840eb74c094bef3820f08bbd
hkmangla <hemantmangla78@gmail.com>
2023-10-21 12:02:05 +0530

    backend/fix: github workflow backward incompatible fix

--- Backend/db-check.sh
+++ Backend/db-check.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Introduce a variable to track if issues are found
+issues_found=0
+
 # Fetch all branches
 git fetch origin
 
@@ -8,7 +11,14 @@ changed_files=$(git diff --name-only origin/$GITHUB_BASE_REF..origin/$GITHUB_HEA
 
 for file in $changed_files; do
     # This will print filename:linenumber:detected issue
+    if grep -v -- '--' "$file" | grep -q "DROP TABLE\|DROP COLUMN\|ALTER COLUMN\|DROP PRIMARY KEY"; then
+        issues_found=1
+    fi
+
     grep -v -- '--' "$file" | grep -n "DROP TABLE\|DROP COLUMN\|ALTER COLUMN\|DROP PRIMARY KEY" | while read -r line; do
         echo "$file:$line:Potential backward incompatible DB change detected"
     done
 done
+
+# Exit based on whether issues were detected
+exit $issues_found
