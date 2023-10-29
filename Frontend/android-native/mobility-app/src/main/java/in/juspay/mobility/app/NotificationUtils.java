c5c49e54613c0e40d1feae4e1cb882169090a7be
Rohit Kumar Dhaker <rohit.dhaker@juspay.in>
2023-10-29 22:06:41 +0530

    Frontend/fix: Using applicationContext while creating overlay view

--- Frontend/android-native/mobility-app/src/main/java/in/juspay/mobility/app/NotificationUtils.java
+++ Frontend/android-native/mobility-app/src/main/java/in/juspay/mobility/app/NotificationUtils.java
@@ -477,7 +477,7 @@ public class NotificationUtils {
     }
 
     private static boolean checkPermission(Context context) {
-        return Settings.canDrawOverlays(context);
+        return Settings.canDrawOverlays(context.getApplicationContext());
     }
 
     public static void createNotificationChannel(Context context, String channel_Id) {
