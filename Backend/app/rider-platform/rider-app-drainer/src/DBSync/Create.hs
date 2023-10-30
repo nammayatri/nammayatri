9e1676d49f808942cd1b04dae541d14c31260ee8
Roman Bodavskiy <roman.bodavskiy@metalamp.io>
2023-10-30 20:19:22 +0300

    backend/feat: #3171 use entryId as timestamp

--- Backend/app/rider-platform/rider-app-drainer/src/DBSync/Create.hs
+++ Backend/app/rider-platform/rider-app-drainer/src/DBSync/Create.hs
@@ -10,6 +10,7 @@ import qualified Data.ByteString.Lazy as LBS
 import Data.Maybe (fromJust)
 import qualified Data.Text as T hiding (elem)
 import qualified Data.Text.Encoding as TE
+import qualified Data.Time.Clock.POSIX as Time
 import EulerHS.CachedSqlDBQuery as CDB
 import EulerHS.Language as EL
 import qualified EulerHS.Language as L
@@ -76,7 +77,7 @@ runCreateCommands cmds streamKey = do
     |::| runCreateInKafkaAndDb dbConf streamKey ("Webengage" :: Text) [(obj, val, entryId, WebengageObject obj) | (CreateDBCommand entryId _ _ _ _ (WebengageObject obj), val) <- cmds]
     |::| runCreateInKafkaAndDb dbConf streamKey ("FeedbackForm" :: Text) [(obj, val, entryId, FeedbackFormObject obj) | (CreateDBCommand entryId _ _ _ _ (FeedbackFormObject obj), val) <- cmds]
     |::| runCreateInKafkaAndDb dbConf streamKey ("HotSpotConfig" :: Text) [(obj, val, entryId, HotSpotConfigObject obj) | (CreateDBCommand entryId _ _ _ _ (HotSpotConfigObject obj), val) <- cmds]
-    |::| runCreateInKafkaWithTopicNameAndDb dbConf streamKey ("BecknRequest" :: Text) [(obj, mkKafkaTableTopicName (BR.timeStamp obj), val, entryId, Kafka.mkKafkaTable @BR.BecknRequestT obj (BR.timeStamp obj)) | (CreateDBCommand entryId _ _ _ _ (BecknRequestObject obj), val) <- cmds] -- put KafkaTable to Kafka, not DBCreateObject
+    |::| runCreateInKafkaWithTopicNameAndDb dbConf streamKey ("BecknRequest" :: Text) [(obj, mkKafkaTableTopicName (mkTimeStamp entryId), val, entryId, Kafka.mkKafkaTable @BR.BecknRequestT obj (mkTimeStamp entryId)) | (CreateDBCommand entryId _ _ _ _ (BecknRequestObject obj), val) <- cmds] -- put KafkaTable to Kafka, not DBCreateObject
     |::| runCreateInKafkaAndDb dbConf streamKey ("Location" :: Text) [(obj, val, entryId, LocationObject obj) | (CreateDBCommand entryId _ _ _ _ (LocationObject obj), val) <- cmds]
     |::| runCreateInKafkaAndDb dbConf streamKey ("LocationMapping" :: Text) [(obj, val, entryId, LocationMappingObject obj) | (CreateDBCommand entryId _ _ _ _ (LocationMappingObject obj), val) <- cmds]
     |::| runCreateInKafkaAndDb dbConf streamKey ("TicketBooking" :: Text) [(obj, val, entryId, TicketBookingObject obj) | (CreateDBCommand entryId _ _ _ _ (TicketBookingObject obj), val) <- cmds]
@@ -173,6 +174,9 @@ streamRiderDrainerCreates producer dbObject streamKey model = do
 
 riderDrainerTopicName :: TopicName
 riderDrainerTopicName = TopicName "rider-drainer"
+
+mkTimeStamp :: EL.KVDBStreamEntryID -> UTCTime
+mkTimeStamp (EL.KVDBStreamEntryID posixTime _) = Time.posixSecondsToUTCTime $ fromInteger (posixTime `div` 1000)
 
 mkKafkaTableTopicName :: UTCTime -> TopicName
 mkKafkaTableTopicName timestamp = do
