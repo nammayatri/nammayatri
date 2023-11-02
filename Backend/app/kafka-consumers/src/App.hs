1939d19d08c9f304ebd124f56cdf6ede5143ad32
Dhruv Sharma <dhruv.sharma@juspay.in>
2023-11-02 12:43:48 +0530

    backup commit

--- Backend/app/kafka-consumers/src/App.hs
+++ Backend/app/kafka-consumers/src/App.hs
@@ -47,7 +47,12 @@ startKafkaConsumers =
         putStrLn ("Running consumers in thread" :: Text)
         let consumerTypes = [BROADCAST_MESSAGE, AVAILABILITY_TIME, PERSON_STATS]
         supervisors <- mapM (\_ -> newSupervisor OneForOne) consumerTypes
-        _ <- mapM_ (\(sup, consumerType) -> forkSupervised sup fibonacciRetryPolicy (runConsumer consumerType)) (zip supervisors consumerTypes)
+        -- _ <- mapM_ (\(sup, consumerType) -> forkSupervised sup fibonacciRetryPolicy (runConsumer consumerType)) (zip supervisors consumerTypes)
+        mapM_
+            (\\ (sup, consumerType)
+               -> forkSupervised
+                    sup fibonacciRetryPolicy (runConsumer consumerType))
+            (zip supervisors consumerTypes)
         _ <- forkIO (go (eventStream (head supervisors)))
         return (head supervisors)
     )
