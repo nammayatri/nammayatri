723e182b04b607140878de4de5cf6c9ed588dc32
Akhilesh Bhadauriya <akhilesh.b@juspay.in>
2023-11-02 14:22:52 +0530

    testing the redis config

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     common.url = "github:nammayatri/common";
 
     # Backend inputs
-    shared-kernel.url = "github:nammayatri/shared-kernel";
+    shared-kernel.url = "github:nammayatri/shared-kernel/Backend/test/redis-config";
     beckn-gateway.url = "github:nammayatri/beckn-gateway";
     beckn-gateway.inputs.common.follows = "common";
     beckn-gateway.inputs.shared-kernel.follows = "shared-kernel";
