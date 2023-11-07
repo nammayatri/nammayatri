c51b9dd1026797ad7e4926e7a9573db30cad2afe
hkmangla <hemantmangla78@gmail.com>
2023-11-07 14:28:57 +0530

    backend/fix: add missing sql files in dashboard

--- Backend/dev/migrations/provider-dashboard/0028-make-server-array-merchant.sql
+++ Backend/dev/migrations/provider-dashboard/0028-make-server-array-merchant.sql
@@ -4,3 +4,5 @@ UPDATE atlas_bpp_dashboard.merchant as T1
 SET server_names = ARRAY[server_name];
 
 ALTER TABLE atlas_bpp_dashboard.merchant ALTER COLUMN server_names SET NOT NULL;
+
+UPDATE atlas_bpp_dashboard.merchant set server_names = '{DRIVER_OFFER_BPP, DRIVER_OFFER_BPP_MANAGEMENT}' where server_name = 'DRIVER_OFFER_BPP';
