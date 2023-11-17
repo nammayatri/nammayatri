24c172d17c0785d1e482610c5fddd2d114ebd0e8
hkmangla <hemantmangla78@gmail.com>
2023-11-17 07:41:45 +0530

    backend/fix: fix zoo booking migrations

--- Backend/dev/migrations/rider-app/1189-zoo-booking-integration.sql
+++ Backend/dev/migrations/rider-app/1189-zoo-booking-integration.sql
@@ -1,37 +1,37 @@
-CREATE TABLE ticket_place (
+CREATE TABLE atlas_app.ticket_place (
     id CHARACTER(36) PRIMARY KEY NOT NULL,
-    merchant_operating_city_id CHARACTER(36) REFERENCES merchant_operating_city(id),
+    merchant_operating_city_id CHARACTER(36),
     name text NOT NULL,
     description text,
     lat double precision,
     lon double precision,
     gallery text[],
-    open_timings TIMESTAMP WITH TIME ZONE,
-    close_timings TIMESTAMP WITH TIME ZONE
+    open_timings time without time zone,
+    close_timings time without time zone
 );
 
-CREATE TABLE ticket_service (
+CREATE TABLE atlas_app.ticket_service (
     id CHARACTER(36) PRIMARY KEY NOT NULL,
-    places_id CHARACTER(36) REFERENCES ticket_place(id),
+    places_id CHARACTER(36),
     service VARCHAR(50) NOT NULL, -- Entrance | Aquarium | Video Photography
-    max_verifications INT NOT NULL DEFAULT 1,
-    open_timings TIMESTAMP WITH TIME ZONE,
-    close_timings TIMESTAMP WITH TIME ZONE
+    max_verification INT NOT NULL DEFAULT 1,
+    open_timings time without time zone,
+    close_timings time without time zone
 );
 
-CREATE TABLE ticket_service_price (
-    ticket_service_id CHARACTER(36) PRIMARY KEY NOT NULL,
-    attendee_type VARCHAR(10) NOT NULL, -- Adult | Kid | CameraUnit
-    price_per_adult NUMERIC(30, 2) NOT NULL,
+CREATE TABLE atlas_app.ticket_service_price (
+    ticket_service_id CHARACTER(36),
+    attendee_type VARCHAR(36) NOT NULL, -- Adult | Kid | CameraUnit
+    price_per_unit NUMERIC(30, 2) NOT NULL,
     PRIMARY KEY (ticket_service_id, attendee_type)
 );
 
 -- User Bookings Table
-CREATE TABLE ticket_booking (
+CREATE TABLE atlas_app.ticket_booking (
     id CHARACTER(36) PRIMARY KEY NOT NULL,
     short_id VARCHAR(36) NOT NULL,
-    merchant_operating_city_id CHARACTER(36) REFERENCES merchant_operating_city(id),
-    ticket_place_id CHARACTER(36) REFERENCES ticket_place(id),
+    merchant_operating_city_id CHARACTER(36),
+    ticket_place_id CHARACTER(36),
     person_id character (36),
     amount NUMERIC (30, 2) NOT NULL,
     visit_date DATE NOT NULL,
@@ -40,25 +40,39 @@ CREATE TABLE ticket_booking (
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE TABLE ticket_booking_service (
+CREATE TABLE atlas_app.ticket_booking_service (
     id CHARACTER(36) PRIMARY KEY NOT NULL,
     short_id VARCHAR(36) NOT NULL,
-    ticket_booking_id CHARACTER(36) REFERENCES ticket_booking(id),
-    ticket_service_id CHARACTER(36) REFERENCES ticket_service(id),
+    ticket_booking_id CHARACTER(36),
+    ticket_service_id CHARACTER(36),
 
     amount NUMERIC (30, 2) NOT NULL,
     status VARCHAR(10) NOT NULL, -- pending | failed | confirmed | verified
     verification_count INT DEFAULT 0,
-    expiry_date TIMESTAMP WITH TIME ZONE DEFAULT,
+    expiry_date TIMESTAMP WITH TIME ZONE,
 
-    merchant_operating_city_id CHARACTER(36) REFERENCES merchant_operating_city(id),
+    merchant_operating_city_id CHARACTER(36),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE TABLE ticket_booking_service_price_breakup (
-    ticket_booking_service_id CHARACTER(36) REFERENCES ticket_booking_service(id),
-    attendee_type VARCHAR(10) NOT NULL,
+CREATE TABLE atlas_app.ticket_booking_service_price_breakup (
+    ticket_booking_service_id CHARACTER(36),
+    attendee_type VARCHAR(36) NOT NULL,
     number_of_units INT NOT NULL,
     price_per_unit NUMERIC(30, 2) NOT NULL
 );
+
+INSERT INTO atlas_app.ticket_place (id, merchant_operating_city_id, name, description, lat, lon, gallery, open_timings, close_timings) VALUES ('1ef78db2-90de-4ed7-a38a-fcbb7ce28135', '7ef7af35-19be-9f28-9330-b346ed992a1d', 'Alipore Zoo',
+    'The Zoological Garden, Alipore is Indias oldest formally stated zoological park (as opposed to royal and British menageries) and a big tourist attraction in Kolkata, West Bengal.',
+    22.537351383029193,88.33227051149596, '{}', '03:30:00', '12:00:00');
+
+INSERT INTO atlas_app.ticket_service (id, places_id, service, max_verification, open_timings, close_timings) VALUES ('b73378dc-427f-4efa-9b55-8efe7e3352c2', '1ef78db2-90de-4ed7-a38a-fcbb7ce28135', 'Entrance Fee', 2, '03:30:00', '12:00:00');
+INSERT INTO atlas_app.ticket_service (id, places_id, service, max_verification, open_timings, close_timings) VALUES ('a7eba6ed-99f7-442f-a9d8-00c8b380657b', '1ef78db2-90de-4ed7-a38a-fcbb7ce28135', 'Aquarium Fee', 2, '05:00:00', '10:50:00');
+INSERT INTO atlas_app.ticket_service (id, places_id, service, max_verification, open_timings, close_timings) VALUES ('d8f47b42-50a5-4a97-8dda-e80a3633d7ab', '1ef78db2-90de-4ed7-a38a-fcbb7ce28135', 'Videography Fee', 2, '03:30:00', '12:00:00');
+
+INSERT INTO atlas_app.ticket_service_price (ticket_service_id, attendee_type, price_per_unit) VALUES ('b73378dc-427f-4efa-9b55-8efe7e3352c2', 'Adult', 50.00);
+INSERT INTO atlas_app.ticket_service_price (ticket_service_id, attendee_type, price_per_unit) VALUES ('b73378dc-427f-4efa-9b55-8efe7e3352c2', 'Child', 20.00);
+INSERT INTO atlas_app.ticket_service_price (ticket_service_id, attendee_type, price_per_unit) VALUES ('a7eba6ed-99f7-442f-a9d8-00c8b380657b', 'Adult', 20.00);
+INSERT INTO atlas_app.ticket_service_price (ticket_service_id, attendee_type, price_per_unit) VALUES ('a7eba6ed-99f7-442f-a9d8-00c8b380657b', 'Child', 10.00);
+INSERT INTO atlas_app.ticket_service_price (ticket_service_id, attendee_type, price_per_unit) VALUES ('d8f47b42-50a5-4a97-8dda-e80a3633d7ab', 'Camera Unit', 250.00);
