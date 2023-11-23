b354ad4bed0b3dba9a972a41e8854e01540f4101
hkmangla <hemantmangla78@gmail.com>
2023-11-23 17:23:02 +0530

    backend/feat: add code generator for Domain types and sql queries

--- Backend/app/alchemist/src/Alchemist/Generator/Haskell/DomainType.hs
+++ Backend/app/alchemist/src/Alchemist/Generator/Haskell/DomainType.hs
@@ -1,1 +1,23 @@
 module Alchemist.Generator.Haskell.DomainType where
+
+import Alchemist.DSL.Syntax.Storage
+import Data.List (intercalate)
+import Kernel.Prelude
+
+-- Generate Haskell module code from TableDef
+generateHaskellModule :: TableDef -> String
+generateHaskellModule tableDef =
+  "module Domain.Types." ++ tableNameHaskell tableDef ++ " where\n\n"
+    ++ intercalate "\n" (map (\i -> "import qualified " ++ i ++ " as " ++ i) (imports tableDef))
+    ++ "\n\ndata "
+    ++ tableNameHaskell tableDef
+    ++ " = "
+    ++ tableNameHaskell tableDef
+    ++ "\n  { "
+    ++ intercalate "\n  , " (map fieldDefToHaskell (fields tableDef))
+    ++ "\n  }\n  deriving (Generic, Show)\n"
+
+-- Convert FieldDef to Haskell field
+fieldDefToHaskell :: FieldDef -> String
+fieldDefToHaskell fieldDef =
+  fieldName fieldDef ++ " :: " ++ haskellType fieldDef
