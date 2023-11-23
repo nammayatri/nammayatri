21ad23d64760bf9d9f5290607d752d28763d9399
hkmangla <hemantmangla78@gmail.com>
2023-11-23 14:48:38 +0530

    backend/feat: add basic code generator for beam table and queries

---
+++ Backend/app/alchemist/src/Alchemist/DSL/Syntax/Storage.hs
@@ -0,0 +1,25 @@
+module Alchemist.DSL.Syntax.Storage where
+
+import Kernel.Prelude
+
+-- Define the data types
+data TableDef = TableDef
+  { tableNameHaskell :: String,
+    tableNameSql :: String,
+    imports :: [String],
+    fields :: [FieldDef],
+    primaryKey :: [String],
+    secondaryKey :: [String]
+  }
+  deriving (Show)
+
+data FieldDef = FieldDef
+  { fieldName :: String,
+    haskellType :: String,
+    sqlType :: String,
+    constraints :: [FieldConstraint]
+  }
+  deriving (Show)
+
+data FieldConstraint = PrimaryKey | SecondaryKey | NotNull | Default String | CustomConstraint String
+  deriving (Show, Eq)
--- Backend/app/alchemist/src/Alchemist/Generator/Haskell/BeamQueries.hs
+++ Backend/app/alchemist/src/Alchemist/Generator/Haskell/BeamQueries.hs
@@ -1,1 +1,59 @@
 module Alchemist.Generator.Haskell.BeamQueries where
+
+import Alchemist.DSL.Syntax.Storage
+import Data.List (intercalate)
+import Kernel.Prelude
+
+-- Determines the conversion function based on the Haskell type
+toTTypeConversionFunction :: String -> String -> String
+toTTypeConversionFunction haskellType fieldName =
+  case haskellType of
+    "Int" -> "roundToIntegral " ++ fieldName -- Example for Int to Float conversion
+    "Id" -> "Id " ++ fieldName -- Example for a custom ID type
+    _ -> fieldName -- Default case with no conversion
+
+fromTTypeConversionFunction :: String -> String -> String
+fromTTypeConversionFunction haskellType fieldName =
+  case haskellType of
+    "Int" -> "realToFrac " ++ fieldName -- Example for Int to Float conversion
+    "Id" -> "getId " ++ fieldName -- Example for a custom ID type
+    _ -> fieldName -- Default case with no conversion
+
+-- Generates the FromTType' instance
+fromTTypeInstance :: TableDef -> String
+fromTTypeInstance tableDef =
+  "instance FromTType' BeamCS." ++ tableNameHaskell tableDef ++ " " ++ tableNameHaskell tableDef ++ " where\n"
+    ++ "  fromTType' BeamCS."
+    ++ tableNameHaskell tableDef
+    ++ "T {..} = do\n"
+    ++ "    pure $\n"
+    ++ "      Just\n"
+    ++ "        "
+    ++ tableNameHaskell tableDef
+    ++ "\n"
+    ++ "          { "
+    ++ intercalate ",\n            " (map fromField (fields tableDef))
+    ++ "\n          }\n\n"
+  where
+    fromField field = fieldName field ++ " = " ++ fromTTypeConversionFunction (haskellType field) (fieldName field)
+
+-- Generates the ToTType' instance
+toTTypeInstance :: TableDef -> String
+toTTypeInstance tableDef =
+  "instance ToTType' BeamCS." ++ tableNameHaskell tableDef ++ " " ++ tableNameHaskell tableDef ++ " where\n"
+    ++ "  toTType' "
+    ++ tableNameHaskell tableDef
+    ++ " {..} = do\n"
+    ++ "    BeamCS."
+    ++ tableNameHaskell tableDef
+    ++ "T\n"
+    ++ "      { "
+    ++ intercalate ",\n        " (map toField (fields tableDef))
+    ++ "\n      }\n\n"
+  where
+    toField field = "BeamCS." ++ fieldName field ++ " = " ++ toTTypeConversionFunction (haskellType field) (fieldName field)
+
+-- Generates both FromTType' and ToTType' instances
+generateTTypeInstances :: TableDef -> String
+generateTTypeInstances tableDef =
+  fromTTypeInstance tableDef ++ toTTypeInstance tableDef
