f0e043d7416d33019f30954dcb950fdd4057f7a3
hkmangla <hemantmangla78@gmail.com>
2023-11-24 19:30:49 +0530

    backend/fix: fix compilation issues in generated code

--- Backend/app/rider-platform/rider-app/Main/spec/API/TicketBooking.hs
+++ Backend/app/rider-platform/rider-app/Main/spec/API/TicketBooking.hs
@@ -10,23 +10,22 @@ dslAPIInput :: String
 dslAPIInput =
   [r|
   Module TicketService
-  GET /ticket/places/{placeId:Id DTB.TicketPlace}/services?*status:Data.Types.BookingStatus&limit:Int&offset:Int
+  GET /ticket/places/{placeId:Id Domain.Types.Tickets.TicketPlace}/services?*status:Domain.Types.Tickets.TicketBooking.BookingStatus&limit:Int&offset:Int
   AUTH TokenAuth
-  Header vt {Domain.Action.UI.Tickets.Vehicle}
-  REQJ {Id Domain.Action.UI.Tickets.Driver}
-  RESPJ {Domain.Action.UI.Tickets.TicketPlace}
+  REQJ {Domain.Types.Tickets.TicketPlace}
+  RESPJ {Domain.Types.Tickets.TicketPlace}
   ---
-  GET /ticket/places1/{placeId:Id DTB.TicketPlace}
+  GET /ticket/places1/{placeId:Id Domain.Types.Tickets.TicketPlace}
   AUTH TokenAuth
-  RESPJ {Domain.Action.UI.Tickets.TicketPlace}
+  RESPJ {Domain.Types.Tickets.TicketPlace}
   ---
-  GET /ticket/places2/{placeId:Id DTB.TicketPlace}
+  GET /ticket/places2/{placeId:Id Domain.Types.Tickets.TicketPlace}
   AUTH TokenAuth
-  RESPJ {Domain.Action.UI.Tickets.TicketPlace}
+  RESPJ {Domain.Types.Tickets.TicketPlace}
   ---
-  GET /ticket/places3/{placeId:Id DTB.TicketPlace}
+  GET /ticket/places3/{placeId:Id Domain.Types.Tickets.TicketPlace}
   AUTH TokenAuth
-  RESPJ {Domain.Action.UI.Tickets.TicketPlace}
+  RESPJ {Domain.Types.Tickets.TicketPlace}
 |]
 
 mkAPI :: String -> IO ()
