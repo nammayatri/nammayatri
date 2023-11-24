be26742b2beea7c13d8ea305c2bae888bb897cb3
Aman Dwivedi <57112545+DwivediAman-exe@users.noreply.github.com>
2023-11-24 17:19:05 +0530

    backend/fix/earnings_query (#4442)

--- Backend/dev/migrations/dynamic-offer-driver-app/0285-dailyStats-table.sql
+++ Backend/dev/migrations/dynamic-offer-driver-app/0285-dailyStats-table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS atlas_driver_offer_bpp.daily_stats (
     id VARCHAR(36) NOT NULL PRIMARY KEY,
     driver_id character(36) NOT NULL,
-    total_earnings INT,
-    num_rides INT,
-    total_distance INT,
+    total_earnings INT NOT NULL,
+    num_rides INT NOT NULL,
+    total_distance INT NOT NULL,
     merchant_local_date Date NOT NULL
 );
 
