3a08198be938604f8cbea5686272c192641f822f
aayushagarwal2001 <aayush2001fzd@gmail.com>
2023-11-28 03:13:20 +0530

    backend/enhancement:merge yatri saathi with subscriptions

---
+++ Backend/dev/migrations/dynamic-offer-driver-app/0309-enable-yatri-saath-in-subscription-flow.sql
@@ -0,0 +1,24 @@
+alter table atlas_dynamic_offer_driver_app.transporter_config add column consider_special_zone_rides_for_plan_charges boolean not null default false;
+alter table atlas_dynamic_offer_driver_app.driver_fee add column special_zone_ride_count int not null default 0;
+alter table atlas_dynamic_offer_driver_app.driver_fee add column special_zone_amount  numeric(30,2) NOT NULL DEFAULT 0.0;
+
+----- enable plan mandatory for yatri saath ----
+------ *** while running in prod pls use the correct merchant id **** ----
+update atlas_dynamic_offer_driver_app.transporter_config
+set is_plan_mandatory = true,
+    driver_fee_calculation_time = 0,
+    consider_special_zone_rides_for_plan_charges = false,
+where merchant_id = '25ade579-fd9c-4288-a015-337af085e66c';
+
+
+----- backfill for feeWithout discount ------
+
+update atlas_dynamic_offer_driver_app.driver_fee
+set fee_without_discount = 3.5 * num_rides
+where plan_offer_title like 'DAILY PER RIDE%'
+
+update atlas_dynamic_offer_driver_app.driver_fee
+set fee_without_discount = 25
+where plan_offer_title like 'DAILY UNLIMITED%'
+
+
