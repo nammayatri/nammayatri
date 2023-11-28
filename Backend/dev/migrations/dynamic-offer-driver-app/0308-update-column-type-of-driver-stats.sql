673b5cee5fae6a844f24b56df9df004e0a81ff13
Prashant Singh <prashantsingh061201@gmail.com>
2023-11-28 15:16:15 +0530

    Backend/fix/driver-stats-type-mismatch (#4479)

---
+++ Backend/dev/migrations/dynamic-offer-driver-app/0308-update-column-type-of-driver-stats.sql
@@ -0,0 +1,3 @@
+ALTER TABLE atlas_driver_offer_bpp.driver_stats ALTER COLUMN total_earnings SET DATA TYPE double precision;
+ALTER TABLE atlas_driver_offer_bpp.driver_stats ALTER COLUMN bonus_earned SET DATA TYPE double precision;
+ALTER TABLE atlas_driver_offer_bpp.driver_stats ALTER COLUMN earnings_missed SET DATA TYPE double precision;
