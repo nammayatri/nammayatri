55393e0c9519d50ab7c8da97f0fe8685a042c8b8
hkmangla <hemantmangla78@gmail.com>
2023-12-05 17:20:41 +0530

    fix

--- Backend/app/alchemist/spec/Main.hs
+++ Backend/app/alchemist/spec/Main.hs
@@ -23,13 +23,14 @@ main = do
   maybeGitRoot <- findGitRoot currentDir
   let rootDir = fromMaybe (error "Could not find git root") maybeGitRoot
   let targetFolder = rootDir </> "Backend/app/rider-platform/rider-app/Main/src-read-only/"
+  let sqlTargetFolder = rootDir </> "Backend/dev/migrations-read-only/rider-app"
   let inputFolder = rootDir </> "Backend/app/alchemist/spec/rider-platform/rider-app/"
   let inputFile = inputFolder </> "Storage/ticket.yaml"
   let apiInputFile = inputFolder </> "API/ticket.yaml"
   Alchemist.mkBeamTable (targetFolder </> "Storage/Beam") inputFile
   Alchemist.mkBeamQueries (targetFolder </> "Storage/Queries") inputFile
   Alchemist.mkDomainType (targetFolder </> "Domain/Types") inputFile
-  Alchemist.mkSQLFile targetFolder inputFile
+  Alchemist.mkSQLFile sqlTargetFolder inputFile
   Alchemist.mkServantAPI (targetFolder </> "API/UI") apiInputFile
   Alchemist.mkDomainHandler (targetFolder </> "Domain/Action/UI") apiInputFile
 
