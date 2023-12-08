1bd206288feb84f8be3b03260d93e09fff7ed011
vaibhav dubey <vaibhav.dubey@vaibhavdubey-YN9HGXR17N.local>
2023-12-08 21:56:57 +0530

    backend/refractor: removed previous version of ticket booking service

--- Backend/app/rider-platform/rider-app/Main/src/Domain/Types/Tickets.hs
+++
@@ -1,5 +0,0 @@
-module Domain.Types.Tickets (module Reexport) where
-
-import Domain.Types.Tickets.TicketBookingService as Reexport
-import Domain.Types.Tickets.TicketPlace as Reexport
-import Domain.Types.Tickets.TicketService as Reexport
---
+++ Backend/dev/migrations/rider-app/1197-modifying-ticket-booking-system-data.sql
@@ -0,0 +1,14 @@
+insert into atlas_app.service_category (allowed_seats, available_seats, description, id, name, people_category) values (null, null, 'all', '7b03af02-bf06-2c52-3aa6-5042f50b9000', 'all', '{ebfef970-5e49-0e40-9529-222354e1783a}');
+update atlas_app.business_hour set category_id = '{7b03af02-bf06-2c52-3aa6-5042f50b9000}' where id = '885be4e6-dd20-f0d4-0f34-63b655d84327';
+
+insert into atlas_app.service_category (allowed_seats, available_seats, description, id, name, people_category) values (null, null, 'all', '7b03af02-bf06-2c52-3aa6-5042f50b9001', 'all', '{ebfef970-5e49-0e40-9529-222354e1783a}');
+update atlas_app.business_hour set category_id = '{7b03af02-bf06-2c52-3aa6-5042f50b9001}' where id = '485be4e6-dd20-f0d4-0f34-63b655d84327';
+
+insert into atlas_app.service_category (allowed_seats, available_seats, description, id, name, people_category) values (null, null, 'all', '7b03af02-bf06-2c52-3aa6-5042f50b9003', 'all', '{ebfef970-5e49-0e40-9529-222354e1783b}');
+update atlas_app.business_hour set category_id = '{7b03af02-bf06-2c52-3aa6-5042f50b9003}' where id = '685be4e6-dd20-f0d4-0f34-63b655d84327';
+
+insert into atlas_app.service_category (allowed_seats, available_seats, description, id, name, people_category) values (150, 150, 'all', '7b03af02-bf06-2c52-3aa6-5042f50b9004', 'all', '{cbfef970-5e49-0e40-9529-222354e1783a}');
+update atlas_app.business_hour set category_id = '{7b03af02-bf06-2c52-3aa6-5042f50b9004}' where id = '285be4e6-dd20-f0d4-0f34-63b655d84327';
+
+insert into atlas_app.service_category (allowed_seats, available_seats, description, id, name, people_category) values (150, 150, 'all', '7b03af02-bf06-2c52-3aa6-5042f50b9005', 'all', '{cbfef970-5e49-0e40-9529-222354e1783a}');
+update atlas_app.business_hour set category_id = '{7b03af02-bf06-2c52-3aa6-5042f50b9005}' where id = '385be4e6-dd20-f0d4-0f34-63b655d84327';
