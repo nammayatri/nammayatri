1dbdcef5125c4354a82d2b864fc0ab178a1a2d7a
Shailesh Gahlawat <shailesh.techcourse@gmail.com>
2023-12-13 13:10:14 +0530

    frontend/refactor: added manual linter action for formatting

--- Frontend/format.sh
+++ Frontend/format.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env sh
-purscriptFiles=$(git diff --cached --name-only --diff-filter=ACM | grep ".purs$" || true;)
-jsFiles=$(git diff --cached --name-only --diff-filter=ACM | grep '.js$' || true;)
+#!/usr/bin/env sh NOTE::- Updating the script to run on all the files in the repo first via bot
+purscriptFiles=purscriptFiles=$(find . -type d -name "src" -exec find {} -type f -name "*.purs" \;) # $(git diff --cached --name-only --diff-filter=ACM | grep ".purs$" || true;)
+jsFiles=purscriptFiles=$(find . -type d -name "src" -exec find {} -type f -name "*.purs" \;) # $(git diff --cached --name-only --diff-filter=ACM | grep '.js$' || true;)
 currentDir=$(pwd)
 # Will Enable the pre-commit hook for purs files later
 for file in $purscriptFiles
