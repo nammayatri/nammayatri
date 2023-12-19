889e6db907035ab23c9aec420f0f175a46df8d7e
Vijay Gupta <vijayrauniyar1818@gmail.com>
2023-12-19 19:34:12 +0530

    backend/drainer:made kafka changes for driver app

--- Backend/app/provider-platform/dynamic-offer-driver-drainer/src/DBSync/Delete.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-drainer/src/DBSync/Delete.hs
@@ -1,6 +1,6 @@
 module DBSync.Delete where
 
-import Config.Env
+-- import Config.Env
 import DBQuery.Functions
 import DBQuery.Types
 import qualified Data.Aeson as A
@@ -15,36 +15,20 @@ import Text.Casing (pascal)
 import "dynamic-offer-driver-app" Tools.Beam.UtilsTH (currentSchemaName)
 import Types.DBSync
 import Types.DBSync.Delete
-import Utils.Utils
+
+-- import Utils.Utils
 
 -- we are not pushing delete events to kafka | Todo : To be removed in new drainer PR
 
 -- | This function is used to run the delete operation for a single entry in the stream
 runDelete :: (EL.KVDBStreamEntryID, ByteString) -> Text -> ReaderT Env EL.Flow (Either EL.KVDBStreamEntryID EL.KVDBStreamEntryID)
-runDelete deleteEntry streamName = do
-  Env {..} <- ask
-  isPushToKafka' <- EL.runIO isPushToKafka
+runDelete deleteEntry _ = do
   let (entryId, streamData) = deleteEntry
   EL.logDebug ("BYTE STRING" :: Text) (show streamData)
   case A.eitherDecode @DBDeleteObject . LBS.fromStrict $ streamData of
     Right deleteDBModel -> do
       EL.logDebug ("DB OBJECT" :: Text) (show deleteDBModel)
-      let tableName = deleteDBModel.dbModel
-      -- uncomment for debug purposes
-      -- writeDebugFile "delete" tableName entryId "streamData.json" streamData
-      -- writeDebugFile "delete" tableName entryId "dbObject.txt" $ show deleteDBModel
-      if shouldPushToDbOnly tableName _dontEnableForKafka || not isPushToKafka'
-        then runDeleteQuery deleteEntry deleteDBModel
-        else do
-          let deleteObject = getDbDeleteDataJson tableName deleteDBModel.whereObj
-          res <- EL.runIO $ createInKafka _kafkaConnection deleteObject streamName tableName
-          case res of
-            Left err -> do
-              EL.logError ("KAFKA DELETE FAILED" :: Text) (err <> " for Object :: " <> show deleteDBModel.contents)
-              return $ Left entryId
-            Right _ -> do
-              EL.logInfo ("KAFKA DELETE SUCCESSFUL" :: Text) (" Delete successful for object :: " <> show deleteDBModel.contents)
-              runDeleteQuery deleteEntry deleteDBModel
+      runDeleteQuery deleteEntry deleteDBModel
     _ -> do
       EL.logError ("DELETE FAILED" :: Text) ("Invalid streamData or Extraction of data from redis stream failed :: " <> TE.decodeUtf8 streamData)
       return $ Left entryId
@@ -53,29 +37,25 @@ runDeleteQuery :: (EL.KVDBStreamEntryID, ByteString) -> DBDeleteObject -> Reader
 runDeleteQuery deleteEntries dbDeleteObject = do
   Env {..} <- ask
   let (entryId, byteString) = deleteEntries
-  let dbModel = dbDeleteObject.dbModel
-  if shouldPushToKafkaOnly dbModel _dontEnableDbTables
-    then return $ Right entryId
-    else do
-      let deleteQuery = getDeleteQueryForTable dbDeleteObject
-      case deleteQuery of
-        Just query -> do
-          EL.logDebug ("QUERY" :: Text) query
-          result <- EL.runIO $ try $ executeQuery _pgConnection (Query $ TE.encodeUtf8 query)
-          case result of
-            Left (QueryError errorMsg) -> do
-              EL.logError ("QUERY DELETE FAILED" :: Text) (errorMsg <> " for query :: " <> query)
-              -- uncomment for debug purposes
-              -- writeDebugFile "delete" dbModel entryId "queryFailed.sql" $ encodeUtf8 query
-              return $ Left entryId
-            Right _ -> do
-              EL.logInfo ("QUERY DELETE SUCCESSFUL" :: Text) (" Delete successful for query :: " <> query <> " with streamData :: " <> TE.decodeUtf8 byteString)
-              -- uncomment for debug purposes
-              -- writeDebugFile "delete" dbModel entryId "querySuccessful.sql" $ encodeUtf8 query
-              return $ Right entryId
-        Nothing -> do
-          EL.logError ("No query generated for streamData: " :: Text) (TE.decodeUtf8 byteString)
+  let deleteQuery = getDeleteQueryForTable dbDeleteObject
+  case deleteQuery of
+    Just query -> do
+      EL.logDebug ("QUERY" :: Text) query
+      result <- EL.runIO $ try $ executeQuery _pgConnection (Query $ TE.encodeUtf8 query)
+      case result of
+        Left (QueryError errorMsg) -> do
+          EL.logError ("QUERY DELETE FAILED" :: Text) (errorMsg <> " for query :: " <> query)
+          -- uncomment for debug purposes
+          -- writeDebugFile "delete" dbModel entryId "queryFailed.sql" $ encodeUtf8 query
           return $ Left entryId
+        Right _ -> do
+          EL.logInfo ("QUERY DELETE SUCCESSFUL" :: Text) (" Delete successful for query :: " <> query <> " with streamData :: " <> TE.decodeUtf8 byteString)
+          -- uncomment for debug purposes
+          -- writeDebugFile "delete" dbModel entryId "querySuccessful.sql" $ encodeUtf8 query
+          return $ Right entryId
+    Nothing -> do
+      EL.logError ("No query generated for streamData: " :: Text) (TE.decodeUtf8 byteString)
+      return $ Left entryId
 
 getDeleteQueryForTable :: DBDeleteObject -> Maybe Text
 getDeleteQueryForTable DBDeleteObject {dbModel, contents, mappings} = do
