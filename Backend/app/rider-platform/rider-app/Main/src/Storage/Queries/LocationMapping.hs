9026e03e72f51c254c53ad680aee4c492cc300e8
JanhaviSoni <janhavi.soni097@gmail.com>
2023-12-21 15:49:14 +0530

    backend/fix : fixed query for version updation in location mapping table

--- Backend/app/rider-platform/rider-app/Main/src/Storage/Queries/LocationMapping.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Storage/Queries/LocationMapping.hs
@@ -59,7 +59,7 @@ getNewVersion oldVersion =
 updateVersion :: MonadFlow m => Text -> Int -> Text -> m ()
 updateVersion entityId order version = do
   now <- getCurrentTime
-  updateOneWithKV
+  updateWithKV
     [ Se.Set BeamLM.version version,
       Se.Set BeamLM.updatedAt now
     ]
