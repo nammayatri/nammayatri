f8b62e3b4b1f873dbb2625cf4a7199c2fee35651
shivaraj-bh <sbh69840@gmail.com>
2023-12-29 00:52:51 +0530

    fmt

--- Backend/nix/services.nix
+++ Backend/nix/services.nix
@@ -113,8 +113,8 @@
             # FIXME: https://github.com/juspay/passetto/issues/2
             package = lib.getBin
               (if pkgs.stdenv.isDarwin
-                then inputs.passetto.packages.x86_64-darwin.passetto-service
-                else inputs'.passetto.packages.passetto-service);
+              then inputs.passetto.packages.x86_64-darwin.passetto-service
+              else inputs'.passetto.packages.passetto-service);
           };
 
           settings.processes.osrm-server = {
