f33f71049f5f2cb8730e93a311e5bb59f26ba03f
Chakradhar <chakradhar.k@juspay.in>
2024-01-05 13:11:44 +0530

    backend/fix: added new fields in internal auth for lts

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/Internal/Auth.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/Internal/Auth.hs
@@ -16,6 +16,7 @@ module Domain.Action.Internal.Auth where
 
 import Data.OpenApi (ToSchema)
 import Domain.Types.Merchant
+import Domain.Types.Merchant.MerchantOperatingCity
 import qualified Domain.Types.Person as DP
 import Environment
 import EulerHS.Prelude
@@ -24,8 +25,10 @@ import Kernel.Types.Id
 import Kernel.Utils.Common
 import Tools.Auth (verifyPerson)
 
-newtype InternalResp = InternalResp
-  { driverId :: Id DP.Person
+data InternalResp = InternalResp
+  { driverId :: Id DP.Person,
+    merchantId :: Id Merchant,
+    merchantOperatingCityId :: Id MerchantOperatingCity
   }
   deriving (Generic, ToJSON, FromJSON, ToSchema, Show)
 
@@ -34,10 +37,12 @@ internalAuth token apiKey merchantId = do
   locationTrackingServiceKey <- asks (.locationTrackingServiceKey)
   unless (apiKey == Just locationTrackingServiceKey) $ do
     throwError $ InvalidRequest "Invalid API key"
-  (driverId, currentMerchantId, _merchantOpCityId) <- verifyPerson (fromMaybe "" token)
+  (driverId, currentMerchantId, merchantOpCityId) <- verifyPerson (fromMaybe "" token)
   unless (currentMerchantId == fromMaybe "" merchantId) $ do
     throwError $ InvalidRequest "Invalid merchant id"
   pure $
     InternalResp
-      { driverId
+      { driverId,
+        merchantId = currentMerchantId,
+        merchantOperatingCityId = merchantOpCityId
       }
