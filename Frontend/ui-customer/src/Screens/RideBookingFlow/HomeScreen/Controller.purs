f02e03d215f36ded9a2cd5747ce3b3950e94e66a
ashish singh <ashish.singh.7424@gmail.com>
2024-01-10 15:45:11 +0530

    frontend/#1365/ride not reallocating

--- Frontend/ui-customer/src/Screens/RideBookingFlow/HomeScreen/Controller.purs
+++ Frontend/ui-customer/src/Screens/RideBookingFlow/HomeScreen/Controller.purs
@@ -744,6 +744,7 @@ data ScreenOutput = LogoutUser
                   | RepeatTrip HomeScreenState Trip
                   | ExitToTicketing HomeScreenState
                   | GoToHelpAndSupport HomeScreenState
+                  | ReAllocateRide HomeScreenState
 
 data Action = NoAction
             | BackPressed
@@ -931,7 +932,8 @@ eval (Scroll item) state = do
 eval ReAllocate state =
   if isLocalStageOn ReAllocated then do
     void $ pure $ setValueToLocalStore LOCAL_STAGE ( show FindingQuotes)
-    exit $ Retry state{ props{ currentStage = FindingQuotes } }
+    let updatedState = state{ props{ currentStage = FindingQuotes } }
+    updateAndExit updatedState $ ReAllocateRide updatedState
   else continue state
 
 eval SearchForSelectedLocation state = do
@@ -1295,6 +1297,7 @@ eval BackPressed state = do
     RideRating ->     do
                       _ <- pure $ updateLocalStage RideCompleted
                       continue state {props {currentStage = RideCompleted}}
+    ReAllocated ->    continue state
     _               -> do
                         if state.props.isLocationTracking then continue state{props{isLocationTracking = false}}
                           else if state.props.cancelSearchCallDriver then continue state{props{cancelSearchCallDriver = false}}
