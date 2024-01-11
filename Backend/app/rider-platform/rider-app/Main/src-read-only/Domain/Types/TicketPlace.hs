a6994cc1d6a86b97c3665598b874269b4a91bb35
hkmangla <hemantmangla78@gmail.com>
2024-01-11 14:45:43 +0530

    backend/namma-dsl: add exclude default fields feature

--- Backend/app/rider-platform/rider-app/Main/src-read-only/Domain/Types/TicketPlace.hs
+++ Backend/app/rider-platform/rider-app/Main/src-read-only/Domain/Types/TicketPlace.hs
@@ -18,13 +18,13 @@ data TicketPlace = TicketPlace
     lat :: Kernel.Prelude.Maybe Kernel.Prelude.Double,
     lon :: Kernel.Prelude.Maybe Kernel.Prelude.Double,
     mapImageUrl :: Kernel.Prelude.Maybe Kernel.Prelude.Text,
+    merchantOperatingCityId :: Kernel.Types.Id.Id Domain.Types.Merchant.MerchantOperatingCity.MerchantOperatingCity,
     name :: Kernel.Prelude.Text,
     openTimings :: Kernel.Prelude.Maybe Kernel.Prelude.TimeOfDay,
     placeType :: Domain.Types.TicketPlace.PlaceType,
     shortDesc :: Kernel.Prelude.Text,
     termsAndConditions :: [Kernel.Prelude.Text],
     merchantId :: Kernel.Prelude.Maybe (Kernel.Types.Id.Id Domain.Types.Merchant.Merchant),
-    merchantOperatingCityId :: Kernel.Prelude.Maybe (Kernel.Types.Id.Id Domain.Types.Merchant.MerchantOperatingCity.MerchantOperatingCity),
     createdAt :: Kernel.Prelude.UTCTime,
     updatedAt :: Kernel.Prelude.UTCTime
   }
