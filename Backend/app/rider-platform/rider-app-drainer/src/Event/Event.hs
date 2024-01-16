fb94b2887e5f38b7d8b545ddb9c2e052d3293f59
Vijay Gupta <vijayrauniyar1818@gmail.com>
2024-01-16 15:21:33 +0530

    backend/drainer: added flush producer and metrics for rider

--- Backend/app/rider-platform/rider-app-drainer/src/Event/Event.hs
+++ Backend/app/rider-platform/rider-app-drainer/src/Event/Event.hs
@@ -22,6 +22,7 @@ mkDBSyncMetric = do
       DrainerStopStatus status -> setGauge (metrics </> #drainer_stop_status) status
       KafkaUpdateMissing -> inc (metrics </> #rider_kafka_update_missing)
       KafkaPushFailure -> inc (metrics </> #rider_kafka_push_failure)
+      ProcessLatency processName latency -> observe (metrics </> #rider_process_latency) latency processName
   where
     collectionDBSyncMetric =
       peek_db_command_error
@@ -34,4 +35,5 @@ mkDBSyncMetric = do
         .> drainer_stop_status
         .> rider_kafka_update_missing
         .> rider_kafka_push_failure
+        .> rider_process_latency
         .> MNil
