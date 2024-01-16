b825c0b14ebdb388eea09244706f8498a1174632
shikhar.gupta@juspay.in <shikhar.gupta@juspay.in>
2024-01-16 20:22:59 +0530

    frontend/fix/clear-goHome-timer

--- Frontend/ui-common/src/Engineering/Helpers/Timers.purs
+++ Frontend/ui-common/src/Engineering/Helpers/Timers.purs
@@ -21,7 +21,7 @@ import Effect.Uncurried
 import Engineering.Helpers.Commons
 
 foreign import waitingCountdownTimerV2Impl :: forall action. EffectFn5 Int String String (action -> Effect Unit) (String -> String -> Int -> action) Unit
-foreign import countDownImpl :: forall action. EffectFn4 Int String (action -> Effect Unit) (Int -> String -> String-> action) Unit
+foreign import countDownImpl :: forall action. EffectFn5 Int String String (action -> Effect Unit) (Int -> String -> String-> action) Unit
 foreign import clearTimerWithId :: String -> Unit
 foreign import startTimerWithTimeV2Impl :: forall action. EffectFn5 String String String (action -> Effect Unit) (Int -> String -> String -> action) Unit
 
@@ -31,11 +31,11 @@ waitingCountdownTimerV2 = runEffectFn5 waitingCountdownTimerV2Impl
 startTimerWithTimeV2 :: forall action. String -> String -> String -> (action -> Effect Unit) -> (Int -> String -> String -> action)  -> Effect Unit
 startTimerWithTimeV2 = runEffectFn5 startTimerWithTimeV2Impl 
 
-countDown :: forall action. Int -> String -> (action -> Effect Unit) -> (Int -> String -> String-> action) -> Effect Unit
-countDown = runEffectFn4 countDownImpl
+countDown :: forall action. Int -> String -> String -> (action -> Effect Unit) -> (Int -> String -> String-> action) -> Effect Unit
+countDown = runEffectFn5 countDownImpl
 
 startTimer :: forall action. Int -> String -> String -> (action -> Effect Unit) -> (Int -> String -> String-> action) -> Effect Unit
 startTimer seconds id interval push action = 
   if os == "IOS" 
     then startTimerWithTimeV2 (show seconds) id interval push action
-    else countDown seconds id push action
+    else countDown seconds id interval push action
