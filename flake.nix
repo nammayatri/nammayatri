8cdc52b4e864eed9684f3c175b3a0908246cd20b
ratnadeep.b <ratnadeep.b@juspay.in>
2024-01-18 01:02:54 +0530

    flake fix 1.

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
       url = "github:piyushKumar-1/haskell_cac_client/Testing";
       inputs = {
         common.follows = "common";
+        nixpkgs.follows = "common/nixpkgs";
       };
     };
 
