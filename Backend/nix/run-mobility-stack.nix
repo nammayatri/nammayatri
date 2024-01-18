cb3a105935db42e2b9501dfd9f9b49329f8daa30
Sridhar Ratnakumar <srid@srid.ca>
2024-01-18 21:01:29 +0530

    shell app

--- Backend/nix/run-mobility-stack.nix
+++ Backend/nix/run-mobility-stack.nix
@@ -20,18 +20,24 @@
                       pcLib.lookupEnv "CABAL_TARGET" (p.environment or null))
                     (lib.attrValues config.settings.processes));
             in
-            ''
-              set -x
-              cd ./Backend  # These processes expect $PWD to be backend, for reading dhall configs
-              rm -f ./*.log # Clean up the log files
+            pkgs.writeShellApplication {
+              name = "run-mobility-stack-init";
+              runtimeInputs = with pkgs; [
+                redis
+              ];
+              text = ''
+                set -x
+                cd ./Backend  # These processes expect $PWD to be backend, for reading dhall configs
+                rm -f ./*.log # Clean up the log files
 
-              ${if config.services.nammayatri.useCabal then ''
-                  cabal build ${builtins.concatStringsSep " " (builtins.trace cabalTargets cabalTargets)}
-                '' else ""}
+                ${if config.services.nammayatri.useCabal then ''
+                    cabal build ${builtins.concatStringsSep " " (builtins.trace cabalTargets cabalTargets)}
+                  '' else ""}
 
-              #${pkgs.redis}/bin/redis-cli -p 30001 -c XGROUP CREATE Available_Jobs myGroup  0 MKSTREAM # TODO: remove this once cluster funtions from euler are fixed
-              #${pkgs.redis}/bin/redis-cli XGROUP CREATE Available_Jobs myGroup 0 MKSTREAM
-            '';
+                #redis-cli -p 30001 -c XGROUP CREATE Available_Jobs myGroup  0 MKSTREAM # TODO: remove this once cluster funtions from euler are fixed
+                #redis-cli XGROUP CREATE Available_Jobs myGroup 0 MKSTREAM
+              '';
+            };
         };
 
         external = {
