8e29a6256bdff5b440b6ad7053b84428f8c106e5
Vijay Gupta <vijayrauniyar1818@gmail.com>
2024-01-18 18:28:17 +0530

    backend/refactor:changed queries into Kv queries

--- Backend/app/dashboard/Lib/src/Storage/Queries/Role.hs
+++ Backend/app/dashboard/Lib/src/Storage/Queries/Role.hs
@@ -20,54 +20,76 @@ import Kernel.Beam.Functions
 import Kernel.Prelude
 import Kernel.Storage.Esqueleto as Esq
 import Kernel.Types.Id
+import Sequelize as Se
+import Storage.Beam.BeamFlow
 import qualified Storage.Beam.Role as BeamR
 import Storage.Tabular.Role
 
-create :: Role -> SqlDB ()
-create = Esq.create
+-- create :: Role -> SqlDB ()
+-- create = Esq.create
 
-findById ::
-  Transactionable m =>
-  Id Role ->
-  m (Maybe Role)
-findById = Esq.findById
+create :: BeamFlow m r => Role -> m ()
+create = createWithKV
 
-findByName ::
-  Transactionable m =>
-  Text ->
-  m (Maybe Role)
-findByName name = findOne $ do
-  role <- from $ table @RoleT
-  where_ $
-    role ^. RoleName ==. val name
-  return role
+-- findById ::
+--   Transactionable m =>
+--   Id Role ->
+--   m (Maybe Role)
+-- findById = Esq.findById
 
-findByDashboardAccessType ::
-  Transactionable m =>
-  Role.DashboardAccessType ->
-  m (Maybe Role)
+findById :: BeamFlow m r => Id Role -> m (Maybe Role)
+findById roleId = findOneWithKV [Se.Is BeamR.id $ Se.Eq $ getId roleId]
+
+-- findByName ::
+--   Transactionable m =>
+--   Text ->
+--   m (Maybe Role)
+-- findByName name = findOne $ do
+--   role <- from $ table @RoleT
+--   where_ $
+--     role ^. RoleName ==. val name
+--   return role
+
+findByName :: BeamFlow m r => Text -> m (Maybe Role)
+findByName name = findOneWithKV [Se.Is BeamR.name $ Se.Eq name]
+
+-- findByDashboardAccessType ::
+--   Transactionable m =>
+--   Role.DashboardAccessType ->
+--   m (Maybe Role)
+-- findByDashboardAccessType dashboardAccessType =
+--   findOne $ do
+--     role <- from $ table @RoleT
+--     where_ $
+--       role ^. RoleDashboardAccessType ==. val dashboardAccessType
+--     return role
+
+findByDashboardAccessType :: BeamFlow m r => Role.DashboardAccessType -> m (Maybe Role)
 findByDashboardAccessType dashboardAccessType =
-  findOne $ do
-    role <- from $ table @RoleT
-    where_ $
-      role ^. RoleDashboardAccessType ==. val dashboardAccessType
-    return role
+  findOneWithKV [Se.Is BeamR.dashboardAccessType $ Se.Eq dashboardAccessType]
 
-findAllByLimitOffset ::
-  Transactionable m =>
-  Maybe Integer ->
-  Maybe Integer ->
-  m [Role]
+-- findAllByLimitOffset ::
+--   Transactionable m =>
+--   Maybe Integer ->
+--   Maybe Integer ->
+--   m [Role]
+-- findAllByLimitOffset mbLimit mbOffset = do
+--   let limitVal = fromIntegral $ fromMaybe 10 mbLimit
+--       offsetVal = fromIntegral $ fromMaybe 0 mbOffset
+--   Esq.findAll $ do
+--     role <- from $ table @RoleT
+--     orderBy [asc $ role ^. RoleName]
+--     limit limitVal
+--     offset offsetVal
+--     pure role
+
+findAllByLimitOffset :: BeamFlow m r => Maybe Integer -> Maybe Integer -> m [Role]
 findAllByLimitOffset mbLimit mbOffset = do
   let limitVal = fromIntegral $ fromMaybe 10 mbLimit
       offsetVal = fromIntegral $ fromMaybe 0 mbOffset
-  Esq.findAll $ do
-    role <- from $ table @RoleT
-    orderBy [asc $ role ^. RoleName]
-    limit limitVal
-    offset offsetVal
-    pure role
+  findAllWithOptionsKV [Se.Is BeamR.id $ Se.Not $ Se.Eq ""] (Se.Desc BeamR.name) (Just limitVal) (Just offsetVal)
 
+-- TODO :: Beam Query for this
 findAllWithLimitOffset ::
   Transactionable m =>
   Maybe Integer ->
