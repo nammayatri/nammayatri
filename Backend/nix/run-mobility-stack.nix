5ae9eaded5b43c9ed63e9fe97425870ea254232f
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2024-01-19 03:32:47 -0500

    backend/nix: refactor, move external to nammayatri service (#5108)
    Co-authored-by: soumyajit <soumyajit.behera@juspay.in>

--- Backend/nix/run-mobility-stack.nix
+++ Backend/nix/run-mobility-stack.nix
@@ -4,44 +4,25 @@
   perSystem = perSystem@{ inputs', self', pkgs, lib, ... }: {
     process-compose =
       let
-        local = { config, ... }: {
+        common = { config, ... }: {
           imports = [
             (import ./services/nammayatri.nix { inherit (perSystem) config; inherit inputs; })
           ];
           apiServer = false;
           services.nammayatri.enable = true;
         };
-
-        external = {
-          settings.processes = {
-            beckn-gateway = {
-              command = perSystem.config.haskellProjects.default.outputs.finalPackages.beckn-gateway;
-              working_dir = "Backend";
-            };
-            mock-registry = {
-              command = perSystem.config.haskellProjects.default.outputs.finalPackages.mock-registry;
-              working_dir = "Backend";
-            };
-            location-tracking-service = {
-              command = inputs'.location-tracking-service.packages.default;
-              working_dir = "Backend";
-            };
-          };
-        };
       in
       {
         run-mobility-stack-nix = {
           imports = [
-            local
-            external
+            common
           ];
           services.nammayatri.useCabal = false;
         };
 
         run-mobility-stack-dev = {
           imports = [
-            local
-            external
+            common
           ];
           services.nammayatri.useCabal = true;
         };
