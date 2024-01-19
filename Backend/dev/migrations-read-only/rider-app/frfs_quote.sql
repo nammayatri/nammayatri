85d6f3df65eb28b2ea0be01e549f41685fdeaee1
Ritika <ritikahanish@gmail.com>
2024-01-19 14:10:34 +0530

    compiled code

--- Backend/dev/migrations-read-only/rider-app/frfs_quote.sql
+++ Backend/dev/migrations-read-only/rider-app/frfs_quote.sql
@@ -135,3 +135,135 @@ ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
 
 ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
 ALTER TABLE atlas_app.frfs_quote DROP COLUMN type;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+ALTER TABLE atlas_app.frfs_quote DROP COLUMN type;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+ALTER TABLE atlas_app.frfs_quote DROP COLUMN type;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+ALTER TABLE atlas_app.frfs_quote DROP COLUMN type;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+ALTER TABLE atlas_app.frfs_quote DROP COLUMN type;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+ALTER TABLE atlas_app.frfs_quote DROP COLUMN type;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+ALTER TABLE atlas_app.frfs_quote DROP COLUMN type;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+ALTER TABLE atlas_app.frfs_quote DROP COLUMN type;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+ALTER TABLE atlas_app.frfs_quote DROP COLUMN type;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+ALTER TABLE atlas_app.frfs_quote DROP COLUMN type;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+ALTER TABLE atlas_app.frfs_quote DROP COLUMN type;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+ALTER TABLE atlas_app.frfs_quote DROP COLUMN type;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+ALTER TABLE atlas_app.frfs_quote DROP COLUMN type;
