ca9eaeacba462d9fd81aca354ef08b5f1070a4de
Ritika <ritikahanish@gmail.com>
2024-01-19 14:10:34 +0530

    compiled code

--- Backend/app/rider-platform/rider-app/Main/src-read-only/API/Types/UI/FRFSTicketService.hs
+++ Backend/app/rider-platform/rider-app/Main/src-read-only/API/Types/UI/FRFSTicketService.hs
@@ -4,11 +4,11 @@
 module API.Types.UI.FRFSTicketService where
 
 import Data.OpenApi (ToSchema)
+import qualified Data.Text
 import qualified Domain.Types.FRFSQuote
 import qualified Domain.Types.FRFSSearch
 import qualified Domain.Types.FRFSTicket
 import qualified Domain.Types.FRFSTicketBooking
-import qualified Domain.Types.FRFSTicketBookingStatus
 import qualified Domain.Types.FRFSTrip
 import qualified Domain.Types.Merchant
 import qualified Domain.Types.Person
@@ -29,28 +29,14 @@ data FRFSBookingPaymentAPI = FRFSBookingPaymentAPI
 data FRFSBookingPaymentStatusAPI = PENDING | SUCCESS | FAILURE | REFUNDED
   deriving (Eq, Show, Generic, ToJSON, FromJSON, ToSchema)
 
-data FRFSBookingStatusAPIRes = FRFSBookingStatusAPIRes
-  { _type :: Domain.Types.FRFSQuote.FRFSQuoteType,
-    bookingId :: Kernel.Types.Id.Id Domain.Types.FRFSTicketBooking.FRFSTicketBooking,
-    payment :: Kernel.Prelude.Maybe API.Types.UI.FRFSTicketService.FRFSBookingPaymentAPI,
-    price :: Kernel.Types.Common.HighPrecMoney,
-    quantity :: Kernel.Prelude.Int,
-    stations :: [API.Types.UI.FRFSTicketService.FRFSStationAPI],
-    status :: Domain.Types.FRFSTicketBookingStatus.FRFSTicketBookingStatus,
-    ticket :: [API.Types.UI.FRFSTicketService.FRFSTicketAPI],
-    validTill :: Kernel.Prelude.UTCTime,
-    vehicleType :: Kernel.Prelude.Text
-  }
-  deriving (Generic, ToJSON, FromJSON, ToSchema)
-
-data FRFSQuoteAPI = FRFSQuoteAPI
+data FRFSQuoteAPIRes = FRFSQuoteAPIRes
   { _type :: Domain.Types.FRFSQuote.FRFSQuoteType,
     price :: Kernel.Types.Common.HighPrecMoney,
     quantity :: Kernel.Prelude.Int,
     quoteId :: Kernel.Types.Id.Id Domain.Types.FRFSQuote.FRFSQuote,
     stations :: [API.Types.UI.FRFSTicketService.FRFSStationAPI],
     validTill :: Kernel.Prelude.UTCTime,
-    vehicleType :: Kernel.Prelude.Text
+    vehicleType :: Data.Text.Text
   }
   deriving (Generic, ToJSON, FromJSON, ToSchema)
 
@@ -67,20 +53,34 @@ data FRFSSearchAPIRes = FRFSSearchAPIRes
   deriving (Generic, ToJSON, FromJSON, ToSchema)
 
 data FRFSStationAPI = FRFSStationAPI
-  { address :: Kernel.Prelude.Maybe Kernel.Prelude.Text,
-    code :: Kernel.Prelude.Text,
-    color :: Kernel.Prelude.Maybe Kernel.Prelude.Text,
+  { address :: Kernel.Prelude.Maybe Data.Text.Text,
+    code :: Data.Text.Text,
+    color :: Kernel.Prelude.Maybe Data.Text.Text,
     lat :: Kernel.Prelude.Double,
     lon :: Kernel.Prelude.Double,
-    name :: Kernel.Prelude.Text,
+    name :: Data.Text.Text,
     stationType :: Kernel.Prelude.Maybe Domain.Types.FRFSTrip.StationType
   }
   deriving (Generic, ToJSON, FromJSON, ToSchema)
 
 data FRFSTicketAPI = FRFSTicketAPI
-  { qrData :: Kernel.Prelude.Text,
+  { qrData :: Data.Text.Text,
     status :: Domain.Types.FRFSTicket.FRFSTicketStatus,
-    ticketNumber :: Kernel.Prelude.Text,
+    ticketNumber :: Data.Text.Text,
     validTill :: Kernel.Prelude.UTCTime
+  }
+  deriving (Generic, ToJSON, FromJSON, ToSchema)
+
+data FRFSTicketBookingStatusAPIRes = FRFSTicketBookingStatusAPIRes
+  { _type :: Domain.Types.FRFSQuote.FRFSQuoteType,
+    bookingId :: Kernel.Types.Id.Id Domain.Types.FRFSTicketBooking.FRFSTicketBooking,
+    payment :: Kernel.Prelude.Maybe API.Types.UI.FRFSTicketService.FRFSBookingPaymentAPI,
+    price :: Kernel.Types.Common.HighPrecMoney,
+    quantity :: Kernel.Prelude.Int,
+    stations :: [API.Types.UI.FRFSTicketService.FRFSStationAPI],
+    status :: Domain.Types.FRFSTicketBooking.FRFSTicketBookingStatus,
+    ticket :: [API.Types.UI.FRFSTicketService.FRFSTicketAPI],
+    validTill :: Kernel.Prelude.UTCTime,
+    vehicleType :: Data.Text.Text
   }
   deriving (Generic, ToJSON, FromJSON, ToSchema)
