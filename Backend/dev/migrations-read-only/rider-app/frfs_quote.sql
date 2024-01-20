c2e7b5045bd4e1443c39bffaabffe8e1796d01d2
Ritika <ritikahanish@gmail.com>
2024-01-20 18:27:48 +0530

    changes

--- Backend/dev/migrations-read-only/rider-app/frfs_quote.sql
+++ Backend/dev/migrations-read-only/rider-app/frfs_quote.sql
@@ -44,3 +44,9 @@ ALTER TABLE atlas_app.frfs_quote DROP COLUMN type;
 ------- SQL updates -------
 
 ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.frfs_quote ADD COLUMN type text NOT NULL;
+ALTER TABLE atlas_app.frfs_quote DROP COLUMN type;
