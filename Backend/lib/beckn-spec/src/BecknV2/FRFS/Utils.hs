d48ada7b566a812818e9ede78aa04cc1d1e2479d
Nikith Shetty <shettynikith3@gmail.com>
2024-01-20 16:22:45 +0530

    added onInit transformer

--- Backend/lib/beckn-spec/src/BecknV2/FRFS/Utils.hs
+++ Backend/lib/beckn-spec/src/BecknV2/FRFS/Utils.hs
@@ -1,7 +1,9 @@
 module BecknV2.FRFS.Utils where
 
 import qualified BecknV2.FRFS.Types as Spec
+import qualified Data.Text as T
 import Kernel.Prelude
+import Kernel.Types.Common
 
 tfDescriptor :: Maybe Text -> Maybe Text -> Maybe Spec.Descriptor
 tfDescriptor mCode mName = do
@@ -13,3 +15,8 @@ tfDescriptor mCode mName = do
         descriptorImages = Nothing,
         descriptorName = Just $ name
       }
+
+parseMoney :: Spec.Price -> Maybe HighPrecMoney
+parseMoney price =
+  price.priceValue >>= (readMaybe . T.unpack)
+    >>= return . HighPrecMoney
