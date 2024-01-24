872e65338a30cdcdb65a3c4c48045ba1821f5edc
Nikith Shetty <shettynikith3@gmail.com>
2024-01-24 15:05:40 +0530

    revert frfs registry and gatewayurl in Merchant table

--- Backend/app/rider-platform/rider-app/Main/src/Storage/Beam/Merchant.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Storage/Beam/Merchant.hs
@@ -51,8 +51,6 @@ data MerchantT f = MerchantT
     destinationRestriction :: B.C f GeoRestriction,
     gatewayUrl :: B.C f Text,
     registryUrl :: B.C f Text,
-    frfsGatewayUrl :: B.C f Text,
-    frfsRegistryUrl :: B.C f Text,
     driverOfferBaseUrl :: B.C f Text,
     driverOfferApiKey :: B.C f Text,
     driverOfferMerchantId :: B.C f Text,
