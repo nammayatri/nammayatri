73ec5687e0d44b0deb4b40f9c52edd49f4f3d76a
Roman Bodavskiy <roman.bodavskiy@metalamp.io>
2024-02-06 12:11:54 +0300

    backend/clickhouse: fixed testing issues

--- Backend/dev/migrations/dynamic-offer-driver-app/0347-whitelisting-and-blacklisting.sql
+++ Backend/dev/migrations/dynamic-offer-driver-app/0347-whitelisting-and-blacklisting.sql
@@ -14,5 +14,7 @@ CREATE TABLE atlas_driver_offer_bpp.black_list_org (
 
 insert into atlas_driver_offer_bpp.white_list_org (id,subscriber_id,type) values ('45a16bb9-69e1-4dc6-a751-65b82a84dbc9', 'localhost/beckn/cab/v1/da4e23a5-3ce6-4c37-8b9b-41377c3c1a52', 'APP');
 
+insert into atlas_driver_offer_bpp.white_list_org (id,subscriber_id,type) values ('45a16bb9-69e1-4dc6-a751-65b82a84dbc8', 'localhost/beckn/cab/v1/da4e23a5-3ce6-4c37-8b9b-41377c3c1a51', 'APP');
+
 insert into atlas_driver_offer_bpp.white_list_org (id,subscriber_id,type) values ('45a16bb9-69e1-4dc6-a751-65b82a84dbc0', 'JUSPAY.BG.1', 'APP');
 
