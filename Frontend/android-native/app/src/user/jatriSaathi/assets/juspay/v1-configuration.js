37fd3b80343008b50da19ccfb51530124cd430d7
Aryan Chaudhary <aryanchaudhary1381@gmail.com>
2024-02-07 16:10:02 +0530

    frontend/fix: Updated assets and default language for contests page

--- Frontend/android-native/app/src/user/jatriSaathi/assets/juspay/v1-configuration.js
+++ Frontend/android-native/app/src/user/jatriSaathi/assets/juspay/v1-configuration.js
@@ -278,6 +278,7 @@ window.getMerchantConfig = function () {
       , "showNearByDrivers": true
       , "enableBookingPreference" : false
       , "showInfoIcon" : false
+      , "genericLoaderLottie" : "ic_vehicle_processing,https://assets.juspay.in//beckn/jatrisaathi/user/lottie/ic_vehicle_processing.json"
       , "variantInfo" : {
           "hatchback" : {
             "name" : "AC Cab",
