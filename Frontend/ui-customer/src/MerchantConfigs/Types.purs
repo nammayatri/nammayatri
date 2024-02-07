f4f80ab7305c0dc4058d4f093dd6841c5d6999bc
Aryan Chaudhary <aryanchaudhary1381@gmail.com>
2024-02-07 05:30:26 +0530

    frontend/fix: Updated configs and default language for contests

--- Frontend/ui-customer/src/MerchantConfigs/Types.purs
+++ Frontend/ui-customer/src/MerchantConfigs/Types.purs
@@ -211,7 +211,8 @@ type EstimateAndQuoteConfig = {
   enableBookingPreference :: Boolean, 
   textColor :: String,
   showInfoIcon :: Boolean,
-  variantInfo :: VariantConfig
+  variantInfo :: VariantConfig,
+  genericLoaderLottie :: String
 }
 
 type CustomerTip = {
