474f5e89171b42534fb4473a3ac604c9979a4fa1
Akhilesh Bhadauriya <akhilesh.b@juspay.in>
2024-02-08 15:25:06 +0530

    fixup

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/Dashboard/Ride.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/Dashboard/Ride.hs
@@ -512,5 +512,5 @@ bookingWithVehicleNumberAndPhone merchant merchantOpCityId req = do
 endActiveRide :: Id DRide.Ride -> Id DM.Merchant -> Id DMOC.MerchantOperatingCity -> Flow ()
 endActiveRide rideId merchantId merchantOperatingCityId = do
   let dashboardReq = EHandler.DashboardEndRideReq {point = Nothing, merchantId, merchantOperatingCityId, odometer = Nothing}
-  shandle <- EHandler.buildEndRideHandle merchantId merchantOperatingCityId
+  shandle <- EHandler.buildEndRideHandle merchantId merchantOperatingCityId Nothing
   void $ EHandler.dashboardEndRide shandle rideId dashboardReq
