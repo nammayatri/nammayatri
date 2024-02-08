70e777b7d7cc32a0d6bf82bac9b14f3d85b7ad8a
Nikith Shetty <shettynikith3@gmail.com>
2024-02-08 17:15:30 +0530

    spec-migration: added valueadded np list

---
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/Domain/Types/ValueAddNP.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+
+module Domain.Types.ValueAddNP where
+
+import Kernel.Prelude
+import qualified Tools.Beam.UtilsTH
+
+data ValueAddNP = ValueAddNP
+  { enabled :: Kernel.Prelude.Bool,
+    subscriberId :: Kernel.Prelude.Text,
+    createdAt :: Kernel.Prelude.UTCTime,
+    updatedAt :: Kernel.Prelude.UTCTime
+  }
+  deriving (Generic, Show, ToJSON, FromJSON, ToSchema)
---
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Storage/CachedQueries/ValueAddNP.hs
@@ -0,0 +1,46 @@
+{-
+ Copyright 2022-23, Juspay India Pvt Ltd
+
+ This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License
+
+ as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program
+
+ is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+
+ or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of
+
+ the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+-}
+{-# OPTIONS_GHC -Wno-deprecations #-}
+
+module Storage.CachedQueries.ValueAddNP
+  ( isValueAddNP,
+  )
+where
+
+import Domain.Types.ValueAddNP
+import Kernel.Prelude
+import qualified Kernel.Storage.Hedis as Hedis
+import Kernel.Utils.Common
+import qualified Storage.Queries.ValueAddNP as Queries
+
+isValueAddNP :: (CacheFlow m r, EsqDBFlow m r, MonadFlow m) => Text -> m Bool
+isValueAddNP subscriberId =
+  Hedis.safeGet lookupKey >>= \case
+    Just npList -> return $ checkIfValueAddNP subscriberId npList
+    Nothing -> do
+      npList <- Queries.findAll True
+      cacheValueAddNPList npList
+      return $ checkIfValueAddNP subscriberId npList
+
+checkIfValueAddNP :: Text -> [ValueAddNP] -> Bool
+checkIfValueAddNP subscriberId npList = subscriberId `elem` (map (.subscriberId) npList)
+
+lookupKey :: Text
+lookupKey = "CachedQueries:ValueAddNP"
+
+cacheValueAddNPList :: (CacheFlow m r) => [ValueAddNP] -> m ()
+cacheValueAddNPList npList = do
+  expTime <- fromIntegral <$> asks (.cacheConfig.configsExpTime)
+  let ids = map (.subscriberId) npList
+  Hedis.setExp lookupKey ids expTime
