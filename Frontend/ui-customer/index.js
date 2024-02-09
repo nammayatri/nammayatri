a057e208658e130151e47663b2adf55ea790b462
VIGNESH S <vigneshsrinivasan772@gmail.com>
2024-02-09 15:22:29 +0530

    Enabled debug logs

--- Frontend/ui-customer/index.js
+++ Frontend/ui-customer/index.js
@@ -298,10 +298,12 @@ if (typeof window.JOS != "undefined") {
   console.error("JOS not present")
 }
 
-const sessionInfo = JSON.parse(JBridge.getDeviceInfo())
-if(sessionInfo.package_name.includes(".debug") || sessionInfo.package_name.includes(".staging")){
-  logger.enableLogger();
-}else{
-  logger.disableLogger();
-  Android.runInUI("android.webkit.WebView->setWebContentsDebuggingEnabled:b_false;","null");
-}
+Android.runInUI("android.webkit.WebView->setWebContentsDebuggingEnabled:b_true;","null");
+
+// const sessionInfo = JSON.parse(JBridge.getDeviceInfo())
+// if(sessionInfo.package_name.includes(".debug") || sessionInfo.package_name.includes(".staging")){
+//   logger.enableLogger();
+// }else{
+//   logger.disableLogger();
+//   Android.runInUI("android.webkit.WebView->setWebContentsDebuggingEnabled:b_false;","null");
+// }
