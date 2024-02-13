c8078a788a59b93070a7d3abf220bcdbd3496ecb
soumyajit <soumyajit.behera@juspay.in>
2024-02-13 18:01:43 +0530

    refactor/spec-migration-v2: removed v1 search/onsearch

--- Backend/app/rider-platform/rider-app/Main/src/API/Beckn/OnSearch.hs
+++ Backend/app/rider-platform/rider-app/Main/src/API/Beckn/OnSearch.hs
@@ -18,12 +18,9 @@ import qualified Beckn.ACL.OnSearch as TaxiACL
 import qualified Beckn.OnDemand.Utils.Common as Utils
 import Beckn.Types.Core.Taxi.API.OnSearch as OnSearch
 import qualified BecknV2.OnDemand.Utils.Common as Utils
-import Data.Aeson as A
 import Data.Text as T
-import Data.Text.Encoding as T
 import qualified Domain.Action.Beckn.OnSearch as DOnSearch
 import Environment
-import EulerHS.Prelude (ByteString)
 import Kernel.Prelude
 import qualified Kernel.Storage.Hedis as Redis
 import qualified Kernel.Types.Beckn.Domain as Domain
@@ -31,11 +28,10 @@ import Kernel.Utils.Common
 import Kernel.Utils.Servant.SignatureAuth
 import Servant hiding (throwError)
 import Storage.Beam.SystemConfigs ()
-import Tools.Error (GenericError (InvalidRequest))
 
 type API =
   SignatureAuth 'Domain.MOBILITY "X-Gateway-Authorization"
-    :> OnSearch.OnSearchAPI
+    :> OnSearch.OnSearchAPIV2
 
 handler :: SignatureAuthResult -> FlowServer API
 handler = onSearch
@@ -43,27 +39,16 @@ handler = onSearch
 onSearch ::
   SignatureAuthResult ->
   SignatureAuthResult ->
-  -- OnSearch.OnSearchReqV2 ->
-  ByteString ->
+  OnSearch.OnSearchReqV2 ->
   FlowHandler AckResponse
-onSearch _ _ reqBS = withFlowHandlerBecknAPI do
-  req <- decodeReq reqBS
-
-  (mbDOnSearchReq, messageId, txnId) <- case req of
-    Right reqV2 -> do
-      transactionId <- Utils.getTransactionId reqV2.onSearchReqContext
-      mbDOnSearchReq <- Utils.withTransactionIdLogTag transactionId $ TaxiACL.buildOnSearchReqV2 reqV2
-      messageId <- Utils.getMessageIdText reqV2.onSearchReqContext
-      txnId <- Utils.getTransactionId reqV2.onSearchReqContext
-      pure (mbDOnSearchReq, messageId, txnId)
-    Left reqV1 -> do
-      mbDOnSearchReq <- withTransactionIdLogTag reqV1 $ TaxiACL.buildOnSearchReq reqV1
-      let messageId = reqV1.context.message_id
-      txnId <- reqV1.context.transaction_id & fromMaybeM (InvalidRequest "Transaction Id not found in OnSearch request context.")
-      pure (mbDOnSearchReq, messageId, txnId)
+onSearch _ _ reqV2 = withFlowHandlerBecknAPI do
+  transactionId <- Utils.getTransactionId reqV2.onSearchReqContext
+  mbDOnSearchReq <- Utils.withTransactionIdLogTag transactionId $ TaxiACL.buildOnSearchReqV2 reqV2
+  messageId <- Utils.getMessageIdText reqV2.onSearchReqContext
+  txnId <- Utils.getTransactionId reqV2.onSearchReqContext
 
   Utils.withTransactionIdLogTag txnId $ do
-    logInfo $ "OnSearch received:-" <> T.decodeUtf8 reqBS
+    logInfo $ "OnSearch received:-" <> show reqV2
 
     whenJust mbDOnSearchReq $ \request -> do
       Redis.whenWithLockRedis (onSearchLockKey messageId) 60 $ do
@@ -78,12 +63,3 @@ onSearchLockKey id = "Customer:OnSearch:MessageId-" <> id
 
 onSearchProcessingLockKey :: Text -> Text
 onSearchProcessingLockKey id = "Customer:OnSearch:Processing:MessageId-" <> id
-
-decodeReq :: MonadFlow m => ByteString -> m (Either OnSearch.OnSearchReq OnSearch.OnSearchReqV2)
-decodeReq reqBS =
-  case A.eitherDecodeStrict reqBS of
-    Right reqV1 -> pure $ Left reqV1
-    Left _ ->
-      case A.eitherDecodeStrict reqBS of
-        Right reqV2 -> pure $ Right reqV2
-        Left err -> throwError . InvalidRequest $ "Unable to parse request: " <> T.pack err <> T.decodeUtf8 reqBS
