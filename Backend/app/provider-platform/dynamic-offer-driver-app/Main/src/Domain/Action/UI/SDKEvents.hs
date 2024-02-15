d67a96de7fadf6060d19c5fda7a544993dba7cc1
khuzema786 <khomosikhuzema894@gmail.com>
2024-02-15 13:04:39 +0530

    backend: SDK Pipeline

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/SDKEvents.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/SDKEvents.hs
@@ -1,29 +1,24 @@
-{-
- Copyright 2022-23, Juspay India Pvt Ltd
-
- This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License
-
- as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program
-
- is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-
- or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of
-
- the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
--}
+{-# OPTIONS_GHC -Wno-orphans #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
 
 module Domain.Action.UI.SDKEvents where
 
-import qualified Domain.Types.Merchant as Merchant
+import API.Types.UI.SDKEvents
+import Data.OpenApi (ToSchema)
+import qualified Domain.Types.Merchant as DM
 import qualified Domain.Types.Merchant.MerchantOperatingCity as DMOC
 import qualified Domain.Types.Person as DP
 import Environment
+import qualified Environment
+import EulerHS.Prelude hiding (id)
 import Kernel.Prelude
-import qualified Kernel.Types.APISuccess as APISuccess
+import Kernel.Types.APISuccess
 import Kernel.Types.Id
+import Servant
+import Tools.Auth
 import Tools.Event
 
-sdkEvents :: (Id DP.Person, Id Merchant.Merchant, Id DMOC.MerchantOperatingCity) -> Text -> Flow APISuccess.APISuccess
-sdkEvents (personId, merchantId, merchantOperatingCityId) req = do
-  triggerSDKEvent $ SDKEventData personId merchantId merchantOperatingCityId req
-  return APISuccess.Success
+postSdkEvents :: (Maybe (Id DP.Person), Id DM.Merchant, Id DMOC.MerchantOperatingCity) -> SDKEventsReq -> Flow APISuccess
+postSdkEvents (personId, merchantId, merchantOperatingCityId) SDKEventsReq {..} = do
+  triggerSDKEvent $ SDKEventData personId merchantId merchantOperatingCityId event
+  return Success
