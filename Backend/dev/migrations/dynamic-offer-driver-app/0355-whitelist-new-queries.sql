0baa5a0d59c9128bb26771aa299dde52f7f3c855
Jaypal Mudaliyar <77069644+jaypalmudaliyar24@users.noreply.github.com>
2024-02-16 15:03:44 +0530

    backend/fix: white-listing & black-listing queries

--- Backend/dev/migrations/dynamic-offer-driver-app/0355-whitelist-new-queries.sql
+++ Backend/dev/migrations/dynamic-offer-driver-app/0355-whitelist-new-queries.sql
@@ -1,12 +1,21 @@
 ALTER TABLE atlas_driver_offer_bpp.white_list_org
 ADD COLUMN domain character varying(255) default NULL;
 
-update atlas_driver_offer_bpp.white_list_org set type = 'BPP' where type = 'APP';
+INSERT INTO atlas_driver_offer_bpp.white_list_org (id, subscriber_id, type, domain)
+SELECT
+    (md5(random()::TEXT || clock_timestamp()::TEXT || random()::TEXT || random()::TEXT || random()::TEXT || random()::TEXT)::UUID) AS id,
+    subscriber_id,
+    'BAP' AS type,
+    'MOBILITY' AS domain
+FROM atlas_driver_offer_bpp.white_list_org
+WHERE type = 'APP';
 
-update atlas_driver_offer_bpp.white_list_org set domain = 'MOBILITY' where type = 'BPP';
+UPDATE atlas_driver_offer_bpp.white_list_org
+SET domain = 'MOBILITY_OLD'
+WHERE type = 'APP';
 
-
-insert into atlas_driver_offer_bpp.white_list_org (id,subscriber_id,type,domain) values ('45a16bb9-69e1-4dc6-a751-65b82a84dbd9', 'localhost/beckn/cab/v1/da4e23a5-3ce6-4c37-8b9b-41377c3c1a52', 'BPP', 'MOBILITY');
+ALTER TABLE atlas_driver_offer_bpp.white_list_org
+ALTER COLUMN domain SET NOT NULL;
 
 ALTER TABLE atlas_driver_offer_bpp.black_list_org
-ADD COLUMN domain character varying(255) default NULL;
+ADD COLUMN domain character varying(255) NOT NULL;
