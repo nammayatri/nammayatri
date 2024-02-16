58db3d6e6ed6d958a7ff1f0e7c882149a5c13972
Jaypal Mudaliyar <77069644+jaypalmudaliyar24@users.noreply.github.com>
2024-02-16 15:03:44 +0530

    backend/fix: white-listing & black-listing queries

--- Backend/dev/migrations/rider-app/1223-whitelist-new-queries.sql
+++ Backend/dev/migrations/rider-app/1223-whitelist-new-queries.sql
@@ -1,9 +1,21 @@
 ALTER TABLE atlas_app.white_list_org
 ADD COLUMN domain character varying(255) default NULL;
 
-update atlas_app.white_list_org set type = 'BAP' where type = 'APP';
+INSERT INTO atlas_app.white_list_org (id, subscriber_id, type, domain)
+SELECT
+    (md5(random()::TEXT || clock_timestamp()::TEXT || random()::TEXT || random()::TEXT || random()::TEXT || random()::TEXT)::UUID) AS id,
+    subscriber_id,
+    'BPP' AS type,
+    'MOBILITY' AS domain
+FROM atlas_app.white_list_org
+WHERE type = 'PROVIDER';
 
-update atlas_app.white_list_org set domain = 'MOBILITY' where type = 'BAP';
+UPDATE atlas_driver_offer_bpp.white_list_org
+SET domain = 'MOBILITY_OLD'
+WHERE type = 'PROVIDER';
+
+ALTER TABLE atlas_app.white_list_org
+ALTER COLUMN domain SET NOT NULL;
 
 ALTER TABLE atlas_app.black_list_org
-ADD COLUMN domain character varying(255) default NULL;
+ADD COLUMN domain character varying(255) NOT NULL;
