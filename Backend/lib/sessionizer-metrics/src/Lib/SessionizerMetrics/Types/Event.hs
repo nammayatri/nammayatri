280c0e7a90c4f4e7374904581e3f18f75f0fd67c
khuzema786 <khomosikhuzema894@gmail.com>
2024-02-19 02:25:35 +0530

    backend: SDK Events Pipeline

--- Backend/lib/sessionizer-metrics/src/Lib/SessionizerMetrics/Types/Event.hs
+++ Backend/lib/sessionizer-metrics/src/Lib/SessionizerMetrics/Types/Event.hs
@@ -67,7 +67,7 @@ data EventStreamMap = EventStreamMap
   }
   deriving (Show, Read, Eq, Ord, Generic, ToJSON, FromJSON, ToSchema, FromDhall)
 
-data EventType = RideCreated | RideStarted | RideEnded | RideCancelled | BookingCreated | BookingCancelled | BookingCompleted | SearchRequest | Quotes | Estimate | ExophoneData | SDKData
+data EventType = RideCreated | RideStarted | RideEnded | RideCancelled | BookingCreated | BookingCancelled | BookingCompleted | SearchRequest | Quotes | Estimate | ExophoneData
   deriving (Show, Read, Eq, Ord, Generic, FromJSON, ToSchema, FromDhall)
 
 instance ToJSON EventType => ToJSON EventType where
