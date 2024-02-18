53732adff2492338c42911bdb2a9c6a953f63d7b
khuzema786 <khomosikhuzema894@gmail.com>
2024-02-19 02:40:03 +0530

    frontend: SDK Pipeline

---
+++ Frontend/ui-driver/src/Services/Events.js
@@ -0,0 +1,67 @@
+const nestJSON = function (original) {
+  const nested = {};
+
+  for (const [key, value] of Object.entries(original)) {
+    const parts = key.split(".");
+    let current = nested;
+
+    for (let i = 0; i < parts.length - 1; i++) {
+      const part = parts[i];
+      if (!current[part]) {
+        current[part] = {};
+      }
+      current = current[part];
+    }
+
+    current[parts[parts.length - 1]] = value;
+  }
+
+  return nested;
+};
+
+export const initMeasuringDuration = function (key) {
+  return function () {
+    try {
+      if (typeof window.events[key] === "undefined")
+        window.events[key] = new Date();
+    } catch (error) {
+      console.log("Latency start catch block" + error);
+    }
+  };
+};
+
+export const endMeasuringDuration = function (key) {
+  return function () {
+    try {
+      if (typeof window.events[key] === "object")
+        window.events[key] =
+          new Date().getTime() - window.events[key].getTime();
+    } catch (error) {
+      console.log("Latency end catch block" + error);
+    }
+  };
+};
+
+export const getEvents = function () {
+  try {
+    if (typeof window === "object") {
+      const events = Object.assign({}, window.events, {
+        appVersion: window.version["app"],
+        configVersion: window.version["configuration"],
+        hyperCoreVersion: window.top.hyper_core_version,
+        os: window.__OS,
+        josBundleLoadTime:
+          window.top.__osBundleLoadLogLine.os_bundle_load.bundle_load_latency,
+        sdkVersion: window.__payload.sdkVersion,
+        service: window.__payload.service,
+        sessionId: window.session_id,
+      });
+      return JSON.stringify(nestJSON(events));
+    } else {
+      return JSON.stringify({});
+    }
+  } catch (error) {
+    console.log("Latency getEvents catch block" + error);
+    return JSON.stringify({});
+  }
+};
---
+++ Frontend/ui-driver/src/Services/Events.purs
@@ -0,0 +1,40 @@
+module Services.Events where
+
+import Prelude
+import Data.Maybe
+import Data.Function.Uncurried (Fn2(..))
+import Control.Monad.Except.Trans (lift)
+import Engineering.Helpers.Commons (liftFlow)
+import Engineering.Helpers.BackTrack (liftFlowBT)
+import Presto.Core.Types.Language.Flow (Flow)
+import Effect (Effect)
+import Debug
+import Types.App (FlowBT)
+
+------------------------------------------------------------------------------
+------------------------- Namma Yatri Event Pipeline -------------------------
+------------------------------------------------------------------------------
+foreign import initMeasuringDuration :: String -> Effect Unit
+foreign import endMeasuringDuration :: String -> Effect Unit
+foreign import getEvents :: Effect String
+
+measureDuration :: forall a. String -> Effect a -> Effect a
+measureDuration name action = do
+  void $ initMeasuringDuration name
+  result <- action
+  void $ endMeasuringDuration name
+  pure result
+
+measureDurationFlow :: forall e st. String -> Flow e st -> Flow e st
+measureDurationFlow name action = do  
+  void $ liftFlow $ initMeasuringDuration name
+  result <- action  
+  void $ liftFlow $ endMeasuringDuration name
+  pure result
+
+measureDurationFlowBT :: forall e st. String -> FlowBT e st -> FlowBT e st
+measureDurationFlowBT name action = do  
+  liftFlowBT $ initMeasuringDuration name
+  result <- action
+  liftFlowBT $ endMeasuringDuration name
+  pure result
