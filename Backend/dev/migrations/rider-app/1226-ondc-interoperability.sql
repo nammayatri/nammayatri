da2b2386e580f445a3d8c40cd0d508160ba871a6
Jaypal Mudaliyar <77069644+jaypalmudaliyar24@users.noreply.github.com>
2024-02-20 18:15:23 +0530

    backend/fix: fixes ride table not null column

--- Backend/dev/migrations/rider-app/1226-ondc-interoperability.sql
+++ Backend/dev/migrations/rider-app/1226-ondc-interoperability.sql
@@ -21,3 +21,6 @@ ALTER COLUMN provider_mobile_number DROP NOT NULL;
 
 ALTER TABLE atlas_app.booking
 ALTER COLUMN provider_name DROP NOT NULL;
+
+ALTER TABLE atlas_app.ride
+ALTER COLUMN driver_registered_at DROP NOT NULL;
