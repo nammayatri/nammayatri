8a5aa16de8afc5398490049eadbceafdd501b489
Anuragini Paunikar <aapaunikar2018@gmail.com>
2024-02-24 23:35:47 +0530

    backend/fix/lms_reels_action_file

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/Reels.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/Reels.hs
@@ -3,19 +3,33 @@
 
 module Domain.Action.UI.Reels where
 
-import API.Types.UI.Reels
 import qualified API.Types.UI.Reels
 import Data.OpenApi (ToSchema)
 import qualified Domain.Types.Merchant
 import qualified Domain.Types.Merchant.MerchantOperatingCity
 import qualified Domain.Types.Person
+import qualified Domain.Types.ReelsData as DTRD
 import qualified Environment
 import EulerHS.Prelude hiding (id)
+import Kernel.External.Types (Language (..))
 import qualified Kernel.External.Types
 import qualified Kernel.Prelude
 import qualified Kernel.Types.Id
+import Kernel.Utils.Common
 import Servant
+import Storage.CachedQueries.ReelsData as CQReels
+import qualified Storage.Queries.Person as QPerson
+import Storage.Queries.ReelsData as SQReels
 import Tools.Auth
+import Tools.Error
 
 getReelsGetAllReelVideos :: (Kernel.Prelude.Maybe (Kernel.Types.Id.Id Domain.Types.Person.Person), Kernel.Types.Id.Id Domain.Types.Merchant.Merchant, Kernel.Types.Id.Id Domain.Types.Merchant.MerchantOperatingCity.MerchantOperatingCity) -> Kernel.Prelude.Maybe (Kernel.External.Types.Language) -> Kernel.Prelude.Text -> Environment.Flow API.Types.UI.Reels.ReelsResp
-getReelsGetAllReelVideos = error "Logic yet to be decided"
+getReelsGetAllReelVideos (mbPersonId, _merchantId, merchantOpCityId) mbLanguage reelKey = do
+  personId <- fromMaybeM (PersonDoesNotExist "Nothing") mbPersonId
+  _person <- QPerson.findById personId >>= fromMaybeM (PersonDoesNotExist personId.getId)
+  let language = fromMaybe ENGLISH mbLanguage
+  reels <- CQReels.findAllByMerchantOpCityIdLanguageAndKey merchantOpCityId language reelKey
+  return $
+    API.Types.UI.Reels.ReelsResp
+      { reels = reels
+      }
