90116cd70685a5751ea61ec4f705944c88d6c104
Akhilesh Bhadauriya <akhilesh.b@juspay.in>
2024-02-27 18:46:52 +0530

    Backend/fix:updated the flake for shared-kernel

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
 
     # Backend inputs
     shared-kernel = {
-      url = "github:nammayatri/shared-kernel/Backend/fix/moving-kafka-properties-to-dhall-map";
+      url = "github:nammayatri/shared-kernel";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
