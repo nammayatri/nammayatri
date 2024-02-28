b80e8c207838dad1a1fea54165fe75f67ffaedd7
ratnadeep.b <ratnadeep.b@juspay.in>
2024-02-28 16:37:48 +0530

    backend/cac: added MerchantServiceUSageConfig to CAC.

--- Backend/app/rider-platform/rider-app/Main/src/Storage/CachedQueries/Merchant/MerchantServiceUsageConfig.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Storage/CachedQueries/Merchant/MerchantServiceUsageConfig.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeApplications #-}
 {-
  Copyright 2022-23, Juspay India Pvt Ltd
 
@@ -21,21 +22,80 @@ module Storage.CachedQueries.Merchant.MerchantServiceUsageConfig
   )
 where
 
+import qualified Client.Main as CM
+import Control.Lens
+import qualified Data.Aeson as DA
+import Data.Aeson.Key as DAK
+import qualified Data.Aeson.KeyMap as DAKM
+import Data.Aeson.Lens
 import Data.Coerce (coerce)
+import qualified Data.HashMap.Strict as HashMap
+import qualified Data.Text as Text
 import Domain.Types.Common
 import Domain.Types.Merchant.MerchantServiceUsageConfig
 import Domain.Types.MerchantOperatingCity (MerchantOperatingCity)
+import qualified EulerHS.Language as L
+import qualified Kernel.Beam.Types as KBT
 import Kernel.Prelude
 import qualified Kernel.Storage.Hedis as Hedis
+import qualified Kernel.Storage.Queries.SystemConfigs as KSQS
+import qualified Kernel.Types.Cac as KTC
 import Kernel.Types.Id
 import Kernel.Utils.Common
+import Storage.Beam.SystemConfigs ()
 import qualified Storage.Queries.Merchant.MerchantServiceUsageConfig as Queries
+import qualified System.Environment as Se
+import System.Random
 
 create :: (MonadFlow m, CacheFlow m r, EsqDBFlow m r) => MerchantServiceUsageConfig -> m ()
 create = Queries.create
 
 findByMerchantOperatingCityId :: (CacheFlow m r, EsqDBFlow m r, MonadFlow m) => Id MerchantOperatingCity -> m (Maybe MerchantServiceUsageConfig)
-findByMerchantOperatingCityId id =
+findByMerchantOperatingCityId id = do
+  systemConfigs <- L.getOption KBT.Tables
+  let useCACConfig = maybe False (\sc -> sc.useCAC) systemConfigs
+  case useCACConfig of
+    True -> do
+      gen <- newStdGen
+      let toss = fst (randomR (1, 100) gen :: (Int, StdGen))
+      logDebug $ "The toss value is for MerchantServiceUsageConfig " <> show toss
+      context <- liftIO $ CM.hashMapToString $ HashMap.fromList [(Text.pack "merchantOperatingCityId", DA.String (getId id))]
+      mbTenant <- liftIO $ Se.lookupEnv "RIDER_TENANT"
+      let tenant = fromMaybe "atlas_app_v2" mbTenant
+      config <- liftIO $ CM.evalExperimentAsString tenant context toss
+      logDebug $ "The fetched config is : " <> Text.pack config
+      let formattedConfig = (config ^@.. _Object . reindexed (KTC.dropPrefixFromConfig (Text.pack "merchantServiceUsageConfig:")) (itraversed . indices (\k -> Text.isPrefixOf "merchantServiceUsageConfig:" (DAK.toText k))))
+      logDebug $ "res8 = " <> show formattedConfig
+      logDebug $ "The json version of res8 : " <> show (DA.Object $ DAKM.fromList formattedConfig)
+      let mbParsedConfig = (DA.Object $ DAKM.fromList formattedConfig) ^? _JSON :: Maybe (MerchantServiceUsageConfigD 'Unsafe)
+      logDebug $ "The decoded Merchant service usage config is 1 : " <> show mbParsedConfig
+      case mbParsedConfig of
+        Just cfg -> return $ Just $ coerce @(MerchantServiceUsageConfigD 'Unsafe) @MerchantServiceUsageConfig cfg
+        _ -> do
+          logError $ "Could not find the config for tenant " <> tenant <> " the stated merchantOperatingCityId. Trying to restart Client with backup...." <> show (fromMaybe "atlas_app_v2" tenant)
+          cfg <- KSQS.findById' $ Text.pack tenant
+          mbHost <- liftIO $ Se.lookupEnv "CAC_HOST"
+          mbInterval <- liftIO $ Se.lookupEnv "CAC_INTERVAL"
+          logDebug $ "The fetched Backup config is : " <> show cfg.configValue
+          KTC.initializeCACThroughConfig CM.createClientFromConfig cfg.configValue tenant (fromMaybe 10 (readMaybe =<< mbInterval)) (fromMaybe "http://localhost:8080" mbHost)
+          Just <$> getMerchantServiceUsageConfigFromCACStrict id toss
+    False -> getCfgFromCache id
+
+getMerchantServiceUsageConfigFromCACStrict :: (MonadFlow m, CacheFlow m r, EsqDBFlow m r) => Id MerchantOperatingCity -> Int -> m MerchantServiceUsageConfig
+getMerchantServiceUsageConfigFromCACStrict id' toss = do
+  logDebug $ "Fetching MerchantServiceUsageConfig from CAC second time"
+  context <- liftIO $ CM.hashMapToString $ HashMap.fromList [(Text.pack "merchantOperatingCityId", DA.String (getId id'))]
+  tenant <- liftIO $ Se.lookupEnv "RIDER_TENANT"
+  config <- liftIO $ CM.evalExperimentAsString (fromMaybe "atlas_app_v2" tenant) context toss
+  logDebug $ "The fetched config is 2 new : " <> Text.pack config
+  let formattedConfig = (config ^@.. _Object . reindexed (KTC.dropPrefixFromConfig (Text.pack "merchantServiceUsageConfig:")) (itraversed . indices (\k -> Text.isPrefixOf "merchantServiceUsageConfig:" (DAK.toText k))))
+  logDebug $ "res8 2 = " <> show formattedConfig
+  let parsedConfig = (DA.Object $ DAKM.fromList formattedConfig) ^? _JSON :: Maybe (MerchantServiceUsageConfigD 'Unsafe)
+  logDebug $ "The decoded Merchant service usage config is 2 new : " <> show parsedConfig
+  return $ coerce @(MerchantServiceUsageConfigD 'Unsafe) @MerchantServiceUsageConfig $ fromMaybe (error $ Text.pack $ "Could not find merchantServiceUsageConfig corresponding to the stated merchantOperatingCityId : " <> show id') parsedConfig
+
+getCfgFromCache :: (CacheFlow m r, EsqDBFlow m r, MonadFlow m) => Id MerchantOperatingCity -> m (Maybe MerchantServiceUsageConfig)
+getCfgFromCache id =
   Hedis.safeGet (makeMerchantOperatingCityIdKey id) >>= \case
     Just a -> return . Just $ coerce @(MerchantServiceUsageConfigD 'Unsafe) @MerchantServiceUsageConfig a
     Nothing -> flip whenJust cacheMerchantServiceUsageConfig /=<< Queries.findByMerchantOperatingCityId id
