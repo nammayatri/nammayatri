4e0ff2adb581cff9f527e16753f06d3064b9b967
Jaypal Mudaliyar <77069644+jaypalmudaliyar24@users.noreply.github.com>
2024-02-29 19:48:16 +0530

    backend/fix: flake update

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
 
     # Backend inputs
     shared-kernel = {
-      url = "github:nammayatri/shared-kernel/backend/fix/adds-derivation-for-timeRFC339";
+      url = "github:nammayatri/shared-kernel";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
