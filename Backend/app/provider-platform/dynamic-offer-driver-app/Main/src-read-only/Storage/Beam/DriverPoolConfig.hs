44bd95c63479531881aba970ee7fbd838ee1b806
Akhilesh Bhadauriya <akhilesh.b@juspay.in>
2024-03-06 12:51:12 +0530

    added template haskell for type verification

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/Storage/Beam/DriverPoolConfig.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/Storage/Beam/DriverPoolConfig.hs
@@ -9,6 +9,7 @@ import qualified Database.Beam as B
 import qualified Domain.Types.DriverPoolConfig
 import qualified Domain.Types.Merchant
 import qualified Domain.Types.Merchant.MerchantOperatingCity
+import Domain.Types.UtilsTH
 import qualified Domain.Types.Vehicle.Variant
 import Kernel.External.Encryption
 import Kernel.Prelude
@@ -59,3 +60,4 @@ type DriverPoolConfig = DriverPoolConfigT Identity
 $(enableKVPG ''DriverPoolConfigT ['id] [])
 
 $(mkTableInstances ''DriverPoolConfigT "driver_pool_config")
+$(mkCacParseInstance ''DriverPoolConfigT)
