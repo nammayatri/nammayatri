188dfbbbf4c3c55c156234941621831f9b9af204
S S Mercy Queen <mercyqueen71@gmail.com>
2024-03-08 22:16:29 +0530

    backend/fix filtered drivers lat long on the basis of vehicle variant

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Beckn/OnDemand/Utils/OnSearch.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Beckn/OnDemand/Utils/OnSearch.hs
@@ -31,7 +31,8 @@ import Kernel.Types.Common
 
 mkProviderLocations :: [Maybe NearestDriverInfo] -> [Spec.Location]
 mkProviderLocations driverLocationsInfo =
-  foldl (<>) [] $ map mkProviderLocation (catMaybes driverLocationsInfo)
+  let duplicatesRemoved = List.nubBy ((==) `on` locationId) (catMaybes driverLocationsInfo)
+   in foldl (<>) [] $ map mkProviderLocation duplicatesRemoved
 
 mkProviderLocation :: NearestDriverInfo -> [Spec.Location]
 mkProviderLocation NearestDriverInfo {..} = do
@@ -52,7 +53,10 @@ mkProviderLocation NearestDriverInfo {..} = do
 
 mkItemLocationIds :: [Maybe NearestDriverInfo] -> Maybe [Text]
 mkItemLocationIds driverLocationsInfo = do
-  Just $ map (.locationId) (catMaybes driverLocationsInfo)
+  let dLocInfo' = List.nubBy ((==) `on` locationId) (catMaybes driverLocationsInfo)
+  case dLocInfo' of
+    [] -> Nothing
+    dLocInfo -> Just $ map (.locationId) dLocInfo
 
 buildFareParamsBreakupsTags :: FareParamsBreakupItem -> Spec.Tag
 buildFareParamsBreakupsTags FareParamsBreakupItem {..} = do
--- Backend/app/rider-platform/rider-app/Main/src/Beckn/OnDemand/Transformer/OnSearch.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Beckn/OnDemand/Transformer/OnSearch.hs
@@ -76,7 +76,7 @@ tfQuotesInfo provider fulfillments validTill item = do
       pure $ Right $ Domain.Action.Beckn.OnSearch.QuoteInfo {descriptions = descriptions_, discount = discount_, estimatedFare = estimatedFare_, estimatedTotalFare = estimatedTotalFare_, itemId = itemId_, quoteDetails = quoteDetails_, specialLocationTag = specialLocationTag_, vehicleVariant = vehicleVariant_, validTill, serviceTierName = serviceTierName_}
     _ -> do
       let bppEstimateId_ = Id quoteOrEstId_
-      driversLocation_ <- Beckn.OnDemand.Utils.OnSearch.getProviderLocation provider
+      driversLocation_ <- Beckn.OnDemand.Utils.OnSearch.getProviderLocation provider vehicleVariant_
       let nightShiftInfo_ = Beckn.OnDemand.Utils.OnSearch.buildNightShiftInfo item
       totalFareRange_ <- Beckn.OnDemand.Utils.OnSearch.getTotalFareRange item
       waitingCharges_ <- Beckn.OnDemand.Utils.OnSearch.buildWaitingChargeInfo item
