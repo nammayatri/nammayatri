6542f4c2ccaaeb3222a631847d2c53939d009d30
Praveen Gongada <praveengongada0603@gmail.com>
2024-03-08 19:52:42 +0530

    frontend/fix: Fixed Choose Ride Peak Height

--- Frontend/ui-customer/src/Screens/RideBookingFlow/HomeScreen/Controller.purs
+++ Frontend/ui-customer/src/Screens/RideBookingFlow/HomeScreen/Controller.purs
@@ -2534,8 +2534,9 @@ eval (MenuButtonActionController (MenuButtonController.OnClick config)) state = 
       _ <- animateCamera config.lat config.lng 25.0 "NO_ZOOM"
       pure NoAction
     ]
-eval (ChooseYourRideAction (ChooseYourRideController.ChooseVehicleAC ChooseVehicleController.NoAction)) state =
-  continue state{ props{ defaultPickUpPoint = "" } }
+eval (ChooseYourRideAction (ChooseYourRideController.ChooseVehicleAC ChooseVehicleController.NoAction)) state = do
+  let height = (runFn1 getLayoutBounds $ getNewIDWithTag state.props.estimateId).height
+  continue state{ props{ defaultPickUpPoint = "", selectedEstimateHeight = height } }
 
 eval (ChooseYourRideAction (ChooseYourRideController.ChooseVehicleAC (ChooseVehicleController.OnSelect config))) state = do
   let updatedQuotes = map (\item -> item{activeIndex = config.index}) state.data.specialZoneQuoteList
