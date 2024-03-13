88395e9f9bcde8810b5f23a8fddcec2f49a82346
Mercy Queen <mercy.queen@Mercy-Queen-HQN2XQGR96.local>
2024-03-13 21:51:53 +0530

    Added error code check

--- Frontend/ui-customer/src/Services/Backend.purs
+++ Frontend/ui-customer/src/Services/Backend.purs
@@ -328,8 +328,12 @@ rideSearchBT payload = do
         withAPIResultBT (EP.searchReq "") identity errorHandler (lift $ lift $ callAPI headers payload)
     where
       errorHandler errorPayload = do
-            if errorPayload.code == 400 then
+            let errResp = errorPayload.response
+                codeMessage = decodeError errResp.errorMessage "errorCode"
+            if  errorPayload.code == 400 && codeMessage == "RIDE_NOT_SERVICEABLE" then
                 pure $ toast (getString RIDE_NOT_SERVICEABLE)
+            else if errorPayload.code == 400 then
+                pure $ toast codeMessage
               else pure $ toast (getString SOMETHING_WENT_WRONG_PLEASE_TRY_AGAIN)
             modifyScreenState $ HomeScreenStateType (\homeScreen -> homeScreen {props{currentStage = SearchLocationModel}})
             _ <- pure $ setValueToLocalStore LOCAL_STAGE "SearchLocationModel"
