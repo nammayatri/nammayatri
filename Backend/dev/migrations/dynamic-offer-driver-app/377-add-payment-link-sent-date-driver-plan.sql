8672369dbe5bd6aea0199b8bb710d5e9db627fd8
aayushagarwal2001 <aayush2001fzd@gmail.com>
2024-03-30 03:42:03 +0530

    backend/fix:add current time as default for send payment link at

--- Backend/dev/migrations/dynamic-offer-driver-app/377-add-payment-link-sent-date-driver-plan.sql
+++ Backend/dev/migrations/dynamic-offer-driver-app/377-add-payment-link-sent-date-driver-plan.sql
@@ -1,1 +1,1 @@
-alter table atlas_driver_offer_bpp.driver_plan add column last_payment_link_sent_at_ist_date timestamp with time zone;
+alter table atlas_driver_offer_bpp.driver_plan add column last_payment_link_sent_at_ist_date timestamp with time zone default CURRENT_TIMESTAMP;
