13bd8ae7d22a0ab3355f11d9e6ec53018f831b86
ErasedSoul <anirbandas0206@gmail.com>
2024-04-02 13:42:04 +0530

    backend/dsl/enh/rider/SearchReqLocation to dsl

---
+++ Backend/app/rider-platform/rider-app/Main/src/Storage/Queries/Transformers/SearchReqLocation.hs
@@ -0,0 +1,15 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+
+module Storage.Queries.Transformers.SearchReqLocation where
+
+import qualified Domain.Types.LocationAddress
+import Kernel.Beam.Functions
+import Kernel.External.Encryption
+import Kernel.Prelude
+import qualified Kernel.Prelude
+import Kernel.Types.Error
+import Kernel.Utils.Common (CacheFlow, EsqDBFlow, MonadFlow, fromMaybeM, getCurrentTime)
+
+mkLocationAddress :: (Kernel.Prelude.Maybe Kernel.Prelude.Text -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> Domain.Types.LocationAddress.LocationAddress)
+mkLocationAddress area areaCode building city country door placeId state street ward = Domain.Types.LocationAddress.LocationAddress {..}
--- Backend/dev/sql-seed/rider-app-seed.sql
+++ Backend/dev/sql-seed/rider-app-seed.sql
@@ -266,25 +266,6 @@ CREATE INDEX idx_16386_requestor ON atlas_app.search_request USING btree (rider_
 
 
 
-
-
-
-CREATE TABLE atlas_app.search_request_location (
-id character(36) NOT NULL,
-lat double precision NOT NULL,
-lon double precision NOT NULL,
-city character varying(255),
-state character varying(255),
-country character varying(255),
-created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-street character varying(255),
-door character varying(255),
-building character varying(255),
-area_code character varying(255),
-area character varying(255)
-,CONSTRAINT  idx_16434_primary PRIMARY KEY (id)
-);
 ALTER TABLE atlas_app.search_request_location OWNER TO atlas_app_user;
 CREATE INDEX idx_16434_city ON atlas_app.search_request_location USING btree (city);
 CREATE INDEX idx_16434_state ON atlas_app.search_request_location USING btree (state);
