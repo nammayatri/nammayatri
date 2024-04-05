0e107e5daa02880a3b92d445fe759a7b054b0e5e
ashish singh <ashish.singh.7424@gmail.com>
2024-04-05 19:55:46 +0530

    frontend/fixed ride request pincode update issue and made it configurable

--- Frontend/android-native/mobility-app/src/main/java/in/juspay/mobility/app/OverlaySheetService.java
+++ Frontend/android-native/mobility-app/src/main/java/in/juspay/mobility/app/OverlaySheetService.java
@@ -243,38 +243,43 @@ public class OverlaySheetService extends Service implements View.OnTouchListener
             if (model.getSourcePinCode() != null && model.getSourcePinCode().trim().length() > 0) {
                 holder.sourcePinCode.setText(model.getSourcePinCode().trim());
                 holder.sourcePinCode.setVisibility(View.VISIBLE);
-            } else {
+            } else if (remoteConfigs.hasKey("enable_pincode") && remoteConfigs.getBoolean("enable_pincode")) {
                 Handler handler = new Handler();
                 Runnable runnable = () -> {
                     String pincode = getPincodeFromRR(model.getSrcLat(), model.getSrcLng());
-                    if(holder != null && holder.sourcePinCode != null) {
-                        if (pincode != null) {
-                            holder.sourcePinCode.setText(pincode);
-                            holder.sourcePinCode.setVisibility(View.VISIBLE);
-                        } else {
-                            holder.sourceAddress.setMaxLines(2);
-                            holder.sourcePinCode.setVisibility(View.GONE);
-                        }
-                    }
+                    mainLooper.post(() -> {
+                            if (holder != null && holder.sourcePinCode != null) {
+                                if (pincode != null) {
+                                    holder.sourcePinCode.setText(pincode);
+                                    holder.sourcePinCode.setVisibility(View.VISIBLE);
+                                } else {
+                                    holder.sourceAddress.setMaxLines(2);
+                                    holder.sourcePinCode.setVisibility(View.GONE);
+                                }
+                            }
+                        // }
+                    });
                 };
                 handler.postDelayed(runnable, 300);
             }
             if (model.getDestinationPinCode() != null && model.getDestinationPinCode().trim().length() > 0) {
                 holder.destinationPinCode.setText(model.getDestinationPinCode());
                 holder.destinationPinCode.setVisibility(View.VISIBLE);
-            } else {
+            } else if (remoteConfigs.hasKey("enable_pincode") && remoteConfigs.getBoolean("enable_pincode")) {
                 Handler handler = new Handler();
                 Runnable runnable = () -> {
                     String pincode = getPincodeFromRR(model.getDestLat(), model.getDestLng());
-                    if(holder != null && holder.sourcePinCode != null) {
-                        if (pincode != null) {
-                            holder.destinationPinCode.setText(pincode);
-                            holder.destinationPinCode.setVisibility(View.VISIBLE);
-                        } else {
-                            holder.destinationAddress.setMaxLines(2);
-                            holder.destinationPinCode.setVisibility(View.GONE);
-                        }
-                    }
+                    mainLooper.post(() -> {
+                            if (holder != null && holder.sourcePinCode != null) {
+                                if (pincode != null) {
+                                    holder.destinationPinCode.setText(pincode);
+                                    holder.destinationPinCode.setVisibility(View.VISIBLE);
+                                } else {
+                                    holder.destinationAddress.setMaxLines(2);
+                                    holder.destinationPinCode.setVisibility(View.GONE);
+                                }
+                            }
+                    });
                 };
                 handler.postDelayed(runnable, 300);
             }
