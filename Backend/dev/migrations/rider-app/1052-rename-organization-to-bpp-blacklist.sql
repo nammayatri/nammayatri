4a686db7eae071a752426eaec5437f12f82a6433
saimanohar.veeravajhula <saimanohar.veeravajhula@juspay.in>
2024-04-13 04:18:39 +0530

    backend/DSL/move-tables-to-DSL: BlackListOrg

--- Backend/dev/migrations/rider-app/1052-rename-organization-to-bpp-blacklist.sql
+++ Backend/dev/migrations/rider-app/1052-rename-organization-to-bpp-blacklist.sql
@@ -1,3 +1,1 @@
 DELETE FROM atlas_app.organization;
-
-ALTER TABLE atlas_app.organization RENAME TO black_list_org;
--- Backend/dev/migrations/rider-app/1223-whitelist-new-queries.sql
+++ Backend/dev/migrations/rider-app/1223-whitelist-new-queries.sql
@@ -7,13 +7,7 @@ SET domain = 'MOBILITY';
 ALTER TABLE atlas_app.white_list_org
 ALTER COLUMN domain SET NOT NULL;
 
-ALTER TABLE atlas_app.black_list_org
-ADD COLUMN domain character varying(255) NOT NULL;
-
 -- Run these queries in prod after successful release to maintain backward compatibility
 
 ALTER TABLE atlas_app.white_list_org
-DROP COLUMN type;
-
-ALTER TABLE atlas_app.black_list_org
 DROP COLUMN type;
