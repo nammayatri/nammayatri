000d00d01a3292a71803fc09e7b5ead99bc84c89
saimanohar.veeravajhula <saimanohar.veeravajhula@juspay.in>
2024-04-14 01:24:39 +0530

    minor file,migration changes

---
+++ Backend/dev/migrations/rider-app/1250-add-createdAt-updatedAt-to-tables.sql
@@ -0,0 +1,20 @@
+--- Please run these queries while doing deployment
+
+ALTER TABLE atlas_app.black_list_org ADD COLUMN created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL;
+ALTER TABLE atlas_app.black_list_org ADD COLUMN updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL;
+
+ALTER TABLE atlas_app.white_list_org ADD COLUMN created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL;
+ALTER TABLE atlas_app.white_list_org ADD COLUMN updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL;
+
+ALTER TABLE atlas_app.trip_terms ADD COLUMN created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL;
+ALTER TABLE atlas_app.trip_terms ADD COLUMN updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL;
+
+ALTER TABLE atlas_app.special_zone_quote ADD COLUMN created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL;
+ALTER TABLE atlas_app.special_zone_quote ADD COLUMN updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL;
+
+ALTER TABLE atlas_app.quote ADD COLUMN updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL;
+
+ALTER TABLE atlas_app.person_disability ADD COLUMN created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL;
+
+ALTER TABLE atlas_app.special_zone_quote ADD COLUMN created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL;
+ALTER TABLE atlas_app.special_zone_quote ADD COLUMN updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL;
