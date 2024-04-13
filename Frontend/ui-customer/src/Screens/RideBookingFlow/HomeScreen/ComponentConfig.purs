40967b102aed630af0a98f60b1cc769745de136d
Pravinkumar S <spravinkumar9952@gmail.com>
2024-04-13 15:16:21 +0530

    frontend-feat: added change vehicle varient in onclick ride

--- Frontend/ui-customer/src/Screens/RideBookingFlow/HomeScreen/ComponentConfig.purs
+++ Frontend/ui-customer/src/Screens/RideBookingFlow/HomeScreen/ComponentConfig.purs
@@ -1716,6 +1716,8 @@ chooseVehicleConfig state = let
     , nightShiftMultiplier = selectedEstimates.nightShiftMultiplier
     , nightCharges = selectedEstimates.nightCharges
     , baseFare = selectedEstimates.baseFare
+    , showEditButton = true
+    , editBtnText = getString CHANGE
     }
   in chooseVehicleConfig'
 
