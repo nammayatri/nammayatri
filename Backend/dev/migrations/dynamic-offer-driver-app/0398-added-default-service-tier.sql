a433f56b3be1b392b2076b864ff8e0eba44e867a
Ashish Saini <ashish.saini@ashishsaini-Y7WP7YXLCD.local>
2024-04-24 21:22:14 +0530

    backend/feat/added_default_service_tier_in_ride_details

---
+++ Backend/dev/migrations/dynamic-offer-driver-app/0398-added-default-service-tier.sql
@@ -0,0 +1,2 @@
+ALTER TABLE atlas_driver_offer_bpp.ride_details ADD COLUMN created_at timestamp with time zone ;
+ALTER TABLE atlas_driver_offer_bpp.ride_details ADD COLUMN default_service_tier_name Text;
