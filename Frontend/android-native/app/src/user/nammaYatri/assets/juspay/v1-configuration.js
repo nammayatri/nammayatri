4866c573377d097a5a31eb89ff325fb44d570631
Chakradhar <chakradhar.k@juspay.in>
2024-04-30 17:03:39 +0530

    frontend/fix: metro kochi config custom changes

--- Frontend/android-native/app/src/user/nammaYatri/assets/juspay/v1-configuration.js
+++ Frontend/android-native/app/src/user/nammaYatri/assets/juspay/v1-configuration.js
@@ -238,8 +238,8 @@ window.getMerchantConfig = function () {
     , "metroTicketingConfig" : [
       { "cityName" : "kochi"
       , "cityCode" : "std:0484"
-      , "customEndTime" : "" 
-      , "customDates" : []       
+      , "customEndTime" : "23:59:59" 
+      , "customDates" : ["03/05/2024", "04/05/2024", "05/05/2024", "06/05/2024", "07/05/2024", "08/05/2024", "09/05/2024", "10/05/2024","11/05/2024"]       
       , "metroStationTtl" : 10080
       , "bookingStartTime" : "05:45:00"
       , "bookingEndTime" : "22:30:00"
