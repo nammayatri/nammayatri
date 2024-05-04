eeb79c65c60c27d919ea383a00871ccaef97303f
saimanohar.veeravajhula <saimanohar.veeravajhula@juspay.in>
2024-05-05 00:57:52 +0530

    backend/fix/revert-white-list-queries-for-local

---
+++ Backend/dev/migrations/rider-app/1218-white-list-org-table.sql
@@ -0,0 +1,4 @@
+
+insert into atlas_app.white_list_org (id,subscriber_id,type) values ('45a16bb9-69e1-4dc6-a751-65b82a84dbc9', 'NAMMA_YATRI', 'APP');
+
+insert into atlas_app.white_list_org (id,subscriber_id,type) values ('45a16bb9-69e1-4dc6-a751-65b82a84dbc0', 'JUSPAY.BG.1', 'APP');
