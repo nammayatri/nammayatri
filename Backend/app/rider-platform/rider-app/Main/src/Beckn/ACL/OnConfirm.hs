bb6ad1812991056c457283814f5863fd836e88ee
Dushyant Singh <dushyantsingh@Dushyants-MacBook-Pro.local>
2024-05-06 16:09:50 +0530

    backend/enhancement: Added EstimatedFareBreakup in BookingAPIEntity

--- Backend/app/rider-platform/rider-app/Main/src/Beckn/ACL/OnConfirm.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Beckn/ACL/OnConfirm.hs
@@ -14,11 +14,13 @@
 
 module Beckn.ACL.OnConfirm (buildOnConfirmReqV2) where
 
+import Beckn.ACL.Common as ACL
 import qualified BecknV2.OnDemand.Enums as Enums
 import qualified BecknV2.OnDemand.Types as Spec
 import qualified BecknV2.OnDemand.Utils.Common as Utils
 import qualified BecknV2.OnDemand.Utils.Context as ContextV2
 import qualified Data.Text as T
+import Domain.Action.Beckn.Common as Common
 import qualified Domain.Action.Beckn.OnConfirm as DOnConfirm
 import Kernel.Prelude
 import qualified Kernel.Types.Beckn.Context as Context
@@ -28,7 +30,8 @@ import Tools.Error
 
 buildOnConfirmReqV2 ::
   ( HasFlowEnv m r '["_version" ::: Text],
-    MonadFlow m
+    MonadFlow m,
+    CacheFlow m r
   ) =>
   Spec.OnConfirmReq ->
   Bool ->
@@ -36,7 +39,10 @@ buildOnConfirmReqV2 ::
 buildOnConfirmReqV2 req isValueAddNP = do
   ContextV2.validateContext Context.ON_CONFIRM req.onConfirmReqContext
   handleErrorV2 req $ \message -> do
-    case parseData message of
+    let order = message.confirmReqMessageOrder
+    fareParamsQuotationBreakup <- order.orderQuote >>= (.quotationBreakup) & fromMaybeM (InvalidRequest "quote breakup is not present in RideAssigned Event.") --new
+    fareParams <- traverse ACL.mkDFareBreakup fareParamsQuotationBreakup
+    case parseData message (Just fareParams) of
       Right dReq -> do
         case dReq of
           DOnConfirm.BookingConfirmed _ | not isValueAddNP -> do
@@ -48,8 +54,8 @@ buildOnConfirmReqV2 req isValueAddNP = do
         return $ Just dReq
       Left err -> throwError . InvalidBecknSchema $ "on_confirm error:-" <> show err
   where
-    parseData :: Spec.ConfirmReqMessage -> Either Text DOnConfirm.OnConfirmReq
-    parseData message = do
+    parseData :: Spec.ConfirmReqMessage -> Maybe [Common.DFareBreakup] -> Either Text DOnConfirm.OnConfirmReq
+    parseData message mbFareParams = do
       let order = message.confirmReqMessageOrder
       bppBookingIdText <- order.orderId & maybe (Left "Missing OrderId") Right
       let bppBookingId = Id bppBookingIdText
@@ -74,12 +80,11 @@ buildOnConfirmReqV2 req isValueAddNP = do
           bppRideId <- fulf >>= (.fulfillmentId) & maybe (Left "Missing fulfillmentId") (Right . Id)
           driverName <- fulf >>= (.fulfillmentAgent) >>= (.agentPerson) >>= (.personName) & maybe (Left "Missing fulfillment.agent.person.name in on_confirm") Right
           driverMobileNumber <- fulf >>= (.fulfillmentAgent) >>= (.agentContact) >>= (.contactPhone) & maybe (Left "Missing fulfillment.agent.contact.phone in on_confirm") Right
-
           vehicleNumber <- fulf >>= (.fulfillmentVehicle) >>= (.vehicleRegistration) & maybe (Left "Missing fulfillment.vehicle.registration in on_confirm") Right
           let vehicleColor = fulf >>= (.fulfillmentVehicle) >>= (.vehicleColor)
           vehicleModel <- fulf >>= (.fulfillmentVehicle) >>= (.vehicleModel) & maybe (Left "Missing fulfillment.vehicle.model in on_confirm") Right
-          Right $ DOnConfirm.RideAssigned DOnConfirm.RideAssignedInfo {..}
-        else Right $ DOnConfirm.BookingConfirmed DOnConfirm.BookingConfirmedInfo {bppBookingId, specialZoneOtp = mbRideOtp}
+          Right $ DOnConfirm.RideAssigned DOnConfirm.RideAssignedInfo {fareParams = mbFareParams, ..}
+        else Right $ DOnConfirm.BookingConfirmed DOnConfirm.BookingConfirmedInfo {fareParams = mbFareParams, bppBookingId, specialZoneOtp = mbRideOtp}
 
 handleErrorV2 ::
   (MonadFlow m) =>
