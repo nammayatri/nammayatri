b17932baf97826600da0b57da30395cd60fbc48e
saimanohar.veeravajhula <saimanohar.veeravajhula@juspay.in>
2024-05-07 20:09:07 +0530

    backend/enh/DSL/move-tables-dsl-driver-offer-person-part: Plan

---
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/Storage/Beam/Plan.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+
+module Storage.Beam.Plan where
+
+import qualified Database.Beam as B
+import qualified Domain.Types.Extra.Plan
+import qualified Domain.Types.Plan
+import Kernel.External.Encryption
+import Kernel.Prelude
+import qualified Kernel.Prelude
+import qualified Kernel.Types.Common
+import Tools.Beam.UtilsTH
+
+data PlanT f = PlanT
+  { basedOnEntity :: (B.C f Domain.Types.Plan.BasedOnEntity),
+    cgstPercentage :: (B.C f Kernel.Types.Common.HighPrecMoney),
+    description :: (B.C f Kernel.Prelude.Text),
+    eligibleForCoinDiscount :: (B.C f Kernel.Prelude.Bool),
+    freeRideCount :: (B.C f Kernel.Prelude.Int),
+    frequency :: (B.C f Domain.Types.Plan.Frequency),
+    id :: (B.C f Kernel.Prelude.Text),
+    isDeprecated :: (B.C f Kernel.Prelude.Bool),
+    isOfferApplicable :: (B.C f Kernel.Prelude.Bool),
+    maxAmount :: (B.C f Kernel.Types.Common.HighPrecMoney),
+    maxCreditLimit :: (B.C f Kernel.Types.Common.HighPrecMoney),
+    maxMandateAmount :: (B.C f Kernel.Types.Common.HighPrecMoney),
+    merchantId :: (B.C f Kernel.Prelude.Text),
+    merchantOpCityId :: (B.C f Kernel.Prelude.Text),
+    name :: (B.C f Kernel.Prelude.Text),
+    paymentMode :: (B.C f Domain.Types.Plan.PaymentMode),
+    planBaseAmount :: (B.C f Domain.Types.Extra.Plan.PlanBaseAmount),
+    planType :: (B.C f Domain.Types.Plan.PlanType),
+    registrationAmount :: (B.C f Kernel.Types.Common.HighPrecMoney),
+    serviceName :: (B.C f Domain.Types.Plan.ServiceNames),
+    sgstPercentage :: (B.C f Kernel.Types.Common.HighPrecMoney),
+    subscribedFlagToggleAllowed :: (B.C f Kernel.Prelude.Bool),
+    merchantOperatingCityId :: (B.C f (Kernel.Prelude.Maybe (Kernel.Prelude.Text))),
+    createdAt :: (B.C f Kernel.Prelude.UTCTime),
+    updatedAt :: (B.C f Kernel.Prelude.UTCTime)
+  }
+  deriving (Generic, B.Beamable)
+
+instance B.Table PlanT where
+  data PrimaryKey PlanT f = PlanId (B.C f Kernel.Prelude.Text) deriving (Generic, B.Beamable)
+  primaryKey = PlanId . id
+
+type Plan = PlanT Identity
+
+$(enableKVPG (''PlanT) [('id)] [])
+
+$(mkTableInstances (''PlanT) "plan")
---
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Types/Extra/Plan.hs
@@ -0,0 +1,73 @@
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wno-dodgy-exports #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+
+module Domain.Types.Extra.Plan where
+
+import Data.Aeson
+import qualified Data.List as List
+import qualified Data.Text as T
+import qualified Domain.Types.Merchant as DMerchant
+import Domain.Types.Merchant.MerchantOperatingCity (MerchantOperatingCity)
+import Kernel.Prelude
+import Kernel.Types.Common
+import Kernel.Types.Common (HighPrecMoney, Money)
+import Kernel.Types.Id
+import Kernel.Utils.GenericPretty
+import Kernel.Utils.TH (mkHttpInstancesForEnum)
+import qualified Text.Show
+import Tools.Beam.UtilsTH (mkBeamInstancesForEnum)
+
+instance Show PlanBaseAmount where
+  show (PERRIDE_BASE amount) = "PERRIDE_" <> T.unpack (show amount)
+  show (DAILY_BASE amount) = "DAILY_" <> T.unpack (show amount)
+  show (WEEKLY_BASE amount) = "WEEKLY_" <> T.unpack (show amount)
+  show (MONTHLY_BASE amount) = "MONTHLY_" <> T.unpack (show amount)
+
+data PlanBaseAmount
+  = PERRIDE_BASE HighPrecMoney
+  | DAILY_BASE HighPrecMoney
+  | WEEKLY_BASE HighPrecMoney
+  | MONTHLY_BASE HighPrecMoney
+  deriving stock (Eq, Ord, Generic)
+  deriving anyclass (FromJSON, ToJSON, ToSchema)
+  deriving (PrettyShow) via Showable PlanBaseAmount
+
+$(mkBeamInstancesForEnum ''PlanBaseAmount)
+
+instance Read PlanBaseAmount where
+  readsPrec d' =
+    readParen
+      (d' > app_prec)
+      ( \r ->
+          [ (PERRIDE_BASE (read r1 :: HighPrecMoney), "")
+            | r1 <- stripPrefix "PERRIDE_" r
+          ]
+            ++ [ (DAILY_BASE (read r1 :: HighPrecMoney), "")
+                 | r1 <- stripPrefix "DAILY_" r
+               ]
+            ++ [ (WEEKLY_BASE (read r1 :: HighPrecMoney), "")
+                 | r1 <- stripPrefix "WEEKLY_" r
+               ]
+            ++ [ (MONTHLY_BASE (read r1 :: HighPrecMoney), "")
+                 | r1 <- stripPrefix "MONTHLY_" r
+               ]
+      )
+    where
+      app_prec = 10
+      stripPrefix pref r = bool [] [List.drop (length pref) r] $ List.isPrefixOf pref r
+
+newtype RideCountBasedFeePolicyConfig = RideCountBasedFeePolicyConfig [RideCountBasedFeePolicy] deriving (Generic, ToJSON, FromJSON, Show)
+
+data RideCountBasedFeePolicy = RideCountBasedFeePolicy
+  { baseRideCount :: Int,
+    platformFee :: Money,
+    platformFeeCgst :: HighPrecMoney,
+    platformFeeSgst :: HighPrecMoney,
+    perRideFee :: Money,
+    perRideCgst :: HighPrecMoney,
+    perRideSgst :: HighPrecMoney
+  }
+  deriving (Generic, Eq, Show, FromJSON, ToJSON, ToSchema)
