3bd8361326fa00c6d0f6f4b42920a6e1ac7776bd
khuzema786 <khomosikhuzema894@gmail.com>
2024-05-13 13:16:52 +0530

    backend/enhancement: Bridge Onboarding

--- Backend/dev/migrations-read-only/dynamic-offer-driver-app/document_verification_config.sql
+++ Backend/dev/migrations-read-only/dynamic-offer-driver-app/document_verification_config.sql
@@ -41,3 +41,13 @@ ALTER TABLE atlas_driver_offer_bpp.document_verification_config ADD COLUMN is_de
 ------- SQL updates -------
 
 ALTER TABLE atlas_driver_offer_bpp.document_verification_config ALTER COLUMN is_default_enabled_on_manual_verification SET DEFAULT true;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_driver_offer_bpp.document_verification_config ADD COLUMN is_image_validation_required boolean NOT NULL default true;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_driver_offer_bpp.document_verification_config ADD COLUMN do_strict_verifcation boolean NOT NULL default true;
