465d6b103c7eabc5b298e4fe6e7575e933abc932
Akhilesh Bhadauriya <akhilesh.b@juspay.in>
2024-05-20 19:23:28 +0530

    Backend/refactor:CAC-enable-pollling-optional

--- Backend/app/provider-platform/dynamic-offer-driver-app/Allocator/src/App.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Allocator/src/App.hs
@@ -55,14 +55,14 @@ import qualified Storage.CachedQueries.Merchant as Storage
 
 createCAC :: AppCfg -> IO ()
 createCAC appCfg = do
-  cacStatus <- CM.initCACClient appCfg.cacConfig.host (fromIntegral appCfg.cacConfig.interval) appCfg.cacTenants
+  cacStatus <- CM.initCACClient appCfg.cacConfig.host (fromIntegral appCfg.cacConfig.interval) appCfg.cacTenants appCfg.cacConfig.enablePolling
   case cacStatus of
     0 -> CM.startCACPolling appCfg.cacTenants
     _ -> do
       -- logError "CAC client failed to start"
       threadDelay 1000000
       B.bool (pure ()) (createCAC appCfg) appCfg.cacConfig.retryConnection
-  superPositionStatus <- CM.initSuperPositionClient appCfg.superPositionConfig.host (fromIntegral appCfg.superPositionConfig.interval) appCfg.superPositionConfig.tenants
+  superPositionStatus <- CM.initSuperPositionClient appCfg.superPositionConfig.host (fromIntegral appCfg.superPositionConfig.interval) appCfg.superPositionConfig.tenants appCfg.superPositionConfig.enablePolling
   case superPositionStatus of
     0 -> CM.runSuperPositionPolling appCfg.superPositionConfig.tenants
     _ -> do
