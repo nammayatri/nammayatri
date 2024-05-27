b0a69a0db62f3ae72e64742cd30bd656e06f6e47
Shailesh Gahlawat <shailesh.techcourse@gmail.com>
2024-05-27 22:33:43 +0530

    frontend/log: add log for trigger service

--- Frontend/android-native/mobility-app/src/main/java/in/juspay/mobility/app/OverlayMessagingService.java
+++ Frontend/android-native/mobility-app/src/main/java/in/juspay/mobility/app/OverlayMessagingService.java
@@ -502,7 +502,7 @@ public class OverlayMessagingService extends Service {
         switch (action) {
             case "SET_DRIVER_ONLINE":
                 RideRequestUtils.updateDriverStatus(true, "ONLINE", this, false);
-                RideRequestUtils.restartLocationService(this);
+                RideRequestUtils.restartLocationService(this, "");
                 break;
             case "NAVIGATE":
                 openNavigation(editLat, editlon);
