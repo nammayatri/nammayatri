78379fcf8c679c237f5d8d9ad6d3f7dae17c86a3
Utkarsh Pandey <utkarshpandey911@gmail.com>
2024-06-11 13:02:48 +0530

    backend/fix/list_bookings_and_ambulance

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/Driver.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/Driver.hs
@@ -1940,8 +1940,8 @@ acceptScheduledBooking ::
 acceptScheduledBooking (personId, merchantId, _) clientId bookingId = do
   booking <- runInReplica $ QBooking.findById bookingId >>= fromMaybeM (BookingDoesNotExist bookingId.getId)
   driver <- runInReplica $ QPerson.findById personId >>= fromMaybeM (PersonNotFound personId.getId)
-  driverInfo <- QDriverInformation.findById personId >>= fromMaybeM DriverInfoNotFound
-  when driverInfo.onRide $ throwError DriverOnRide
+  activeRide <- runInReplica $ QRide.getActiveByDriverId driver.id
+  unless (isNothing activeRide) $ throwError (RideInvalidStatus "Cannot accept booking during active ride.")
   mbActiveSearchTry <- QST.findActiveTryByQuoteId booking.quoteId
   void $ acceptStaticOfferDriverRequest mbActiveSearchTry driver booking.quoteId Nothing merchantId clientId -- handle driver blocked
   pure Success
