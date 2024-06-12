435175b31d58bc24066a4228bfa3d79dc7ed2752
Jaypal Mudaliyar <77069644+jaypalmudaliyar24@users.noreply.github.com>
2024-06-13 04:10:44 +0530

    backend/fix: metro local migration fixes

--- Backend/dev/migrations/rider-app/1268-partner-organization.sql
+++ Backend/dev/migrations/rider-app/1268-partner-organization.sql
@@ -1,4 +1,5 @@
--- NOTE: Only for Local, DON'T RUN IN MATSER OR PROD
+-- NOTE: Below block queries only for Local, DON'T RUN IN MATSER OR PROD
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 INSERT INTO atlas_app.partner_organization (org_id, merchant_id, name, api_key_hash, api_key_encrypted, created_at, updated_at)
 VALUES
     (md5(random()::TEXT || clock_timestamp()::TEXT || random()::TEXT || random()::TEXT || random()::TEXT || random()::TEXT)::UUID,
@@ -6,6 +7,28 @@ VALUES
     'here goes the encrypted api key',
     now(), now());
 -- Local api key: d4212a34-9beb-ae42-124d-31e3c48a935c
+
+DELETE FROM atlas_app.station where code in ('LC5TKn9rGqWQHwXxWbdzeEZ6vXjdNV', '6ZVHrYAsHdGxShBlNypUstSxs5wRk4');
+
+UPDATE atlas_app.beckn_config SET vehicle_category = 'METRO' WHERE vehicle_category = 'CAB' and domain = 'FRFS';
+
+INSERT INTO atlas_app.station
+    (address, code, id, lat, lon, name, vehicle_type, merchant_id, merchant_operating_city_id, created_at, updated_at)
+VALUES
+    (null, 'LC5TKn9rGqWQHwXxWbdzeEZ6vXjdNV', atlas_app.uuid_generate_v4(), 9.9834, 76.2823, 'MG Road', 'METRO', 'da4e23a5-3ce6-4c37-8b9b-41377c3c1a52', 'namma-yatri-0-0000-0000-00000000city', now(), now()),
+    (null, '6ZVHrYAsHdGxShBlNypUstSxs5wRk4', atlas_app.uuid_generate_v4(), 10.0251, 76.3083, 'Edapally Jn.', 'METRO', 'da4e23a5-3ce6-4c37-8b9b-41377c3c1a52', 'namma-yatri-0-0000-0000-00000000city', now(), now());
+
+WITH MGS AS (SELECT * from atlas_app.station WHERE code = 'LC5TKn9rGqWQHwXxWbdzeEZ6vXjdNV' and merchant_id = 'da4e23a5-3ce6-4c37-8b9b-41377c3c1a52'),
+POrg AS (SELECT * from atlas_app.partner_organization WHERE name = 'GOOGLE_MAPS_METRO')
+INSERT INTO atlas_app.partner_org_station (partner_org_id, station_id, name, partner_org_station_id)
+SELECT org_id as partner_org_id, id as station_id, 'M.G. Road' as name, 'ChIJfeB9G1ANCDsRWdypiGiYrJo' as partner_org_station_id FROM MGS CROSS JOIN POrg;
+
+WITH EDS AS (SELECT * from atlas_app.station WHERE code = '6ZVHrYAsHdGxShBlNypUstSxs5wRk4' and merchant_id = 'da4e23a5-3ce6-4c37-8b9b-41377c3c1a52'),
+POrg AS (SELECT * from atlas_app.partner_organization WHERE name = 'GOOGLE_MAPS_METRO')
+INSERT INTO atlas_app.partner_org_station (partner_org_id, station_id, name, partner_org_station_id)
+SELECT org_id as partner_org_id, id as station_id, 'Edapally' as name, 'ChIJrwwkGa8NCDsRSc6sGbTBQC4' as partner_org_station_id FROM EDS CROSS JOIN POrg;
+----------------------------------------------------------------------------** END **--------------------------------------------------------------------------------------------
+
 
 -- INSERT entries for partner_org_config for each partner_organization for REGISTRATION, RATE_LIMIT, TICKET_SMS and BPP_STATUS_CALL config types
 WITH RCfg AS (SELECT 'REGISTRATION' as config_type, '{ "fakeOtp": "7891", "sessionConfig": {"attempts": 0, "authExpiry": 0, "tokenExpiry": 1} }'::json as config_json)
@@ -23,25 +46,6 @@ SELECT org_id as partner_org_id, TSCfg.config_type as config_type, TSCfg.config_
 WITH BPPStCallCfg AS (SELECT 'BPP_STATUS_CALL' as config_type, '{ "intervalInSec": 10 }'::json as config_json)
 INSERT INTO atlas_app.partner_org_config (partner_org_id, config_type, config_json)
 SELECT org_id as partner_org_id, BPPStCallCfg.config_type as config_type, BPPStCallCfg.config_json as config_json FROM atlas_app.partner_organization CROSS JOIN BPPStCallCfg;
-
--- NOTE: Only for Local, DON'T RUN IN MATSER OR PROD
-INSERT INTO atlas_app.station
-    (address, code, id, lat, lon, name, vehicle_type, merchant_id, merchant_operating_city_id, created_at, updated_at)
-VALUES
-    (null, 'LC5TKn9rGqWQHwXxWbdzeEZ6vXjdNV', atlas_app.uuid_generate_v4(), 9.9834, 76.2823, 'MG Road', 'METRO', 'da4e23a5-3ce6-4c37-8b9b-41377c3c1a52', 'namma-yatri-0-0000-0000-00000000city', now(), now()),
-    (null, '6ZVHrYAsHdGxShBlNypUstSxs5wRk4', atlas_app.uuid_generate_v4(), 10.0251, 76.3083, 'Edapally Jn.', 'METRO', 'da4e23a5-3ce6-4c37-8b9b-41377c3c1a52', 'namma-yatri-0-0000-0000-00000000city', now(), now());
-
--- NOTE: Only for Local, DON'T RUN IN MATSER OR PROD
-WITH MGS AS (SELECT * from atlas_app.station WHERE code = 'LC5TKn9rGqWQHwXxWbdzeEZ6vXjdNV' and merchant_id = 'da4e23a5-3ce6-4c37-8b9b-41377c3c1a52'),
-POrg AS (SELECT * from atlas_app.partner_organization WHERE name = 'GOOGLE_MAPS_METRO')
-INSERT INTO atlas_app.partner_org_station (partner_org_id, station_id, name, partner_org_station_id)
-SELECT org_id as partner_org_id, id as station_id, 'M.G. Road' as name, 'ChIJfeB9G1ANCDsRWdypiGiYrJo' as partner_org_station_id FROM MGS CROSS JOIN POrg;
-
--- NOTE: Only for Local, DON'T RUN IN MATSER OR PROD
-WITH EDS AS (SELECT * from atlas_app.station WHERE code = '6ZVHrYAsHdGxShBlNypUstSxs5wRk4' and merchant_id = 'da4e23a5-3ce6-4c37-8b9b-41377c3c1a52'),
-POrg AS (SELECT * from atlas_app.partner_organization WHERE name = 'GOOGLE_MAPS_METRO')
-INSERT INTO atlas_app.partner_org_station (partner_org_id, station_id, name, partner_org_station_id)
-SELECT org_id as partner_org_id, id as station_id, 'Edapally' as name, 'ChIJrwwkGa8NCDsRSc6sGbTBQC4' as partner_org_station_id FROM EDS CROSS JOIN POrg;
 
 -- DROP QUERY, can be run before release as well, as table is not used anywhere in old / new version.
 DROP TABLE atlas_app.frfs_trip;
