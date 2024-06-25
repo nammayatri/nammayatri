0b7ab850e188548234eff69e4a2992564c3d1e90
Vijay Gupta <vijayrauniyar1818@gmail.com>
2024-06-25 14:48:28 +0530

    backend/removing search req dependency from estimate

--- Backend/app/rider-platform/rider-app/Main/src/Domain/Action/UI/EstimateBP.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Domain/Action/UI/EstimateBP.hs
@@ -27,29 +27,16 @@ import qualified Storage.Queries.Booking as QRB
 import qualified Storage.Queries.Estimate as QEst
 import qualified Storage.Queries.Quote as QQuote
 import qualified Storage.Queries.Ride as QRide
-import qualified Storage.Queries.SearchRequest as QSearch
 import Tools.Auth
 
 getRideEstimateBreakup :: ((Kernel.Prelude.Maybe (Id.Id Person.Person), Id.Id Merchant.Merchant) -> Id.Id Ride.Ride -> Environment.Flow API.Types.UI.EstimateBP.EstimateDetailsRes)
 getRideEstimateBreakup (_, _) rideId_ = do
   ride <- B.runInReplica $ QRide.findById rideId_ >>= fromMaybeM (RideDoesNotExist rideId_.getId)
   booking <- B.runInReplica $ QRB.findById ride.bookingId >>= fromMaybeM (BookingDoesNotExist ride.bookingId.getId)
-  case booking.quoteId of
-    Just quoteId -> do
-      mbQuote <- B.runInReplica $ QQuote.findById quoteId
-      case mbQuote of
-        Just quote -> do
-          mbSearchReq <- B.runInReplica $ QSearch.findById quote.requestId
-          case mbSearchReq of
-            Just searchreq -> do
-              mbEstimate <- B.runInReplica $ QEst.findBySRIdAndStatus COMPLETED searchreq.id
-              case mbEstimate of
-                Just estimate -> do
-                  breakup <- CH.findAllByEstimateIdT (Id.cast estimate.id)
-                  return $ EstimateDetailsRes (transformEstimate <$> breakup)
-                Nothing -> pure (EstimateDetailsRes [])
-            Nothing -> pure (EstimateDetailsRes [])
-        Nothing -> pure (EstimateDetailsRes [])
+  case booking.estimateId of
+    Just estimateId -> do
+      breakup <- CH.findAllByEstimateIdT estimateId
+      return $ EstimateDetailsRes (transformEstimate <$> breakup)
     Nothing -> pure (EstimateDetailsRes [])
 
 transformEstimate :: CH.EstimateBreakup -> API.Types.UI.EstimateBP.EstimateBreakup
