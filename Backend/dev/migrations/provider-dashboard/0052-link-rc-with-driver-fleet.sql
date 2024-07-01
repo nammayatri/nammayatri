66b90983403869004477dc29df7c5e21d8a590f6
Ashish Saini <ashish.saini@ashishsaini-Y7WP7YXLCD.local>
2024-07-01 16:15:38 +0530

    backend/fleet/added_cancellation_ride_count

--- Backend/dev/migrations/provider-dashboard/0052-link-rc-with-driver-fleet.sql
+++ Backend/dev/migrations/provider-dashboard/0052-link-rc-with-driver-fleet.sql
@@ -1,4 +1,4 @@
-`INSERT INTO atlas_bpp_dashboard.access_matrix
+INSERT INTO atlas_bpp_dashboard.access_matrix
         (id, role_id, api_entity, user_action_type, user_access_type, created_at, updated_at)
 (SELECT atlas_bpp_dashboard.uuid_generate_v4() as id,
         id as role_id,
