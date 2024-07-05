8705b60e13e4035f421bd90f207b82ad2f2adb4f
Jaypal Mudaliyar <77069644+jaypalmudaliyar24@users.noreply.github.com>
2024-07-05 15:30:10 +0530

    backend/fix: makes card charge multiplier result always positive

--- Backend/dev/migrations/dynamic-offer-driver-app/0461-time-based-farepolicy.sql
+++ Backend/dev/migrations/dynamic-offer-driver-app/0461-time-based-farepolicy.sql
@@ -1,9 +1,19 @@
 -- ADD COLUMN: fare_policy table
 ALTER TABLE
     atlas_driver_offer_bpp.fare_policy
-ADD COLUMN per_distance_unit_insurance_charge double precision NOT NULL DEFAULT 0.0,
-ADD COLUMN card_charge_per_distance_unit_multiplier double precision NOT NULL DEFAULT 0,
-ADD COLUMN fixed_card_charge double precision NOT NULL DEFAULT 0.0;
+ADD COLUMN per_distance_unit_insurance_charge double precision,
+ADD COLUMN card_charge_per_distance_unit_multiplier double precision,
+ADD COLUMN fixed_card_charge double precision;
+
+-- NOTE: Queries for MASTER
+-- ALTER TABLE atlas_driver_offer_bpp.fare_policy ALTER COLUMN per_distance_unit_insurance_charge DROP NOT NULL;
+-- UPDATE atlas_driver_offer_bpp.fare_policy SET per_distance_unit_insurance_charge = NULL;
+--
+-- ALTER TABLE atlas_driver_offer_bpp.fare_policy ALTER COLUMN card_charge_per_distance_unit_multiplier DROP NOT NULL;
+-- UPDATE atlas_driver_offer_bpp.fare_policy SET card_charge_per_distance_unit_multiplier = NULL;
+--
+-- ALTER TABLE atlas_driver_offer_bpp.fare_policy ALTER COLUMN fixed_card_charge DROP NOT NULL;
+-- UPDATE atlas_driver_offer_bpp.fare_policy SET fixed_card_charge = NULL;
 
 -- ADD COLUMN: fare_parameters table
 ALTER TABLE
