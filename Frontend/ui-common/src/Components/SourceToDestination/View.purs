dd3095765e346c11949d4e9d30fd8dbb4fdefebc
Radhika Soni <radhika.soni@Radhika-Soni-C02G93UDMD6M.local>
2024-07-08 18:50:25 +0530

    changes

--- Frontend/ui-common/src/Components/SourceToDestination/View.purs
+++ Frontend/ui-common/src/Components/SourceToDestination/View.purs
@@ -226,6 +226,7 @@ separatorConfig config =
   , layoutWidth : config.destinationImageConfig.width
   , layoutHeight : config.destinationImageConfig.height
   , color : Color.black500
+  , margin : MarginVertical 2 2 
   }
 
 getDistanceLayoutHeight :: Config -> Length
