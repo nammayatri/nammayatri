aa4a6b5f89ca9ac5298a15b427f52e9c619ffa59
Aryan chaudhary <aryanchaudhary1381@gmail.com>
2024-07-11 13:08:41 +0530

    frontend: Disabled check version

--- Frontend/ui-customer/src/Flow.purs
+++ Frontend/ui-customer/src/Flow.purs
@@ -219,7 +219,7 @@ baseAppFlow :: GlobalPayload -> Boolean -> FlowBT String Unit
 baseAppFlow gPayload callInitUI = do
   lift $ lift $ void $ fork $ doAff $ makeAff \cb -> runEffectFn3 renewFile "v1-assets_downloader.jsa" "https://assets.moving.tech/beckn/bundles/mobility-core/0.0.5/v1-assets_downloader.jsa" (cb <<< Right) $> nonCanceler
   liftFlowBT $ markPerformance "BASE_APP_FLOW"
-  checkVersion
+  -- checkVersion
   baseAppStorage -- TODO:: Restructure the files and names
   baseAppLogs
   liftFlowBT $ runEffectFn1 resetIdMap ""
