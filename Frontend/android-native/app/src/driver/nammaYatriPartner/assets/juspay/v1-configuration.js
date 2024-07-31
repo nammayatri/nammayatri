f305312f6ccaffecc6c3f6140a0449d3a0035782
kavyashree.s <kavyashree.s@juspay.in>
2024-07-31 16:56:36 +0530

    fix udupi city code

--- Frontend/android-native/app/src/driver/nammaYatriPartner/assets/juspay/v1-configuration.js
+++ Frontend/android-native/app/src/driver/nammaYatriPartner/assets/juspay/v1-configuration.js
@@ -1387,7 +1387,7 @@ window.getMerchantConfig = function () {
       {
         "cityName" : "Udupi",
         "mapImage" : "ny_ic_udupi_map",
-        "cityCode" : "std:0820",
+        "cityCode" : "std:08200",
         "showSubscriptions" : false,
         "enableAdvancedBooking" : true,
         "advancedRidePopUpYoutubeLink" : "" ,
