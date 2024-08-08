a25eb35ca23224fecc124532ddf92fb4f39c5bf6
Ritika <ritikahanish@gmail.com>
2024-08-08 16:08:16 +0530

    backend/new autocomplete api integration

---
+++ Backend/dev/migrations/rider-app/1296-added-new-place-api-config-merchant-service-config.sql
@@ -0,0 +1,15 @@
+UPDATE atlas_app.merchant_service_config
+SET config_json = jsonb_set(
+    config_json::jsonb,
+    '{googlePlaceNewUrl}',
+    ('"https://places.googleapis.com/v1/"')::jsonb
+)
+WHERE service_name = 'Maps_Google';
+
+UPDATE atlas_app.merchant_service_config
+SET config_json = jsonb_set(
+    config_json::jsonb,
+    '{useNewPlaces}',
+    json('true')::jsonb
+)::json
+WHERE service_name = 'Maps_Google';
