32cf0a4f96f807a09a8aef555a02670a5529f51e
Akhilesh Singh Bhadauriya <akhileshsingh2314@gmail.com>
2024-08-20 12:24:06 +0530

    Backend/fix:no-fare-update-if-driver-passed-through-drop

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Tools/Utils.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Tools/Utils.hs
@@ -3,7 +3,13 @@ module Tools.Utils where
 import qualified Data.Aeson as A
 import qualified Data.Aeson.Key as A
 import qualified Data.Text as T
+import qualified Domain.Types.Booking as DB
+import qualified Domain.Types.TransporterConfig as DTConf
+import Kernel.External.Maps.HasCoordinates (getCoordinates)
+import Kernel.External.Maps.Types
 import Kernel.Prelude
+import Kernel.Types.Distance (metersToHighPrecMeters)
+import Kernel.Utils.CalculateDistance (distanceBetweenInMeters)
 import Lib.Yudhishthira.Tools.Utils
 
 convertTags :: [Text] -> A.Value
@@ -15,3 +21,9 @@ convertTags input = A.object $ map toObject pairs
     toObject xs = do
       let reconstructed = T.intercalate "#" xs
       (A.fromText $ T.strip reconstructed :: A.Key) A..= A.Null
+
+isDropInsideThreshold :: DB.Booking -> DTConf.TransporterConfig -> LatLong -> Bool
+isDropInsideThreshold booking thresholdConfig currLoation = do
+  let dropLocThreshold = metersToHighPrecMeters thresholdConfig.dropLocThreshold
+      locationDiff = maybe 0 (\toLocation -> abs $ distanceBetweenInMeters (getCoordinates toLocation) currLoation) booking.toLocation
+   in locationDiff <= dropLocThreshold
