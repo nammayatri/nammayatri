2fea69ed3c2bb7c13eae2b5eb518f9fc4496981e
khuzema786 <khomosikhuzema894@gmail.com>
2024-08-23 04:23:35 +0530

    backend/feat: Ticket's People Category Dynamic Time Based Fare

---
+++ Backend/app/rider-platform/rider-app/Main/src/Storage/Queries/ServicePeopleCategoryExtra.hs
@@ -0,0 +1,25 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+
+module Storage.Queries.ServicePeopleCategoryExtra where
+
+import Control.Applicative ((<|>))
+import Data.Time hiding (getCurrentTime)
+import Domain.Types.ServicePeopleCategory
+import Kernel.Beam.Functions
+import Kernel.External.Encryption
+import Kernel.Prelude
+import Kernel.Types.Error
+import Kernel.Types.Id
+import Kernel.Types.TimeBound
+import Kernel.Utils.Common (CacheFlow, EsqDBFlow, MonadFlow, fromMaybeM, getCurrentTime)
+import Sequelize as Se
+import qualified Storage.Beam.ServicePeopleCategory as BeamR
+import Storage.Queries.OrphanInstances.ServicePeopleCategory
+
+-- Extra code goes here --
+findServicePeopleCategoryById :: (EsqDBFlow m r, MonadFlow m, CacheFlow m r) => Id ServicePeopleCategory -> Day -> m (Maybe ServicePeopleCategory)
+findServicePeopleCategoryById id day = do
+  servicePeopleCategories :: [ServicePeopleCategory] <- findAllWithKV [Se.Is BeamR.id $ Se.Eq $ getId id]
+  let boundedServicePeopleCategories = findBoundedDomain (filter (\cfg -> cfg.timeBounds /= Unbounded) servicePeopleCategories) (UTCTime day $ secondsToDiffTime 19800)
+  pure $ listToMaybe boundedServicePeopleCategories <|> find (\cfg -> cfg.timeBounds == Unbounded) servicePeopleCategories
