Aj-cmd-afk/NY_DEV_Main

backend/enhance/stop_details_dashboard

Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Storage/Queries/LocationMappingExtra.hs
@@ -44,6 +44,12 @@ findByEntityId entityId =
     ]
     (Just (Se.Desc BeamLM.createdAt))
 
+findAllStopIds :: (MonadFlow m, EsqDBFlow m r, CacheFlow m r) => Text -> m [LocationMapping]
+findAllStopIds entityId = do
+  findAllWithKVAndConditionalDB
+    [Se.And [Se.Is BeamLM.entityId $ Se.Eq entityId, Se.Is BeamLM.version $ Se.Eq latestTag]]
+    (Just (Se.Asc BeamLM.createdAt))
+
 getLatestStartByEntityId :: (MonadFlow m, EsqDBFlow m r, CacheFlow m r) => Text -> m (Maybe LocationMapping)
 getLatestStartByEntityId entityId =
   findOneWithKV

nammayatri/nammayatri

Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Storage/Queries/LocationMappingExtra.hs
@@ -44,6 +44,12 @@ findByEntityId entityId =
     ]
     (Just (Se.Desc BeamLM.createdAt))
 
+findAllStops :: (MonadFlow m, EsqDBFlow m r, CacheFlow m r) => Text -> m [LocationMapping]
+findAllStops entityId = do
+  findAllWithKVAndConditionalDB
+    [Se.And [Se.Is BeamLM.entityId $ Se.Eq entityId, Se.Is BeamLM.version $ Se.Eq latestTag]]
+    (Just (Se.Asc BeamLM.createdAt))
+
 getLatestStartByEntityId :: (MonadFlow m, EsqDBFlow m r, CacheFlow m r) => Text -> m (Maybe LocationMapping)
 getLatestStartByEntityId entityId =
   findOneWithKV