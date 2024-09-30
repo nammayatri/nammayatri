9a1bf6ca41b84542b84ccfe36bf128a70b327ab7
Ritika <ritikahanish@gmail.com>
2024-09-30 16:16:03 +0530

    backend/debug log added as error for debugging DP

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/SharedLogic/FarePolicy.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/SharedLogic/FarePolicy.hs
@@ -648,7 +648,7 @@ getCongestionChargeMultiplierFromModel' timeDiffFromUtc (Just fromLocGeohash) to
   let allLogics = appDynamicLogics <&> (.logic)
   let dynamicPricingData = DynamicPricingData {speedKmh, distanceInKm, supplyDemandRatioFromLoc = fromMaybe 0.0 mbSupplyDemandRatioFromLoc, supplyDemandRatioToLoc = fromMaybe 0.0 mbSupplyDemandRatioToLoc, toss}
   response <- try @_ @SomeException $ LYTU.runLogics allLogics dynamicPricingData
-  logError $ "DynamicPricing Req Logics : " <> show allLogics <> " and data is : " <> show dynamicPricingData <> " and response is : " <> show response
+  logInfo $ "DynamicPricing Req Logics : " <> show allLogics <> " and data is : " <> show dynamicPricingData <> " and response is : " <> show response
   case response of
     Left e -> do
       logError $ "Error in running DynamicPricingLogics - " <> show e <> " - " <> show dynamicPricingData <> " - " <> show allLogics
@@ -657,7 +657,7 @@ getCongestionChargeMultiplierFromModel' timeDiffFromUtc (Just fromLocGeohash) to
       case (A.fromJSON resp.result :: Result DynamicPricingResult) of
         A.Success result -> return $ fmap (\congestionCharge -> (congestionCharge, result.version)) result.congestionFeePerMin
         A.Error err -> do
-          logError $ "Error in parsing DynamicPricingResult - " <> show err
+          logError $ "Error in parsing DynamicPricingResult - " <> show err <> " - " <> show resp <> " - " <> show dynamicPricingData <> " - " <> show allLogics
           return Nothing
 getCongestionChargeMultiplierFromModel' _ _ _ _ _ _ _ = pure Nothing
 
