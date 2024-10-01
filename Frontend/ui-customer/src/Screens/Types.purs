a180af45cedd3cd1bc3da6fcf410023a8d76a5d6
Pravinkumar S <spravinkumar9952@gmail.com>
2024-10-01 23:54:36 +0530

    frontend-fix: driver side prod issue fix

--- Frontend/ui-customer/src/Screens/Types.purs
+++ Frontend/ui-customer/src/Screens/Types.purs
@@ -1594,6 +1594,7 @@ type LocationListItemState = {
   , recencyDate :: Maybe String
   , locationScore :: Maybe Number
   , dynamicAction :: Maybe CRT.RemoteAC
+  , types :: Maybe (Array String)
 }
 
 type SuggestionsMap = Map SourceGeoHash Suggestions
