9353fc9102e7ba73c1fc99aea4e32f05af9bebb4
ARYAN SHARMA <aryansharma@Nammayatris-MacBook-Pro.local>
2024-10-09 19:53:23 +0530

    frontend/fix:update location format in scheduled ride booking list api

--- Frontend/ui-driver/src/Services/Endpoint.purs
+++ Frontend/ui-driver/src/Services/Endpoint.purs
@@ -16,7 +16,7 @@
 module Services.EndPoints where
 
 import Data.Maybe (Maybe(..))
-import Prelude (show, unit, (<>), (==), (*))
+import Prelude (show, unit, (<>), (==), (*) , (&&))
 import Services.Config (getBaseUrl)
 
 triggerOTP :: String -> String
@@ -387,7 +387,7 @@ verifyUPI dummy = (getBaseUrl "") <> "/driver/profile/verify/vpaStatus"
 getCoinInfo :: String -> String
 getCoinInfo _ = (getBaseUrl "") <> "/coins/info"
 getScheduledBookingList :: String -> String -> String -> String ->String -> String -> String -> String
-getScheduledBookingList limit offset from to  tripCategory lat lon  =  (getBaseUrl "") <> "/driver/scheduledBooking/list?limit="<>limit<>"&offset="<>offset<> (if from == "null" then "" else "&from=" <> from) <> (if to == "null" then "" else "&to=" <> to)<>(if lat == "0.0" then "" else "&lat=" <> lat) <> (if lon == "0.0" then "" else "&lon=" <> lon)  
+getScheduledBookingList limit offset from to  tripCategory lat lon  =  (getBaseUrl "") <> "/driver/scheduledBooking/list?limit="<>limit<>"&offset="<>offset<> (if from == "null" then "" else "&from=" <> from) <> (if to == "null" then "" else "&to=" <> to)<>(if lat == "0.0" && lon == "0.0" then ""  else ("&currentLocation=" <> lat <> "," <> lon))  
 
 scheduleBookingAccept :: String -> String
 scheduleBookingAccept bookingId = (getBaseUrl "") <> "/driver/accept/scheduledBooking?bookingId="<>bookingId
