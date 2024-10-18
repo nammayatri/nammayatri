5d6d34d54b59bebcde7a142db832934a016d7f91
rupakkorde <rupak.korde@juspay.in>
2024-10-18 17:40:21 +0530

    backend/enh/increasing-cancellation-rate-based-on-nammatag

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Types/Yudhishthira.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Types/Yudhishthira.hs
@@ -5,12 +5,23 @@ module Domain.Types.Yudhishthira where
 
 import qualified Data.Aeson as A
 import qualified Domain.Types.Booking as SRB
+import qualified Domain.Types.BookingCancellationReason as DBCR
 import qualified Domain.Types.Ride as DRide
 import qualified Domain.Types.SearchRequest as DSR
 import Kernel.Prelude
 import qualified Lib.Yudhishthira.Types.Application as YA
 import qualified Lib.Yudhishthira.Types.Common as YTC
 import qualified Lib.Yudhishthira.TypesTH as YTH
+
+data CancelRideTagData = CancelRideTagData
+  { ride :: DRide.Ride,
+    booking :: SRB.Booking,
+    cancellationReason :: DBCR.BookingCancellationReason,
+    callAtemptByDriver :: Bool,
+    isDestinationEdited :: Bool,
+    currentTime :: UTCTime
+  }
+  deriving (Generic, Show, FromJSON, ToJSON)
 
 data TagData = TagData
   { searchRequest :: DSR.SearchRequest,
@@ -28,10 +39,12 @@ data EndRideTagData = EndRideTagData
 
 $(YTH.generateGenericDefault ''TagData)
 $(YTH.generateGenericDefault ''EndRideTagData)
+$(YTH.generateGenericDefault ''CancelRideTagData)
 
 instance YTC.LogicInputLink YA.ApplicationEvent where
   getLogicInputDef a =
     case a of
       YA.Search -> fmap A.toJSON . listToMaybe $ YTH.genDef (Proxy @TagData)
       YA.RideEnd -> fmap A.toJSON . listToMaybe $ YTH.genDef (Proxy @EndRideTagData)
+      YA.RideCancel -> fmap A.toJSON . listToMaybe $ YTH.genDef (Proxy @CancelRideTagData)
       _ -> Nothing
