48c32593319ba6cca72fce515e3d888ad23f1854
shreyaxh <shreyyash2@gmail.com>
2024-10-21 20:42:17 +0530

    backend/fix/adds driverTagScore to Search Request for Driver

--- Backend/dev/migrations-read-only/dynamic-offer-driver-app/search_request_for_driver.sql
+++ Backend/dev/migrations-read-only/dynamic-offer-driver-app/search_request_for_driver.sql
@@ -151,25 +151,8 @@ ALTER TABLE atlas_driver_offer_bpp.search_request_for_driver ADD COLUMN parcel_t
 ALTER TABLE atlas_driver_offer_bpp.search_request_for_driver ADD COLUMN parcel_quantity integer ;
 
 
-------- SQL updates -------
-
-
 
 
 ------- SQL updates -------
 
-
-
-
-------- SQL updates -------
-
-
-
-
-------- SQL updates -------
-
-
-
-
-------- SQL updates -------
-
+ALTER TABLE atlas_driver_offer_bpp.search_request_for_driver ADD COLUMN driver_tag_score text ;
