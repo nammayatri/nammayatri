efc51a89dd5251f10fa5534764bbbbb31b530f9f
Rohit Saini <rohitsaini@Rohits-MBP.domain.name>
2024-10-22 18:34:33 +0530

    Backend-RideAssignedCard

--- Backend/dev/migrations-read-only/dynamic-offer-driver-app/search_request_for_driver.sql
+++ Backend/dev/migrations-read-only/dynamic-offer-driver-app/search_request_for_driver.sql
@@ -124,3 +124,7 @@ ALTER TABLE atlas_driver_offer_bpp.search_request_for_driver ADD COLUMN pooling_
 ------- SQL updates -------
 
 ALTER TABLE atlas_driver_offer_bpp.search_request_for_driver ADD COLUMN upgrade_cab_request boolean ;
+
+------- SQL updates -------
+
+ALTER TABLE atlas_driver_offer_bpp.search_request_for_driver ADD COLUMN is_favourite boolean ;
