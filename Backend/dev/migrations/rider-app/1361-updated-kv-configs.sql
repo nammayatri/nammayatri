2248fa6a4996b63c7be6ec56699b45c282673c33
dushyant-2002 <Dushyant.workspace@gmail.com>
2024-11-19 18:37:42 +0530

    backend/fix : Tables Compilation fix and added kv configs query

---
+++ Backend/dev/migrations/rider-app/1361-updated-kv-configs.sql
@@ -0,0 +1,17 @@
+UPDATE atlas_app.system_configs
+SET config_value = '{
+  "enableKVForRead": ["table1", "table2", "table3"],
+  "enableKVForWriteAlso": [
+    { "nameOfTable": "table1", "percentEnable": 100 }
+  ],
+  "disableForKV": ["table1", "table2", "table3"],
+  "kvTablesTtl": {
+    "table1": 3600,
+    "table2": 7200,
+    "table3": 14400
+  },
+  "useCAC": ["table4", "table5"],
+  "useCACForFrontend": true,
+  "readFromMasterDb": ["table6", "table7"]
+}'
+WHERE id ='kv_configs';
