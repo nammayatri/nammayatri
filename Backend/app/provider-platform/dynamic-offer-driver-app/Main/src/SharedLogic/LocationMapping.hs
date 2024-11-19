76ddab02fe2b8169633a38f9f24203a4e1c85ef8
saimanohar.veeravajhula <saimanoharveeravajhula@gmail.com>
2024-11-19 15:23:45 +0530

    backend/fix/version-edit-location

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/SharedLogic/LocationMapping.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/SharedLogic/LocationMapping.hs
@@ -38,8 +38,18 @@ buildPickUpLocationMapping locationId entityId tag merchantId merchantOperatingC
 buildDropLocationMapping :: (MonadFlow m, EsqDBFlow m r, CacheFlow m r) => Id DL.Location -> Text -> DLM.LocationMappingTags -> Maybe (Id Merchant) -> Maybe (Id MerchantOperatingCity) -> m DLM.LocationMapping
 buildDropLocationMapping locationId entityId tag merchantId merchantOperatingCityId = do
   id <- generateGUID
-  noOfEntries <- QLM.countOrders entityId
-  let order = if noOfEntries == 0 then 1 else noOfEntries
+  prevOrder <- QLM.maxOrderByEntity entityId
+  let order = prevOrder + 1
+  now <- getCurrentTime
+  let version = QLM.latestTag
+      createdAt = now
+      updatedAt = now
+  QLM.updatePastMappingVersions entityId order
+  return DLM.LocationMapping {..}
+
+buildLocationMapping' :: (MonadFlow m, EsqDBFlow m r, CacheFlow m r) => Id DL.Location -> Text -> DLM.LocationMappingTags -> Maybe (Id Merchant) -> Maybe (Id MerchantOperatingCity) -> Int -> m DLM.LocationMapping
+buildLocationMapping' locationId entityId tag merchantId merchantOperatingCityId order = do
+  id <- generateGUID
   now <- getCurrentTime
   let version = QLM.latestTag
       createdAt = now
@@ -49,9 +59,8 @@ buildDropLocationMapping locationId entityId tag merchantId merchantOperatingCit
 
 buildStopsLocationMapping :: (MonadFlow m, CacheFlow m r, EsqDBFlow m r) => [DL.Location] -> Text -> DLM.LocationMappingTags -> Maybe (Id Merchant) -> Maybe (Id MerchantOperatingCity) -> m [DLM.LocationMapping]
 buildStopsLocationMapping locations entityId tag merchantId merchantOperatingCityId = do
-  noOfEntries <- QLM.countOrders entityId
-  let order = if noOfEntries == 0 then 1 else noOfEntries
-  mapM (\(location, order') -> buildStopLocationMapping location entityId tag merchantId merchantOperatingCityId order') $ zip locations [order ..]
+  let order = 1
+  mapM (\(location, order') -> buildStopLocationMapping location entityId tag merchantId merchantOperatingCityId order') (zip locations [order ..])
 
 buildStopLocationMapping :: (MonadFlow m, CacheFlow m r, EsqDBFlow m r) => DL.Location -> Text -> DLM.LocationMappingTags -> Maybe (Id Merchant) -> Maybe (Id MerchantOperatingCity) -> Int -> m DLM.LocationMapping
 buildStopLocationMapping location entityId tag merchantId merchantOperatingCityId order = do
