e7341c5daf1bc31e6cc013f42eb09274989e1d36
ErasedSoul <anirbandas0206@gmail.com>
2024-11-27 13:29:32 +0530

    backend/feat/addBlockedReasonFlag to profile api

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/Driver.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/Driver.hs
@@ -341,7 +341,8 @@ data DriverInformationRes = DriverInformationRes
     favCount :: Maybe Int,
     subscriptionEnabledForVehicleCategory :: Bool,
     isSubscriptionEnabledAtCategoryLevel :: Bool,
-    isSpecialLocWarrior :: Bool
+    isSpecialLocWarrior :: Bool,
+    blockedReasonFlag :: Maybe BlockReasonFlag
   }
   deriving (Generic, ToJSON, FromJSON, ToSchema)
 
@@ -359,6 +360,7 @@ data DriverEntityRes = DriverEntityRes
     enabled :: Bool,
     blocked :: Bool,
     blockExpiryTime :: Maybe UTCTime,
+    blockedReasonFlag :: Maybe BlockReasonFlag,
     subscribed :: Bool,
     paymentPending :: Bool,
     verified :: Bool,
@@ -906,6 +908,7 @@ buildDriverEntityRes (person, driverInfo, driverStats, merchantOpCityId) = do
         enabled = driverInfo.enabled,
         blocked = driverInfo.blocked,
         blockExpiryTime = driverInfo.blockExpiryTime,
+        blockedReasonFlag = driverInfo.blockReasonFlag,
         verified = driverInfo.verified,
         subscribed = driverInfo.subscribed,
         paymentPending = driverInfo.paymentPending,
