b1a9ad8a074c1c10c821363e31c9ddec5617f1be
Akhilesh Singh Bhadauriya <74489799+enigma-commits@users.noreply.github.com>
2024-11-28 20:02:55 +0530

    Revert "Backend/feat:removing-ride-in-progress"
    This reverts commit 62e3198c71d1282eb504fcd47d97cd6811553172.

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/SharedLogic/Ride.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/SharedLogic/Ride.hs
@@ -89,8 +89,7 @@ initializeRide merchant driver booking mbOtpCode enableFrequentLocationUpdates m
             pure otpCode
           Just otp -> pure otp
   ghrId <- CQDGR.setDriverGoHomeIsOnRideStatus driver.id booking.merchantOperatingCityId True
-  previousRideInprogress <- bool (QDI.findByPrimaryKey driver.id) (pure Nothing) (booking.isScheduled)
-  let isDriverOnRide = bool (Just False) (previousRideInprogress >>= Just <$> (.onRide)) (isJust previousRideInprogress)
+  previousRideInprogress <- bool (QRide.getInProgressByDriverId driver.id) (pure Nothing) (booking.isScheduled)
   now <- getCurrentTime
   vehicle <- QVeh.findById driver.id >>= fromMaybeM (VehicleNotFound driver.id.getId)
   ride <- buildRide driver booking ghrId otpCode enableFrequentLocationUpdates mbClientId previousRideInprogress now vehicle merchant.onlinePayment enableOtpLessRide
@@ -105,7 +104,7 @@ initializeRide merchant driver booking mbOtpCode enableFrequentLocationUpdates m
         QDI.updateTripCategoryAndTripEndLocationByDriverId (cast driver.id) (Just ride.tripCategory) (Just (Maps.LatLong toLoc.lat toLoc.lon))
       QDI.updateOnRide True (cast driver.id)
     Redis.unlockRedis (offerQuoteLockKeyWithCoolDown ride.driverId)
-    when (isDriverOnRide == Just True) $ QDI.updateHasAdvancedRide (cast ride.driverId) True
+    when (isJust previousRideInprogress) $ QDI.updateHasAdvancedRide (cast ride.driverId) True
     Redis.unlockRedis (editDestinationLockKey ride.driverId)
   unless booking.isScheduled $ void $ LF.rideDetails ride.id DRide.NEW merchantId ride.driverId booking.fromLocation.lat booking.fromLocation.lon
 
@@ -179,7 +178,7 @@ buildRide ::
   Text ->
   Maybe Bool ->
   Maybe (Id DC.Client) ->
-  Maybe DDI.DriverInformation ->
+  Maybe DRide.Ride ->
   UTCTime ->
   DVeh.Vehicle ->
   Bool ->
@@ -190,7 +189,7 @@ buildRide driver booking ghrId otp enableFrequentLocationUpdates clientId previo
   shortId <- generateShortId
   deploymentVersion <- asks (.version)
   trackingUrl <- buildTrackingUrl guid
-  let previousRideToLocation = previousRide >>= (.driverTripEndLocation)
+  let previousRideToLocation = previousRide >>= (.toLocation)
   let status = bool DRide.NEW DRide.UPCOMING booking.isScheduled
   return
     DRide.Ride
