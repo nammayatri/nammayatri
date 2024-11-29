173fb56dd3ebad67c5678f5c87e0eda2a5c4d24f
VIGNESH S <vigneshsrinivasan772@gmail.com>
2024-11-29 16:47:58 +0530

    frontend/fix: updated asset_downloader to v0.0.11

--- Frontend/ui-driver/src/Flow.purs
+++ Frontend/ui-driver/src/Flow.purs
@@ -177,7 +177,7 @@ import Data.Newtype (unwrap)
 
 baseAppFlow :: Boolean -> Maybe Event -> Maybe (Either ErrorResponse GetDriverInfoResp) -> FlowBT String Unit
 baseAppFlow baseFlow event driverInfoResponse = do
-    lift $ lift $ void $ fork $ doAff $ makeAff \cb -> runEffectFn3 renewFile "v1-assets_downloader.jsa" "https://assets.moving.tech/beckn/bundles/mobility-core/0.0.10/v1-assets_downloader.jsa" (cb <<< Right) $> nonCanceler
+    lift $ lift $ void $ fork $ doAff $ makeAff \cb -> runEffectFn3 renewFile "v1-assets_downloader.jsa" "https://assets.moving.tech/beckn/bundles/mobility-core/0.0.11/v1-assets_downloader.jsa" (cb <<< Right) $> nonCanceler
     when baseFlow $ do 
       lift $ lift $ initUI
     let bundleSplashConfig = RemoteConfig.getBundleSplashConfig "splash"
