bb43cdaee70ed7b76b413b8639a4ff8387bb20fb
prakhar.saxena <prakhar.saxena@juspay.in>
2024-12-18 18:07:33 +0530

    enh : Add implementations of some functions in journey module

--- Backend/multimodal-tech-design.hs
+++ Backend/multimodal-tech-design.hs
@@ -20,6 +20,9 @@ data JourneyLegStatus =
   | Completed
   deriving (Eq, Show)
 
+completedStatus :: [JourneyLegStatus]
+completedStatus = [Cancelled, Completed]
+
 data ServiceStatus = Pending | Failed | Confirmed | Verified | Cancelled deriving (Eq, Ord, Show, Read, Generic, ToJSON, FromJSON, ToSchema)
 
 data RideStatus = UPCOMING | NEW | INPROGRESS | COMPLETED | CANCELLED deriving (Eq, Ord, Show, Read, Generic, ToJSON, FromJSON, ToSchema, ToParamSchema)
@@ -201,25 +204,55 @@ addLeg :: JourneyLeg leg m => leg -> m ()
 addLeg leg = JL.search leg -- output could be the search request
 
 getCurrentLeg :: JourneyLeg leg m => Journey -> m leg
+getCurrentLeg 
   -- get the current leg based on status
-getRemainingLegs :: JourneyLeg leg m => Journey -> m [leg]
+  -- loop through all legs and based on getState
+  journeyLegs <- getAllLegs journeyId
+  let currentLeg = find (\leg -> notElem leg.status [completedStatus]) journeyLegs
+  return currentLeg
+
+getRemainingLegs :: JourneyLeg leg m => Id Journey -> m [leg]
+getRemainingLegs journeyId = do
   -- get the remaining legs
---   getAllLegs 
+  journeyLegs <- getAllLegs journeyId
+  let remainingLegs = filter (\leg -> notElem leg.status [completedStatus]) journeyLegs
+  return remainingLegs
 --   filter -> based on status
 
 -- createLeg :: RequiedData (OTP response for each leg)
-deleteLeg :: JourneyLeg leg m => Journey -> leg -> m ()
-  -- cancel the leg if possible
-  -- remove it from the journey @@do we actually need to remove?
+deleteLeg :: JourneyLeg leg m => leg -> m ()
+  JL.cancel leg
+  -- remove it from the journey mark it deleted/skipped
   
-updateLeg :: JourneyLeg leg m => Journey -> leg -> m ()
+updateLeg :: JourneyLeg leg m => UpdateJourneyLeg -> leg -> m () 
   -- update the leg
   -- update it in the journey
 
 skipJourney :: Journey -> [leg] -> m () 
+-- skipJourney journey 
     -- getRemainingLegs 
     map update [leg]
     -- @@ call cancel for current leg
+
+endJourney :: Journey -> m ()
+endJourney 
+-- if last leg then update leg
+-- loop through and delete/update legs and journey as required
+-- call leg level cancel
+
+startJourney :: Id Journey -> ConfirmReq -> m () -- confirm request
+startJourney journeyId = do
+  -- journey <- getJourney journeyId
+  allLegs <- getAllLegs journeyId
+  mapM JL.confirm allLegs
+
+
+getAllLegs :: Id Journey -> m ()
+getAllLegs journeyId = do
+  searchRequests <- findAllByJourneyId journeyId -- in searchRequest
+  frfsSearchRequests <- findAllByJourneyId journeyId -- in FRFS search 
+  let legs = sortBy order $ transformSearchReqToJourneyLeg searchRequests  <> transformFRFSSearchReqToJourneyLegfrfs SearchRequests 
+  return legs
 
 replaceLeg :: JourneyLeg leg1 leg2 m => Journey -> [leg1] -> leg2 -> m () -- leg2 can be an array
 replaceLeg journey oldLegs newLeg = 
@@ -227,15 +260,9 @@ replaceLeg journey oldLegs newLeg =
 
 extendLeg :: JourneyLeg leg1 leg2 m => Journey -> [leg1] -> leg2 -> m ()
   forM_ (deleteLeg journey) oldLegs >> updateLeg journey newLeg
--- getAllLegs (findAll), 
--- when to call getFare 
--- every time we call otp do we need to use new journey?
 
-updateCurrentJourneyLeg :: JourneyLegStatus -> m ()
--- let leg = getCurrentLeg 
--- leg' = leg {status = 2} @@ modify obj
--- updateLeg leg'
--- 
+
+
 
 
 -- Journey Module (utils function) 
@@ -264,15 +291,7 @@ continueWithAuto journey = do
   let autoLeg = currentAutoLeg { dropPoint = lastLegInfo.endLocation}
   extendLeg journey legs autoLeg
 
-endJourney :: Journey -> m ()
--- if last leg then update leg
--- loop through and delete/update legs and journey as required
--- call leg level cancel
 
-startJourney :: Journey -> m ()
--- getFirst leg
--- call start for first leg @@update
--- update the journey status
 
 -- what to be shown in case of skip on UI
 
@@ -283,4 +302,4 @@ recalculate :: Journey -> m ()
 let legs = getRemainingLegs 
 -- for now nothing just call replace with auto
 
-completeCurrentLeg :: Journey -> m ()
+-- completeCurrentLeg :: Journey -> m ()
