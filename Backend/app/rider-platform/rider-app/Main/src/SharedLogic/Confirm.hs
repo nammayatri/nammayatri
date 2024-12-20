7290708cbcb9421dd61c942c78b17c3ea9313f23
hkmangla <hemantmangla78@gmail.com>
2024-12-20 15:52:21 +0530

    feat: fix multiple booking race condition

--- Backend/app/rider-platform/rider-app/Main/src/SharedLogic/Confirm.hs
+++ Backend/app/rider-platform/rider-app/Main/src/SharedLogic/Confirm.hs
@@ -131,7 +131,7 @@ confirm DConfirmReq {..} = do
   when merchant.onlinePayment $ do
     when (isNothing paymentMethodId) $ throwError PaymentMethodRequired
     QPerson.updateDefaultPaymentMethodId paymentMethodId personId -- Make payment method as default payment method for customer
-  activeBooking <- QRideB.findByQuoteId (Just quote.id)
+  activeBooking <- QRideB.findByTransactionIdAndStatus searchRequest.id.getId DRB.activeBookingStatus
   case activeBooking of
     Just booking -> DQuote.processActiveBooking booking OnConfirm
     _ -> pure ()
