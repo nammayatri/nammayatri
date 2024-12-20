195e5bcf17005dcefd8030671d29c2b86547b6a9
hkmangla <hemantmangla78@gmail.com>
2024-12-20 19:23:59 +0530

    fix: build issue and expose api

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/API/Internal/Multimodal.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/API/Internal/Multimodal.hs
@@ -1,4 +1,4 @@
-module API.Internal.MultiModal
+module API.Internal.Multimodal
   ( API,
     handler,
   )
@@ -6,9 +6,10 @@ where
 
 import qualified API.Types.UI.FareCalculator
 import qualified Domain.Action.UI.FareCalculator as Domain
-import Domain.Types.Ride
+import Domain.Types.Merchant
 import Environment
 import EulerHS.Prelude hiding (id)
+import qualified Kernel.Types.Beckn.Context as Context
 import Kernel.Types.Id
 import Kernel.Utils.Common
 import Servant
@@ -19,12 +20,12 @@ type API =
     :> Capture "merchantCity" Context.City
     :> "calculateFare"
     :> Header "token" Text
-    :> ReqBody '[JSON] CalculateFareReq
+    :> ReqBody '[JSON] Domain.CalculateFareReq
     :> Post '[JSON] API.Types.UI.FareCalculator.FareResponse
 
 handler :: FlowServer API
 handler =
   calculateFare
 
-calculateFare :: Id Merchant -> Context.City -> Maybe Text -> CalculateFareReq -> FlowHandler API.Types.UI.FareCalculator.FareResponse
+calculateFare :: Id Merchant -> Context.City -> Maybe Text -> Domain.CalculateFareReq -> FlowHandler API.Types.UI.FareCalculator.FareResponse
 calculateFare merchantId merchantCity apiKey req = withFlowHandlerAPI $ Domain.calculateFare merchantId merchantCity apiKey req
