f08cc5635fa39a269dcd8108e87198f78c4e721d
hkmangla <hemantmangla78@gmail.com>
2024-12-23 14:51:21 +0530

    feat: info api response

--- Backend/app/rider-platform/rider-app/Main/src/Lib/JourneyModule/Types.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Lib/JourneyModule/Types.hs
@@ -6,6 +6,7 @@ import Domain.Types.SearchRequest as DSR
 import Kernel.External.MultiModal.Interface
 import Kernel.Types.Id
 import Kernel.Utils.Common
+import API.Types.RiderPlatform.Management.FRFSTicket
 
 data JourneyInitData = JourneyInitData
   { legs :: [MultiModalLeg],
@@ -23,10 +24,40 @@ data LegInfo = LegInfo
   , travelMode :: Trip.TravelMode
   , startTime :: UTCTime
   , order :: Int
+  , estimatedDuration :: Maybe Seconds
+  , estimatedFare :: Maybe PriceAPIEntity
+  , estimatedDistance :: Maybe Distance
+  , legExtraInfo :: LegExtraInfo
+  }
+
+data LegExtraInfo = Walk WalkLegExtraInfo | Taxi TaxiLegExtraInfo | Metro MetroLegExtraInfo
+
+data WalkLegExtraInfo = WalkLegExtraInfo
+  { origin :: Locaton
+  , destination :: Location
+  }
+
+data TaxiLegExtraInfo = TaxiLegExtraInfo
+  { origin :: Locaton
+  , destination :: Location
+  }
+
+data MetroLegExtraInfo = MetroLegExtraInfo 
+  { originStop :: FRFSStationAPI,
+    destinationStop :: FRFSStationAPI,
+    lineColor :: Text,
+    frequency :: Seconds
   }
 
 mkLegInfoFromSearchRequest :: SR.SearchRequest -> m LegInfo
 mkLegInfoFromSearchRequest SR.SearchRequest {..} = do
+  mbEstimatedFare <- 
+    case journeyLegInfo.pricingId of
+      Just estId -> do
+        mbEst <- QEstimate.findById estId
+        return $ mkPriceAPIEntity <$> (mbEst <&> (.estimatedFare))
+      Nothing -> return Nothing
+  toLocation' <- toLocation & fromMaybeM (InvalidRequest "To location not found") -- make it proper
   return $
     LegInfo
       { skipBooking = journeyLegInfo.skipBooking
@@ -34,11 +65,23 @@ mkLegInfoFromSearchRequest SR.SearchRequest {..} = do
       , travelMode = Trip.Taxi
       , startTime = startTime
       , order = journeyLegInfo.journeyLegOrder
+      , estimatedDuration = estimatedRideDuration
+      , estimatedFare = mbEstimatedFare
+      , estimatedDistance = distance
+      , legExtraInfo = Taxi $ TaxiLegExtraInfo { origin = fromLocation, destination = toLocation'}
       }
 
 mkLegInfoFromFrfsSearchRequest :: FRFSSR.FRFSSearch -> m LegInfo
 mkLegInfoFromFrfsSearchRequest FRFSSR.FRFSSearch {..} = do
   now <- getCurrentTime
+  mbEstimatedFare <- 
+    case journeyLegInfo.pricingId of
+      Just quoteId -> do
+        mbQuote <- QFRFSQuote.findById quoteId
+        return $ mkPriceAPIEntity <$> (mbQuote <&> (.price))
+      Nothing -> return Nothing
+  fromStation <- QStation.findById fromStationId >>= fromMaybeM (InternalError "From Station not found")
+  toStation <- QStation.findById toStationId >>= fromMaybeM (InternalError "To Station not found")
   return $
     LegInfo
       { skipBooking = journeyLegInfo.skipBooking
@@ -46,7 +89,25 @@ mkLegInfoFromFrfsSearchRequest FRFSSR.FRFSSearch {..} = do
       , travelMode = Trip.Metro
       , startTime = now
       , order = journeyLegInfo.journeyLegOrder
+      , estimatedDuration = Nothing -- Fix this @kavya
+      , estimatedFare = mbEstimatedFare
+      , estimatedDistance = Nothing -- Fix this @kavya
+      , legExtraInfo = Metro $ MetroLegExtraInfo 
+          { originStop = stationToStationAPI fromStation,
+            destinationStop = stationToStationAPI toStation,
+            lineColor = "Green", -- fix this @kavya
+            frequency = 300 -- fix this @kavya
+          }
       }
+  where
+    stationToStationAPI station = 
+      FRFSStationAPI
+        { name = station.name,
+          code = station.code,
+          lat = station.lat,
+          lon = station.lon,
+          address = station.address
+        }
 
 mkTaxiLegConfirmReq :: LegInfo -> TaxiLegRequest
 mkTaxiLegConfirmReq LegInfo {..} =
