5176ca5c7570d3f82d663e45d04514a2dd08ffa3
Rahul Gupta <rhlgupta0706@gmail.com>
2024-12-23 23:45:38 +0530

    backend/feat/multimodal-get-state-addition

--- Backend/app/rider-platform/rider-app/Main/src/Lib/JourneyLeg/Walk.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Lib/JourneyLeg/Walk.hs
@@ -1,5 +1,8 @@
 module Lib.JourneyLeg.Walk where
 
+import qualified Lib.JourneyLeg.Types as JT
+
+
 data WalkLegUpdateData = WalkLegUpdateData
   { id :: Id LegID
     mbFromLocation :: Location
@@ -16,7 +19,7 @@ data WalkSearchData = WalkSearchData
   , merchantOperatingCity :: Id MerchantOperatingCity
   }
 
-data WalkLegRequest = WalkLegRequestSearch MultiModalLeg WalkSearchData | WalkLegRequestConfirm WalkLegConfirmRequest | WalkLegRequestCancel WalkLegCancelRequest | WalkLegRequestUpdate WalkLegUpdateData
+data WalkLegRequest = WalkLegRequestSearch MultiModalLeg WalkSearchData | WalkLegRequestConfirm WalkLegConfirmRequest | WalkLegRequestCancel WalkLegCancelRequest | WalkLegRequestUpdate WalkLegUpdateData | WalkLegGetState (Id LegID)
 
 updateWalkLegById :: (EsqDBFlow m r, MonadFlow m, CacheFlow m r) => Domain.Types.Walkleg.Walkleg  ->  -> Id LegID -> m ()
 updateWalkLegById (Domain.Types.Walkleg.Walkleg {..}) legId = do
@@ -32,5 +35,17 @@ instance JourneyLeg WalkLeg m where
   cancel (WalkLeg $ Cancel WalkLegCancelRequest) JourneyLegStatus = return ()
     -- update JourneyLegStatus: Cancelled
     WalkLegCancelRequest :: id
-  getState (WalkLeg _legData) = return JourneyLegState -- id
-  get (WalkLeg _legData) = return WalkLeg -- id
+
+  getState (WalkLegGetState legId) = do
+    legData <- findById legId 
+    if legData.isSkipped == False && legData.isCancelled == False 
+      then do
+        if legData.legNo > 1 then return $ JT.JourneyLegState { status = InPlan, currentPosition = Nothing }
+          else return $ JT.JourneyLegState { status = Ongoing, currentPosition = Nothing }
+      else if legData.isSkipped == True
+        then return $ JT.JourneyLegState { status = Skipped, currentPosition = Nothing }
+        else return $ JT.JourneyLegState { status = Cancelled, currentPosition = Nothing }
+   
+  get (WalkLegGetState legId) = do
+    legData <- findById legId 
+    return legData
