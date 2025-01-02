395b0eec06d1ec6c68435149f919e58a8d1cd797
Yashika Kaushik <yashikakaushik09@gmail.com>
2025-01-02 19:14:34 +0530

    backend/feat/multimodal-apis

--- Backend/app/rider-platform/rider-app/Main/src/Lib/JourneyLeg/Types/Walk.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Lib/JourneyLeg/Types/Walk.hs
@@ -1,12 +1,12 @@
 module Lib.JourneyLeg.Types.Walk where
 
+import qualified Domain.Types.JourneyLeg as DJourenyLeg
 import qualified Domain.Types.Merchant
 import qualified Domain.Types.MerchantOperatingCity
-import qualified Domain.Types.JourneyLeg as DJourenyLeg
 import qualified Domain.Types.SearchRequest as DSR
 import qualified Domain.Types.WalkLegMultimodal as DWalkLeg
-import Kernel.Types.Id
 import Kernel.Prelude
+import Kernel.Types.Id
 import SharedLogic.Search
 
 data WalkLegRequestSearchData = WalkLegRequestSearchData
@@ -15,7 +15,7 @@ data WalkLegRequestSearchData = WalkLegRequestSearchData
     origin :: SearchReqLocation,
     destination :: SearchReqLocation,
     merchantId :: Kernel.Types.Id.Id Domain.Types.Merchant.Merchant,
-    merchantOperatingCityId ::  Kernel.Types.Id.Id Domain.Types.MerchantOperatingCity.MerchantOperatingCity
+    merchantOperatingCityId :: Kernel.Types.Id.Id Domain.Types.MerchantOperatingCity.MerchantOperatingCity
   }
 
 newtype WalkLegRequestGetStateData = WalkLegRequestGetStateData
