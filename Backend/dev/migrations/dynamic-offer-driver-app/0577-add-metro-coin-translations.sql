cf6d47ce3e3fa96acf497a5e21ba5fbf84f2ebb5
DwivediAman-exe <19dwivediaman@gmail.com>
2025-01-16 12:07:34 +0530

    Backend/feat/metro-x-rides-coin-event

---
+++ Backend/dev/migrations/dynamic-offer-driver-app/0577-add-metro-coin-translations.sql
@@ -0,0 +1,9 @@
+INSERT INTO atlas_driver_offer_bpp.translations (id, message_key, language, message, created_at, updated_at)
+  VALUES
+    (atlas_driver_offer_bpp.uuid_generate_v4(), 'EndRide_MetroRideCompleted FromMetro 3', 'ENGLISH', '3 Rides from Metro Station', now(), now()),
+    (atlas_driver_offer_bpp.uuid_generate_v4(), 'EndRide_MetroRideCompleted FromMetro 3', 'TELUGU', 'మెట్రో స్టేషన్ నుండి 3 రైడ్స్', now(), now()),
+    (atlas_driver_offer_bpp.uuid_generate_v4(), 'EndRide_MetroRideCompleted FromMetro 3', 'HINDI', 'मेट्रो स्टेशन से 3 सवारी', now(), now()),
+    (atlas_driver_offer_bpp.uuid_generate_v4(), 'EndRide_MetroRideCompleted FromMetro 3', 'KANNADA', 'ಮೆಟ್ರೋ ನಿಲ್ದಾಣದಿಂದ 3 ಪ್ರಯಾಣಗಳು', now(), now()),
+    (atlas_driver_offer_bpp.uuid_generate_v4(), 'EndRide_MetroRideCompleted FromMetro 3', 'MALAYALAM', 'മെട്രോ സ്റ്റേഷനിൽ നിന്ന് 3 യാത്രകൾ', now(), now()),
+    (atlas_driver_offer_bpp.uuid_generate_v4(), 'EndRide_MetroRideCompleted FromMetro 3', 'BENGALI', 'মেট্রো স্টেশন থেকে 3 যাত্রা', now(), now()),
+    (atlas_driver_offer_bpp.uuid_generate_v4(), 'EndRide_MetroRideCompleted FromMetro 3', 'TAMIL', 'மெட்ரோ நிலையத்திலிருந்து 3 பயணங்கள்', now(), now());
