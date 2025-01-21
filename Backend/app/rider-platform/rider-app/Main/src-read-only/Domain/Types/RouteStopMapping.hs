2044b0c994a35bf18133e6fd7558e0a98457eb10
khuzema786 <khomosikhuzema894@gmail.com>
2025-01-21 21:09:01 +0530

    backend: Get Intermeidate Stops

--- Backend/app/rider-platform/rider-app/Main/src-read-only/Domain/Types/RouteStopMapping.hs
+++ Backend/app/rider-platform/rider-app/Main/src-read-only/Domain/Types/RouteStopMapping.hs
@@ -29,4 +29,4 @@ data RouteStopMapping = RouteStopMapping
     createdAt :: Kernel.Prelude.UTCTime,
     updatedAt :: Kernel.Prelude.UTCTime
   }
-  deriving (Generic, Show, ToJSON, FromJSON, ToSchema)
+  deriving (Generic, Show, Eq, FromJSON, ToJSON, ToSchema)
