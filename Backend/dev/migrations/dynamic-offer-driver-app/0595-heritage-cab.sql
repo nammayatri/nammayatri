00d404a1946e193ea227dcad37d7790e1edb2f3f
Utkarsh Pandey <utkarshpandey911@gmail.com>
2025-01-31 12:47:56 +0530

    backend/queries/ev_auto_rickshaw

--- Backend/dev/migrations/dynamic-offer-driver-app/0595-heritage-cab.sql
+++ Backend/dev/migrations/dynamic-offer-driver-app/0595-heritage-cab.sql
@@ -84,29 +84,3 @@ insert into atlas_driver_offer_bpp.vehicle_service_tier (id, name, merchant_id, 
     true,
     2
     from atlas_driver_offer_bpp.merchant_operating_city as m where city = 'Kolkata');
-
-
-insert into atlas_driver_offer_bpp.vehicle_service_tier (id, name, merchant_id, merchant_operating_city_id, seating_capacity, air_conditioned, driver_rating, vehicle_rating, luggage_capacity, short_description, long_description, allowed_vehicle_variant, default_for_vehicle_variant, service_tier_type, created_at, updated_at, auto_selected_vehicle_variant, oxygen, ventilator, is_air_conditioned, air_conditioned_threshold)
-(select
-    atlas_driver_offer_bpp.uuid_generate_v4(),
-    'EGM-ECO-Auto',
-    m.merchant_id,
-    m.id,
-    3,
-    null,
-    null,
-    null,
-    null,
-    'EV Auto',
-    null,
-    '{EV_AUTO_RICKSHAW}',
-    '{EV_AUTO_RICKSHAW}',
-    'EV_AUTO_RICKSHAW',
-    now(),
-    now(),
-    '{EV_AUTO_RICKSHAW}',
-    null,
-    null,
-    false,
-    null
-    from atlas_driver_offer_bpp.merchant_operating_city as m where city = 'Trichy');
