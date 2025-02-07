6e3381c7fda380c56c982fd87e189dfa7bb2baa8
hkmangla <hemantmangla78@gmail.com>
2025-02-07 18:46:13 +0530

    fix: remove duplicate indicies

--- Backend/dev/migrations/rider-app/1370-multimodal-indices.sql
+++ Backend/dev/migrations/rider-app/1370-multimodal-indices.sql
@@ -1,10 +1,6 @@
-CREATE INDEX idx_journey_search_request_id ON atlas_app.journey USING btree (search_request_id);
-
 CREATE INDEX idx_journey_rider_id ON atlas_app.journey USING btree (rider_id);
 
 CREATE INDEX idx_journey_booking_journey_id ON atlas_app.journey_booking USING btree (journey_id);
-
-CREATE INDEX idx_journey_leg_journey_id ON atlas_app.journey_leg USING btree (journey_id);
 
 CREATE INDEX idx_journey_feedback_rider_id ON atlas_app.journey_feedback USING btree (rider_id);
 
