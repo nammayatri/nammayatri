480f69d4fb6c0a4b56541da9c688edab876c2acb
piyushKumar-1 <pkkapoor98@gmail.com>
2025-02-17 22:46:12 +0530

    backend/fix/duplicate-chakra-jobs-on-failure-and-logged-failed-chakra-queries-for-debugging

--- Backend/lib/scheduler/src/Lib/Scheduler/JobStorageType/SchedulerType.hs
+++ Backend/lib/scheduler/src/Lib/Scheduler/JobStorageType/SchedulerType.hs
@@ -85,7 +85,7 @@ createJobIn merchantId merchantOperatingCityId inTime jobData = do
       logDebug "DB BASED JOB "
       DBQ.createJobIn @t @e merchantId merchantOperatingCityId uuid inTime maxShards jobData
 
-isLongRunning :: (JobCreator r m) => Text -> m Bool
+isLongRunning :: (HasJobInfoMap r, JobMonad r m) => Text -> m Bool
 isLongRunning jType = do
   jobInfoMap <- asks (.jobInfoMap)
   logDebug $ "jobInfoMap : " <> show jobInfoMap
@@ -139,6 +139,20 @@ getTasksById jobs = do
   case schedulerType of
     RedisBased -> RQ.getTasksById jobs
     DbBased -> DBQ.getTasksById jobs
+
+getJobByTypeAndScheduleTime :: forall t m r. (FromTType'' BeamST.SchedulerJob (AnyJob t), JobMonad r m, JobProcessor t, HasJobInfoMap r) => Text -> UTCTime -> m [AnyJob t]
+getJobByTypeAndScheduleTime jobType minScheduleTime = do
+  schedulerType <- asks (.schedulerType)
+  case schedulerType of
+    RedisBased -> do
+      longRunning <- isLongRunning jobType
+      logDebug $ "LONG RUNNING " <> show longRunning <> " getJobByTypeAndScheduleTime: " <> show jobType <> " time: " <> show minScheduleTime
+      if longRunning
+        then do
+          DBQ.getJobByTypeAndScheduleTime jobType minScheduleTime
+        else do
+          RQ.getJobByTypeAndScheduleTime jobType minScheduleTime
+    DbBased -> DBQ.getJobByTypeAndScheduleTime jobType minScheduleTime
 
 getReadyTasks ::
   forall t m r.
