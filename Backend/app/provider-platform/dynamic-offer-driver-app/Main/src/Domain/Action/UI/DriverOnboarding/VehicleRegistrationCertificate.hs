b4b665a9b1ad65e8370cfaefb73460e24ca08f37
Vijay <66685378+vijaygupta18@users.noreply.github.com>
2025-03-05 18:25:30 +0530

    backend: add runInMasterDbAndRedis function for enhanced Redis support (#10412)
    ## Type of Change
    <!-- Put an `x` in the boxes that apply -->
    
    - [ ] Bugfix
    - [ ] New feature
    - [ ] Enhancement
    - [ ] Refactoring
    - [ ] Dependency updates
    
    ## Description
    <!-- Describe your changes in detail -->
    
    
    ### Additional Changes
    
    - [ ] This PR modifies the database schema (database migration added)
    - [ ] This PR modifies dhall configs/environment variables
    - [ ] This PR contains API breaking changes
    <!-- 
    Provide links to the files with corresponding changes.
    
    You can find config files in `dhall-configs`
    -->
    
    
    ## Motivation and Context
    <!--
    Why is this change required? What problem does it solve?
    If it fixes an open issue, please link to the issue here.
    
    If you don't have an issue, we'd recommend starting with one first so
    the PR
    can focus on the implementation (unless its an obvious bug or
    documentation fix
    that will have little conversation).
    -->
    
    
    ## How did you test it?
    <!--
    Did you write an integration/unit/API test to verify the code changes?
    Or did you test this change manually (provide relevant screenshots)?
    -->
    
    ## Screenshot of test results
    <!-- Provide screenshot of the test results -->
    
    ## Checklist
    <!-- Put an `x` in the boxes that apply -->
    
    - [ ] I formatted the code and addressed linter errors
    `./dev/format-all-files.sh`
    - [ ] I reviewed submitted code
    - [ ] I added unit tests for my changes where possible
    - [ ] I added integration tests for my changes where possible
    - [ ] I tested the changes using integration tests
    - [ ] I added a [CHANGELOG](/CHANGELOG.md) entry if applicable
    - [ ] No leak detected in leakcanary
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **Refactor**
    - Enhanced backend data handling for vehicle onboarding and transaction
    cancellation processes, improving overall consistency and reliability.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/DriverOnboarding/VehicleRegistrationCertificate.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/DriverOnboarding/VehicleRegistrationCertificate.hs
@@ -55,6 +55,7 @@ import qualified Domain.Types.VehicleRegistrationCertificate as DVRC
 import qualified Domain.Types.VehicleRegistrationCertificate as Domain
 import qualified Domain.Types.VehicleVariant as DV
 import Environment
+import Kernel.Beam.Functions
 import Kernel.External.Encryption
 import Kernel.External.Types (VerificationFlow)
 import qualified Kernel.External.Verification.Types as VT
@@ -483,7 +484,7 @@ compareRegistrationDates actualDate providedDate =
     && ((convertUTCTimetoDate <$> providedDate) /= (convertUTCTimetoDate <$> convertTextToUTC actualDate))
 
 linkRCStatus :: (Id Person.Person, Id DM.Merchant, Id DMOC.MerchantOperatingCity) -> RCStatusReq -> Flow APISuccess
-linkRCStatus (driverId, merchantId, merchantOpCityId) req@RCStatusReq {..} = do
+linkRCStatus (driverId, merchantId, merchantOpCityId) req@RCStatusReq {..} = runInMasterDbAndRedis $ do
   driverInfo <- DIQuery.findById (cast driverId) >>= fromMaybeM (PersonNotFound driverId.getId)
   transporterConfig <- SCTC.findByMerchantOpCityId merchantOpCityId (Just (DriverId (cast driverId))) >>= fromMaybeM (TransporterConfigNotFound merchantOpCityId.getId)
   unless (driverInfo.subscribed || transporterConfig.openMarketUnBlocked) $ throwError (RCActivationFailedPaymentDue driverId.getId)
