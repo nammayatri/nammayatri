59af55e42d2918ec7bfa3f5335c5ca31be06a902
00abhishek0000 <96061321+00abhishek0000@users.noreply.github.com>
2025-03-24 14:02:53 +0530

    wmb added isdriveractive (#10581)

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/Dashboard/Fleet/Driver.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/Dashboard/Fleet/Driver.hs
@@ -359,8 +359,10 @@ getDriverFleetGetAllDriver _merchantShortId _opCity fleetOwnerId mbLimit mbOffse
   return $ Common.FleetListDriverRes fleetDriversInfos
 
 convertToDriverAPIEntity :: (FleetDriverAssociation, DP.Person) -> Flow Common.FleetDriversAPIEntity
-convertToDriverAPIEntity (fleetDriver, person) = do
+convertToDriverAPIEntity (_, person) = do
   unencryptedMobileNumber <- mapM decrypt person.mobileNumber
+  vehicle <- QVehicle.findById person.id
+  let isActive = isJust vehicle
   pure $
     Common.FleetDriversAPIEntity
       { driverId = cast @DP.Person @Common.Driver person.id,
@@ -369,7 +371,7 @@ convertToDriverAPIEntity (fleetDriver, person) = do
         lastName = person.lastName,
         mobileNumber = unencryptedMobileNumber,
         mobileCountryCode = person.mobileCountryCode,
-        isActive = Just fleetDriver.isActive
+        isActive = Just isActive
       }
 
 ---------------------------------------------------------------------
