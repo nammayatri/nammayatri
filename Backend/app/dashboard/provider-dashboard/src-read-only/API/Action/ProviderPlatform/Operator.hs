ee875ebba2315a9db51ff5895dc6125756f96da4
Roman Bodavskiy <roman.bodavskiy@metalamp.io>
2025-03-25 08:15:54 +0300

    backend/feat/msil: create operator by admin

--- Backend/app/dashboard/provider-dashboard/src-read-only/API/Action/ProviderPlatform/Operator.hs
+++ Backend/app/dashboard/provider-dashboard/src-read-only/API/Action/ProviderPlatform/Operator.hs
@@ -3,13 +3,14 @@
 module API.Action.ProviderPlatform.Operator where
 
 import qualified API.Action.ProviderPlatform.Operator.Driver
+import qualified API.Action.ProviderPlatform.Operator.Registration
 import qualified "lib-dashboard" Domain.Types.Merchant
 import qualified "lib-dashboard" Environment
 import qualified Kernel.Types.Beckn.Context
 import qualified Kernel.Types.Id
 import Servant
 
-type API = API.Action.ProviderPlatform.Operator.Driver.API
+type API = (API.Action.ProviderPlatform.Operator.Driver.API :<|> API.Action.ProviderPlatform.Operator.Registration.API)
 
 handler :: (Kernel.Types.Id.ShortId Domain.Types.Merchant.Merchant -> Kernel.Types.Beckn.Context.City -> Environment.FlowServer API)
-handler merchantId city = API.Action.ProviderPlatform.Operator.Driver.handler merchantId city
+handler merchantId city = API.Action.ProviderPlatform.Operator.Driver.handler merchantId city :<|> API.Action.ProviderPlatform.Operator.Registration.handler merchantId city
--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/API/Action/Dashboard/Operator.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/API/Action/Dashboard/Operator.hs
@@ -7,13 +7,14 @@ module API.Action.Dashboard.Operator
 where
 
 import qualified API.Action.Dashboard.Operator.Driver
+import qualified API.Action.Dashboard.Operator.Registration
 import qualified Domain.Types.Merchant
 import qualified Environment
 import qualified Kernel.Types.Beckn.Context
 import qualified Kernel.Types.Id
 import Servant
 
-type API = API.Action.Dashboard.Operator.Driver.API
+type API = (API.Action.Dashboard.Operator.Driver.API :<|> API.Action.Dashboard.Operator.Registration.API)
 
 handler :: (Kernel.Types.Id.ShortId Domain.Types.Merchant.Merchant -> Kernel.Types.Beckn.Context.City -> Environment.FlowServer API)
-handler merchantId city = API.Action.Dashboard.Operator.Driver.handler merchantId city
+handler merchantId city = API.Action.Dashboard.Operator.Driver.handler merchantId city :<|> API.Action.Dashboard.Operator.Registration.handler merchantId city
