ae9a237860c806868d1ac6d2eb142ace727fa360
Chakradhar Kamminana <chakradhar007007kc@gmail.com>
2025-03-25 18:06:07 +0530

    backend/fix request to delete account fixes

--- Backend/dev/migrations-read-only/rider-app/deleted_person.sql
+++ Backend/dev/migrations-read-only/rider-app/deleted_person.sql
@@ -8,3 +8,7 @@ ALTER TABLE atlas_app.deleted_person ADD COLUMN merchant_operating_city_id chara
 ALTER TABLE atlas_app.deleted_person ADD COLUMN person_id character varying(36) NOT NULL;
 ALTER TABLE atlas_app.deleted_person ADD COLUMN updated_at timestamp with time zone NOT NULL default CURRENT_TIMESTAMP;
 ALTER TABLE atlas_app.deleted_person ADD PRIMARY KEY ( person_id);
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.deleted_person ADD COLUMN reason_to_delete text ;
