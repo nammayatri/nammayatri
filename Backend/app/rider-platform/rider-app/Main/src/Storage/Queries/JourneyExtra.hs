a0714c27cb9de01eb87ccee269096a1ed2643915
hkmangla <hemantmangla78@gmail.com>
2025-03-26 00:48:48 +0530

    fix: old journey being return

--- Backend/app/rider-platform/rider-app/Main/src/Storage/Queries/JourneyExtra.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Storage/Queries/JourneyExtra.hs
@@ -3,6 +3,7 @@
 
 module Storage.Queries.JourneyExtra where
 
+import Data.Time hiding (getCurrentTime)
 import qualified Domain.Types.Journey as DJ
 import qualified Domain.Types.Person
 import Kernel.Beam.Functions
@@ -17,7 +18,10 @@ import Storage.Queries.OrphanInstances.Journey
 
 -- Extra code goes here --
 findAllActiveByRiderId :: (EsqDBFlow m r, MonadFlow m, CacheFlow m r) => Kernel.Types.Id.Id Domain.Types.Person.Person -> m [DJ.Journey]
-findAllActiveByRiderId riderId = do findAllWithKV [Se.And [Se.Is Beam.status $ Se.In [Just DJ.CONFIRMED, Just DJ.INPROGRESS, Just DJ.FEEDBACK_PENDING], Se.Is Beam.riderId $ Se.Eq (Kernel.Types.Id.getId riderId)]]
+findAllActiveByRiderId riderId = do
+  now <- getCurrentTime
+  let lastDay = addUTCTime (-60 * 60 * 24) now
+  findAllWithKV [Se.And [Se.Is Beam.status $ Se.In [Just DJ.CONFIRMED, Just DJ.INPROGRESS, Just DJ.FEEDBACK_PENDING], Se.Is Beam.riderId $ Se.Eq (Kernel.Types.Id.getId riderId), Se.Is Beam.createdAt $ Se.GreaterThanOrEq lastDay]]
 
 findAllByRiderId :: (MonadFlow m, CacheFlow m r, EsqDBFlow m r) => Kernel.Types.Id.Id Domain.Types.Person.Person -> Maybe Integer -> Maybe Integer -> Maybe UTCTime -> Maybe UTCTime -> [DJ.JourneyStatus] -> m [DJ.Journey]
 findAllByRiderId (Kernel.Types.Id.Id personId) mbLimit mbOffset mbFromDate mbToDate mbJourneyStatusList = do
