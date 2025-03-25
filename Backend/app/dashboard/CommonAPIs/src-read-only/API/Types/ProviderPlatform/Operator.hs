c485d67483c94f8c35328799e776ac7f8da8731a
Roman Bodavskiy <roman.bodavskiy@metalamp.io>
2025-03-25 08:15:54 +0300

    backend/feat/msil: create operator by admin

--- Backend/app/dashboard/CommonAPIs/src-read-only/API/Types/ProviderPlatform/Operator.hs
+++ Backend/app/dashboard/CommonAPIs/src-read-only/API/Types/ProviderPlatform/Operator.hs
@@ -4,6 +4,7 @@
 module API.Types.ProviderPlatform.Operator where
 
 import qualified API.Types.ProviderPlatform.Operator.Driver
+import qualified API.Types.ProviderPlatform.Operator.Registration
 import qualified Data.List
 import Data.OpenApi (ToSchema)
 import qualified Data.Singletons.TH
@@ -11,17 +12,30 @@ import EulerHS.Prelude
 import qualified Text.Read
 import qualified Text.Show
 
-newtype OperatorUserActionType
+data OperatorUserActionType
   = DRIVER API.Types.ProviderPlatform.Operator.Driver.DriverUserActionType
+  | REGISTRATION API.Types.ProviderPlatform.Operator.Registration.RegistrationUserActionType
   deriving stock (Generic, Eq, Ord)
   deriving anyclass (ToJSON, FromJSON, ToSchema)
 
 instance Text.Show.Show OperatorUserActionType where
   show = \case
     DRIVER e -> "DRIVER/" <> show e
+    REGISTRATION e -> "REGISTRATION/" <> show e
 
 instance Text.Read.Read OperatorUserActionType where
-  readsPrec d' = Text.Read.readParen (d' > app_prec) (\r -> [(DRIVER v1, r2) | r1 <- stripPrefix "DRIVER/" r, (v1, r2) <- Text.Read.readsPrec (app_prec + 1) r1])
+  readsPrec d' =
+    Text.Read.readParen
+      (d' > app_prec)
+      ( \r ->
+          [(DRIVER v1, r2) | r1 <- stripPrefix "DRIVER/" r, (v1, r2) <- Text.Read.readsPrec (app_prec + 1) r1]
+            ++ [ ( REGISTRATION v1,
+                   r2
+                 )
+                 | r1 <- stripPrefix "REGISTRATION/" r,
+                   (v1, r2) <- Text.Read.readsPrec (app_prec + 1) r1
+               ]
+      )
     where
       app_prec = 10
       stripPrefix pref r = bool [] [Data.List.drop (length pref) r] $ Data.List.isPrefixOf pref r
