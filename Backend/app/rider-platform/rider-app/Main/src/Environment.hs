3e85ad84fcdd24714f0c916336d0c60fadb585dc
Akhilesh Bhadauriya <akhileshsingh2314@gmail.com>
2025-03-28 13:56:01 +0530

    Backend/feat/adding-lts-redis-inatlas-app

--- Backend/app/rider-platform/rider-app/Main/src/Environment.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Environment.hs
@@ -86,6 +86,7 @@ data AppCfg = AppCfg
     esqDBReplicaCfg :: EsqDBConfig,
     hedisCfg :: HedisCfg,
     hedisClusterCfg :: HedisCfg,
+    ltsRedis :: HedisCfg,
     hedisNonCriticalCfg :: HedisCfg,
     hedisNonCriticalClusterCfg :: HedisCfg,
     cutOffHedisCluster :: Bool,
@@ -209,6 +210,7 @@ data AppEnv = AppEnv
     nwAddress :: BaseUrl,
     selfUIUrl :: BaseUrl,
     hedisEnv :: HedisEnv,
+    ltsHedisEnv :: HedisEnv,
     hedisNonCriticalEnv :: HedisEnv,
     hedisNonCriticalClusterEnv :: HedisEnv,
     hedisClusterEnv :: HedisEnv,
@@ -313,6 +315,7 @@ buildAppEnv cfg@AppCfg {..} = do
   dashboardClickhouseEnv <- createConn dashboardClickhouseCfg
   let serviceClickhouseCfg = riderClickhouseCfg
   let ondcTokenHashMap = HM.fromList $ M.toList ondcTokenMap
+  ltsHedisEnv <- connectHedis ltsRedis identity
   return AppEnv {minTripDistanceForReferralCfg = convertHighPrecMetersToDistance Meter <$> minTripDistanceForReferralCfg, ..}
 
 releaseAppEnv :: AppEnv -> IO ()
