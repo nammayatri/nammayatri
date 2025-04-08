bef70bc1e411b56f2003026f618c650b2e74e292
Vijay <66685378+vijaygupta18@users.noreply.github.com>
2025-04-08 20:18:42 +0530

    Revert "backend/feat: add status and rideStartTime to meterRide"
    This reverts commit 03a907cb919284e9de266a026f489e334e5d20de.

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/API/Types/UI/PriceBreakup.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/API/Types/UI/PriceBreakup.hs
@@ -3,23 +3,16 @@
 module API.Types.UI.PriceBreakup where
 
 import Data.OpenApi (ToSchema)
-import qualified Domain.Types.Ride
 import EulerHS.Prelude hiding (id)
 import qualified Kernel.External.Maps.Types
-import qualified Kernel.Prelude
 import qualified Kernel.Types.Common
 import Servant
 import Tools.Auth
 
-data MeterRidePriceReq = MeterRidePriceReq {locationUpdates :: Kernel.Prelude.Maybe [Kernel.External.Maps.Types.LatLong]}
+data MeterRidePriceReq = MeterRidePriceReq {locationUpdates :: [Kernel.External.Maps.Types.LatLong]}
   deriving stock (Generic)
   deriving anyclass (ToJSON, FromJSON, ToSchema)
 
-data MeterRidePriceRes = MeterRidePriceRes
-  { distance :: Kernel.Types.Common.HighPrecMeters,
-    fare :: Kernel.Types.Common.HighPrecMoney,
-    status :: Kernel.Prelude.Maybe Domain.Types.Ride.RideStatus,
-    tripStartTime :: Kernel.Prelude.Maybe Kernel.Prelude.UTCTime
-  }
+data MeterRidePriceRes = MeterRidePriceRes {distance :: Kernel.Types.Common.HighPrecMeters, fare :: Kernel.Types.Common.HighPrecMoney}
   deriving stock (Generic)
   deriving anyclass (ToJSON, FromJSON, ToSchema)
