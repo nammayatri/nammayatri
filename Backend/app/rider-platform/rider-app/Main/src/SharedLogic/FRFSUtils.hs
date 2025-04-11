4aa8594e404cf3a1a7ca49032bb289bc198b337b
khuzema786 <khomosikhuzema894@gmail.com>
2025-04-12 01:46:30 +0530

    backend/fix: Possible Routes

--- Backend/app/rider-platform/rider-app/Main/src/SharedLogic/FRFSUtils.hs
+++ Backend/app/rider-platform/rider-app/Main/src/SharedLogic/FRFSUtils.hs
@@ -171,7 +171,7 @@ getPossibleRoutesBetweenTwoStops startStationCode endStationCode integratedBPPCo
           catMaybes $
             map
               ( \stops ->
-                  let stopsSortedBySequenceNumber = sortBy (compare `on` RouteStopMapping.sequenceNum) serviceableStops
+                  let stopsSortedBySequenceNumber = sortBy (compare `on` RouteStopMapping.sequenceNum) stops
                       mbStartStopSequence = (.sequenceNum) <$> find (\stop -> stop.stopCode == startStationCode) stopsSortedBySequenceNumber
                    in find
                         ( \stop ->
