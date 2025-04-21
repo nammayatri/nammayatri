aa030523a23e6727209d0706eecc660040af5da8
KavyashreeS <kavyashree.s1998@gmail.com>
2025-04-21 11:55:06 +0530

    Backend: feat multimodal split settlement

--- Backend/app/rider-platform/rider-app/Main/src/Domain/Action/UI/RidePayment.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Domain/Action/UI/RidePayment.hs
@@ -38,12 +38,42 @@ data DFareBreakup = DFareBreakup
   }
   deriving (Generic, Show)
 
+-- getcustomer ::
+--   Domain.Types.Person.Person -> Environment.Flow Payment.CreateCustomerResp
+-- getcustomer person =
+--   case person.juspayCustomerPaymentID of
+--     Just customerId -> do
+--       customer <- QPaymentCustomer.findByCustomerId customerId >>= fromMaybeM (InternalError $ "No payment customer found for the customer id " <> customerId)
+--       now <- getCurrentTime
+--       if maybe False (> now) customer.clientAuthTokenExpiry
+--         then
+--           return $
+--             Payment.CreateCustomerResp
+--               { customerId = customer.customerId,
+--                 clientAuthToken = customer.clientAuthToken,
+--                 clientAuthTokenExpiry = customer.clientAuthTokenExpiry
+--               }
+--         else do
+--           getCustomer <- Payment.getCustomer person.merchantId person.merchantOperatingCityId customerId
+--           QPaymentCustomer.updateCATAndExipry getCustomer.clientAuthToken getCustomer.clientAuthTokenExpiry getCustomer.customerId
+--           return $ getCustomer
+--     Nothing -> do
+--       uuid <- generateGUID
+--       mbEmailDecrypted <- mapM decrypt person.email
+--       phoneDecrypted <- mapM decrypt person.mobileNumber
+--       let req = Payment.CreateCustomerReq {email = mbEmailDecrypted, name = person.firstName, phone = phoneDecrypted, lastName = Nothing, objectReferenceId = Just uuid, mobileCountryCode = Nothing, optionsGetClientAuthToken = Just True}
+--       customer <- Payment.createCustomer person.merchantId person.merchantOperatingCityId req
+--       paymentCustomer <- buildCreateCustomer customer
+--       QPaymentCustomer.create paymentCustomer
+--       QPerson.updateJuspayCustomerPaymentId (Just customer.customerId) person.id
+--       return $ customer
+
 getcustomer ::
   Domain.Types.Person.Person -> Environment.Flow Payment.CreateCustomerResp
-getcustomer person =
-  case person.juspayCustomerPaymentID of
-    Just customerId -> do
-      customer <- QPaymentCustomer.findByCustomerId customerId >>= fromMaybeM (InternalError $ "No payment customer found for the customer id " <> customerId)
+getcustomer person = do
+  mbCustomer <- QPaymentCustomer.findByCustomerId person.id.getId -- >>= fromMaybeM (InternalError $ "No payment customer found for the customer id " <> person.id.getId)
+  case mbCustomer of
+    Just customer -> do
       now <- getCurrentTime
       if maybe False (> now) customer.clientAuthTokenExpiry
         then
@@ -54,18 +84,16 @@ getcustomer person =
                 clientAuthTokenExpiry = customer.clientAuthTokenExpiry
               }
         else do
-          getCustomer <- Payment.getCustomer person.merchantId person.merchantOperatingCityId customerId
+          getCustomer <- Payment.getCustomer person.merchantId person.merchantOperatingCityId person.id.getId
           QPaymentCustomer.updateCATAndExipry getCustomer.clientAuthToken getCustomer.clientAuthTokenExpiry getCustomer.customerId
           return $ getCustomer
     Nothing -> do
-      uuid <- generateGUID
-      mbEmailDecrypted <- mapM decrypt person.email
-      phoneDecrypted <- mapM decrypt person.mobileNumber
-      let req = Payment.CreateCustomerReq {email = mbEmailDecrypted, name = person.firstName, phone = phoneDecrypted, lastName = Nothing, objectReferenceId = Just uuid, mobileCountryCode = Nothing, optionsGetClientAuthToken = Just True}
-      customer <- Payment.createCustomer person.merchantId person.merchantOperatingCityId req
+      -- mbEmailDecrypted <- mapM decrypt person.email
+      -- phoneDecrypted <- mapM decrypt person.mobileNumber
+      -- let req = Payment.CreateCustomerReq {email = mbEmailDecrypted, name = person.firstName, phone = phoneDecrypted, lastName = Nothing, objectReferenceId = Just person.id.getId, mobileCountryCode = Nothing, optionsGetClientAuthToken = Just True}
+      customer <- Payment.getCustomer person.merchantId person.merchantOperatingCityId person.id.getId
       paymentCustomer <- buildCreateCustomer customer
       QPaymentCustomer.create paymentCustomer
-      QPerson.updateJuspayCustomerPaymentId (Just customer.customerId) person.id
       return $ customer
 
 buildCreateCustomer ::
