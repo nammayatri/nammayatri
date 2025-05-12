69eb2fe9dc2a0765029ef8d11157c65c0b713773
khuzema786 <khomosikhuzema894@gmail.com>
2025-05-12 15:43:01 +0530

    backend/enhancement: Add Driver and Conductor Name in TripTransactionDetails

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/WMB.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/WMB.hs
@@ -227,6 +227,8 @@ postWmbQrStart (mbDriverId, merchantId, merchantOperatingCityId) req = do
     TripTransactionDetails
       { tripTransactionId = tripTransaction.id,
         endRideApprovalRequestId = tripTransaction.endRideApprovalRequestId,
+        driverName = tripTransaction.driverName,
+        conductorName = tripTransaction.conductorName,
         vehicleNumber = vehicleNumber,
         vehicleType = tripTransaction.vehicleServiceTierType,
         source = sourceStopInfo,
@@ -245,6 +247,8 @@ getTripTransactionDetails tripTransaction = do
     TripTransactionDetails
       { tripTransactionId = tripTransaction.id,
         endRideApprovalRequestId = tripTransaction.endRideApprovalRequestId,
+        driverName = tripTransaction.driverName,
+        conductorName = tripTransaction.conductorName,
         vehicleNumber = tripTransaction.vehicleNumber,
         vehicleType = tripTransaction.vehicleServiceTierType,
         source = sourceStopInfo,
