ee7104986b4602c73d9240f2ab57ec55ce169be7
Anuj Kumar <whoanujyadav@gmail.com>
2025-05-15 19:10:08 +0530

    Frontend/Enhance/ Added a new field initalSourceLocation in rideSearch request body

--- Frontend/ui-customer/src/Services/API.purs
+++ Frontend/ui-customer/src/Services/API.purs
@@ -501,6 +501,7 @@ newtype OneWaySearchReq = OneWaySearchReq {
   sessionToken :: Maybe String,
   isSpecialLocation :: Maybe Boolean,
   startTime :: Maybe String,
+  initialSourceLatLong :: Maybe LatLong,
   quotesUnifiedFlow :: Maybe Boolean,
   rideRequestAndRideOtpUnifiedFlow :: Maybe Boolean
 }
