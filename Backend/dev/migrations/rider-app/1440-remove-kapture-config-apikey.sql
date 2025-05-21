007b3dc2754cfe71d67145d23eeba79e10738b5d
Chakradhar Kamminana <chakradhar007007kc@gmail.com>
2025-05-21 13:23:46 +0530

    backend/enh/added drunk and drive warning

--- Backend/dev/migrations/rider-app/1440-remove-kapture-config-apikey.sql
+++ Backend/dev/migrations/rider-app/1440-remove-kapture-config-apikey.sql
@@ -1,15 +1,8 @@
-
--- we are removing the apiKey from the config_json as it is no longer needed
--- this query both Master and Prod
-UPDATE atlas_app.merchant_service_config
-SET config_json = config_json - 'apiKey'
-WHERE service_name = 'Ticket_Kapture';
-
 -- this query is only for master
 UPDATE atlas_app.merchant_service_config
 SET config_json = jsonb_set(
     config_json::jsonb,
     '{auth}',
-    to_jsonb('0.1.0|4|pqbiEmDw0gGyJ9zd62C/AEiVxOUvUe2uixvu6neyyhAA1z7Hd7r7hvdBqYVHTw9E5zSDbUy5rIJXtbyClbCQEKxdU5wsSHUNLG8gqxcTQ0alCpYKBFbBkDPpd0O1++MeTfkAKtOHoigl0GjkIX6AmoqyLkgmry1i70t7WOQ='::text)
+    to_jsonb('0.1.0|2|lJI1P6OvckakTCNbgJP0wfucipCGt39sFof8XAXy7zqP+jYTU0Z69jLzIazQNCBSI32z/X5xdENeEL/nXlc8O96xbpGnEKA93vl9/rPCgbCg/tMy/Ot0h80mSLFqbuAf2twQjkZcRWCW1eiWwiNpBLTEJD+K5ta3OcxHGPA='::text)
 )
 WHERE service_name = 'Ticket_Kapture';
