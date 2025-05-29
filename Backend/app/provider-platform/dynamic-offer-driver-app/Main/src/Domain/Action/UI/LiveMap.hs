7376c9687682cd1110fa8293ec0afb5f0058f3be
Shamil Nizamov <na-80@yandex.ru>
2025-05-29 20:14:50 +0500

    Create logic for UI

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/LiveMap.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/LiveMap.hs
@@ -3,16 +3,31 @@
 module Domain.Action.UI.LiveMap (getLiveMapDrivers) where
 
 import qualified API.Types.UI.LiveMap as Common
+--import Servant
+
+import qualified Data.List as L
 import Data.OpenApi (ToSchema)
+import Data.Ord (comparing)
+--import Kernel.Utils.Common (fromMaybeM)
+import Data.Time (UTCTime (UTCTime, utctDay), getCurrentTime)
+import Domain.Action.UI.Invoice (getSourceAndDestination)
 import qualified Domain.Types.Merchant
 import qualified Domain.Types.MerchantOperatingCity
 import qualified Domain.Types.Person
 import qualified Environment
 import EulerHS.Prelude hiding (id)
 import qualified Kernel.Prelude
-import Kernel.Types.Error (PersonError (PersonNotFound))
+import Kernel.Types.Error
+  ( PersonError (PersonNotFound),
+    RideError (RideDoesNotExist),
+    VehicleError (VehicleNotFound),
+  )
 import qualified Kernel.Types.Id
-import Servant
+import Kernel.Utils.Error.Throwing (fromMaybeM, throwError)
+import SharedLogic.WMB (getDriverCurrentLocation)
+import qualified Storage.Clickhouse.Booking as CHB
+import qualified Storage.Clickhouse.Ride as CHR
+import qualified Storage.Clickhouse.RideDetails as CHRD
 import qualified Storage.Queries.Person as QP
 import Tools.Auth
 
@@ -21,18 +36,28 @@ getLiveMapDrivers ::
     Kernel.Types.Id.Id Domain.Types.Merchant.Merchant,
     Kernel.Types.Id.Id Domain.Types.MerchantOperatingCity.MerchantOperatingCity
   ) ->
-  Environment.Flow [API.Types.UI.LiveMap.MapDriverInfoRes]
+  Environment.Flow [Common.MapDriverInfoRes]
 getLiveMapDrivers (mbPersonId, _merchantId, _merchantOpCityId) = do
   driver <- traverse QP.findById mbPersonId >>= fromMaybeM (PersonNotFound $ show mbPersonId) . join
-
-  error "Logic yet to be decided"
-
--- pure $ Common.MapDriverInfoRes
---   { driverName = unwords [driver.firstName, fromMaybe "" driver.lastName],
---     driverStatus :: DriverStatus,
---     rcNo :: Kernel.Prelude.Text,
---     vehicleStatus :: VehicleStatus,
---     position :: Kernel.External.Maps.Types.LatLong,
---     source :: Kernel.Prelude.Text,
---     destination :: Kernel.Prelude.Text
---   }
+  now <- liftIO getCurrentTime
+  let fromDate = UTCTime (utctDay now) 0
+      toDate = now
+  rideLs <- CHR.getAllRidesByDriverId driver.id fromDate toDate
+  when (null rideLs) . throwError . RideDoesNotExist $ "for driverId " <> driver.id.getId
+  let ride = L.maximumBy (comparing (.tripStartTime)) rideLs
+  vehicleNumber <-
+    Kernel.Prelude.msum <$> CHRD.findByIdAndVehicleNumber (Kernel.Types.Id.cast ride.id) Nothing
+      >>= fromMaybeM (VehicleNotFound $ "for rideId" <> ride.id.getId)
+  position <- getDriverCurrentLocation driver.id
+  mbBooking <- CHB.findById ride.bookingId
+  (source, destination) <- getSourceAndDestination mbBooking
+  pure . pure $
+    Common.MapDriverInfoRes
+      { driverName = unwords [driver.firstName, fromMaybe "" driver.lastName],
+        driverStatus = Common.ONLINE,
+        rcNo = vehicleNumber,
+        vehicleStatus = Common.OnRide,
+        position = position,
+        source = source,
+        destination = destination
+      }
