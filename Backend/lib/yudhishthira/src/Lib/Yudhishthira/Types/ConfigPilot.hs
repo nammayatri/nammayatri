88ab7184968f73aabdb77a79f7956fa2e6841517
Mustafa <mustafa.asad@nammayatri.in>
2025-05-29 16:59:55 +0530

    backend/fix:remove-ui-config-from-config-pilot

--- Backend/lib/yudhishthira/src/Lib/Yudhishthira/Types/ConfigPilot.hs
+++ Backend/lib/yudhishthira/src/Lib/Yudhishthira/Types/ConfigPilot.hs
@@ -4,10 +4,8 @@
 
 module Lib.Yudhishthira.Types.ConfigPilot where
 
-import Control.Lens.Operators hiding ((.=))
 import Data.Aeson
 import Data.OpenApi as OpenApi hiding (description, name, tags, version)
-import qualified Data.Text as T
 import Kernel.Beam.Lib.UtilsTH
 import Kernel.Prelude
 import Kernel.Types.Version (DeviceType (..))
@@ -49,56 +47,55 @@ data ConfigType
   | Toll
   | CancellationFarePolicy
   | SurgePricing
-  | UiConfig DeviceType PlatformType
-  deriving (Eq, Ord, Show, Read, Generic, ToJSON, FromJSON, ToSchema)
+  deriving (Eq, Ord, Show, Read, Generic, ToJSON, FromJSON, ToSchema, Enum, Bounded, ToParamSchema)
 
-class ConfigTypeEnumerable a where
-  allValuesConfigTypes :: [a]
+-- class ConfigTypeEnumerable a where
+--   allValuesConfigTypes :: [a]
 
-instance ConfigTypeEnumerable ConfigType where
-  allValuesConfigTypes =
-    [ DriverPoolConfig,
-      TransporterConfig,
-      RiderConfig,
-      FRFSConfig,
-      PayoutConfig,
-      MerchantServiceUsageConfig,
-      HotSpotConfig,
-      MerchantConfig,
-      RideRelatedNotificationConfig,
-      MerchantMessage,
-      MerchantPushNotification,
-      DriverIntelligentPoolConfig,
-      LeaderBoardConfig,
-      CoinsConfig,
-      DocumentVerificationConfig,
-      FleetOwnerDocumentVerificationConfig,
-      GoHomeConfig,
-      SubscriptionConfig,
-      Overlay,
-      FarePolicy,
-      FareProduct,
-      Plan,
-      PlanTranslation,
-      Toll,
-      CancellationFarePolicy,
-      SurgePricing
-    ]
-      ++ (UiConfig <$> [minBound .. maxBound] <*> [minBound .. maxBound])
+-- instance ConfigTypeEnumerable ConfigType where
+--   allValuesConfigTypes =
+--     [ DriverPoolConfig,
+--       TransporterConfig,
+--       RiderConfig,
+--       FRFSConfig,
+--       PayoutConfig,
+--       MerchantServiceUsageConfig,
+--       HotSpotConfig,
+--       MerchantConfig,
+--       RideRelatedNotificationConfig,
+--       MerchantMessage,
+--       MerchantPushNotification,
+--       DriverIntelligentPoolConfig,
+--       LeaderBoardConfig,
+--       CoinsConfig,
+--       DocumentVerificationConfig,
+--       FleetOwnerDocumentVerificationConfig,
+--       GoHomeConfig,
+--       SubscriptionConfig,
+--       Overlay,
+--       FarePolicy,
+--       FareProduct,
+--       Plan,
+--       PlanTranslation,
+--       Toll,
+--       CancellationFarePolicy,
+--       SurgePricing
+--     ]
+--       ++ (UiConfig <$> [minBound .. maxBound] <*> [minBound .. maxBound])
 
 $(mkHttpInstancesForEnum ''ConfigType)
 $(mkBeamInstancesForEnum ''ConfigType)
 
-generateConfigTypeShowInstances :: [String]
-generateConfigTypeShowInstances =
-  map show (Lib.Yudhishthira.Types.ConfigPilot.allValuesConfigTypes :: [ConfigType])
+-- generateConfigTypeShowInstances :: [String]
+-- generateConfigTypeShowInstances =
+--   map show (Lib.Yudhishthira.Types.ConfigPilot.allValuesConfigTypes :: [ConfigType])
 
-instance ToParamSchema ConfigType where
-  toParamSchema _ =
-    mempty
-      & title ?~ "ConfigType"
-      & type_ ?~ OpenApiString
-      & enum_ ?~ map (String . T.pack) generateConfigTypeShowInstances
+-- instance ToParamSchema ConfigType where
+--   toParamSchema _ =
+--     mempty
+--       & title ?~ "ConfigType"
+--       & type_ ?~ OpenApiString
+--       & enum_ ?~ map (String . T.pack) generateConfigTypeShowInstances
 
 data Config a = Config
   { config :: a,
