2575aa52a096b3cd33a46ceb93c3f13eac7e03a5
Hari Krishna <harikrishnadupaguntla1@gmail.com>
2025-06-09 16:19:43 +0530

    backend/added_feedback_request_api

---
+++ Backend/app/rider-platform/rider-app/Main/src-read-only/Domain/Types/FRFSTicketBookingFeedback.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE ApplicativeDo #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+
+module Domain.Types.FRFSTicketBookingFeedback where
+
+import Data.Aeson
+import qualified Domain.Types.FRFSTicketBooking
+import qualified Domain.Types.Merchant
+import qualified Domain.Types.MerchantOperatingCity
+import Kernel.Prelude
+import qualified Kernel.Types.Id
+import qualified Tools.Beam.UtilsTH
+
+data FRFSTicketBookingFeedback = FRFSTicketBookingFeedback
+  { bookingId :: Kernel.Types.Id.Id Domain.Types.FRFSTicketBooking.FRFSTicketBooking,
+    createdAt :: Kernel.Prelude.UTCTime,
+    id :: Kernel.Types.Id.Id Domain.Types.FRFSTicketBookingFeedback.FRFSTicketBookingFeedback,
+    isFareAccepted :: Kernel.Prelude.Bool,
+    merchantId :: Kernel.Types.Id.Id Domain.Types.Merchant.Merchant,
+    merchantOperatingCityId :: Kernel.Types.Id.Id Domain.Types.MerchantOperatingCity.MerchantOperatingCity,
+    updatedAt :: Kernel.Prelude.UTCTime
+  }
+  deriving (Generic, Show, ToJSON, FromJSON, ToSchema)
