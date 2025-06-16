114874bcb6300b35a926ee8d8e65e6da758e71a0
Harshit Chauhan <harshit.devjs@gmail.com>
2025-06-16 14:15:01 +0530

    Get Request For Search Request By Driver Id

--- Backend/app/dashboard/provider-dashboard/src/Domain/Action/ProviderPlatform/RideBooking/SearchRequest.hs
+++ Backend/app/dashboard/provider-dashboard/src/Domain/Action/ProviderPlatform/RideBooking/SearchRequest.hs
@@ -1,10 +1,11 @@
 {-# OPTIONS_GHC -Wwarn=unused-imports #-}
 
-module Domain.Action.ProviderPlatform.RideBooking.SearchRequest (postSearchRequestSearchrequests) where
+module Domain.Action.ProviderPlatform.RideBooking.SearchRequest (postSearchRequestSearchrequests, getSearchRequestList) where
 
 import qualified API.Client.ProviderPlatform.RideBooking
 import qualified API.Types.Dashboard.RideBooking.SearchRequest
 import qualified "lib-dashboard" Domain.Types.Merchant
+import qualified "dynamic-offer-driver-app" Domain.Types.Person
 import qualified Domain.Types.Transaction
 import qualified "lib-dashboard" Environment
 import EulerHS.Prelude
@@ -22,3 +23,8 @@ postSearchRequestSearchrequests merchantShortId opCity apiTokenInfo req = do
   checkedMerchantId <- merchantCityAccessCheck merchantShortId apiTokenInfo.merchant.shortId opCity apiTokenInfo.city
   transaction <- SharedLogic.Transaction.buildTransaction (Domain.Types.Transaction.castEndpoint apiTokenInfo.userActionType) (Kernel.Prelude.Just DRIVER_OFFER_BPP) (Kernel.Prelude.Just apiTokenInfo) Kernel.Prelude.Nothing Kernel.Prelude.Nothing (Kernel.Prelude.Just req)
   SharedLogic.Transaction.withTransactionStoring transaction $ (do API.Client.ProviderPlatform.RideBooking.callRideBookingAPI checkedMerchantId opCity (.searchRequestDSL.postSearchRequestSearchrequests) req)
+
+getSearchRequestList :: (Kernel.Types.Id.ShortId Domain.Types.Merchant.Merchant -> Kernel.Types.Beckn.Context.City -> ApiTokenInfo -> Kernel.Types.Id.Id Domain.Types.Person.Person -> Kernel.Prelude.UTCTime -> Kernel.Prelude.UTCTime -> Kernel.Prelude.Int -> Kernel.Prelude.Int -> Environment.Flow API.Types.Dashboard.RideBooking.SearchRequest.SearchRequestsRes)
+getSearchRequestList merchantShortId opCity apiTokenInfo driverId fromDate toDate mbLimit mbOffset = do
+  checkedMerchantId <- merchantCityAccessCheck merchantShortId apiTokenInfo.merchant.shortId opCity apiTokenInfo.city
+  API.Client.ProviderPlatform.RideBooking.callRideBookingAPI checkedMerchantId opCity (.searchRequestDSL.getSearchRequestList) driverId fromDate toDate mbLimit mbOffset
--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/Dashboard/RideBooking/SearchRequest.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/Dashboard/RideBooking/SearchRequest.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS_GHC -Wwarn=unused-imports #-}
 
-module Domain.Action.Dashboard.RideBooking.SearchRequest (postSearchRequestSearchrequests) where
+module Domain.Action.Dashboard.RideBooking.SearchRequest (postSearchRequestSearchrequests, getSearchRequestList) where
 
 import qualified API.Types.Dashboard.RideBooking.SearchRequest
 import qualified API.Types.Dashboard.RideBooking.SearchRequest as SRType
@@ -9,8 +9,10 @@ import Data.Maybe (mapMaybe)
 import Data.Time.Calendar (fromGregorian)
 import Data.Time.Clock (UTCTime (..))
 import qualified Domain.Types.Merchant
+import qualified Domain.Types.Person
 import qualified Environment
 import EulerHS.Prelude hiding (id)
+import qualified Kernel.Prelude
 import Kernel.Storage.ClickhouseV2.ClickhouseValue (DateTime (..))
 import qualified Kernel.Types.Beckn.Context
 import qualified Kernel.Types.Id
@@ -41,24 +43,26 @@ postSearchRequestSearchrequests _merchantShortId _opCity req = do
           filteredReqs
   return SRType.SearchRequestsRes {searchrequests = modifiedreq}
 
--- postSearchRequestSearchrequests :: (Kernel.Types.Id.ShortId Domain.Types.Merchant.Merchant -> Kernel.Types.Beckn.Context.City -> SRType.SearchRequestsReq -> Environment.Flow SRType.SearchRequestsRes)
--- postSearchRequestSearchrequests _merchantShortId _opCity req = do
---   reqs <- SCSRD.findByDriverId req.driverId req.fromDate req.toDate
---   let requestIds = map (\(_, _, requestId, _, _) -> requestId) reqs
---   sreqs <- SQSR.findSearchRequestById requestIds
---   let populatedReqs = zip reqs sreqs
---   let modifiedreq =
---         map
---           ( \(((_dt, srfdId, requestId, dist, dur), searchRequest)) ->
---               SRType.SearchRequestOfDriver
---                 { createdAt = UTCTime (fromGregorian 2025 1 1) 0,
---                   id = srfdId,
---                   requestId = requestId,
---                   tripEstimatedDistance = dist,
---                   tripEstimatedDuration = dur,
---                   fromLocation = searchRequest.fromLocation,
---                   toLocation = searchRequest.toLocation
---                 }
---           )
---           populatedReqs
---   return SRType.SearchRequestsRes {searchrequests = modifiedreq}
+getSearchRequestList :: (Kernel.Types.Id.ShortId Domain.Types.Merchant.Merchant -> Kernel.Types.Beckn.Context.City -> Kernel.Types.Id.Id Domain.Types.Person.Person -> Kernel.Prelude.UTCTime -> Kernel.Prelude.UTCTime -> Kernel.Prelude.Int -> Kernel.Prelude.Int -> Environment.Flow API.Types.Dashboard.RideBooking.SearchRequest.SearchRequestsRes)
+getSearchRequestList _merchantShortId _opCity driverId fromDate toDate mbLimit mbOffset = do
+  reqs <- SCSRD.findByDriverId driverId fromDate toDate mbLimit mbOffset
+  let requestIds = map (\(_, _, requestId, _, _) -> requestId) reqs
+  sreqs <- SQSR.findSearchRequestById requestIds
+  let sreqMap = Map.fromList [(sr.id, sr) | sr <- sreqs]
+  let filteredReqs = filter (\(_, _, requestId, _, _) -> Map.member requestId sreqMap) reqs
+  let modifiedreq =
+        map
+          ( \(dt, srfdId, requestId, dist, dur) ->
+              let searchRequest = sreqMap Map.! requestId
+               in SRType.SearchRequestOfDriver
+                    { createdAt = getDateTime dt,
+                      id = srfdId,
+                      requestId = requestId,
+                      tripEstimatedDistance = dist,
+                      tripEstimatedDuration = dur,
+                      toLocation = searchRequest.toLocation,
+                      fromLocation = Just searchRequest.fromLocation
+                    }
+          )
+          filteredReqs
+  return SRType.SearchRequestsRes {searchrequests = modifiedreq}
