681b24cc9ab25abe0fc00ea7b68190dbbbb05ba8
saimanohar.veeravajhula <saimanoharveeravajhula@gmail.com>
2025-06-16 19:36:23 +0530

    backend/enh/ip-based-auth-rate-limit-block

--- Backend/app/rider-platform/rider-app/Main/src/Domain/Action/Dashboard/RideBooking/Registration.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Domain/Action/Dashboard/RideBooking/Registration.hs
@@ -25,7 +25,7 @@ postRegistrationAuth ::
   Environment.Flow Domain.Action.UI.Registration.AuthRes
 postRegistrationAuth merchantShortId _opCity req = do
   let authReq = buildAuthReq merchantShortId req
-  Domain.Action.UI.Registration.auth authReq Nothing Nothing Nothing Nothing Nothing
+  Domain.Action.UI.Registration.auth authReq Nothing Nothing Nothing Nothing Nothing Nothing
 
 postRegistrationVerify ::
   Kernel.Types.Id.ShortId Domain.Types.Merchant.Merchant ->
