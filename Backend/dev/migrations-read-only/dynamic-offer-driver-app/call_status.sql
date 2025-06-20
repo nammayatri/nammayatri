3b40cb9b58fb45e0a09d2756de8bb76b661b5d73
Pravinkumar S <spravinkumar9952@gmail.com>
2025-06-20 15:39:49 +0530

    backend/feat: live ekd fixes

--- Backend/dev/migrations-read-only/dynamic-offer-driver-app/call_status.sql
+++ Backend/dev/migrations-read-only/dynamic-offer-driver-app/call_status.sql
@@ -22,3 +22,8 @@ ALTER TABLE atlas_driver_offer_bpp.call_status ADD COLUMN call_attempt text ;
 ------- SQL updates -------
 
 ALTER TABLE atlas_driver_offer_bpp.call_status ADD COLUMN merchant_operating_city_id character varying(36) ;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_driver_offer_bpp.call_status ADD COLUMN ai_call_analyzed boolean ;
