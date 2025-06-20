73ef01722897d317d1f839e628c69208144226cf
Madhav-nm <venubrt@gmail.com>
2025-06-20 15:37:47 +0530

    backend/Refactor: Update AlertRequestStatus to remove redundant ToHttpApiData instance and unify data declaration

--- Backend/lib/beckn-spec/src/Domain/Types/Alert/AlertRequestStatus.hs
+++ Backend/lib/beckn-spec/src/Domain/Types/Alert/AlertRequestStatus.hs
@@ -1,12 +1,5 @@
 module Domain.Types.Alert.AlertRequestStatus where
 
 import Kernel.Prelude
-import Servant.API (ToHttpApiData (..))
 
 data AlertRequestStatus = ACCEPTED | REJECTED | AWAITING_APPROVAL | REVOKED deriving (Show, (Eq), (Ord), (Read), (Generic), (ToJSON), (FromJSON), (ToSchema), ToParamSchema)
-
-instance ToHttpApiData AlertRequestStatus where
-  toUrlPiece ACCEPTED = "ACCEPTED"
-  toUrlPiece REJECTED = "REJECTED"
-  toUrlPiece AWAITING_APPROVAL = "AWAITING_APPROVAL"
-  toUrlPiece REVOKED = "REVOKED"
