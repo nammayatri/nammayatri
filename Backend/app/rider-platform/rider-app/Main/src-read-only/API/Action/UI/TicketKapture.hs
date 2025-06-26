e0d66aee92bea13e5f9bdd11175504f4e523217b
Chakradhar Kamminana <chakradhar007007kc@gmail.com>
2025-06-26 19:37:56 +0530

    backend/feat/kapture-pull-api

--- Backend/app/rider-platform/rider-app/Main/src-read-only/API/Action/UI/TicketKapture.hs
+++ Backend/app/rider-platform/rider-app/Main/src-read-only/API/Action/UI/TicketKapture.hs
@@ -16,16 +16,25 @@ import qualified Environment
 import EulerHS.Prelude
 import qualified Kernel.External.Ticket.Interface.Types
 import qualified Kernel.Prelude
+import qualified Kernel.Types.APISuccess
 import qualified Kernel.Types.Id
 import Kernel.Utils.Common
 import Servant
 import Storage.Beam.SystemConfigs ()
 import Tools.Auth
 
-type API = (TokenAuth :> "kaptureCustomerLogin" :> MandatoryQueryParam "ticketType" Kernel.External.Ticket.Interface.Types.TicketType :> Post '[JSON] API.Types.UI.TicketKapture.TicketKaptureResp)
+type API =
+  ( TokenAuth :> "kaptureCustomerLogin" :> MandatoryQueryParam "ticketType" Kernel.External.Ticket.Interface.Types.TicketType
+      :> Post
+           '[JSON]
+           API.Types.UI.TicketKapture.TicketKaptureResp
+      :<|> TokenAuth
+      :> "kaptureCloseTicket"
+      :> Post '[JSON] Kernel.Types.APISuccess.APISuccess
+  )
 
 handler :: Environment.FlowServer API
-handler = postKaptureCustomerLogin
+handler = postKaptureCustomerLogin :<|> postKaptureCloseTicket
 
 postKaptureCustomerLogin ::
   ( ( Kernel.Types.Id.Id Domain.Types.Person.Person,
@@ -35,3 +44,6 @@ postKaptureCustomerLogin ::
     Environment.FlowHandler API.Types.UI.TicketKapture.TicketKaptureResp
   )
 postKaptureCustomerLogin a2 a1 = withFlowHandlerAPI $ Domain.Action.UI.TicketKapture.postKaptureCustomerLogin (Control.Lens.over Control.Lens._1 Kernel.Prelude.Just a2) a1
+
+postKaptureCloseTicket :: ((Kernel.Types.Id.Id Domain.Types.Person.Person, Kernel.Types.Id.Id Domain.Types.Merchant.Merchant) -> Environment.FlowHandler Kernel.Types.APISuccess.APISuccess)
+postKaptureCloseTicket a1 = withFlowHandlerAPI $ Domain.Action.UI.TicketKapture.postKaptureCloseTicket (Control.Lens.over Control.Lens._1 Kernel.Prelude.Just a1)
