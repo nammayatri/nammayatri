f25fb0cdbb91bdb4e724da7d24abafd68cf4ea04
Harshit <97081728+H-C-21@users.noreply.github.com>
2025-07-01 17:29:07 +0530

    Vendor Fees Being Calulcalated for all Driver Fees Instead of Only Th… (#11510)
    …ose Being Processed
    
    ## Type of Change
    <!-- Put an `x` in the boxes that apply -->
    
    - [X ] Bugfix
    - [ ] New feature
    - [ ] Enhancement
    - [ ] Refactoring
    - [ ] Dependency updates
    
    ## Description
    <!-- Describe your changes in detail -->
    
    
    ### Additional Changes
    
    - [ ] This PR modifies the database schema (database migration added)
    - [ ] This PR modifies dhall configs/environment variables
    - [ ] This PR contains API breaking changes
    <!-- 
    Provide links to the files with corresponding changes.
    
    You can find config files in `dhall-configs`
    -->
    
    
    ## Motivation and Context
    <!--
    Why is this change required? What problem does it solve?
    If it fixes an open issue, please link to the issue here.
    
    If you don't have an issue, we'd recommend starting with one first so
    the PR
    can focus on the implementation (unless its an obvious bug or
    documentation fix
    that will have little conversation).
    -->
    
    
    ## How did you test it?
    <!--
    Did you write an integration/unit/API test to verify the code changes?
    Or did you test this change manually (provide relevant screenshots)?
    -->
    
    ## Screenshot of test results
    <!-- Provide screenshot of the test results -->
    
    ## Checklist
    <!-- Put an `x` in the boxes that apply -->
    
    - [ ] I formatted the code and addressed linter errors
    `./dev/format-all-files.sh`
    - [ ] I reviewed submitted code
    - [ ] I added unit tests for my changes where possible
    - [ ] I added integration tests for my changes where possible
    - [ ] I tested the changes using integration tests
    - [ ] I added a [CHANGELOG](/CHANGELOG.md) entry if applicable
    - [ ] No leak detected in leakcanary
    
    Co-authored-by: Harshit Chauhan <harshit.devjs@gmail.com>

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/SharedLogic/Payment.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/SharedLogic/Payment.hs
@@ -96,7 +96,12 @@ createOrder (driverId, merchantId, opCity) serviceName (driverFees, driverFeesTo
       (invoiceId, invoiceShortId) = fromMaybe (genInvoiceId, genShortInvoiceId.getShortId) existingInvoice
       amount = sum $ (\pendingFees -> roundToHalf pendingFees.currency (pendingFees.govtCharges + pendingFees.platformFee.fee + pendingFees.platformFee.cgst + pendingFees.platformFee.sgst)) <$> driverFees
       invoices = mkInvoiceAgainstDriverFee invoiceId.getId invoiceShortId now (mbMandateOrder <&> (.maxAmount)) invoicePaymentMode <$> driverFees
-  splitSettlementDetails <- if splitEnabled then mkSplitSettlementDetails vendorFees amount else pure Nothing
+  let driverFeeIds = map (.id) driverFees
+  let currentVendorFees =
+        if splitEnabled -- Filter vendor fees only for the driver fees that are being settled right now
+          then filter (\vf -> vf.driverFeeId `elem` driverFeeIds) vendorFees
+          else vendorFees
+  splitSettlementDetails <- if splitEnabled then mkSplitSettlementDetails currentVendorFees amount else pure Nothing
   logInfo $ "split details: " <> show splitSettlementDetails
   when (amount <= 0) $ throwError (InternalError "Invalid Amount :- should be greater than 0")
   unless (isJust existingInvoice) $ QIN.createMany invoices
