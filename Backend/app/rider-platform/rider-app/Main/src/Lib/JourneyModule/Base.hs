d9387b4fe7ee85af56103be1b534b11cdbc58dd5
PraveenGongada <praveengongada0603@gmail.com>
2025-07-01 19:07:12 +0530

    Backend/enhc: added logs for multimodal confirm

--- Backend/app/rider-platform/rider-app/Main/src/Lib/JourneyModule/Base.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Lib/JourneyModule/Base.hs
@@ -455,6 +455,7 @@ startJourney confirmElements forcedBookedLegOrder journeyId = do
             childTicketQuantity = mElement >>= (.childTicketQuantity)
         let forcedBooking = Just leg.order == forcedBookedLegOrder
         let crisSdkResponse = find (\element -> element.journeyLegOrder == leg.order) confirmElements >>= (.crisSdkResponse)
+        when (isJust crisSdkResponse) $ logInfo $ "CRIS: crisSdkResponse startJourney: " <> show crisSdkResponse
         when (leg.status /= JL.Skipped) $ do
           JLI.confirm forcedBooking ticketQuantity childTicketQuantity leg crisSdkResponse
     )
