5df5a6b9abfef8135d7aa5edc71fc50dabe41a69
Chakradhar Kamminana <chakradhar007007kc@gmail.com>
2025-07-20 09:10:18 +0530

    backend/enh: kapture get closed ticket details

--- Backend/app/rider-platform/rider-app/Main/src-read-only/API/Types/UI/TicketKapture.hs
+++ Backend/app/rider-platform/rider-app/Main/src-read-only/API/Types/UI/TicketKapture.hs
@@ -2,10 +2,12 @@
 
 module API.Types.UI.TicketKapture where
 
+import qualified Data.Aeson as A
 import Data.OpenApi (ToSchema)
 import qualified Data.Text
 import qualified Domain.Types.Ride
 import EulerHS.Prelude hiding (id)
+import qualified Kernel.External.Ticket.Kapture.Types
 import qualified Kernel.Prelude
 import qualified Kernel.Types.Id
 import Servant
@@ -15,7 +17,19 @@ data ActiveTicketsRes = ActiveTicketsRes {rideId :: Kernel.Prelude.Maybe (Kernel
   deriving stock (Generic)
   deriving anyclass (ToJSON, FromJSON, ToSchema)
 
+data CloseTicketResp = CloseTicketResp {rideId :: Kernel.Prelude.Maybe (Kernel.Types.Id.Id Domain.Types.Ride.Ride), ticketId :: Data.Text.Text}
+  deriving stock (Generic)
+  deriving anyclass (ToJSON, FromJSON, ToSchema)
+
 data GetAllActiveTicketsRes = GetAllActiveTicketsRes {activeTickets :: [ActiveTicketsRes]}
+  deriving stock (Generic)
+  deriving anyclass (ToJSON, FromJSON, ToSchema)
+
+data GetClosedTicketDetailsRes = GetClosedTicketDetailsRes {chatMessages :: [Kernel.External.Ticket.Kapture.Types.ChatMessage]}
+  deriving stock (Generic)
+  deriving anyclass (ToJSON, FromJSON, ToSchema)
+
+data GetClosedTicketIdsRes = GetClosedTicketIdsRes {closedTicketIds :: [CloseTicketResp]}
   deriving stock (Generic)
   deriving anyclass (ToJSON, FromJSON, ToSchema)
 
