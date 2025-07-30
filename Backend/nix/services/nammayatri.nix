5befd82f23199ae67020dc6e1194ea3296eb3d1f
Harshit <97081728+H-C-21@users.noreply.github.com>
2025-07-30 14:12:38 +0530

    Update nammayatri.nix

--- Backend/nix/services/nammayatri.nix
+++ Backend/nix/services/nammayatri.nix
@@ -126,22 +126,7 @@ in
                 set -x
                 pwd
                 rm -f ./*.log # Clean up the log files
-                redis-cli -p 30001 -c XGROUP CREATE Available_Jobs_Rider_1 myGroup_Rider  0 MKSTREAM # TODO: remove this once cluster funtions from euler are fixed
-                redis-cli -p 30001 -c XGROUP CREATE Available_Jobs_Rider_2 myGroup_Rider  0
-                redis-cli -p 30001 -c XGROUP CREATE Available_Jobs_Rider_3 myGroup_Rider  0
-                redis-cli -p 30001 -c XGROUP CREATE Available_Jobs_Rider_4 myGroup_Rider  0
-                redis-cli -p 30001 -c XGROUP CREATE Available_Jobs_Rider_5 myGroup_Rider  0
-                redis-cli -p 30001 -c XGROUP CREATE Available_Jobs_Rider_6 myGroup_Rider  0
-                redis-cli -p 30001 -c XGROUP CREATE Available_Jobs_Rider_7 myGroup_Rider  0
-                redis-cli -p 30001 -c XGROUP CREATE Available_Jobs_Rider_8 myGroup_Rider  0
-                redis-cli -p 30001 -c XGROUP CREATE Available_Jobs_Rider_9 myGroup_Rider  0
-                redis-cli -p 30001 -c XGROUP CREATE Available_Jobs_Rider_10 myGroup_Rider  0
-                redis-cli -p 30001 -c XGROUP CREATE Available_Jobs_Rider_11 myGroup_Rider  0
-                redis-cli -p 30001 -c XGROUP CREATE Available_Jobs_Rider_12 myGroup_Rider  0
-                redis-cli -p 30001 -c XGROUP CREATE Available_Jobs_Rider_13 myGroup_Rider  0
-                redis-cli -p 30001 -c XGROUP CREATE Available_Jobs_Rider_14 myGroup_Rider  0
-                redis-cli -p 30001 -c XGROUP CREATE Available_Jobs_Rider_15 myGroup_Rider  0
-                redis-cli -p 30001 -c XGROUP CREATE Available_Jobs_Rider_16 myGroup_Rider  0
+                redis-cli -p 30001 -c XGROUP CREATE Available_Jobs_Rider myGroup_Rider  0 MKSTREAM # TODO: remove this once cluster funtions from euler are fixed
                 redis-cli -p 30001 -c XGROUP CREATE Available_Jobs myGroup  0 MKSTREAM # TODO: remove this once cluster funtions from euler are fixed
                 redis-cli -p 30001 -c XGROUP CREATE Available_Chakras myGroup_Chakras  0 MKSTREAM # TODO: remove this once cluster funtions from euler are fixed
                 redis-cli XGROUP CREATE Available_Jobs_Rider myGroup_Rider 0 MKSTREAM
