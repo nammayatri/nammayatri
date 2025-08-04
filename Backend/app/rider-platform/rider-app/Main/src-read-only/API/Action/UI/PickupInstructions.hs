e17dae135248c5cb81788945f3397aa28f2ce9f8
Aman Dwivedi <19dwivediaman@gmail.com>
2025-08-04 20:43:33 +0530

    Backend/enh/pickup-instruction-delete

--- Backend/app/rider-platform/rider-app/Main/src-read-only/API/Action/UI/PickupInstructions.hs
+++ Backend/app/rider-platform/rider-app/Main/src-read-only/API/Action/UI/PickupInstructions.hs
@@ -40,10 +40,24 @@ type API =
       :> Get
            ('[JSON])
            API.Types.UI.PickupInstructions.ClosestPickupInstructionResp
+      :<|> TokenAuth
+      :> "pickupinstructions"
+      :> QueryParam
+           "lat"
+           Kernel.Prelude.Double
+      :> QueryParam
+           "lon"
+           Kernel.Prelude.Double
+      :> QueryParam
+           "target"
+           API.Types.UI.PickupInstructions.DeleteTarget
+      :> Delete
+           ('[JSON])
+           Kernel.Types.APISuccess.APISuccess
   )
 
 handler :: Environment.FlowServer API
-handler = postPickupinstructions :<|> getPickupinstructionsClosest
+handler = postPickupinstructions :<|> getPickupinstructionsClosest :<|> deletePickupinstructions
 
 postPickupinstructions ::
   ( ( Kernel.Types.Id.Id Domain.Types.Person.Person,
@@ -63,3 +77,14 @@ getPickupinstructionsClosest ::
     Environment.FlowHandler API.Types.UI.PickupInstructions.ClosestPickupInstructionResp
   )
 getPickupinstructionsClosest a3 a2 a1 = withFlowHandlerAPI $ Domain.Action.UI.PickupInstructions.getPickupinstructionsClosest (Control.Lens.over Control.Lens._1 Kernel.Prelude.Just a3) a2 a1
+
+deletePickupinstructions ::
+  ( ( Kernel.Types.Id.Id Domain.Types.Person.Person,
+      Kernel.Types.Id.Id Domain.Types.Merchant.Merchant
+    ) ->
+    Kernel.Prelude.Maybe (Kernel.Prelude.Double) ->
+    Kernel.Prelude.Maybe (Kernel.Prelude.Double) ->
+    Kernel.Prelude.Maybe (API.Types.UI.PickupInstructions.DeleteTarget) ->
+    Environment.FlowHandler Kernel.Types.APISuccess.APISuccess
+  )
+deletePickupinstructions a4 a3 a2 a1 = withFlowHandlerAPI $ Domain.Action.UI.PickupInstructions.deletePickupinstructions (Control.Lens.over Control.Lens._1 Kernel.Prelude.Just a4) a3 a2 a1
