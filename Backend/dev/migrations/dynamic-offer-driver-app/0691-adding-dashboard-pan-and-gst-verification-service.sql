c02df01502ab7a58a1b73cf6c8f624604044ff0d
Brajmohank1 <brajmohanst9661@gmail.com>
2025-08-29 11:16:57 +0530

    backend/fix: Add dashboard PAN & GST verification columns (missed earlier)

--- Backend/dev/migrations/dynamic-offer-driver-app/0691-adding-dashboard-pan-and-gst-verification-service.sql
+++ Backend/dev/migrations/dynamic-offer-driver-app/0691-adding-dashboard-pan-and-gst-verification-service.sql
@@ -1,4 +1,11 @@
-UPDATE atlas_driver_offer_bpp.merchant_service_usage_config
+ALTER TABLE atlas_driver_offer_bpp.merchant_service_usage_config ADD  COLUMN
+dashboard_pan_verification_service character varying(30) ;
+
+ALTER TABLE atlas_driver_offer_bpp.merchant_service_usage_config  ADD COLUMN
+dashboard_gst_verification_service  character varying(30) ;
+
+-- please run this also , after running above query
+UPDATE atlas_driver_offer_bpp.merchant_service_usage_config --
 SET
     dashboard_pan_verification_service = 'Idfy',
     dashboard_gst_verification_service = 'Idfy';
