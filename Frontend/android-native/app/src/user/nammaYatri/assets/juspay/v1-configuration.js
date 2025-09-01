b175ab72191c1b8d36d63d334a71ff24fbc3cd51
Anuj Kumar <whoanujyadav@gmail.com>
2025-09-01 19:46:07 +0530

    frontend:enhanc/helpAndSupportDisabledForOY

--- Frontend/android-native/app/src/user/nammaYatri/assets/juspay/v1-configuration.js
+++ Frontend/android-native/app/src/user/nammaYatri/assets/juspay/v1-configuration.js
@@ -764,6 +764,7 @@ const odishaYatriConfig = function () {
       , "enableEditPickupLocation" : true
       , "enableRepeatTripBackfilling" : true
       , "enableEditDestination" : true
+      , "enableHelpAndSupport" : true
     }
     , "appData" : {
       "link" : getAppLink(window.__OS)
