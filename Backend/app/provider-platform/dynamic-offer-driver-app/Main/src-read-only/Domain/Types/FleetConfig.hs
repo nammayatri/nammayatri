fa68dd29414b576a740a7631d5f0ab752fef1b27
Karan <itsofficialkaran26@gmail.com>
2025-09-04 17:46:56 +0530

    feat: blacklist driver coin on FleetOwnerID level

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/Domain/Types/FleetConfig.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/Domain/Types/FleetConfig.hs
@@ -10,12 +10,14 @@ import qualified Domain.Types.Person
 import Kernel.Prelude
 import qualified Kernel.Types.Common
 import qualified Kernel.Types.Id
+import qualified Lib.DriverCoins.Types
 import qualified Tools.Beam.UtilsTH
 
 data FleetConfig = FleetConfig
   { allowAutomaticRoundTripAssignment :: Kernel.Prelude.Bool,
     allowEndingMidRoute :: Kernel.Prelude.Bool,
     allowStartRideFromQR :: Kernel.Prelude.Bool,
+    blacklistCoinEvents :: Kernel.Prelude.Maybe [Lib.DriverCoins.Types.DriverCoinsFunctionType],
     directlyStartFirstTripAssignment :: Kernel.Prelude.Bool,
     endRideDistanceThreshold :: Kernel.Types.Common.HighPrecMeters,
     fleetOwnerId :: Kernel.Types.Id.Id Domain.Types.Person.Person,
