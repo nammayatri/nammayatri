c7bc4c1cdebab99fa942734f418a9b95d19c2509
Karan <itsofficialkaran26@gmail.com>
2025-09-04 17:46:56 +0530

    feat: blacklist driver coin on FleetOwnerID level

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/Storage/Beam/FleetConfig.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/Storage/Beam/FleetConfig.hs
@@ -10,21 +10,23 @@ import Kernel.External.Encryption
 import Kernel.Prelude
 import qualified Kernel.Prelude
 import qualified Kernel.Types.Common
+import qualified Lib.DriverCoins.Types
 import Tools.Beam.UtilsTH
 
 data FleetConfigT f = FleetConfigT
-  { allowAutomaticRoundTripAssignment :: (B.C f Kernel.Prelude.Bool),
-    allowEndingMidRoute :: (B.C f Kernel.Prelude.Bool),
-    allowStartRideFromQR :: (B.C f Kernel.Prelude.Bool),
-    directlyStartFirstTripAssignment :: (B.C f (Kernel.Prelude.Maybe Kernel.Prelude.Bool)),
-    endRideDistanceThreshold :: (B.C f (Kernel.Prelude.Maybe Kernel.Types.Common.HighPrecMeters)),
-    fleetOwnerId :: (B.C f Data.Text.Text),
-    rideEndApproval :: (B.C f Kernel.Prelude.Bool),
-    unlinkDriverAndVehicleOnTripTermination :: (B.C f (Kernel.Prelude.Maybe Kernel.Prelude.Bool)),
-    merchantId :: (B.C f (Kernel.Prelude.Maybe (Data.Text.Text))),
-    merchantOperatingCityId :: (B.C f (Kernel.Prelude.Maybe (Data.Text.Text))),
-    createdAt :: (B.C f Kernel.Prelude.UTCTime),
-    updatedAt :: (B.C f Kernel.Prelude.UTCTime)
+  { allowAutomaticRoundTripAssignment :: B.C f Kernel.Prelude.Bool,
+    allowEndingMidRoute :: B.C f Kernel.Prelude.Bool,
+    allowStartRideFromQR :: B.C f Kernel.Prelude.Bool,
+    blacklistCoinEvents :: (B.C f (Kernel.Prelude.Maybe [Lib.DriverCoins.Types.DriverCoinsFunctionType])),
+    directlyStartFirstTripAssignment :: B.C f (Kernel.Prelude.Maybe Kernel.Prelude.Bool),
+    endRideDistanceThreshold :: B.C f (Kernel.Prelude.Maybe Kernel.Types.Common.HighPrecMeters),
+    fleetOwnerId :: B.C f Data.Text.Text,
+    rideEndApproval :: B.C f Kernel.Prelude.Bool,
+    unlinkDriverAndVehicleOnTripTermination :: B.C f (Kernel.Prelude.Maybe Kernel.Prelude.Bool),
+    merchantId :: B.C f (Kernel.Prelude.Maybe Data.Text.Text),
+    merchantOperatingCityId :: B.C f (Kernel.Prelude.Maybe Data.Text.Text),
+    createdAt :: B.C f Kernel.Prelude.UTCTime,
+    updatedAt :: B.C f Kernel.Prelude.UTCTime
   }
   deriving (Generic, B.Beamable)
 
