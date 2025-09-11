9f5c64672c1dc5d552d72833be19b6c7150a5b16
Shivam Rai <shiv143shir@gmail.com>
2025-09-11 18:09:15 +0530

    Backend/fix/frfs_ticket_categories_queries

--- Backend/dev/migrations-read-only/rider-app/frfs_quote_category.sql
+++ Backend/dev/migrations-read-only/rider-app/frfs_quote_category.sql
@@ -5,7 +5,7 @@ ALTER TABLE atlas_app.frfs_quote_category ADD COLUMN id character varying(36) NO
 ALTER TABLE atlas_app.frfs_quote_category ADD COLUMN max_ticket_allowed integer ;
 ALTER TABLE atlas_app.frfs_quote_category ADD COLUMN merchant_id character varying(36) NOT NULL;
 ALTER TABLE atlas_app.frfs_quote_category ADD COLUMN merchant_operating_city_id character varying(36) NOT NULL;
-ALTER TABLE atlas_app.frfs_quote_category ADD COLUMN offered_price text NOT NULL;
+ALTER TABLE atlas_app.frfs_quote_category ADD COLUMN offered_price double precision NOT NULL;
 ALTER TABLE atlas_app.frfs_quote_category ADD COLUMN currency text ;
 ALTER TABLE atlas_app.frfs_quote_category ADD COLUMN price double precision NOT NULL;
 ALTER TABLE atlas_app.frfs_quote_category ADD COLUMN quote_id character varying(36) NOT NULL;
@@ -13,9 +13,3 @@ ALTER TABLE atlas_app.frfs_quote_category ADD COLUMN ticket_category_metadata_co
 ALTER TABLE atlas_app.frfs_quote_category ADD COLUMN created_at timestamp with time zone NOT NULL default CURRENT_TIMESTAMP;
 ALTER TABLE atlas_app.frfs_quote_category ADD COLUMN updated_at timestamp with time zone NOT NULL default CURRENT_TIMESTAMP;
 ALTER TABLE atlas_app.frfs_quote_category ADD PRIMARY KEY ( id);
-
-
-
-------- SQL updates -------
-
-ALTER TABLE atlas_app.frfs_quote_category ALTER COLUMN offered_price TYPE double precision;
