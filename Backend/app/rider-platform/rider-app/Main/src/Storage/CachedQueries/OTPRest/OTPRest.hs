87b5fd1da28278a8fdfdcf8aa7feae5115bbd692
khuzema786 <khomosikhuzema894@gmail.com>
2025-09-16 10:51:31 +0530

    backend/feat: Single Mode Bus OTP Flow

--- Backend/app/rider-platform/rider-app/Main/src/Storage/CachedQueries/OTPRest/OTPRest.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Storage/CachedQueries/OTPRest/OTPRest.hs
@@ -105,6 +105,17 @@ getRouteStopMappingByRouteCode routeCode integratedBPPConfig = do
   logDebug $ "routeStopMapping from rest api after parsing: " <> show routeStopMapping
   return routeStopMapping
 
+getRouteStopMappingByRouteCodeInMem ::
+  (CoreMetrics m, MonadFlow m, MonadReader r m, HasShortDurationRetryCfg r c, Log m, CacheFlow m r, EsqDBFlow m r) =>
+  Text ->
+  IntegratedBPPConfig ->
+  m [RouteStopMappingInMemoryServer]
+getRouteStopMappingByRouteCodeInMem routeCode integratedBPPConfig = do
+  baseUrl <- MM.getOTPRestServiceReq integratedBPPConfig.merchantId integratedBPPConfig.merchantOperatingCityId
+  routeStopMapping' <- Flow.getRouteStopMappingInMemoryServer baseUrl integratedBPPConfig.feedKey (Just routeCode) Nothing
+  logDebug $ "routeStopMapping from rest api: " <> show routeStopMapping'
+  return routeStopMapping'
+
 getRouteStopMappingByStopCode ::
   (CoreMetrics m, MonadFlow m, MonadReader r m, HasShortDurationRetryCfg r c, Log m, CacheFlow m r, EsqDBFlow m r) =>
   Text ->
