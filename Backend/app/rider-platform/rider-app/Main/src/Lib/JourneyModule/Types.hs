c05c71a2a7fe22acd7d844dd499c3a7daa1ec395
Akhilesh Singh Bhadauriya <akhileshsingh2314@gmail.com>
2025-09-16 11:14:12 +0530

    backend/fix: update pricing logic in mkLegInfoFromFrfsBooking to use FRFSQuote for accurate fare estimation

--- Backend/app/rider-platform/rider-app/Main/src/Lib/JourneyModule/Types.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Lib/JourneyModule/Types.hs
@@ -86,7 +86,7 @@ import qualified Storage.Queries.Transformers.Booking as QTB
 import Tools.Error
 import Tools.Maps as Maps
 import Tools.Metrics.BAPMetrics.Types
-import Tools.Payment (roundToTwoDecimalPlaces)
+-- import Tools.Payment (roundToTwoDecimalPlaces)
 import qualified Tools.SharedRedisKeys as SharedRedisKeys
 import TransactionLogs.Types
 
@@ -779,13 +779,10 @@ mkLegInfoFromFrfsBooking booking journeyLeg = do
   now <- getCurrentTime
 
   let startTime = fromMaybe now booking.startTime
-  let singleAdultPrice = roundToTwoDecimalPlaces . HighPrecMoney $ safeDiv (getHighPrecMoney booking.price.amount) (fromIntegral booking.quantity) -- TODO :: To be handled as single price cannot be obtained from this if more than 1 fare breakup (Child Quantity / Discounts)
-      estimatedPrice =
-        Price
-          { amount = singleAdultPrice,
-            amountInt = Money $ roundToIntegral singleAdultPrice,
-            currency = booking.price.currency
-          }
+
+  -- Fetch the FRFSQuote to get accurate pricing information
+  quote <- QFRFSQuote.findById booking.quoteId >>= fromMaybeM (QuoteNotFound booking.quoteId.getId)
+  let estimatedPrice = quote.price
 
   (oldStatus, bookingStatus, trackingStatuses) <- JMStateUtils.getFRFSAllStatuses journeyLeg (Just booking)
   journeyLegInfo' <- getLegRouteInfo (zip journeyLeg.routeDetails trackingStatuses) integratedBPPConfig
@@ -802,7 +799,7 @@ mkLegInfoFromFrfsBooking booking journeyLeg = do
         order = journeyLeg.sequenceNumber,
         estimatedDuration = journeyLeg.duration,
         estimatedMinFare = Just $ mkPriceAPIEntity estimatedPrice,
-        estimatedChildFare = Nothing,
+        estimatedChildFare = mkPriceAPIEntity <$> quote.childPrice,
         estimatedMaxFare = Just $ mkPriceAPIEntity estimatedPrice,
         estimatedTotalFare = Nothing,
         estimatedDistance = journeyLeg.distance,
@@ -922,9 +919,10 @@ mkLegInfoFromFrfsBooking booking journeyLeg = do
                   childTicketQuantity = booking.childTicketQuantity,
                   refund = refundBloc
                 }
-    safeDiv :: (Eq a, Fractional a) => a -> a -> a
-    safeDiv x 0 = x
-    safeDiv x y = x / y
+
+-- safeDiv :: (Eq a, Fractional a) => a -> a -> a
+-- safeDiv x 0 = x
+-- safeDiv x y = x / y
 
 getLegRouteInfo :: (CacheFlow m r, EncFlow m r, EsqDBFlow m r, MonadFlow m, HasShortDurationRetryCfg r c) => [(RouteDetails, (Int, JMState.TrackingStatus, UTCTime))] -> DIBC.IntegratedBPPConfig -> m [LegRouteInfo]
 getLegRouteInfo journeyRouteDetailsWithTrackingStatuses integratedBPPConfig = do
