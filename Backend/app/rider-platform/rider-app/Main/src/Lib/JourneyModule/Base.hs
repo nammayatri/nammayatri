1cad423e58cd01e31359e3cc5fc0504b6d795862
khuzema786 <khomosikhuzema894@gmail.com>
2025-09-18 02:06:01 +0530

    backend/enhancement: Added onboareded fleetNo to riderLocation

--- Backend/app/rider-platform/rider-app/Main/src/Lib/JourneyModule/Base.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Lib/JourneyModule/Base.hs
@@ -1333,7 +1333,8 @@ generateJourneyStatusResponse journey legs = do
               trackingStatusLastUpdatedAt = legData.trackingStatusLastUpdatedAt,
               userPosition = legData.userPosition,
               vehiclePositions = legData.vehiclePositions,
-              mode = legData.mode
+              mode = legData.mode,
+              fleetNo = legData.fleetNo
             }
 
 markLegStatus :: (EsqDBFlow m r, MonadFlow m, CacheFlow m r) => Maybe JL.JourneyLegStatus -> Maybe JMState.TrackingStatus -> DJourneyLeg.JourneyLeg -> Maybe Int -> UTCTime -> m ()
