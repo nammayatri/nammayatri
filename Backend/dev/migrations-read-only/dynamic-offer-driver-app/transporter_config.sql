5851928c56c99a79fe13199ceff3910a7308f00d
Rahul Gupta <rhlgupta0706@gmail.com>
2025-09-27 15:55:39 +0530

    fix: query-fix-in-transporter-config

--- Backend/dev/migrations-read-only/dynamic-offer-driver-app/transporter_config.sql
+++ Backend/dev/migrations-read-only/dynamic-offer-driver-app/transporter_config.sql
@@ -558,5 +558,4 @@ ALTER TABLE atlas_driver_offer_bpp.transporter_config ADD COLUMN enable_wallet_t
 
 ------- SQL updates -------
 
-
-ALTER TABLE atlas_driver_offer_bpp.transporter_config ADD COLUMN driver_calling_option_configs json  default {"driverCallingOptions": "AnonymousCall","directCallingAllowedGenderType": "[MALE]"};
+ALTER TABLE atlas_driver_offer_bpp.transporter_config ADD COLUMN driver_calling_option_configs json  default '{"driverCallingOptions": "AnonymousCall","directCallingAllowedGenderType": "[MALE]"}';
