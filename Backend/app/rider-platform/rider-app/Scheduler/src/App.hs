1ca449dba64d618052b690b2a8e628304d5cc958
Yashika Kaushik <yashikakaushik09@gmail.com>
2025-09-30 18:44:51 +0530

    backend/feat/cris-recon-ticket-enquiry

--- Backend/app/rider-platform/rider-app/Scheduler/src/App.hs
+++ Backend/app/rider-platform/rider-app/Scheduler/src/App.hs
@@ -47,6 +47,7 @@ import "rider-app" SharedLogic.Scheduler.Jobs.CheckExotelCallStatusAndNotifyBPP
 import "rider-app" SharedLogic.Scheduler.Jobs.CheckMultimodalConfirmFail
 import "rider-app" SharedLogic.Scheduler.Jobs.CheckPNAndSendSMS
 import "rider-app" SharedLogic.Scheduler.Jobs.CheckRefundStatus
+import "rider-app" SharedLogic.Scheduler.Jobs.CrisRecon
 import "rider-app" SharedLogic.Scheduler.Jobs.ExecutePaymentIntent
 import "rider-app" SharedLogic.Scheduler.Jobs.MetroBusinessHour
 import "rider-app" SharedLogic.Scheduler.Jobs.NyRegularInstance
@@ -99,6 +100,7 @@ schedulerHandle flowRt env =
           & putJobHandlerInList (liftIO . runFlowR flowRt env . updateCrisUtsDataJob)
           & putJobHandlerInList (liftIO . runFlowR flowRt env . checkMultimodalConfirmFailJob)
           & putJobHandlerInList (liftIO . runFlowR flowRt env . checkRefundStatusJob)
+          & putJobHandlerInList (liftIO . runFlowR flowRt env . crisReconJob)
           & putJobHandlerInList (liftIO . runFlowR flowRt env . updateMetroBusinessHour)
           & putJobHandlerInList (liftIO . runFlowR flowRt env . runNyRegularMasterJob)
           & putJobHandlerInList (liftIO . runFlowR flowRt env . runNyRegularInstanceJob)
