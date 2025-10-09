39d736363a9209419cae692401a2b88e5ec5503d
Vijay Gupta <vijayrauniyar1818@gmail.com>
2025-10-09 18:55:28 +0530

    feat: introduce DriverActivityUpdateInProgress error handling
    - Added new error type for driver activity update in progress.
    - Updated error messages and HTTP status codes accordingly.

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/Driver.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/Driver.hs
@@ -816,7 +816,7 @@ getInformation (personId, merchantId, merchantOpCityId) mbClientId toss tnant' c
 setActivity :: (CacheFlow m r, EsqDBFlow m r, HasField "serviceClickhouseCfg" r CH.ClickhouseCfg, HasField "serviceClickhouseEnv" r CH.ClickhouseEnv) => (Id SP.Person, Id DM.Merchant, Id DMOC.MerchantOperatingCity) -> Bool -> Maybe DriverInfo.DriverMode -> m APISuccess.APISuccess
 setActivity (personId, merchantId, merchantOpCityId) isActive mode = do
   isLocked <- withLockDriverIdForSetActivity personId
-  unless isLocked $ throwError (InternalError "Driver activity update is already in progress")
+  unless isLocked $ throwError DriverActivityUpdateInProgress
   finally
     ( do
         void $ QPerson.findById personId >>= fromMaybeM (PersonNotFound personId.getId)
