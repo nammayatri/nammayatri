499ab19ede31fe6cbb5d8e858cf666769fd2b7cb
Roman Bodavskiy <roman.bodavskiy@metalamp.io>
2025-10-13 13:20:52 +0300

    backend/fix/msil: configs migration fix

--- Backend/dev/migrations/dynamic-offer-driver-app/0693-online-duration-switch.sql
+++
@@ -1,9 +0,0 @@
--- date in past means that calculation already switched on (nothing will change in logic)
-UPDATE atlas_driver_offer_bpp.transporter_config
-SET online_duration_calculate_from = '2025-01-01 00:00:00.000000+00'
-WHERE merchant_operating_city_id IN (
-    SELECT id
-    FROM atlas_driver_offer_bpp.merchant_operating_city
-    WHERE merchant_short_id = 'MSIL_PARTNER'
-    AND city = 'Delhi'
-);
