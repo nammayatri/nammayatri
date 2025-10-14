16d3a73163b1a682b085e497d4b4c9b3671e4758
KavyashreeS <kavyashree.s1998@gmail.com>
2025-10-14 08:11:26 +0530

    Backend : offers in order status response

--- Backend/lib/payment/src/Lib/Payment/Domain/Action.hs
+++ Backend/lib/payment/src/Lib/Payment/Domain/Action.hs
@@ -88,7 +88,8 @@ data PaymentStatusResp
         paymentMethodType :: Maybe Text,
         authIdCode :: Maybe Text,
         txnUUID :: Maybe Text,
-        effectAmount :: Maybe HighPrecMoney
+        effectAmount :: Maybe HighPrecMoney,
+        offers :: Maybe [Payment.Offer]
       }
   | MandatePaymentStatus
       { status :: Payment.TransactionStatus,
@@ -649,6 +650,7 @@ orderStatusService personId orderId orderStatusCall = do
             authIdCode = ((.authIdCode) =<< paymentGatewayResponse),
             txnUUID = transactionUUID,
             effectAmount = effectiveAmount,
+            offers = offers,
             ..
           }
     _ -> throwError $ InternalError "Unexpected Order Status Response."
