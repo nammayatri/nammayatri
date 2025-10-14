6741fe56d104175bcf6d9cae4298e6a28ee0127c
ayush-297 <ayushpandey1903@gmail.com>
2025-10-14 15:50:41 +0530

    backend/feat/getting-driver-number-using-RC

---
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/API/Action/UI/DriverToDriverDirectCalling.hs
@@ -0,0 +1,37 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+
+module API.Action.UI.DriverToDriverDirectCalling
+  ( API,
+    handler,
+  )
+where
+
+import qualified Control.Lens
+import qualified Domain.Action.UI.DriverToDriverDirectCalling
+import qualified Domain.Types.Merchant
+import qualified Domain.Types.MerchantOperatingCity
+import qualified Domain.Types.Person
+import qualified Environment
+import EulerHS.Prelude
+import qualified Kernel.Prelude
+import qualified Kernel.Types.Id
+import Kernel.Utils.Common
+import Servant
+import Storage.Beam.SystemConfigs ()
+import Tools.Auth
+
+type API = (TokenAuth :> "driver" :> "getnumber" :> Capture "rcNumber" Kernel.Prelude.Text :> Get ('[JSON]) Kernel.Prelude.Text)
+
+handler :: Environment.FlowServer API
+handler = getDriverGetnumber
+
+getDriverGetnumber ::
+  ( ( Kernel.Types.Id.Id Domain.Types.Person.Person,
+      Kernel.Types.Id.Id Domain.Types.Merchant.Merchant,
+      Kernel.Types.Id.Id Domain.Types.MerchantOperatingCity.MerchantOperatingCity
+    ) ->
+    Kernel.Prelude.Text ->
+    Environment.FlowHandler Kernel.Prelude.Text
+  )
+getDriverGetnumber a2 a1 = withFlowHandlerAPI $ Domain.Action.UI.DriverToDriverDirectCalling.getDriverGetnumber (Control.Lens.over Control.Lens._1 Kernel.Prelude.Just a2) a1
