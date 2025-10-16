1bf6c6383ddced02b0eea77ea33d66937df3dc0e
piyushKumar-1 <pkkapoor98@gmail.com>
2025-10-16 14:10:51 +0530

    backend/fix/added-logs-for-debugging

--- Backend/app/rider-platform/rider-app/Main/src/Domain/Action/UI/TrackRoute.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Domain/Action/UI/TrackRoute.hs
@@ -43,10 +43,12 @@ getTrackVehicles (mbPersonId, merchantId) routeCode _mbCurrentLat _mbCurrentLon 
   personId <- mbPersonId & fromMaybeM (InvalidRequest "Person not found")
   personCityInfo <- QP.findCityInfoById personId >>= fromMaybeM (PersonNotFound personId.getId)
   integratedBPPConfig <- SIBC.findIntegratedBPPConfig mbIntegratedBPPConfigId personCityInfo.merchantOperatingCityId (frfsVehicleCategoryToBecknVehicleCategory vehicleType) (fromMaybe DIBC.APPLICATION mbPlatformType)
+  logInfo $ "mbSelectedSourceStopId: " <> show mbSelectedSourceStopId <> " and mbSelectedDestinationStopId: " <> show mbSelectedDestinationStopId
   routeIdsToTrack <-
     case (mbSelectedSourceStopId, mbSelectedDestinationStopId) of
       (Just sourceStopId, Just destinationStopId) -> do
         possibleRoutes <- getPossibleRoutesBetweenTwoStops sourceStopId destinationStopId integratedBPPConfig
+        logInfo $ "possibleRoutes between two stops: " <> show possibleRoutes
         let routeCodes = map (.route.code) possibleRoutes
         pure $ if null routeCodes then [routeCode] else routeCodes
       _ -> pure [routeCode]
@@ -57,6 +59,7 @@ getTrackVehicles (mbPersonId, merchantId) routeCode _mbCurrentLat _mbCurrentLon 
   let currentLocation = (\ss -> LatLong <$> ss.lat <*> ss.lon) =<< mbSourceStop
   vehicleTrackingWithRoutes <- concatMapM (\routeIdToTrack -> map (routeIdToTrack,) <$> trackVehicles personId merchantId personCityInfo.merchantOperatingCityId vehicleType routeIdToTrack (fromMaybe DIBC.APPLICATION mbPlatformType) currentLocation (Just integratedBPPConfig.id)) routeIdsToTrack
   let deduplicatedVehicles = List.nubBy (\a b -> (snd a).vehicleId == (snd b).vehicleId) vehicleTrackingWithRoutes
+  logInfo $ "deduplicatedVehicles: " <> show deduplicatedVehicles
   let vehiclesYetToReachSelectedStop = filterVehiclesYetToReachSelectedStop deduplicatedVehicles
   let (confirmedHighBuses, ghostBuses) = List.partition (\a -> ((snd a).vehicleInfo >>= (.routeState)) == Just CQMMB.ConfirmedHigh) vehiclesYetToReachSelectedStop
   let sortedTracking = sortOn (distanceToStop mbSourceStop . snd) ghostBuses
