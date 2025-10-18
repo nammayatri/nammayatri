7f91fb689d65d728ca4c552602b3f166839659d8
hkmangla <hemantmangla78@gmail.com>
2025-10-18 18:24:58 +0530

    backend/fix: add x-sender-hash header to registration API and update related functions for enhanced OTP handling

--- Backend/test/src/Mobility/AppBackend/APICalls.hs
+++ Backend/test/src/Mobility/AppBackend/APICalls.hs
@@ -96,10 +96,10 @@ destinationServiceability regToken = destination
   where
     _ :<|> destination :<|> _ = client (Proxy :: Proxy AppServ.API) regToken
 
-appAuth :: Reg.AuthReq -> Maybe Version -> Maybe Version -> Maybe Version -> Maybe Text -> Maybe Text -> Maybe Text -> ClientM Reg.AuthRes
+appAuth :: Reg.AuthReq -> Maybe Version -> Maybe Version -> Maybe Version -> Maybe Text -> Maybe Text -> Maybe Text -> Maybe Text -> ClientM Reg.AuthRes
 appSignatureAuth :: Maybe Version -> Maybe Version -> Maybe Version -> Maybe Text -> Maybe Text -> ClientM Reg.AuthRes
 appVerify :: Id AppSRT.RegistrationToken -> Reg.AuthVerifyReq -> ClientM Reg.AuthVerifyRes
-appReInitiateLogin :: Id AppSRT.RegistrationToken -> ClientM Reg.ResendAuthRes
+appReInitiateLogin :: Id AppSRT.RegistrationToken -> Maybe Text -> ClientM Reg.ResendAuthRes
 logout :: RegToken -> ClientM APISuccess
 appAuth
   :<|> appSignatureAuth
@@ -140,7 +140,7 @@ mkAuthVerifyReq =
     }
 
 initiateAuth :: ClientM Reg.AuthRes
-initiateAuth = appAuth mkAuthReq (Just defaultVersion) (Just defaultVersion) Nothing Nothing Nothing Nothing
+initiateAuth = appAuth mkAuthReq (Just defaultVersion) (Just defaultVersion) Nothing Nothing Nothing Nothing Nothing
 
 verifyAuth :: Id AppSRT.RegistrationToken -> ClientM Reg.AuthVerifyRes
 verifyAuth tokenId = appVerify tokenId mkAuthVerifyReq
