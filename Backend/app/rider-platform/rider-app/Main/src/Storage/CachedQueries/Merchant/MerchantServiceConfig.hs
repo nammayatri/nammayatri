49017c97efa015e764b9cba6b90da69d5df73bdb
arnab.kumar <arnab.kumar@nammayatri.in>
2025-10-23 19:10:48 +0530

    Integration of karix SMS provider

--- Backend/app/rider-platform/rider-app/Main/src/Storage/CachedQueries/Merchant/MerchantServiceConfig.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Storage/CachedQueries/Merchant/MerchantServiceConfig.hs
@@ -94,6 +94,7 @@ cacheMerchantServiceConfig merchantServiceConfig = do
         Sms.TwillioSmsConfig _ -> SmsService Sms.TwillioSms
         Sms.DigoEngageSmsConfig _ -> SmsService Sms.DigoEngage
         Sms.VonageSmsConfig _ -> SmsService Sms.VonageSms
+        Sms.KarixConfig _ -> SmsService Sms.Karix
       WhatsappServiceConfig whatsappCfg -> case whatsappCfg of
         Whatsapp.GupShupConfig _ -> WhatsappService Whatsapp.GupShup
         Whatsapp.TataCommunicationsConfig _ -> WhatsappService Whatsapp.TataCommunications
