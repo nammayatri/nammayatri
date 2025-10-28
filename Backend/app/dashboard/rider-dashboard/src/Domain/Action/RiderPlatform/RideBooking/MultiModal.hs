eb053a2c043f0ee5563b24b734f53afaf71a6ca7
Jayanth-Parthsarathy <jparthaa@gmail.com>
2025-10-28 14:42:34 +0530

    backend/dashboard: add api to send multimodal ride pn to customer

--- Backend/app/dashboard/rider-dashboard/src/Domain/Action/RiderPlatform/RideBooking/MultiModal.hs
+++ Backend/app/dashboard/rider-dashboard/src/Domain/Action/RiderPlatform/RideBooking/MultiModal.hs
@@ -1,20 +1,22 @@
 {-# OPTIONS_GHC -Wwarn=unused-imports #-}
 
-module Domain.Action.RiderPlatform.RideBooking.MultiModal (getMultiModalList) where
+module Domain.Action.RiderPlatform.RideBooking.MultiModal (getMultiModalList, postMultiModalSendMessage) where
 
 import qualified API.Client.RiderPlatform.RideBooking
+import qualified API.Types.Dashboard.RideBooking.MultiModal
 import qualified "rider-app" Domain.Action.UI.Booking
 import qualified "rider-app" Domain.Types.Booking.API
 import qualified "beckn-spec" Domain.Types.BookingStatus
 import qualified "rider-app" Domain.Types.Journey
 import qualified "lib-dashboard" Domain.Types.Merchant
+import qualified Domain.Types.Transaction
 import qualified "lib-dashboard" Environment
 import EulerHS.Prelude
-import qualified EulerHS.Prelude
 import qualified Kernel.Prelude
+import qualified Kernel.Types.APISuccess
 import qualified Kernel.Types.Beckn.Context
 import qualified Kernel.Types.Id
-import Kernel.Utils.Common
+import qualified SharedLogic.Transaction
 import Storage.Beam.CommonInstances ()
 import Tools.Auth.Api
 import Tools.Auth.Merchant
@@ -23,3 +25,9 @@ getMultiModalList :: (Kernel.Types.Id.ShortId Domain.Types.Merchant.Merchant -> 
 getMultiModalList merchantShortId opCity apiTokenInfo limit offset bookingOffset journeyOffset customerPhoneNo countryCode email fromDate toDate rideStatus journeyStatus isPaymentSuccess bookingRequestType customerId = do
   checkedMerchantId <- merchantCityAccessCheck merchantShortId apiTokenInfo.merchant.shortId opCity apiTokenInfo.city
   API.Client.RiderPlatform.RideBooking.callRideBookingAPI checkedMerchantId opCity (.multiModalDSL.getMultiModalList) limit offset bookingOffset journeyOffset customerPhoneNo countryCode email fromDate toDate rideStatus journeyStatus isPaymentSuccess bookingRequestType customerId
+
+postMultiModalSendMessage :: (Kernel.Types.Id.ShortId Domain.Types.Merchant.Merchant -> Kernel.Types.Beckn.Context.City -> ApiTokenInfo -> Kernel.Prelude.Text -> API.Types.Dashboard.RideBooking.MultiModal.CustomerSendMessageReq -> Environment.Flow Kernel.Types.APISuccess.APISuccess)
+postMultiModalSendMessage merchantShortId opCity apiTokenInfo customerId req = do
+  checkedMerchantId <- merchantCityAccessCheck merchantShortId apiTokenInfo.merchant.shortId opCity apiTokenInfo.city
+  transaction <- SharedLogic.Transaction.buildTransaction (Domain.Types.Transaction.castEndpoint apiTokenInfo.userActionType) (Kernel.Prelude.Just APP_BACKEND) (Kernel.Prelude.Just apiTokenInfo) Kernel.Prelude.Nothing Kernel.Prelude.Nothing SharedLogic.Transaction.emptyRequest
+  SharedLogic.Transaction.withTransactionStoring transaction $ (do API.Client.RiderPlatform.RideBooking.callRideBookingAPI checkedMerchantId opCity (.multiModalDSL.postMultiModalSendMessage) customerId req)
