48a4c5c73fafb62b31c4dc2767fc138bffb062f2
Jayanth-Parthsarathy <jparthaa@gmail.com>
2025-10-28 14:42:34 +0530

    backend/dashboard: add api to send multimodal ride pn to customer

--- Backend/app/rider-platform/rider-app/Main/src-read-only/API/Types/Dashboard/RideBooking/Endpoints/MultiModal.hs
+++ Backend/app/rider-platform/rider-app/Main/src-read-only/API/Types/Dashboard/RideBooking/Endpoints/MultiModal.hs
@@ -3,7 +3,6 @@
 
 module API.Types.Dashboard.RideBooking.Endpoints.MultiModal where
 
-import qualified Data.Aeson
 import Data.OpenApi (ToSchema)
 import qualified Data.Singletons.TH
 import qualified "this" Domain.Action.UI.Booking
@@ -14,11 +13,23 @@ import EulerHS.Prelude hiding (id, state)
 import qualified EulerHS.Prelude
 import qualified EulerHS.Types
 import qualified Kernel.Prelude
+import qualified Kernel.Types.APISuccess
 import Kernel.Types.Common
 import Servant
 import Servant.Client
 
-type API = ("multiModal" :> GetMultiModalList)
+data CustomerSendMessageReq = CustomerSendMessageReq {channel :: MediaChannel, message :: Kernel.Prelude.Text, title :: Kernel.Prelude.Text}
+  deriving stock (Generic)
+  deriving anyclass (ToJSON, FromJSON, ToSchema)
+
+data MediaChannel
+  = WHATSAPP
+  | PUSH_NOTIFICATION
+  | SMS
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass (ToJSON, FromJSON, ToSchema)
+
+type API = ("multiModal" :> (GetMultiModalList :<|> PostMultiModalSendMessage))
 
 type GetMultiModalList =
   ( "list" :> QueryParam "limit" EulerHS.Prelude.Integer :> QueryParam "offset" EulerHS.Prelude.Integer
@@ -59,23 +70,22 @@ type GetMultiModalList =
            Domain.Action.UI.Booking.BookingListResV2
   )
 
-newtype MultiModalAPIs = MultiModalAPIs {getMultiModalList :: Kernel.Prelude.Maybe EulerHS.Prelude.Integer -> Kernel.Prelude.Maybe EulerHS.Prelude.Integer -> Kernel.Prelude.Maybe EulerHS.Prelude.Integer -> Kernel.Prelude.Maybe EulerHS.Prelude.Integer -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> Kernel.Prelude.Maybe EulerHS.Prelude.Integer -> Kernel.Prelude.Maybe EulerHS.Prelude.Integer -> Kernel.Prelude.Maybe [Domain.Types.BookingStatus.BookingStatus] -> Kernel.Prelude.Maybe [Domain.Types.Journey.JourneyStatus] -> Kernel.Prelude.Maybe Kernel.Prelude.Bool -> Kernel.Prelude.Maybe Domain.Types.Booking.API.BookingRequestType -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> EulerHS.Types.EulerClient Domain.Action.UI.Booking.BookingListResV2}
+type PostMultiModalSendMessage = ("sendMessage" :> Capture "customerId" Kernel.Prelude.Text :> ReqBody '[JSON] CustomerSendMessageReq :> Post '[JSON] Kernel.Types.APISuccess.APISuccess)
+
+data MultiModalAPIs = MultiModalAPIs
+  { getMultiModalList :: Kernel.Prelude.Maybe EulerHS.Prelude.Integer -> Kernel.Prelude.Maybe EulerHS.Prelude.Integer -> Kernel.Prelude.Maybe EulerHS.Prelude.Integer -> Kernel.Prelude.Maybe EulerHS.Prelude.Integer -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> Kernel.Prelude.Maybe EulerHS.Prelude.Integer -> Kernel.Prelude.Maybe EulerHS.Prelude.Integer -> Kernel.Prelude.Maybe [Domain.Types.BookingStatus.BookingStatus] -> Kernel.Prelude.Maybe [Domain.Types.Journey.JourneyStatus] -> Kernel.Prelude.Maybe Kernel.Prelude.Bool -> Kernel.Prelude.Maybe Domain.Types.Booking.API.BookingRequestType -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> EulerHS.Types.EulerClient Domain.Action.UI.Booking.BookingListResV2,
+    postMultiModalSendMessage :: Kernel.Prelude.Text -> CustomerSendMessageReq -> EulerHS.Types.EulerClient Kernel.Types.APISuccess.APISuccess
+  }
 
 mkMultiModalAPIs :: (Client EulerHS.Types.EulerClient API -> MultiModalAPIs)
 mkMultiModalAPIs multiModalClient = (MultiModalAPIs {..})
   where
-    getMultiModalList = multiModalClient
+    getMultiModalList :<|> postMultiModalSendMessage = multiModalClient
 
 data MultiModalUserActionType
   = GET_MULTI_MODAL_LIST
+  | POST_MULTI_MODAL_SEND_MESSAGE
   deriving stock (Show, Read, Generic, Eq, Ord)
-  deriving anyclass (ToSchema)
-
-instance ToJSON MultiModalUserActionType where
-  toJSON GET_MULTI_MODAL_LIST = Data.Aeson.String "GET_MULTI_MODAL_LIST"
-
-instance FromJSON MultiModalUserActionType where
-  parseJSON (Data.Aeson.String "GET_MULTI_MODAL_LIST") = pure GET_MULTI_MODAL_LIST
-  parseJSON _ = fail "GET_MULTI_MODAL_LIST expected"
+  deriving anyclass (ToJSON, FromJSON, ToSchema)
 
 $(Data.Singletons.TH.genSingletons [''MultiModalUserActionType])
--- Backend/app/rider-platform/rider-app/Main/src/Domain/Action/Dashboard/RideBooking/MultiModal.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Domain/Action/Dashboard/RideBooking/MultiModal.hs
@@ -1,8 +1,9 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
 {-# OPTIONS_GHC -Wwarn=unused-imports #-}
 
-module Domain.Action.Dashboard.RideBooking.MultiModal (getMultiModalList) where
+module Domain.Action.Dashboard.RideBooking.MultiModal (getMultiModalList, postMultiModalSendMessage) where
 
+import qualified API.Types.Dashboard.RideBooking.MultiModal
 import Data.Aeson
 import qualified Data.Bifunctor as BF
 import qualified Data.ByteString.Lazy as BSL
@@ -12,20 +13,62 @@ import qualified Domain.Action.UI.Booking as DBooking
 import qualified "this" Domain.Action.UI.Booking
 import qualified "this" Domain.Types.Booking.API
 import qualified Domain.Types.BookingStatus
+import Domain.Types.EmptyDynamicParam
 import qualified "this" Domain.Types.Journey
 import qualified Domain.Types.Merchant
 import qualified Environment
 import EulerHS.Prelude hiding (id)
+import qualified Kernel.External.Notification as Notification
+import Kernel.External.Types (ServiceFlow)
 import qualified Kernel.Prelude
+import qualified Kernel.Storage.Esqueleto as KEsqueleto
+import qualified Kernel.Types.APISuccess
 import qualified Kernel.Types.Beckn.Context
+import Kernel.Types.Common
+import Kernel.Types.Error
 import qualified Kernel.Types.Id
+import Kernel.Utils.Error
 import Servant
 import SharedLogic.Merchant (findMerchantByShortId)
+import qualified Storage.Queries.Person as QPerson
+import qualified Tools.Notifications as TNotifications
 
 getMultiModalList :: (Kernel.Types.Id.ShortId Domain.Types.Merchant.Merchant -> Kernel.Types.Beckn.Context.City -> Kernel.Prelude.Maybe EulerHS.Prelude.Integer -> Kernel.Prelude.Maybe EulerHS.Prelude.Integer -> Kernel.Prelude.Maybe EulerHS.Prelude.Integer -> Kernel.Prelude.Maybe EulerHS.Prelude.Integer -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> Kernel.Prelude.Maybe Kernel.Prelude.Text -> Kernel.Prelude.Maybe EulerHS.Prelude.Integer -> Kernel.Prelude.Maybe EulerHS.Prelude.Integer -> Kernel.Prelude.Maybe [Domain.Types.BookingStatus.BookingStatus] -> Kernel.Prelude.Maybe [Domain.Types.Journey.JourneyStatus] -> Kernel.Prelude.Maybe Kernel.Prelude.Bool -> Kernel.Prelude.Maybe Domain.Types.Booking.API.BookingRequestType -> Maybe Text -> Environment.Flow Domain.Action.UI.Booking.BookingListResV2)
 getMultiModalList merchantShortId _opCity limit offset bookingOffset journeyOffset customerPhoneNo countryCode email fromDate toDate rideStatus journeyStatus isPaymentSuccess bookingRequestType customerId = do
   m <- findMerchantByShortId merchantShortId
   DBooking.bookingListV2ByCustomerLookup m.id limit offset bookingOffset journeyOffset fromDate toDate rideStatus journeyStatus isPaymentSuccess bookingRequestType customerPhoneNo countryCode email customerId
+
+postMultiModalSendMessage :: (Kernel.Types.Id.ShortId Domain.Types.Merchant.Merchant -> Kernel.Types.Beckn.Context.City -> Kernel.Prelude.Text -> API.Types.Dashboard.RideBooking.MultiModal.CustomerSendMessageReq -> Environment.Flow Kernel.Types.APISuccess.APISuccess)
+postMultiModalSendMessage _merchantShortId _opCity customerId req = do
+  notifyCustomerFromDashboard customerId req
+  pure Kernel.Types.APISuccess.Success
+
+notifyCustomerFromDashboard ::
+  (ServiceFlow m r, EsqDBFlow m r, KEsqueleto.EsqDBReplicaFlow m r) =>
+  Text ->
+  API.Types.Dashboard.RideBooking.MultiModal.CustomerSendMessageReq ->
+  m ()
+notifyCustomerFromDashboard customerId req = do
+  person <- QPerson.findById (Kernel.Types.Id.Id customerId) >>= fromMaybeM (PersonNotFound customerId)
+  case req.channel of
+    API.Types.Dashboard.RideBooking.MultiModal.WHATSAPP -> pure ()
+    API.Types.Dashboard.RideBooking.MultiModal.SMS -> pure ()
+    API.Types.Dashboard.RideBooking.MultiModal.PUSH_NOTIFICATION -> do
+      let notificationData =
+            Notification.NotificationReq
+              { category = Notification.TRIGGER_FCM,
+                subCategory = Nothing,
+                showNotification = Notification.SHOW,
+                messagePriority = Nothing,
+                entity = Notification.Entity Notification.Person person.id.getId (),
+                body = req.message,
+                title = req.title,
+                dynamicParams = EmptyDynamicParam,
+                auth = Notification.Auth person.id.getId person.deviceToken person.notificationToken,
+                ttl = Nothing,
+                sound = Nothing
+              }
+      TNotifications.notifyPerson person.merchantId person.merchantOperatingCityId person.id notificationData Nothing
 
 instance FromHttpApiData [Domain.Types.Journey.JourneyStatus] where
   parseUrlPiece = parseHeader . DT.encodeUtf8
