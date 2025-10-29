96b0a795de8a5e06ca10ae8a43260467a882de85
Adam Dhayfa Umar <adamh.dhayfa@nammayatri.in>
2025-10-29 19:45:04 +0530

    backend/feat: added getPass API endpoint

--- Backend/app/rider-platform/rider-app/Main/src/Domain/Types/Extra/PurchasedPass.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Domain/Types/Extra/PurchasedPass.hs
@@ -3,8 +3,26 @@
 
 module Domain.Types.Extra.PurchasedPass where
 
+import Control.Lens.Operators
+import Data.Aeson
+import Data.OpenApi
+import qualified Data.Text as T
 import qualified Domain.Types.PurchasedPass
 import Kernel.Beam.Lib.UtilsTH
+import Kernel.Prelude
+import Kernel.Utils.TH
+
+allStatusTypes :: [Domain.Types.PurchasedPass.StatusType]
+allStatusTypes = [Domain.Types.PurchasedPass.Pending, Domain.Types.PurchasedPass.Active, Domain.Types.PurchasedPass.Failed, Domain.Types.PurchasedPass.Expired, Domain.Types.PurchasedPass.RefundPending, Domain.Types.PurchasedPass.RefundInitiated, Domain.Types.PurchasedPass.Refunded]
 
 $(mkBeamInstancesForEnum ''Domain.Types.PurchasedPass.BenefitType)
 $(mkBeamInstancesForEnum ''Domain.Types.PurchasedPass.StatusType)
+$(mkHttpInstancesForEnum ''Domain.Types.PurchasedPass.StatusType)
+
+instance ToParamSchema Domain.Types.PurchasedPass.StatusType where
+  toParamSchema _ =
+    mempty
+      & title ?~ "StatusType"
+      & type_ ?~ OpenApiString
+      & enum_
+        ?~ map (String . T.pack . show) allStatusTypes
