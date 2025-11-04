4343fe05a88b0a1390b922571d13fb6cb5d53f04
khuzema786 <khomosikhuzema894@gmail.com>
2025-11-05 04:19:14 +0530

    Fix SQL migration: Backend/dev/migrations-read-only/rider-app/cris_recon.sql

--- Backend/dev/migrations-read-only/rider-app/cris_recon.sql
+++ Backend/dev/migrations-read-only/rider-app/cris_recon.sql
@@ -6,6 +6,6 @@ ALTER TABLE atlas_app.cris_recon ADD COLUMN date_ist date NOT NULL;
 ALTER TABLE atlas_app.cris_recon ADD COLUMN fare_amount double precision NOT NULL;
 ALTER TABLE atlas_app.cris_recon ADD COLUMN id character varying(36) NOT NULL;
 ALTER TABLE atlas_app.cris_recon ADD COLUMN updated_at timestamp with time zone NOT NULL default CURRENT_TIMESTAMP;
-ALTER TABLE atlas_app.cris_recon ADD COLUMN merchant_id character varying(36) ;
+ALTER TABLE atlas_app.cris_recon ADD COLUMN merchant_id character varying(36) ;kkk
 ALTER TABLE atlas_app.cris_recon ADD COLUMN merchant_operating_city_id character varying(36) ;
 ALTER TABLE atlas_app.cris_recon ADD PRIMARY KEY ( id);
