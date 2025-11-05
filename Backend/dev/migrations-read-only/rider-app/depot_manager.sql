64defb344e21e85e23c679c70172639e42a0286b
khuzema786 <khomosikhuzema894@gmail.com>
2025-11-05 19:34:07 +0530

    Fix SQL migration: Backend/dev/migrations-read-only/rider-app/depot_manager.sql

--- Backend/dev/migrations-read-only/rider-app/depot_manager.sql
+++ Backend/dev/migrations-read-only/rider-app/depot_manager.sql
@@ -9,3 +9,4 @@ ALTER TABLE atlas_app.depot_manager ADD COLUMN merchant_operating_city_id charac
 ALTER TABLE atlas_app.depot_manager ADD COLUMN person_id character varying(36) NOT NULL;
 ALTER TABLE atlas_app.depot_manager ADD COLUMN updated_at timestamp with time zone NOT NULL default CURRENT_TIMESTAMP;
 ALTER TABLE atlas_app.depot_manager ADD PRIMARY KEY ( depot_code, person_id);
+njhkhkj
