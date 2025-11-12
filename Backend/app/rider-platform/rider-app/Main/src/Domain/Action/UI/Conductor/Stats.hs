2909ebfeccc388f3720125e77d9ea09a95a1008c
Jayanth-Parthsarathy <jparthaa@gmail.com>
2025-11-12 13:51:46 +0530

    backend/fix: fix issue with data filtering for conductor stats

--- Backend/app/rider-platform/rider-app/Main/src/Domain/Action/UI/Conductor/Stats.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Domain/Action/UI/Conductor/Stats.hs
@@ -8,8 +8,8 @@ import Kernel.Utils.Common
 import qualified Storage.Clickhouse.ConductorStats as CHConductor
 
 data StatsResp = StatsResp
-  { todayTickets :: Int,
-    todayRevenue :: Money,
+  { yesterdayTickets :: Int,
+    yesterdayRevenue :: Money,
     mtdTickets :: Int,
     mtdRevenue :: Money,
     activeCount :: Int, -- Will default to 1 if has today's data
@@ -22,33 +22,35 @@ data StatsResp = StatsResp
 
 statsHandler :: Text -> FlowHandler StatsResp
 statsHandler conductorToken = withFlowHandlerAPI $ do
-  today <- liftIO getCurrentTime
-  let todayStart = Time.UTCTime (Time.utctDay today) 0
-  let todayEnd = Time.addUTCTime 86400 todayStart -- 24 hours later
-  let (year, month, _) = Time.toGregorian (Time.utctDay today)
+  now <- liftIO getCurrentTime
+  let today = Time.utctDay now
+  let yesterday = Time.addDays (-1) today
+  let yesterdayStart = Time.UTCTime yesterday 0
+  let yesterdayEnd = Time.addUTCTime 86400 yesterdayStart -- 24 hours later
+  let (year, month, _) = Time.toGregorian yesterday
   let monthStart = Time.UTCTime (Time.fromGregorian year month 1) 0
 
   allStats <- CHConductor.findConductorStatsByToken conductorToken
 
-  let todayStats =
+  let yesterdayStats =
         filter
           ( \stat ->
-              stat.bookingDate >= todayStart && stat.bookingDate < todayEnd
+              stat.bookingDate >= yesterdayStart && stat.bookingDate < yesterdayEnd
           )
           allStats
 
   let mtdStats =
         filter
           ( \stat ->
-              stat.bookingDate >= monthStart && stat.bookingDate < todayEnd
+              stat.bookingDate >= monthStart && stat.bookingDate < yesterdayEnd
           )
           allStats
 
-  let todayTickets = sum $ map (.numberTicketsBooked) todayStats
+  let yesterdayTickets = sum $ map (.numberTicketsBooked) yesterdayStats
       mtdTickets = sum $ map (.numberTicketsBooked) mtdStats
 
       -- Calculate revenue from ClickHouse data
-      todayRevenue = Money $ floor $ sum $ map (.totalRevenueInADay) todayStats
+      yesterdayRevenue = Money $ floor $ sum $ map (.totalRevenueInADay) yesterdayStats
       mtdRevenue = Money $ floor $ sum $ map (.totalRevenueInADay) mtdStats
 
       -- TODO
