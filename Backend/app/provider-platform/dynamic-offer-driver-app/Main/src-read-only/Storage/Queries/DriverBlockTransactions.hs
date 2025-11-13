7e180f83d8f3fa79fcb6926fc4e091e2ce8b4b2d
kuldeepkumawat144 <kuldeepkumawat144@gmail.com>
2025-11-13 18:44:15 +0530

    backend/dashboard:-Block count in driver Profile

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/Storage/Queries/DriverBlockTransactions.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/Storage/Queries/DriverBlockTransactions.hs
@@ -9,6 +9,7 @@ import qualified Domain.Types.Person
 import Kernel.Beam.Functions
 import Kernel.External.Encryption
 import Kernel.Prelude
+import qualified Kernel.Prelude
 import Kernel.Types.Error
 import qualified Kernel.Types.Id
 import Kernel.Utils.Common (CacheFlow, EsqDBFlow, MonadFlow, fromMaybeM, getCurrentTime)
@@ -20,6 +21,11 @@ create = createWithKV
 
 createMany :: (EsqDBFlow m r, MonadFlow m, CacheFlow m r) => ([Domain.Types.DriverBlockTransactions.DriverBlockTransactions] -> m ())
 createMany = traverse_ create
+
+blockCountByDriverId ::
+  (EsqDBFlow m r, MonadFlow m, CacheFlow m r) =>
+  (Kernel.Types.Id.Id Domain.Types.Person.Person -> Kernel.Prelude.Maybe Domain.Types.DriverBlockTransactions.ActionType -> m [Domain.Types.DriverBlockTransactions.DriverBlockTransactions])
+blockCountByDriverId driverId actionType = do findAllWithKV [Se.And [Se.Is Beam.driverId $ Se.Eq (Kernel.Types.Id.getId driverId), Se.Is Beam.actionType $ Se.Eq actionType]]
 
 findByDriverId :: (EsqDBFlow m r, MonadFlow m, CacheFlow m r) => (Kernel.Types.Id.Id Domain.Types.Person.Person -> m [Domain.Types.DriverBlockTransactions.DriverBlockTransactions])
 findByDriverId driverId = do findAllWithKV [Se.Is Beam.driverId $ Se.Eq (Kernel.Types.Id.getId driverId)]
@@ -45,6 +51,7 @@ updateByPrimaryKey (Domain.Types.DriverBlockTransactions.DriverBlockTransactions
       Se.Set Beam.requestorId requestorId,
       Se.Set Beam.merchantId (Kernel.Types.Id.getId <$> merchantId),
       Se.Set Beam.merchantOperatingCityId (Kernel.Types.Id.getId <$> merchantOperatingCityId),
+      Se.Set Beam.createdAt createdAt,
       Se.Set Beam.updatedAt _now
     ]
     [Se.And [Se.Is Beam.id $ Se.Eq (Kernel.Types.Id.getId id)]]
