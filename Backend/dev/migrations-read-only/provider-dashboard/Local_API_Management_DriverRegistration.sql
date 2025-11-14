ac788be22e7e16837ecb900cfaeabe4ee7d119e6
Karan <itsofficialkaran26@gmail.com>
2025-11-14 15:52:17 +0530

    feat: created common doc create api, added common doc in document-list api, implimentend common doc config in document-config api

--- Backend/dev/migrations-read-only/provider-dashboard/Local_API_Management_DriverRegistration.sql
+++ Backend/dev/migrations-read-only/provider-dashboard/Local_API_Management_DriverRegistration.sql
@@ -45,3 +45,9 @@ INSERT INTO atlas_bpp_dashboard.access_matrix (id, role_id, api_entity, user_acc
 
 -- {"api":"GetDriverRegistrationVerificationStatus","migration":"localAccessForRoleId","param":"37947162-3b5d-4ed6-bcac-08841be1534d","schema":"atlas_bpp_dashboard"}
 INSERT INTO atlas_bpp_dashboard.access_matrix (id, role_id, api_entity, user_access_type, user_action_type) VALUES ( atlas_bpp_dashboard.uuid_generate_v4(), '37947162-3b5d-4ed6-bcac-08841be1534d', 'DSL', 'USER_FULL_ACCESS', 'PROVIDER_MANAGEMENT/DRIVER_REGISTRATION/GET_DRIVER_REGISTRATION_VERIFICATION_STATUS' ) ON CONFLICT DO NOTHING;
+
+
+------- SQL updates -------
+
+-- {"api":"PostDriverRegistrationDocumentsCommon","migration":"localAccessForRoleId","param":"37947162-3b5d-4ed6-bcac-08841be1534d","schema":"atlas_bpp_dashboard"}
+INSERT INTO atlas_bpp_dashboard.access_matrix (id, role_id, api_entity, user_access_type, user_action_type) VALUES ( atlas_bpp_dashboard.uuid_generate_v4(), '37947162-3b5d-4ed6-bcac-08841be1534d', 'DSL', 'USER_FULL_ACCESS', 'PROVIDER_MANAGEMENT/DRIVER_REGISTRATION/POST_DRIVER_REGISTRATION_DOCUMENTS_COMMON' ) ON CONFLICT DO NOTHING;
