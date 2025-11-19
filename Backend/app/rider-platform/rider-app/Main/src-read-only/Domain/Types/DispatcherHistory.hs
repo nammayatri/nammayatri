0f06ae366502bab3141571b9c095352e4d834927
Pranav Sathya A R <pranavsathyaar@gmail.com>
2025-11-20 00:54:07 +0530

    feat: added dispatcher api and data flow

---
+++ Backend/app/rider-platform/rider-app/Main/src-read-only/Domain/Types/DispatcherHistory.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE ApplicativeDo #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+
+module Domain.Types.DispatcherHistory where
+
+import Data.Aeson
+import qualified Domain.Types.Merchant
+import qualified Domain.Types.MerchantOperatingCity
+import qualified Domain.Types.Person
+import Kernel.Prelude
+import qualified Kernel.Types.Id
+import qualified Tools.Beam.UtilsTH
+
+data DispatcherHistory = DispatcherHistory
+  { conductorCode :: Kernel.Prelude.Text,
+    createdAt :: Kernel.Prelude.UTCTime,
+    currentVehicle :: Kernel.Prelude.Text,
+    depotId :: Kernel.Prelude.Text,
+    dispatcherId :: Kernel.Types.Id.Id Domain.Types.Person.Person,
+    driverCode :: Kernel.Prelude.Text,
+    id :: Kernel.Types.Id.Id Domain.Types.DispatcherHistory.DispatcherHistory,
+    merchantId :: Kernel.Types.Id.Id Domain.Types.Merchant.Merchant,
+    merchantOperatingCityId :: Kernel.Types.Id.Id Domain.Types.MerchantOperatingCity.MerchantOperatingCity,
+    reasonContent :: Kernel.Prelude.Maybe Kernel.Prelude.Text,
+    reasonTag :: Kernel.Prelude.Text,
+    replacedVehicle :: Kernel.Prelude.Text,
+    updatedAt :: Kernel.Prelude.UTCTime
+  }
+  deriving (Generic, Show, ToJSON, FromJSON, ToSchema)
