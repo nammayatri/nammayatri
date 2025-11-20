47916e5644c012cf156029268055f2c7ecb7c81f
Harshit Chauhan <harshit.devjs@gmail.com>
2025-11-20 20:33:05 +0530

    dashboard/feat/search-request-info-api

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/API/Action/Dashboard/RideBooking/SearchRequest.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/API/Action/Dashboard/RideBooking/SearchRequest.hs
@@ -21,10 +21,13 @@ import Servant
 import Tools.Auth
 
 handler :: (Kernel.Types.Id.ShortId Domain.Types.Merchant.Merchant -> Kernel.Types.Beckn.Context.City -> Environment.FlowServer API.Types.Dashboard.RideBooking.SearchRequest.API)
-handler merchantId city = postSearchRequestSearchrequests merchantId city :<|> getSearchRequestList merchantId city
+handler merchantId city = postSearchRequestSearchrequests merchantId city :<|> getSearchRequestList merchantId city :<|> getSearchRequestInfo merchantId city
 
 postSearchRequestSearchrequests :: (Kernel.Types.Id.ShortId Domain.Types.Merchant.Merchant -> Kernel.Types.Beckn.Context.City -> API.Types.Dashboard.RideBooking.SearchRequest.SearchRequestsReq -> Environment.FlowHandler API.Types.Dashboard.RideBooking.SearchRequest.SearchRequestsRes)
 postSearchRequestSearchrequests a3 a2 a1 = withDashboardFlowHandlerAPI $ Domain.Action.Dashboard.RideBooking.SearchRequest.postSearchRequestSearchrequests a3 a2 a1
 
 getSearchRequestList :: (Kernel.Types.Id.ShortId Domain.Types.Merchant.Merchant -> Kernel.Types.Beckn.Context.City -> Kernel.Types.Id.Id Domain.Types.Person.Person -> Kernel.Prelude.UTCTime -> Kernel.Prelude.UTCTime -> Kernel.Prelude.Int -> Kernel.Prelude.Int -> Environment.FlowHandler API.Types.Dashboard.RideBooking.SearchRequest.SearchRequestsRes)
 getSearchRequestList a7 a6 a5 a4 a3 a2 a1 = withDashboardFlowHandlerAPI $ Domain.Action.Dashboard.RideBooking.SearchRequest.getSearchRequestList a7 a6 a5 a4 a3 a2 a1
+
+getSearchRequestInfo :: (Kernel.Types.Id.ShortId Domain.Types.Merchant.Merchant -> Kernel.Types.Beckn.Context.City -> Kernel.Prelude.UTCTime -> Kernel.Prelude.UTCTime -> Kernel.Types.Id.Id Domain.Types.Person.Person -> Environment.FlowHandler API.Types.Dashboard.RideBooking.SearchRequest.SearchReqInfoRes)
+getSearchRequestInfo a5 a4 a3 a2 a1 = withDashboardFlowHandlerAPI $ Domain.Action.Dashboard.RideBooking.SearchRequest.getSearchRequestInfo a5 a4 a3 a2 a1
