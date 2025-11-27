d9c3184ad7c7e8cac8f7f010edb4697b96eefdf1
Utkarsh Pandey <utkarshpandey911@gmail.com>
2025-11-27 18:47:43 +0530

    backend/fix/query_local_api

--- Backend/dev/migrations-read-only/provider-dashboard/Local_API_Fleet_Driver.sql
+++ Backend/dev/migrations-read-only/provider-dashboard/Local_API_Fleet_Driver.sql
@@ -249,11 +249,5 @@ INSERT INTO atlas_bpp_dashboard.access_matrix (id, role_id, api_entity, user_acc
 
 ------- SQL updates -------
 
--- {"api":"GetDriverFleetPendingDrivers","migration":"localAccessForRoleId","param":"37947162-3b5d-4ed6-bcac-08841be1534d","schema":"atlas_bpp_dashboard"}
-INSERT INTO atlas_bpp_dashboard.access_matrix (id, role_id, api_entity, user_access_type, user_action_type) VALUES ( atlas_bpp_dashboard.uuid_generate_v4(), '37947162-3b5d-4ed6-bcac-08841be1534d', 'DSL', 'USER_FULL_ACCESS', 'PROVIDER_FLEET/DRIVER/GET_DRIVER_FLEET_PENDING_DRIVERS' ) ON CONFLICT DO NOTHING;
-
-
-------- SQL updates -------
-
 -- {"api":"PostDriverFleetDriverUpdate","migration":"localAccessForRoleId","param":"37947162-3b5d-4ed6-bcac-08841be1534d","schema":"atlas_bpp_dashboard"}
 INSERT INTO atlas_bpp_dashboard.access_matrix (id, role_id, api_entity, user_access_type, user_action_type) VALUES ( atlas_bpp_dashboard.uuid_generate_v4(), '37947162-3b5d-4ed6-bcac-08841be1534d', 'DSL', 'USER_FULL_ACCESS', 'PROVIDER_FLEET/DRIVER/POST_DRIVER_FLEET_DRIVER_UPDATE' ) ON CONFLICT DO NOTHING;
