a4ab49511efbf2828c79dacbcbe8d7c5cca415cc
Harshit Chauhan <harshit.devjs@gmail.com>
2025-11-27 14:11:25 +0530

    compilation-fix

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/Driver.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/Driver.hs
@@ -940,7 +940,7 @@ deactivateGoHomeFeature (personId, _, merchantOpCityId) = do
   when (driverInfo.blocked) $ throwError $ DriverAccountBlocked (BlockErrorPayload driverInfo.blockExpiryTime driverInfo.blockReasonFlag)
   ghInfo <- getDriverGoHomeRequestInfo driverId merchantOpCityId (Just goHomeConfig)
   ghrId <- fromMaybeM DriverGoHomeRequestNotPresent ghInfo.driverGoHomeRequestId
-  currentRide <- Ride.findNewOrInProgressRideByGHRId ghrInfo.driverGoHomeRequestId
+  currentRide <- Ride.findNewOrInProgressRideByGHRId ghrId
   when (isJust currentRide) $ throwError $ DriverGoHomeRequestRideInProgress
   succRide <- Ride.findCompletedRideByGHRId ghrId
   if isJust succRide
