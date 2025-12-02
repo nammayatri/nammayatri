600cc7107c640f47ba0fd0ee96bda7e40cf314b9
KavyashreeS <kavyashree.s1998@gmail.com>
2025-12-02 19:08:38 +0530

    Backend: fix - updated readonly file via DSL for parking endpoint

--- Backend/app/rider-platform/rider-app/Main/src-read-only/API/Action/UI/ParkingBooking.hs
+++ Backend/app/rider-platform/rider-app/Main/src-read-only/API/Action/UI/ParkingBooking.hs
@@ -8,34 +8,25 @@ module API.Action.UI.ParkingBooking
 where
 
 import qualified API.Types.UI.ParkingBooking
-import qualified Control.Lens
 import qualified Data.Text
 import qualified Domain.Action.UI.ParkingBooking
-import qualified Domain.Types.Merchant
-import qualified Domain.Types.Person
 import qualified Environment
 import EulerHS.Prelude
 import qualified Kernel.Prelude
-import qualified Kernel.Types.Id
 import Kernel.Utils.Common
 import Servant
-import qualified SharedLogic.Merchant as SLM
 import Storage.Beam.SystemConfigs ()
 import Tools.Auth
-import Tools.Error
 
 type API =
-  "multimodal" :> "parking" :> "book" :> Header "api-key" Data.Text.Text :> ReqBody '[JSON] API.Types.UI.ParkingBooking.ParkingBookingReq
-    :> Post
-         '[JSON]
-         API.Types.UI.ParkingBooking.ParkingBookingResponse
+  ( "multimodal" :> "parking" :> "book" :> Header "api-key" Data.Text.Text :> ReqBody '[JSON] API.Types.UI.ParkingBooking.ParkingBookingReq
+      :> Post
+           '[JSON]
+           API.Types.UI.ParkingBooking.ParkingBookingResponse
+  )
 
 handler :: Environment.FlowServer API
 handler = postMultimodalParkingBook
 
-postMultimodalParkingBook ::
-  Kernel.Prelude.Maybe (Data.Text.Text) ->
-  API.Types.UI.ParkingBooking.ParkingBookingReq ->
-  Environment.FlowHandler API.Types.UI.ParkingBooking.ParkingBookingResponse
-postMultimodalParkingBook mbApiKey req = withFlowHandlerAPI $ do
-  Domain.Action.UI.ParkingBooking.postMultimodalParkingBook mbApiKey req
+postMultimodalParkingBook :: (Kernel.Prelude.Maybe Data.Text.Text -> API.Types.UI.ParkingBooking.ParkingBookingReq -> Environment.FlowHandler API.Types.UI.ParkingBooking.ParkingBookingResponse)
+postMultimodalParkingBook a2 a1 = withFlowHandlerAPI $ Domain.Action.UI.ParkingBooking.postMultimodalParkingBook a2 a1
