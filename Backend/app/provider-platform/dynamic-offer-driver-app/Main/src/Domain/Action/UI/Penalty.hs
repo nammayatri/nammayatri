1f85ea5badd432bf0de73057213b70f251dc54ac
Rohit Saini <rohitsaini@Rohits-MacBook-Pro.local>
2025-12-11 17:41:16 +0530

    Backend/Enhancement - postPenaltyCheck

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/Penalty.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/Penalty.hs
@@ -5,12 +5,15 @@ module Domain.Action.UI.Penalty (postPenaltyCheck) where
 import qualified API.Types.UI.Penalty
 import Data.Either.Extra (eitherToMaybe)
 import Data.OpenApi (ToSchema)
+import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
 import qualified Domain.Action.UI.Ride.CancelRide.Internal as CancelRideInternal
 import qualified Domain.Types.Booking as SRB
+import qualified Domain.Types.BookingCancellationReason as SBCR
 import qualified Domain.Types.Merchant
 import qualified Domain.Types.MerchantOperatingCity
 import qualified Domain.Types.Person
 import qualified Domain.Types.Ride as DRide
+import qualified Domain.Types.Yudhishthira as TY
 import qualified Environment
 import EulerHS.Prelude hiding (id)
 import qualified Kernel.External.Maps.Types as Maps
@@ -20,8 +23,9 @@ import Kernel.Utils.Common
 import qualified Lib.Yudhishthira.Event as Yudhishthira
 import qualified Lib.Yudhishthira.Types as YT
 import qualified Lib.Yudhishthira.Types.Application as YA
-import qualified Storage.Cac.FarePolicy as QFP
+import qualified Storage.Cac.TransporterConfig as CCT
 import qualified Storage.Queries.Booking as QBooking
+import qualified Storage.Queries.CallStatus as QCallStatus
 import qualified Storage.Queries.Ride as QRide
 import Tools.Auth
 import Tools.Constants
@@ -59,8 +63,55 @@ postPenaltyCheck (mbPersonId, _merchantId, _merchantOpCityId) req = do
       return (isPenaltyApplicable, Just penaltyAmount)
     Nothing -> do
       return (False, Nothing)
+
+  cancellationValidity <- do
+    now <- getCurrentTime
+    mbCallStatus <- QCallStatus.findOneByEntityId (Just ride.id.getId)
+    let callAtemptByDriver = isJust mbCallStatus
+        currentTime = floor $ utcTimeToPOSIXSeconds now
+        rideCreatedTime = floor $ utcTimeToPOSIXSeconds ride.createdAt
+        driverArrivalTime = floor . utcTimeToPOSIXSeconds <$> (ride.driverArrivalTime)
+    (mbDriverDistToPickup, _) <- CancelRideInternal.getDistanceToPickup booking (Just ride)
+    let simulatedCancellationReason =
+          SBCR.BookingCancellationReason
+            { bookingId = booking.id,
+              rideId = Just ride.id,
+              driverId = Just (Kernel.Types.Id.cast driverId),
+              merchantId = Just booking.providerId,
+              merchantOperatingCityId = Just booking.merchantOperatingCityId,
+              source = SBCR.ByDriver,
+              reasonCode = Nothing,
+              additionalInfo = Nothing,
+              driverCancellationLocation = Just req.point,
+              driverDistToPickup = mbDriverDistToPickup,
+              distanceUnit = booking.distanceUnit
+            }
+        tagData =
+          TY.CancelRideTagData
+            { ride = ride{status = DRide.CANCELLED},
+              booking = booking{status = SRB.CANCELLED},
+              cancellationReason = simulatedCancellationReason,
+              callAtemptByDriver = callAtemptByDriver,
+              currentTime = currentTime,
+              rideCreatedTime = rideCreatedTime,
+              driverArrivalTime = driverArrivalTime,
+              merchantOperatingCityId = booking.merchantOperatingCityId
+            }
+    tagsE <- withTryCatch "computeNammaTags:RideCancel" $ Yudhishthira.computeNammaTags YA.RideCancel tagData
+    let tags = fromMaybe [] $ eitherToMaybe tagsE
+        isValid = validDriverCancellation `elem` tags
+        isInvalid = invalidDriverCancellation `elem` tags
+    return $
+      if isValid
+        then Just "Valid"
+        else
+          if isInvalid
+            then Just "Invalid"
+            else Nothing
+
   return $
     API.Types.UI.Penalty.PenaltyCheckRes
       { isCancellationPenaltyApplicable = Just isApplicable,
-        cancellationPenaltyAmount = penaltyAmount
+        cancellationPenaltyAmount = penaltyAmount,
+        cancellationValidity = cancellationValidity
       }
