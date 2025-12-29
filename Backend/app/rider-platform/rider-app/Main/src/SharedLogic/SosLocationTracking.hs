3dc7f72f0bac52f33ea0399a5b7ac47d9fb3b313
Rohan Goel <rohangoel107@gmail.com>
2025-12-29 15:41:46 +0530

    feat: live tracking and sos for non ride flow

---
+++ Backend/app/rider-platform/rider-app/Main/src/SharedLogic/SosLocationTracking.hs
@@ -0,0 +1,70 @@
+{-
+ Copyright 2022-23, Juspay India Pvt Ltd
+
+ This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License
+
+ as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program
+
+ is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+
+ or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of
+
+ the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+-}
+
+module SharedLogic.SosLocationTracking where
+
+import qualified Domain.Types.Sos as DSos
+import Kernel.External.Maps.Types (LatLong (..))
+import Kernel.Prelude
+import Kernel.Storage.Hedis (HedisFlow)
+import qualified Kernel.Storage.Hedis as Redis
+import Kernel.Types.Id
+import Kernel.Utils.Common
+
+data SosLocationData = SosLocationData
+  { lat :: Double,
+    lon :: Double,
+    timestamp :: UTCTime,
+    accuracy :: Maybe Double
+  }
+  deriving (Generic, Show, ToJSON, FromJSON)
+
+makeSosLocationRedisKey :: Id DSos.Sos -> Text
+makeSosLocationRedisKey sosId = "sos-rider-location:" <> sosId.getId
+
+updateSosRiderLocation ::
+  (HedisFlow m env) =>
+  Id DSos.Sos ->
+  LatLong ->
+  Maybe Double ->
+  Maybe UTCTime ->
+  m ()
+updateSosRiderLocation sosId LatLong {..} mbAccuracy mbExpiryTimeStamp = do
+  now <- getCurrentTime
+  let locationData =
+        SosLocationData
+          { lat,
+            lon,
+            timestamp = now,
+            accuracy = mbAccuracy
+          }
+  let key = makeSosLocationRedisKey sosId
+  let expirySeconds = maybe 3600 (\expiry -> max 1 $ floor $ diffUTCTime expiry now) mbExpiryTimeStamp
+  Redis.setExp key locationData expirySeconds
+
+getSosRiderLocation ::
+  (HedisFlow m env) =>
+  Id DSos.Sos ->
+  m (Maybe SosLocationData)
+getSosRiderLocation sosId = do
+  let key = makeSosLocationRedisKey sosId
+  Redis.safeGet key
+
+clearSosRiderLocation ::
+  (HedisFlow m env) =>
+  Id DSos.Sos ->
+  m ()
+clearSosRiderLocation sosId = do
+  let key = makeSosLocationRedisKey sosId
+  Redis.del key
