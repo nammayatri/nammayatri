02567bf5bff81702d5215b1efe4671933dbe39c0
vinit-j13 <vinit.j@nammayatri.in>
2025-12-31 16:12:41 +0530

    backend:Added query for prod for unique constraints on person table

--- Backend/dev/migrations/dynamic-offer-driver-app/0729-add-role-to-email-constraint.sql
+++ Backend/dev/migrations/dynamic-offer-driver-app/0729-add-role-to-email-constraint.sql
@@ -1,3 +1,4 @@
+-- Note: Do not run below alter commands in production.
 -- Drop the existing email constraint that only includes merchant_id and email
 ALTER TABLE atlas_driver_offer_bpp.person DROP CONSTRAINT IF EXISTS unique_email;
 
@@ -9,3 +10,20 @@ ALTER TABLE atlas_driver_offer_bpp.person DROP CONSTRAINT IF EXISTS unique_ident
 
 -- Add new constraint that includes role to allow same identifier for different roles under same merchant
 ALTER TABLE atlas_driver_offer_bpp.person ADD CONSTRAINT unique_identifier_role UNIQUE (merchant_id, identifier, role);
+
+
+
+
+
+
+-- Create indexes concurrently
+CREATE UNIQUE INDEX CONCURRENTLY idx_person_email_role ON atlas_driver_offer_bpp.person (merchant_id, email, role); --(These must be run outside a transaction block.)
+CREATE UNIQUE INDEX CONCURRENTLY idx_person_identifier_role ON atlas_driver_offer_bpp.person (merchant_id, identifier, role); --(These must be run outside a transaction block.)
+
+-- Drop old constraints FIRST
+ALTER TABLE atlas_driver_offer_bpp.person DROP CONSTRAINT IF EXISTS unique_email;
+ALTER TABLE atlas_driver_offer_bpp.person DROP CONSTRAINT IF EXISTS unique_identifier;
+
+-- Then add new constraints using the indexes
+ALTER TABLE atlas_driver_offer_bpp.person ADD CONSTRAINT unique_email_role UNIQUE USING INDEX idx_person_email_role;
+ALTER TABLE atlas_driver_offer_bpp.person ADD CONSTRAINT unique_identifier_role UNIQUE USING INDEX idx_person_identifier_role;
