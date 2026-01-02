ad5836af66e4f6d3ec5d8a32be0da58cb4c2f929
Rohan Goel <rohangoel107@gmail.com>
2026-01-02 19:14:39 +0530

    revert igm NO api calls

--- Backend/app/rider-platform/rider-app/Main/src/SharedLogic/CallIGMBPP.hs
+++ Backend/app/rider-platform/rider-app/Main/src/SharedLogic/CallIGMBPP.hs
@@ -16,95 +16,67 @@ module SharedLogic.CallIGMBPP where
 
 import qualified BecknV2.IGM.APIs as Spec
 import qualified Data.HashMap.Strict as HM
-import qualified Domain.Types.Merchant as Merchant
 import qualified EulerHS.Types as Euler
 import GHC.Records.Extra
-import qualified IGM.Types as IGM
+import qualified IGM.Types as Spec
 import Kernel.Prelude
-import Kernel.Streaming.Kafka.Producer.Types (KafkaProducerTools)
 import Kernel.Types.Error
-import Kernel.Types.Id
 import Kernel.Utils.Common
 import Kernel.Utils.Error.BaseError.HTTPError.BecknAPIError (IsBecknAPI)
 import Kernel.Utils.Monitoring.Prometheus.Servant (SanitizedUrl)
 import Kernel.Utils.Servant.SignatureAuth
 import Tools.Metrics (CoreMetrics)
-import TransactionLogs.PushLogs
-import TransactionLogs.Types
 
 issue ::
   ( MonadFlow m,
     CoreMetrics m,
     HasFlowEnv m r '["internalEndPointHashMap" ::: HM.HashMap BaseUrl BaseUrl],
-    HasRequestId r,
-    ToJSON IGM.IssueReq,
-    HasFlowEnv m r '["kafkaProducerTools" ::: KafkaProducerTools],
-    HasFlowEnv m r '["ondcTokenHashMap" ::: HM.HashMap KeyConfig TokenConfig],
-    EsqDBFlow m r,
-    CacheFlow m r
+    HasRequestId r
   ) =>
   BaseUrl ->
-  Id Merchant.Merchant ->
-  IGM.IssueReq ->
-  m IGM.AckResponse
-issue providerUrl merchantId req = do
+  Spec.IssueReq ->
+  m Spec.AckResponse
+issue providerUrl req = do
   internalEndPointHashMap <- asks (.internalEndPointHashMap)
   bapId <- req.context.contextBapId & fromMaybeM (InvalidRequest "BapId is missing")
-  callBecknAPIWithSignature' merchantId bapId "issue" Spec.issueAPI providerUrl internalEndPointHashMap req
+  callBecknAPIWithSignature bapId "issue" Spec.issueAPI providerUrl internalEndPointHashMap req
 
 onIssue ::
   ( MonadFlow m,
     CoreMetrics m,
     HasFlowEnv m r '["internalEndPointHashMap" ::: HM.HashMap BaseUrl BaseUrl],
-    HasRequestId r,
-    ToJSON IGM.OnIssueReq,
-    HasFlowEnv m r '["kafkaProducerTools" ::: KafkaProducerTools],
-    HasFlowEnv m r '["ondcTokenHashMap" ::: HM.HashMap KeyConfig TokenConfig],
-    EsqDBFlow m r,
-    CacheFlow m r
+    HasRequestId r
   ) =>
   BaseUrl ->
-  Id Merchant.Merchant ->
-  IGM.OnIssueReq ->
-  m IGM.AckResponse
-onIssue providerUrl merchantId req = do
+  Spec.OnIssueReq ->
+  m Spec.AckResponse
+onIssue providerUrl req = do
   internalEndPointHashMap <- asks (.internalEndPointHashMap)
   bppId <- req.onIssueReqContext.contextBppId & fromMaybeM (InvalidRequest "BapId is missing")
-  callBecknAPIWithSignature' merchantId bppId "onIssue" Spec.onIssueAPI providerUrl internalEndPointHashMap req
+  callBecknAPIWithSignature bppId "onIssue" Spec.onIssueAPI providerUrl internalEndPointHashMap req
 
 issueStatus ::
   ( MonadFlow m,
     CoreMetrics m,
     HasFlowEnv m r '["internalEndPointHashMap" ::: HM.HashMap BaseUrl BaseUrl],
-    HasRequestId r,
-    ToJSON IGM.IssueStatusReq,
-    HasFlowEnv m r '["kafkaProducerTools" ::: KafkaProducerTools],
-    HasFlowEnv m r '["ondcTokenHashMap" ::: HM.HashMap KeyConfig TokenConfig],
-    EsqDBFlow m r,
-    CacheFlow m r
+    HasRequestId r
   ) =>
   BaseUrl ->
-  Id Merchant.Merchant ->
-  IGM.IssueStatusReq ->
-  m IGM.AckResponse
-issueStatus providerUrl merchantId req = do
+  Spec.IssueStatusReq ->
+  m Spec.AckResponse
+issueStatus providerUrl req = do
   internalEndPointHashMap <- asks (.internalEndPointHashMap)
   bapId <- req.issueStatusReqContext.contextBapId & fromMaybeM (InvalidRequest "BapId is missing")
-  callBecknAPIWithSignature' merchantId bapId "issueStatus" Spec.issueStatusAPI providerUrl internalEndPointHashMap req
+  callBecknAPIWithSignature bapId "issueStatus" Spec.issueStatusAPI providerUrl internalEndPointHashMap req
 
-callBecknAPIWithSignature' ::
+callBecknAPIWithSignature ::
   ( MonadFlow m,
     CoreMetrics m,
     IsBecknAPI api req res,
     SanitizedUrl api,
-    ToJSON req,
-    CacheFlow m r,
-    HasFlowEnv m r '["kafkaProducerTools" ::: KafkaProducerTools],
-    HasFlowEnv m r '["ondcTokenHashMap" ::: HM.HashMap KeyConfig TokenConfig],
-    EsqDBFlow m r,
-    HasRequestId r
+    HasRequestId r,
+    MonadReader r m
   ) =>
-  Id Merchant.Merchant ->
   Text ->
   Text ->
   Proxy api ->
@@ -112,7 +84,4 @@ callBecknAPIWithSignature' ::
   HM.HashMap BaseUrl BaseUrl ->
   req ->
   m res
-callBecknAPIWithSignature' merchantId a b c d e req' = do
-  fork ("sending " <> show b <> ", pushing ondc logs") do
-    void $ pushLogs b (toJSON req') merchantId.getId "MOBILITY"
-  callBecknAPI (Just $ Euler.ManagerSelector $ getHttpManagerKey a) Nothing b c d e req'
+callBecknAPIWithSignature a = callBecknAPI (Just $ Euler.ManagerSelector $ getHttpManagerKey a) Nothing
