378b523ab910cf30cf057611d718cbdb661b2e69
Yashwanth1906 <yashwanthsakthivel01@gmail.com>
2026-01-03 08:31:34 +0530

    cache-removed when booking fails

--- Backend/app/rider-platform/rider-app/Main/src/ExternalBPP/ExternalAPI/Subway/CRIS/BookJourney.hs
+++ Backend/app/rider-platform/rider-app/Main/src/ExternalBPP/ExternalAPI/Subway/CRIS/BookJourney.hs
@@ -21,9 +21,11 @@ import qualified EulerHS.Types as ET
 import ExternalBPP.ExternalAPI.Subway.CRIS.Auth (callCRISAPI)
 import ExternalBPP.ExternalAPI.Subway.CRIS.Encryption (decryptResponseData, encryptPayload)
 import ExternalBPP.ExternalAPI.Subway.CRIS.Error (CRISError (..), CRISErrorUnhandled (..))
+import ExternalBPP.ExternalAPI.Subway.CRIS.RouteFare (mkRouteFareCacheKey)
 import ExternalBPP.ExternalAPI.Types
 import Kernel.External.Encryption
 import Kernel.Prelude ((!!))
+import qualified Kernel.Storage.Hedis as Hedis
 import Kernel.Tools.Metrics.CoreMetrics (CoreMetrics)
 import Kernel.Types.App
 import Kernel.Types.Error
@@ -318,7 +320,16 @@ createOrder config integratedBPPConfig booking quoteCategories = do
             tpBookType = tpBookType
           }
   logInfo $ "GetBookJourney: " <> show bookJourneyReq
-  bookJourneyResp <- getBookJourney config bookJourneyReq
+  let changeOver = fromMaybe " " $ quote.fareDetails <&> (.via)
+  bookJourneyResp <-
+    getBookJourney config bookJourneyReq `safeCatch` \(err :: SomeException) -> do
+      logError $ "Booking failed, clearing fare cache for route: " <> fromStation.code <> " -> " <> toStation.code
+      let fareCacheKeyTrue = mkRouteFareCacheKey fromStation.code toStation.code changeOver True
+      let fareCacheKeyFalse = mkRouteFareCacheKey fromStation.code toStation.code changeOver False
+      Hedis.del fareCacheKeyTrue
+      Hedis.del fareCacheKeyFalse
+      logInfo $ "Cleared fare cache keys: " <> fareCacheKeyTrue <> ", " <> fareCacheKeyFalse
+      throwM err
 
   qrValidityTime <- parseTicketValidity bookJourneyResp.showTicketValidity
 
