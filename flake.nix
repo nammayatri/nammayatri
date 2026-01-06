956a31a3d61667624fb5ded6f5e538e42437b434
Vijay Gupta <vijayrauniyar1818@gmail.com>
2026-01-06 20:12:53 +0530

    backend/feat: Add BAP host redirection and enhance search functionality
    - Introduced `bapHostRedirectMap` in the configuration to manage redirection for specific hosts.
    - Updated the `search` function to utilize the redirection map, allowing for forwarding requests to BPP when necessary.
    - Enhanced the `AppCfg` and `AppEnv` data structures to include `bapHostRedirectMap`.
    - Modified the `flake.nix` and `flake.lock` to reflect changes in the shared-kernel repository path.

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
 
     # Backend inputs
     shared-kernel = {
-      url = "github:nammayatri/shared-kernel";
+      url = "github:nammayatri/shared-kernel/backend/multi-cloud-setup-shared-kernel";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
