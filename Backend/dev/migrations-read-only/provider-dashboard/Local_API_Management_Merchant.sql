042267269d287538c0c17a266bdbdebb68c738ca
Mohamed Sameer <sameerzohan65@gmail.com>
2026-01-20 09:40:14 +0530

    [fix]:auto_plus_removed_duplicated_api

--- Backend/dev/migrations-read-only/provider-dashboard/Local_API_Management_Merchant.sql
+++ Backend/dev/migrations-read-only/provider-dashboard/Local_API_Management_Merchant.sql
@@ -154,12 +154,6 @@ INSERT INTO atlas_bpp_dashboard.access_matrix (id, role_id, api_entity, user_acc
 
 ------- SQL updates -------
 
--- {"api":"PostMerchantConfigDriverPoolCsvUpsert","migration":"localAccessForRoleId","param":"37947162-3b5d-4ed6-bcac-08841be1534d","schema":"atlas_bpp_dashboard"}
-INSERT INTO atlas_bpp_dashboard.access_matrix (id, role_id, api_entity, user_access_type, user_action_type) VALUES ( atlas_bpp_dashboard.uuid_generate_v4(), '37947162-3b5d-4ed6-bcac-08841be1534d', 'DSL', 'USER_FULL_ACCESS', 'PROVIDER_MANAGEMENT/MERCHANT/POST_MERCHANT_CONFIG_DRIVER_POOL_CSV_UPSERT' ) ON CONFLICT DO NOTHING;
-
-
-------- SQL updates -------
-
 -- {"api":"PostMerchantConfigDriverPoolUpsert","migration":"localAccessForRoleId","param":"37947162-3b5d-4ed6-bcac-08841be1534d","schema":"atlas_bpp_dashboard"}
 INSERT INTO atlas_bpp_dashboard.access_matrix (id, role_id, api_entity, user_access_type, user_action_type) VALUES ( atlas_bpp_dashboard.uuid_generate_v4(), '37947162-3b5d-4ed6-bcac-08841be1534d', 'DSL', 'USER_FULL_ACCESS', 'PROVIDER_MANAGEMENT/MERCHANT/POST_MERCHANT_CONFIG_DRIVER_POOL_UPSERT' ) ON CONFLICT DO NOTHING;
 
