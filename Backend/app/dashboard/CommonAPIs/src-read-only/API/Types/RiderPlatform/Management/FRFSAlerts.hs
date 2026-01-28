c2468a3f589809e16728d84b6c97d7b97661ca88
Hari Krishna <harikrishnadupaguntla1@gmail.com>
2026-01-28 12:53:42 +0530

    feat: added alert api for cumta sla

---
+++ Backend/app/dashboard/CommonAPIs/src-read-only/API/Types/RiderPlatform/Management/FRFSAlerts.hs
@@ -0,0 +1,5 @@
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+
+module API.Types.RiderPlatform.Management.FRFSAlerts (module ReExport) where
+
+import API.Types.RiderPlatform.Management.Endpoints.FRFSAlerts as ReExport
