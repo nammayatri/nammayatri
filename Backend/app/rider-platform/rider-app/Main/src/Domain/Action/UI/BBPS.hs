274dc697c2996c5ce5fffa343f33c67f4dd8e8de
Yashwanth1906 <yashwanthsakthivel01@gmail.com>
2026-02-11 19:08:58 +0530

    [fix]: Passing the isMockPayment flag correctly from DB (Fix)

--- Backend/app/rider-platform/rider-app/Main/src/Domain/Action/UI/BBPS.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Domain/Action/UI/BBPS.hs
@@ -158,7 +158,7 @@ getBbpsGetOrderStatus (_, _) refIdTxt = do
   where
     withPaymentStatusResponseHandler :: DBBPS.BBPS -> DP.Person -> (DPayment.PaymentStatusResp -> Environment.Flow API.Types.UI.BBPS.BBPSPaymentStatusAPIRes) -> Environment.Flow API.Types.UI.BBPS.BBPSPaymentStatusAPIRes
     withPaymentStatusResponseHandler bbpsInfo person action = do
-      paymentOrder <- QPaymentOrder.findById (cast bbpsInfo.refId) >>= fromMaybeM (PaymentOrderNotFound bbpsInfo.refId.getId)
+      paymentOrder <- QOrder.findById (Kernel.Types.Id.cast bbpsInfo.refId) >>= fromMaybeM (PaymentOrderNotFound bbpsInfo.refId.getId)
       let orderStatusCall = Payment.orderStatus bbpsInfo.merchantId bbpsInfo.merchantOperatingCityId Nothing Payment.BBPS (Just person.id.getId) person.clientSdkVersion paymentOrder.isMockPayment
       let commonPersonId = Kernel.Types.Id.cast @DP.Person @DPayment.Person bbpsInfo.customerId
           walletPostingCall = TWallet.walletPosting bbpsInfo.merchantId bbpsInfo.merchantOperatingCityId
