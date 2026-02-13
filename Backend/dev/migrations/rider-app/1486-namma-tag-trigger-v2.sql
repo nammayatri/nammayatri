359642e8b42fdb0536c5713b11cbdc132735fa41
hkmangla <hemantmangla78@gmail.com>
2026-02-13 14:12:23 +0530

    Refactor SQL migration files to update access matrix entries and remove deprecated ones. Adjust table references to include schema names for clarity.

--- Backend/dev/migrations/rider-app/1486-namma-tag-trigger-v2.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE atlas_app.namma_tag_trigger_v2 ();
-
-ALTER TABLE atlas_app.namma_tag_trigger_v2 ADD COLUMN merchant_operating_city_id character varying(36) NOT NULL;
-ALTER TABLE atlas_app.namma_tag_trigger_v2 ADD COLUMN event text NOT NULL;
-ALTER TABLE atlas_app.namma_tag_trigger_v2 ADD COLUMN tag_name text NOT NULL;
-ALTER TABLE atlas_app.namma_tag_trigger_v2 ADD COLUMN created_at timestamp with time zone NOT NULL default CURRENT_TIMESTAMP;
-ALTER TABLE atlas_app.namma_tag_trigger_v2 ADD COLUMN updated_at timestamp with time zone NOT NULL default CURRENT_TIMESTAMP;
-ALTER TABLE atlas_app.namma_tag_trigger_v2 ADD PRIMARY KEY (merchant_operating_city_id, event, tag_name);
