7e16a3620392ff0042bfd4c96d9868126ccee4fa
Jitu Rajak <rajakjitu708@gmail.com>
2026-02-23 14:07:33 +0530

    backend/feat: Integrate plasma lms service for validating driver training information

---
+++ Backend/dev/migrations/dynamic-offer-driver-app/0758-msil-plasma-lms-config.sql
@@ -0,0 +1,19 @@
+-- For Master Environment
+INSERT INTO
+  atlas_driver_offer_bpp.merchant_service_config (
+    merchant_id,
+    service_name,
+    config_json,
+    merchant_operating_city_id
+  )
+VALUES
+  (
+    '840327a8-f17c-4d7c-8199-a583cfaadc5f',
+    'Plasma_LMS',
+    '{
+      "api_key": "0.1.0|1|6fF/KW5FeIrpWM1BeYbM2/czEl2uIJhJksNloVi9C3r1oXwI+HQuLA+RchhyBAtEZLHy0VW8GJetIAFV8UfmrUPnznwRXfKiUoNvoYCcdVYh240=",
+      "url": "https://api.sandbox.moving.tech/dev/dobpp/ui/plasma"
+    }',
+    'db1c62db-01ba-4cbf-9cce-ede8d2e1361e'
+  );
+
