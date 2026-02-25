61b8b2a63bfe8bf0de38ba28399291d91b3aeba2
Akhilesh Singh Bhadauriya <akhileshsingh2314@gmail.com>
2026-02-25 12:29:34 +0530

    feat: special-location-open-market

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/SharedLogic/SpecialLocationUpsert.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/SharedLogic/SpecialLocationUpsert.hs
@@ -231,6 +231,7 @@ makeSpecialLocation locationGeomFiles gateGeomFiles merchantOpCity idx row = do
         DSL.SpecialLocation
           { id = Id locationName,
             enabled = enabled,
+            isOpenMarketEnabled = True,
             locationName = locationName,
             category = category,
             merchantId = Just (cast merchantOpCity.merchantId),
---
+++ Backend/dev/migrations/dynamic-offer-driver-app/0783-add-is-open-market-enabled-special-location.sql
@@ -0,0 +1,1 @@
+ALTER TABLE atlas_driver_offer_bpp.special_location ADD COLUMN is_open_market_enabled boolean;
