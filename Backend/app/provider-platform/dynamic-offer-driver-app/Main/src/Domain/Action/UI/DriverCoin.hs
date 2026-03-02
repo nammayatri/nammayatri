62f64c2fb80ae60ee81d8fb7eea874f8be1f0a46
Yashika Kaushik <yashikakaushik09@gmail.com>
2026-03-02 13:47:51 +0530

    backend/fix:no-plan-error-for-coins

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/DriverCoin.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/DriverCoin.hs
@@ -280,7 +280,8 @@ handler (driverId, merchantId_, merchantOpCityId) ConvertCoinToCashReq {..} = do
   currency <- SMerchant.getCurrencyByMerchantOpCity merchantOpCityId
   unless (transporterConfig.coinFeature) $
     throwError $ CoinServiceUnavailable merchantId_.getId
-  whenM (noDriverPlan driverId && coinRedemptionType /= Just DirectPayout) $
+  noPlan <- noDriverPlan driverId
+  when (noPlan && coinRedemptionType /= Just DirectPayout) $
     throwError $ NoPlanAgaintsDriver driverId.getId
   now <- getCurrentTime
   uuid <- generateGUIDText
