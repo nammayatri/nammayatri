4dd37946fc14d693518d31ed7ac602eb1b32c638
hkmangla <hemantmangla78@gmail.com>
2026-03-13 20:06:19 +0530

    backend/fix: change `tax_config` column type to `json` and add `max_number_of_luggages` column to `transporter_config` table.

--- Backend/dev/migrations-read-only/dynamic-offer-driver-app/transporter_config.sql
+++ Backend/dev/migrations-read-only/dynamic-offer-driver-app/transporter_config.sql
@@ -782,9 +782,4 @@ ALTER TABLE atlas_driver_offer_bpp.transporter_config ADD COLUMN enable_coins_to
 ------- SQL updates -------
 
 ALTER TABLE atlas_driver_offer_bpp.transporter_config ADD COLUMN tax_config json;
-
-
-------- SQL updates -------
-
-ALTER TABLE atlas_driver_offer_bpp.transporter_config ADD COLUMN tax_config text;
 ALTER TABLE atlas_driver_offer_bpp.transporter_config ADD COLUMN max_number_of_luggages integer ;
