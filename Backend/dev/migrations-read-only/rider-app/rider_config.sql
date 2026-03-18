bbed1fd0f37e986d06004a764a52d46dc0cd164d
Tejaswini <tejaswini@Tejaswinis-MacBook-Pro-2.local>
2026-03-18 18:02:09 +0530

    backend/fix: kapture ticket urls

--- Backend/dev/migrations-read-only/rider-app/rider_config.sql
+++ Backend/dev/migrations-read-only/rider-app/rider_config.sql
@@ -635,3 +635,9 @@ ALTER TABLE atlas_app.rider_config ADD COLUMN seat_booking_cleanup_ttl integer ;
 
 
 ALTER TABLE atlas_app.rider_config ADD COLUMN include_vehicles_with_no_eta boolean ;
+
+
+------- SQL updates -------
+
+ALTER TABLE atlas_app.rider_config ALTER COLUMN tracking_short_url_pattern SET DEFAULT 'https://nammayatri.in/t/';
+ALTER TABLE atlas_app.rider_config ALTER COLUMN dashboard_media_file_url_pattern SET DEFAULT 'https://control-center.moving.tech/ops/rides/<RIDE_ID>';
