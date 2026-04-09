0f703395589883ef7b3bdb8c00128f09d8980b7d
Prithivirajkota <prithviraj1490@gmail.com>
2026-04-09 17:01:33 +0530

    added merchant id in internal auth get token

--- Backend/app/rider-platform/rider-app/Main/src/API/Internal/Auth.hs
+++ Backend/app/rider-platform/rider-app/Main/src/API/Internal/Auth.hs
@@ -5,8 +5,10 @@ module API.Internal.Auth
 where
 
 import qualified Domain.Action.Internal.Auth as Domain
+import Domain.Types.Merchant (Merchant)
 import Environment
 import EulerHS.Prelude
+import Kernel.Types.Id (Id)
 import Kernel.Utils.Common
 import Servant
 import Storage.Beam.SystemConfigs ()
@@ -20,6 +22,7 @@ type API =
              :> Header "token" Text
              :> QueryParam "mobileNumber" Text
              :> QueryParam "mobileCountryCode" Text
+             :> QueryParam "merchantId" (Id Merchant)
              :> Get '[JSON] Domain.CustomerAuthTokenResp
        )
 
@@ -31,6 +34,6 @@ handler =
 internalAuth :: Maybe RegToken -> Maybe Text -> FlowHandler Domain.InternalResp
 internalAuth token apiKey = withFlowHandlerAPI $ Domain.internalAuth token apiKey
 
-getToken :: Maybe Text -> Maybe Text -> Maybe Text -> FlowHandler Domain.CustomerAuthTokenResp
-getToken apiKey mobileNumber mobileCountryCode =
-  withFlowHandlerAPI $ Domain.getCustomerAuthToken apiKey mobileNumber mobileCountryCode
+getToken :: Maybe Text -> Maybe Text -> Maybe Text -> Maybe (Id Merchant) -> FlowHandler Domain.CustomerAuthTokenResp
+getToken apiKey mobileNumber mobileCountryCode merchantId =
+  withFlowHandlerAPI $ Domain.getCustomerAuthToken apiKey mobileNumber mobileCountryCode merchantId
