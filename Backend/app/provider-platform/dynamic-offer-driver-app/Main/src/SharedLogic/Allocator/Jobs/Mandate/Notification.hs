34c39443b15cd87e0cec5dce3cd32cb4920c33fa
dushyant-2002 <Dushyant.workspace@gmail.com>
2026-04-13 13:50:10 +0530

    backend/fix/payemnt-service-config-mismatch-fix-in-mandate-notification

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/SharedLogic/Allocator/Jobs/Mandate/Notification.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/SharedLogic/Allocator/Jobs/Mandate/Notification.hs
@@ -215,7 +215,7 @@ sendAsyncNotification driverToNotify merchantId merchantOperatingCityId subscrip
   req <- mkNotificationRequest driverToNotify notificationShortId.getShortId
   driver <- QP.findById driverToNotify.driverId >>= fromMaybeM (PersonDoesNotExist driverToNotify.driverId.getId)
   QNTF.create $ buildNotificationEntity notificationId req driverToNotify.driverFeeId driverToNotify.mandateId now
-  paymentServiceName <- TPayment.decidePaymentServiceForRecurring subscriptionConfig.paymentServiceName driver.id driver.merchantOperatingCityId subscriptionConfig.serviceName
+  paymentServiceName <- TPayment.decidePaymentServiceForRecurring subscriptionConfig.paymentServiceName driver.id merchantOperatingCityId subscriptionConfig.serviceName
   exec <- withTryCatch "createNotificationService:sendAsyncNotification" $ withShortRetry (APayments.createNotificationService req (TPayment.mandateNotification merchantId merchantOperatingCityId paymentServiceName (Just driver.id.getId)))
   case exec of
     Left err -> do
