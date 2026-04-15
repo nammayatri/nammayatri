17abe7b56c0aa89e8479ac35697fb07796dce50e
Mustafa Asad <87380398+MustafaAsad198@users.noreply.github.com>
2026-04-15 07:47:26 +0530

    backend/fix:update driver information on starting scheduled ride from… (#14283)

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/Ride/StartRide.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/Ride/StartRide.hs
@@ -221,6 +221,8 @@ startRide ServiceHandle {..} rideId req = withLogTag ("rideId-" <> rideId.getId)
       whenWithLocationUpdatesLock driverId $ do
         withTimeAPI "startRide" "initializeDistanceCalculation" $ initializeDistanceCalculation updatedRide.id driverId point
         withTimeAPI "startRide" "startRideAndUpdateLocation" $ startRideAndUpdateLocation driverId updatedRide booking.id point booking.providerId odometer transporterConfig driverInfo
+        when booking.isScheduled $
+          void $ QDI.updateLatestScheduledBookingAndPickup Nothing Nothing (cast driverId)
 
       fork "notify customer for ride start" $ notifyBAPRideStarted booking updatedRide (Just point)
       fork "startRide - Notify driver" $ Notify.notifyOnRideStarted ride booking
