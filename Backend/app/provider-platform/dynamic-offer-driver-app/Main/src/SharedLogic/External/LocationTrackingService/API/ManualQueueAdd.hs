b126daea4acc060cf3fc8b4f02ff556023e4f114
Akhilesh Singh Bhadauriya <74489799+enigma-commits@users.noreply.github.com>
2026-04-17 13:27:07 +0530

    Revert "Revert "backend/feat: add dashboard APIs for special zone queue manag…"
    This reverts commit 2131f36c74a850a8e41b8f489763fd0e94789143.

---
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/SharedLogic/External/LocationTrackingService/API/ManualQueueAdd.hs
@@ -0,0 +1,42 @@
+{-
+  Copyright 2022-23, Juspay India Pvt Ltd
+
+  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License
+
+  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is
+
+  distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+
+  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero
+
+  General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+-}
+
+module SharedLogic.External.LocationTrackingService.API.ManualQueueAdd where
+
+import qualified Domain.Types.Merchant as DM
+import qualified Domain.Types.Person as DP
+import qualified EulerHS.Types as ET
+import Kernel.Prelude
+import Kernel.Types.APISuccess (APISuccess)
+import Kernel.Types.Id
+import Servant
+
+type ManualQueueAddAPI =
+  "internal"
+    :> "special-locations"
+    :> Capture "specialLocationId" Text
+    :> "queue"
+    :> Capture "vehicleType" Text
+    :> "drivers"
+    :> Capture "merchantId" (Id DM.Merchant)
+    :> Capture "driverId" (Id DP.Person)
+    :> "position"
+    :> Capture "position" Int
+    :> Post '[JSON] APISuccess
+
+manualQueueAddAPI :: Proxy ManualQueueAddAPI
+manualQueueAddAPI = Proxy
+
+manualQueueAdd :: Text -> Text -> Id DM.Merchant -> Id DP.Person -> Int -> ET.EulerClient APISuccess
+manualQueueAdd = ET.client manualQueueAddAPI
