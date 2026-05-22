2fc1d54c1cefecc5b461a597a669de6e2d911cdf
Bharath A V <avbharath1221@gmail.com>
2026-05-22 17:23:45 +0530

    fix : added back pkgs in settings

--- Backend/default.nix
+++ Backend/default.nix
@@ -148,6 +148,7 @@
           example-service.custom = cacConfig;
           mock-fcm.custom = cacConfig;
           mock-google.custom = cacConfig;
+          mock-public-transport-provider-platform.custom = cacConfig;
           mock-rider-platform.custom = cacConfig;
           mock-sms.custom = cacConfig;
           mock-payment.custom = cacConfig;
