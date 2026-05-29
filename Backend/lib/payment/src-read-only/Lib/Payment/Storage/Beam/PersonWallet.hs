6abdb764c0192b32c612c8c4001bc46a2654d86d
Vijay Gupta <vijayrauniyar1818@gmail.com>
2026-05-29 18:33:52 +0530

    backend/feat: enable KV secondary keys for rider & driver tables
    Add KV secondary keys (enableKVPG) so more tables are served from KV and
    become DB-independent. Each SK verified against euler-hs anchor semantics
    (full PK/SK group must be a subset of a query's eq/IN where-cols). Composite
    PK members that are also queried alone use the PrimaryKey|SecondaryKey form;
    SKs whose only query lives in an Extra .hs file are forced (!SecondaryKey).
    High-fan-out SKs are flagged inline for Redis SET-size monitoring.
    
    Rider: cris_recon(bppOrderId), frfs_recon(frfsTicketBookingId), person(deviceId),
    refund_request(refundsId), call_status(rideId), special_occasion(placeId),
    person_wallet(personId), refunds(shortId); route_stop_fare gets a DB composite
    index (startStopCode+endStopCode) since NammaDSL cannot express a composite SK.
    
    Driver: business_license(driverId), vehicle_permit/puc(documentImageId),
    fleet_rc_association(fleetOwnerId), aadhaar_card(front/back image + numberHash),
    vehicle_registration_certificate(documentImageId, fleetOwnerId),
    feedback(rideId, driverId), fleet_booking_assignments(vehicleNo),
    lms_certificate(driverId), ride(shortId), call_status(entityId).

--- Backend/lib/payment/src-read-only/Lib/Payment/Storage/Beam/PersonWallet.hs
+++ Backend/lib/payment/src-read-only/Lib/Payment/Storage/Beam/PersonWallet.hs
@@ -11,18 +11,18 @@ import qualified Kernel.Prelude
 import qualified Kernel.Types.Common
 
 data PersonWalletT f = PersonWalletT
-  { cashAmount :: (B.C f Kernel.Types.Common.HighPrecMoney),
-    cashFromPointsRedemption :: (B.C f Kernel.Types.Common.HighPrecMoney),
-    createdAt :: (B.C f Kernel.Prelude.UTCTime),
-    expiredBalance :: (B.C f Kernel.Types.Common.HighPrecMoney),
-    id :: (B.C f Kernel.Prelude.Text),
-    merchantId :: (B.C f Kernel.Prelude.Text),
-    merchantOperatingCityId :: (B.C f (Kernel.Prelude.Maybe Kernel.Prelude.Text)),
-    personId :: (B.C f Kernel.Prelude.Text),
-    pointsAmount :: (B.C f Kernel.Types.Common.HighPrecMoney),
-    updatedAt :: (B.C f Kernel.Prelude.UTCTime),
-    usableCashAmount :: (B.C f Kernel.Types.Common.HighPrecMoney),
-    usablePointsAmount :: (B.C f Kernel.Types.Common.HighPrecMoney)
+  { cashAmount :: B.C f Kernel.Types.Common.HighPrecMoney,
+    cashFromPointsRedemption :: B.C f Kernel.Types.Common.HighPrecMoney,
+    createdAt :: B.C f Kernel.Prelude.UTCTime,
+    expiredBalance :: B.C f Kernel.Types.Common.HighPrecMoney,
+    id :: B.C f Kernel.Prelude.Text,
+    merchantId :: B.C f Kernel.Prelude.Text,
+    merchantOperatingCityId :: B.C f (Kernel.Prelude.Maybe Kernel.Prelude.Text),
+    personId :: B.C f Kernel.Prelude.Text,
+    pointsAmount :: B.C f Kernel.Types.Common.HighPrecMoney,
+    updatedAt :: B.C f Kernel.Prelude.UTCTime,
+    usableCashAmount :: B.C f Kernel.Types.Common.HighPrecMoney,
+    usablePointsAmount :: B.C f Kernel.Types.Common.HighPrecMoney
   }
   deriving (Generic, B.Beamable)
 
@@ -32,6 +32,6 @@ instance B.Table PersonWalletT where
 
 type PersonWallet = PersonWalletT Identity
 
-$(enableKVPG (''PersonWalletT) [('id)] [])
+$(enableKVPG ''PersonWalletT ['id] [['personId]])
 
-$(mkTableInstancesGenericSchema (''PersonWalletT) "person_wallet")
+$(mkTableInstancesGenericSchema ''PersonWalletT "person_wallet")
