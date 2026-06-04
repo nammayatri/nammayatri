ffea5dbf116b425889b9d62d5ee281cb88930dbf
Harshit <97081728+H-C-21@users.noreply.github.com>
2026-06-04 19:59:45 +0530

    Backend/fix/scheduled rides driver location fix prod (#15046)

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/Driver.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/Driver.hs
@@ -3006,7 +3006,8 @@ listScheduledBookings (personId, _, cityId) mbLimit mbOffset mbFromDay mbToDay m
         _ -> pure $ ScheduledBookingRes []
   where
     getCurrentDriverLocUsingLTS driverId = do
-      result <- withTryCatch "driversLocation:getCurrentDriverLocUsingLTS" $ LTF.driversLocation [driverId]
+      mbPerson <- runInReplica $ QPerson.findById driverId
+      result <- withTryCatch "driversLocation:getCurrentDriverLocUsingLTS" $ LTF.driversLocationByCloudType [driverId] (mbPerson >>= (.cloudType))
       return $ case result of
         Left _ -> Nothing
         Right locations -> listToMaybe locations >>= \x -> Just LatLong {lat = x.lat, lon = x.lon}
