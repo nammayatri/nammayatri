cad44de521996416b8631714c2f1344ad667dd2f
Pranav Sathya A R <pranavsathyaar@gmail.com>
2026-06-10 23:09:45 +0530

    feat: return is_completed bool across all trips in routeServiceability

--- Backend/app/rider-platform/rider-app/Main/src/Domain/Action/UI/MultimodalConfirm.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Domain/Action/UI/MultimodalConfirm.hs
@@ -1940,6 +1940,7 @@ postMultimodalRouteServiceability (mbPersonId, _merchantId) req =
                     vehicleNumber = vno,
                     tripId = thisTripId,
                     isActiveTrip = detail.is_active_trip,
+                    isCompleted = detail.is_completed,
                     serviceSubTypes = mbServiceSubTypes,
                     vehicleTagNumber = mbVehicleTagNumber,
                     availableSeats = Nothing,
