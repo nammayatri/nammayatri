ffa7cb5a81b50180ffc71b5db42e5bffe056698d
dhruv-1010 <dhruvsingh235443@gmail.com>
2026-06-20 12:52:10 +0530

    backend/fix: store GeoJSON in geometry geom_geo_json on city create

--- Backend/app/dashboard/CommonAPIs/src/Dashboard/Common/Merchant.hs
+++ Backend/app/dashboard/CommonAPIs/src/Dashboard/Common/Merchant.hs
@@ -676,6 +676,7 @@ newtype CreateMerchantOperatingCityRes = CreateMerchantOperatingCityRes
 
 data CreateMerchantOperatingCityReqT = CreateMerchantOperatingCityReqT
   { geom :: Text,
+    geomGeoJson :: Text,
     city :: Context.City,
     state :: Context.IndianState,
     country :: Context.Country,
