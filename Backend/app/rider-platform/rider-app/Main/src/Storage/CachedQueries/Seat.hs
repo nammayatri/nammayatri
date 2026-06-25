1cd0d3138aa2635e1edc5265055e30a6f5a224e1
Mohamed Hamdan A <mdhamdan1111@gmail.com>
2026-06-25 17:55:57 +0530

    feat: CRUD api's for the seatLayout

--- Backend/app/rider-platform/rider-app/Main/src/Storage/CachedQueries/Seat.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Storage/CachedQueries/Seat.hs
@@ -4,7 +4,6 @@ import qualified Domain.Types.Seat as Domain
 import qualified Domain.Types.SeatLayout as DomainLayout
 import Kernel.Prelude
 import qualified Kernel.Storage.Hedis as Hedis
-import qualified Kernel.Storage.InMem as IM
 import Kernel.Types.Id (Id, getId)
 import Kernel.Utils.Common
 import qualified Storage.Queries.Seat as Queries
@@ -15,15 +14,14 @@ findById ::
   m (Maybe Domain.Seat)
 findById seatId = do
   let cacheKey = makeSeatIdKey seatId
-  IM.withInMemCache [cacheKey] 3600 $ do
-    Hedis.safeGet cacheKey >>= \case
-      Just val -> return val
-      Nothing -> do
-        val <- Queries.findById seatId
-        whenJust val $ \v -> do
-          expTime <- fromIntegral <$> asks (.cacheConfig.configsExpTime)
-          Hedis.setExp cacheKey v expTime
-        return val
+  Hedis.safeGet cacheKey >>= \case
+    Just val -> return val
+    Nothing -> do
+      val <- Queries.findById seatId
+      whenJust val $ \v -> do
+        expTime <- fromIntegral <$> asks (.cacheConfig.configsExpTime)
+        Hedis.setExp cacheKey v expTime
+      return val
 
 makeSeatIdKey :: Id Domain.Seat -> Text
 makeSeatIdKey seatId = "CachedQueries:Seat:Id-" <> getId seatId
@@ -34,14 +32,22 @@ findAllByLayoutId ::
   m [Domain.Seat]
 findAllByLayoutId layoutId = do
   let cacheKey = makeLayoutIdKey layoutId
-  IM.withInMemCache [cacheKey] 3600 $ do
-    Hedis.safeGet cacheKey >>= \case
-      Just val -> return val
-      Nothing -> do
-        val <- Queries.findAllByLayoutId layoutId
+  Hedis.safeGet cacheKey >>= \case
+    Just val -> return val
+    Nothing -> do
+      val <- Queries.findAllByLayoutId layoutId
+      unless (null val) $ do
         expTime <- fromIntegral <$> asks (.cacheConfig.configsExpTime)
         Hedis.setExp cacheKey val expTime
-        return val
+      return val
 
 makeLayoutIdKey :: Id DomainLayout.SeatLayout -> Text
 makeLayoutIdKey layoutId = "CachedQueries:Seat:LayoutId-" <> getId layoutId
+
+invalidateCacheByLayoutId :: (CacheFlow m r, EsqDBFlow m r, MonadFlow m) => Id DomainLayout.SeatLayout -> m ()
+invalidateCacheByLayoutId layoutId = do
+  let cacheKey = makeLayoutIdKey layoutId
+  void $ Hedis.del cacheKey
+
+invalidateCacheBySeatId :: (CacheFlow m r, EsqDBFlow m r, MonadFlow m) => Id Domain.Seat -> m ()
+invalidateCacheBySeatId seatId = void $ Hedis.del (makeSeatIdKey seatId)
