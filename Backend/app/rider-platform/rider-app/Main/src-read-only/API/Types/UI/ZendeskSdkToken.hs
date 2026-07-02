359cb55ad0cc1caa09d377df9739a85bab1bbf9d
kuldeepkumawat144 <kuldeepkumawat144@gmail.com>
2026-07-02 18:05:40 +0530

    backend/enhancement:Added Zendesk api to get token

---
+++ Backend/app/rider-platform/rider-app/Main/src-read-only/API/Types/UI/ZendeskSdkToken.hs
@@ -0,0 +1,9 @@
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+
+module API.Types.UI.ZendeskSdkToken where
+
+import Data.OpenApi (ToSchema)
+import EulerHS.Prelude hiding (id)
+import qualified Kernel.Prelude
+import Servant
+import Tools.Auth
