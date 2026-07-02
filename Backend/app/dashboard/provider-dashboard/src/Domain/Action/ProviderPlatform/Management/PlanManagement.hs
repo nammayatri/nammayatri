c19caf850e8fe89da2b73c25a8867c9668ba6ac0
arnab.kumar <arnab.kumar@nammayatri.in>
2026-07-02 21:57:24 +0530

    Get apis for Plan_Translations and Vendor Split Details

--- Backend/app/dashboard/provider-dashboard/src/Domain/Action/ProviderPlatform/Management/PlanManagement.hs
+++ Backend/app/dashboard/provider-dashboard/src/Domain/Action/ProviderPlatform/Management/PlanManagement.hs
@@ -5,6 +5,7 @@ module Domain.Action.ProviderPlatform.Management.PlanManagement
     postPlanManagementDeletePlan,
     postPlanManagementActivatePlan,
     getPlanManagementListPlans,
+    getPlanManagementPlanTranslations,
   )
 where
 
@@ -46,3 +47,8 @@ postPlanManagementActivatePlan merchantShortId opCity apiTokenInfo planId = do
   checkedMerchantId <- merchantCityAccessCheck merchantShortId apiTokenInfo.merchant.shortId opCity apiTokenInfo.city
   transaction <- SharedLogic.Transaction.buildTransaction (Domain.Types.Transaction.castEndpoint apiTokenInfo.userActionType) (Kernel.Prelude.Just DRIVER_OFFER_BPP_MANAGEMENT) (Kernel.Prelude.Just apiTokenInfo) Kernel.Prelude.Nothing Kernel.Prelude.Nothing SharedLogic.Transaction.emptyRequest
   SharedLogic.Transaction.withTransactionStoring transaction $ (do API.Client.ProviderPlatform.Management.callManagementAPI checkedMerchantId opCity (.planManagementDSL.postPlanManagementActivatePlan) planId)
+
+getPlanManagementPlanTranslations :: (Kernel.Types.Id.ShortId Domain.Types.Merchant.Merchant -> Kernel.Types.Beckn.Context.City -> ApiTokenInfo -> Kernel.Prelude.Text -> Environment.Flow [Common.PlanTranslationAPIEntity])
+getPlanManagementPlanTranslations merchantShortId opCity apiTokenInfo planId = do
+  checkedMerchantId <- merchantCityAccessCheck merchantShortId apiTokenInfo.merchant.shortId opCity apiTokenInfo.city
+  API.Client.ProviderPlatform.Management.callManagementAPI checkedMerchantId opCity (.planManagementDSL.getPlanManagementPlanTranslations) planId
