5b11810648ddb2f35029d19e3b91339fc9e11f10
Aadil-Ahmed <aadil27ahmed@gmail.com>
2026-07-20 11:23:18 +0530

    Fix: Adjust QR data expiry time calculation in postMultimodalOrderSublegSetOnboardedVehicleDetails

--- Backend/app/rider-platform/rider-app/Main/src/Domain/Action/UI/MultimodalConfirm.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Domain/Action/UI/MultimodalConfirm.hs
@@ -2868,7 +2868,7 @@ postMultimodalOrderSublegSetOnboardedVehicleDetails (mbPersonId, merchantId) jou
         mapConcurrently
           ( \ticketPayload -> do
               qrData <- DirectExternalBPP.generateQR integratedBPPConfig ticketPayload
-              QFRFSTicket.udpateQrDataAndValidTill qrData ticketPayload.expiryIST ticketBookingId ticketPayload.ticketNumber
+              QFRFSTicket.udpateQrDataAndValidTill qrData (addUTCTime (-19800) ticketPayload.expiryIST) ticketBookingId ticketPayload.ticketNumber
               pure qrData
           )
           updatedTickets
