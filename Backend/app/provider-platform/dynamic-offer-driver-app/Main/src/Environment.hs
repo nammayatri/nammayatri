7dd146587c2571d11d73dbee4f69e33511c44af5
Yashwanth1906 <yashwanthsakthivel01@gmail.com>
2026-07-22 20:24:52 +0530

    [chore]: Add env for jobFanouts and then put everything in MasterCell

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Environment.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Environment.hs
@@ -273,6 +273,7 @@ data AppEnv = AppEnv
     maxNotificationShards :: Int,
     gateNotifiedKeyShards :: Int,
     activeDriversListKeyShards :: Int,
+    enableDriverFeeShardedFanOut :: Bool,
     version :: Metrics.DeploymentVersion,
     enableRedisLatencyLogging :: Bool,
     enablePrometheusMetricLogging :: Bool,
@@ -396,6 +397,7 @@ buildAppEnv cfg@AppCfg {searchRequestExpirationSeconds = _searchRequestExpiratio
       Right env -> pure (Just env)
   let requestId = Nothing
   shouldLogRequestId <- fromMaybe False . (>>= readMaybe) <$> lookupEnv "SHOULD_LOG_REQUEST_ID"
+  enableDriverFeeShardedFanOut <- fromMaybe False . (>>= readMaybe) <$> lookupEnv "ENABLE_DRIVER_FEE_SHARDED_FAN_OUT"
   let sessionId = Nothing
   let kafkaProducerForART = Just kafkaProducerTools
   bppMetrics <- registerBPPMetricsContainer metricsSearchDurationTimeout
