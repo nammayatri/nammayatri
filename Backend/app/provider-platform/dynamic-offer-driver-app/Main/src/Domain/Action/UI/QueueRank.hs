418afdece8c1f50f7658e31706055baf5a72d4e7
manu johnson <manujohnson2002@gmail.com>
2026-07-31 15:28:08 +0530

    ench: send isBlockedForAirport for blocking UI

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/QueueRank.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Action/UI/QueueRank.hs
@@ -1,16 +1,21 @@
 module Domain.Action.UI.QueueRank where
 
+import qualified Domain.Types.DriverInformation as DI
 import qualified Domain.Types.Merchant as DM
 import qualified Domain.Types.MerchantOperatingCity as DMOC
 import qualified Domain.Types.Person as DP
 import qualified Environment
 import Kernel.Prelude
 import Kernel.Types.Id
+import Kernel.Utils.Common
 import qualified SharedLogic.External.LocationTrackingService.Flow as LTSFlow
+import qualified Storage.Queries.DriverInformation as QDI
+import Tools.Error
 
 data QueueRankResponse = QueueRankResponse
   { queuePositionRange :: Maybe (Int, Int),
-    queueSize :: Int
+    queueSize :: Int,
+    isBlockedForAirport :: Bool
   }
   deriving (Generic, FromJSON, ToJSON, ToSchema, Show)
 
@@ -20,9 +25,22 @@ getQueuePosition ::
   Text ->
   Environment.Flow QueueRankResponse
 getQueuePosition (driverId, _merchantId, _merchantOpCityId) specialLocationId vehicleType = do
-  ltsResp <- LTSFlow.getQueueDriverPosition specialLocationId vehicleType driverId
-  pure $
-    QueueRankResponse
-      { queuePositionRange = ltsResp.queuePositionRange,
-        queueSize = ltsResp.queueSize
-      }
+  driverInfo <- QDI.findById driverId >>= fromMaybeM DriverInfoNotFound
+  now <- getCurrentTime
+  enableForAirport <- QDI.resolveAirportRestriction now driverInfo
+  if enableForAirport == DI.BLOCKED
+    then
+      pure $
+        QueueRankResponse
+          { queuePositionRange = Nothing,
+            queueSize = 0,
+            isBlockedForAirport = True
+          }
+    else do
+      ltsResp <- LTSFlow.getQueueDriverPosition specialLocationId vehicleType driverId
+      pure $
+        QueueRankResponse
+          { queuePositionRange = ltsResp.queuePositionRange,
+            queueSize = ltsResp.queueSize,
+            isBlockedForAirport = False
+          }
