bbbb8a3583b3220122b697dfda0649234a6fc0a0
Om Dutt Pandey <omdutt2004@gmail.com>
2026-08-11 23:26:54 +0530

    fix : issue fetch api update

--- Backend/app/rider-platform/rider-app/Main/src/Domain/Action/UI/XyneWebhook.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Domain/Action/UI/XyneWebhook.hs
@@ -8,6 +8,7 @@ import qualified IssueManagement.Common as Common
 import qualified IssueManagement.Domain.Action.UI.XyneWebhook as XyneShared
 import qualified Kernel.External.Ticket.Interface.Types as Ticket
 import qualified Kernel.External.Ticket.XyneSpaces.Config as Xyne
+import qualified Kernel.External.Ticket.XyneSpaces.Types as XyneTypes
 import Kernel.External.Ticket.XyneSpaces.Webhook (RawByteString)
 import Kernel.Types.APISuccess (APISuccess)
 import Kernel.Types.Error
@@ -34,10 +35,10 @@ postXyneBearerWebhook mbAuth rawBody = do
   bearerToken <- asks (.xyneWebhookBearerToken)
   XyneShared.processXyneBearerWebhook bearerToken DDIM.dashboardIssueHandle Common.CUSTOMER mbAuth rawBody
 
-getXyneIssues :: Maybe UTCTime -> Maybe Int -> Maybe Int -> Maybe Text -> Flow [XyneShared.XyneIssueListItem]
+getXyneIssues :: Maybe UTCTime -> Maybe Int -> Maybe Int -> Maybe Text -> Flow [XyneTypes.XyneInboundReq]
 getXyneIssues mbSince mbLimit mbOffset mbAuth = do
   bearerToken <- asks (.xyneWebhookBearerToken)
-  XyneShared.fetchXyneIssues bearerToken mbSince mbLimit mbOffset mbAuth
+  XyneShared.fetchXyneIssues bearerToken DDIM.dashboardIssueHandle Common.CUSTOMER mbSince mbLimit mbOffset mbAuth
 
 lookupXyneCfg ::
   KId.Id Common.Merchant ->
