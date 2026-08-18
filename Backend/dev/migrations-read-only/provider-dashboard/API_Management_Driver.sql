1f7ad62f36e48da1a61f8455add5c8f4a92378ff
J-Karthikeyan <jkarthik2611@gmail.com>
2026-08-18 13:19:26 +0530

    feat: API to expose driver login OTP on BPP dashboard

--- Backend/dev/migrations-read-only/provider-dashboard/API_Management_Driver.sql
+++ Backend/dev/migrations-read-only/provider-dashboard/API_Management_Driver.sql
@@ -350,3 +350,9 @@ UPDATE atlas_dashboard.transaction
 
 -- {"api":"PostDriverUpdateTagBulk","migration":"userActionType","param":"ApiAuth DRIVER_OFFER_BPP_MANAGEMENT DRIVERS UPDATE_TAG_BULK","schema":"atlas_dashboard"}
 INSERT INTO atlas_dashboard.access_matrix (id, role_id, api_entity, user_access_type, user_action_type) ( SELECT atlas_dashboard.uuid_generate_v4(), T1.role_id, 'DSL', 'USER_FULL_ACCESS', 'PROVIDER_MANAGEMENT/DRIVER/POST_DRIVER_UPDATE_TAG_BULK' FROM atlas_dashboard.access_matrix AS T1 WHERE T1.user_access_type = 'USER_FULL_ACCESS' AND T1.api_entity = 'DRIVERS' AND T1.user_action_type = 'UPDATE_TAG_BULK' ) ON CONFLICT DO NOTHING;
+
+
+------- SQL updates -------
+
+-- {"api":"GetDriverLoginOtp","migration":"capability","param":"city-operations.pii.read","schema":"atlas_dashboard"}
+INSERT INTO atlas_dashboard.capability_endpoint (capability_id, server_name, endpoint_id) VALUES ( 'city-operations.pii.read', 'DASHBOARD', 'PROVIDER_MANAGEMENT/DRIVER/GET_DRIVER_LOGIN_OTP' ) ON CONFLICT DO NOTHING;
