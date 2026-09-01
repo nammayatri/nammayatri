30c140418c7010c24d877e84d6bad6f4ad290d3c
Shivam Rai <77096402+blaze-198@users.noreply.github.com>
2026-09-01 11:13:12 +0530

    Backend/enh/eligible_drivers_list_to_pick_from_ScheduleBoookingDashboard (#16534)

--- Backend/dev/migrations-read-only/provider-dashboard/Local_API_Management_ScheduledBooking.sql
+++ Backend/dev/migrations-read-only/provider-dashboard/Local_API_Management_ScheduledBooking.sql
@@ -33,3 +33,9 @@ INSERT INTO atlas_dashboard.role_capability (role_id, capability_id) VALUES ( '3
 
 -- {"api":"PostScheduledBookingUnassign","migration":"localAccessForRoleId","param":"37947162-3b5d-4ed6-bcac-08841be1534d","schema":"atlas_dashboard"}
 INSERT INTO atlas_dashboard.role_capability (role_id, capability_id) VALUES ( '37947162-3b5d-4ed6-bcac-08841be1534d', 'city-operations.scheduled-bookings.write' ) ON CONFLICT DO NOTHING;
+
+
+------- SQL updates -------
+
+-- {"api":"GetScheduledBookingNearbyDrivers","migration":"localAccessForRoleId","param":"37947162-3b5d-4ed6-bcac-08841be1534d","schema":"atlas_dashboard"}
+INSERT INTO atlas_dashboard.role_capability (role_id, capability_id) VALUES ( '37947162-3b5d-4ed6-bcac-08841be1534d', 'city-operations.scheduled-bookings.read' ) ON CONFLICT DO NOTHING;
