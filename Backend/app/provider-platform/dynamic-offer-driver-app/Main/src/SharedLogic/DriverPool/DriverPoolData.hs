dbd4698b35813f0ee6c10b76d3af6e4351e36f90
khuzema786 <khomosikhuzema894@gmail.com>
2026-09-06 12:17:24 +0530

    enhancement: Added GST & Udyam support within the Unified Document Register API

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/SharedLogic/DriverPool/DriverPoolData.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/SharedLogic/DriverPool/DriverPoolData.hs
@@ -57,7 +57,7 @@ data DriverPoolData = DriverPoolData
     -- Class 1 (preferences)
     enabled :: Bool,
     blocked :: Bool,
-    isDisabledReasonFlag :: Bool,
+    isDisabledReasonFlag :: Maybe Bool,
     subscribed :: Bool,
     canSwitchToRental :: Bool,
     canSwitchToInterCity :: Bool,
@@ -197,7 +197,7 @@ defaultDriverPoolData dId =
       selectedAutoAcceptTiers = Nothing,
       enabled = False,
       blocked = False,
-      isDisabledReasonFlag = False,
+      isDisabledReasonFlag = Nothing,
       subscribed = False,
       canSwitchToRental = False,
       canSwitchToInterCity = False,
