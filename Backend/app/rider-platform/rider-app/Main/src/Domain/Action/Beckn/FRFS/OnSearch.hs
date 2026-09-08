2e3cf723d593ed70c1e9be6aca6ba0548a2f013e
Mohamed Hamdan A <mdhamdan1111@gmail.com>
2026-09-08 13:30:30 +0530

    feat: live aware cancellation and rescheduling

--- Backend/app/rider-platform/rider-app/Main/src/Domain/Action/Beckn/FRFS/OnSearch.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Domain/Action/Beckn/FRFS/OnSearch.hs
@@ -715,6 +715,8 @@ createEntriesInFareTables merchantId merchantOperatingCityId routeCode startStop
                   maxRescheduleDaysAhead = Nothing,
                   maxCancellationCount = Nothing,
                   cancellationWindowSeconds = Nothing,
+                  cancellationDelayThresholdSeconds = Nothing,
+                  useLiveForCancellationAndRescheduling = Nothing,
                   createdAt = now,
                   updatedAt = now
                 }
