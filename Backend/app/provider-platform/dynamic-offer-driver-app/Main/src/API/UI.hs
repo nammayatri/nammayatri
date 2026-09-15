1ec8f2175da0f6db770e1be9a93340ea01e8029b
Yukesh <yukesh@Yukeshs-Office-MacBook-Pro.local>
2026-09-15 19:45:49 +0530

    fix: FY earnings

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/API/UI.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/API/UI.hs
@@ -26,6 +26,7 @@ import qualified API.Action.UI.CancellationReasonLookup as CancellationReasonLoo
 import qualified API.Action.UI.DemandHotspots as DemandHotspots
 import qualified API.Action.UI.DriverAreaPreference as DriverAreaPreference
 import qualified API.Action.UI.DriverDocument as DriverDocument
+import qualified API.Action.UI.DriverFyEarnings as DriverFyEarnings
 import qualified API.Action.UI.DriverIncentiveCoins as DriverIncentiveCoins
 import qualified API.Action.UI.DriverOnboardingV2 as DriverOnboardingV2
 import qualified API.Action.UI.DriverProfile as DriverProfile
@@ -195,6 +196,7 @@ type API =
            :<|> DriverDocument.API
            :<|> DriverAreaPreference.API
            :<|> AvailableForRides.API
+           :<|> DriverFyEarnings.API
        )
 
 handler :: FlowServer API
@@ -279,3 +281,4 @@ handler =
     :<|> DriverDocument.handler
     :<|> DriverAreaPreference.handler
     :<|> AvailableForRides.handler
+    :<|> DriverFyEarnings.handler
