11a380e351520c9a8a52c249f7ba33b16e4c6d05
Shivam Rai <77096402+blaze-198@users.noreply.github.com>
2026-09-21 18:40:45 +0530

    Backend/fix/minimum_wallet_shedule_ride_config_on_trip_and_service_ti… (#16843)

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Types/Extra/ConditionalCharges.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src/Domain/Types/Extra/ConditionalCharges.hs
@@ -10,8 +10,21 @@ import Kernel.Prelude
 import Kernel.Utils.GenericPretty (PrettyShow, Showable (..))
 import Tools.Beam.UtilsTH (mkBeamInstancesForEnumAndList)
 
-data ConditionalChargesCategories = SAFETY_PLUS_CHARGES | NO_CHARGES | NYREGULAR_SUBSCRIPTION_CHARGE | BOOKING_DEPOSIT
+data ConditionalChargesCategories
+  = SAFETY_PLUS_CHARGES
+  | NO_CHARGES
+  | NYREGULAR_SUBSCRIPTION_CHARGE
+  | BOOKING_DEPOSIT
+  | SCHEDULED_RIDE_MIN_WALLET_BALANCE
   deriving (Eq, Ord, ToJSON, FromJSON, ToSchema, Generic, Show, Read)
   deriving (PrettyShow) via Showable ConditionalChargesCategories
 
 $(mkBeamInstancesForEnumAndList ''ConditionalChargesCategories)
+
+isFareComponent :: ConditionalChargesCategories -> Bool
+isFareComponent = \case
+  SAFETY_PLUS_CHARGES -> True
+  NO_CHARGES -> True
+  NYREGULAR_SUBSCRIPTION_CHARGE -> True
+  BOOKING_DEPOSIT -> False
+  SCHEDULED_RIDE_MIN_WALLET_BALANCE -> False
