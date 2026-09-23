014a981deb99e3e5b23eb89a6ac43b65811da1a0
vinit-j13 <vinit.j@nammayatri.in>
2026-09-23 23:42:30 +0530

    backend/fix: Make downward fare recompute configurable via tripCategoriesForNoRecalc

--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/Storage/Beam/TransporterConfig.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/Storage/Beam/TransporterConfig.hs
@@ -10,6 +10,7 @@ import qualified Domain.Types.DriverInformation
 import qualified Domain.Types.Extra.MerchantPaymentMethod
 import qualified Domain.Types.Extra.TransporterConfig
 import qualified Domain.Types.TransporterConfig
+import qualified Domain.Types.Trip
 import qualified Domain.Types.UtilsTH
 import qualified Domain.Types.VehicleVariant
 import qualified Email.Types
@@ -351,6 +352,7 @@ data TransporterConfigT f = TransporterConfigT
     timeDiffFromUtc :: B.C f Kernel.Types.Common.Seconds,
     toNotifyDriverForExtraKmsLimitExceed :: B.C f Kernel.Prelude.Bool,
     trackingShortUrlPattern :: B.C f (Kernel.Prelude.Maybe Kernel.Prelude.Text),
+    tripCategoriesForNoRecalc :: B.C f (Kernel.Prelude.Maybe [Domain.Types.Trip.TripCategory]),
     tripEndGeofenceRadius :: B.C f (Kernel.Prelude.Maybe Kernel.Types.Common.Meters),
     tripStartGeofenceRadius :: B.C f (Kernel.Prelude.Maybe Kernel.Types.Common.Meters),
     tripStartLeadTime :: B.C f (Kernel.Prelude.Maybe Kernel.Types.Common.Minutes),
--- Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/Storage/Queries/TransporterConfig.hs
+++ Backend/app/provider-platform/dynamic-offer-driver-app/Main/src-read-only/Storage/Queries/TransporterConfig.hs
@@ -433,6 +433,7 @@ updateByPrimaryKey (Domain.Types.TransporterConfig.TransporterConfig {..}) = do
       Se.Set Beam.timeDiffFromUtc timeDiffFromUtc,
       Se.Set Beam.toNotifyDriverForExtraKmsLimitExceed toNotifyDriverForExtraKmsLimitExceed,
       Se.Set Beam.trackingShortUrlPattern trackingShortUrlPattern,
+      Se.Set Beam.tripCategoriesForNoRecalc tripCategoriesForNoRecalc,
       Se.Set Beam.tripEndGeofenceRadius tripEndGeofenceRadius,
       Se.Set Beam.tripStartGeofenceRadius tripStartGeofenceRadius,
       Se.Set Beam.tripStartLeadTime tripStartLeadTime,
