cdfcc754368a01bbe34c3ff458f7293e8f8475a6
Hari Krishna <harikrishnadupaguntla1@gmail.com>
2026-09-23 21:01:28 +0530

    fix withWaitAndLockMasterCloudCrossAppRedis

--- Backend/app/rider-platform/rider-app/Main/src/SharedLogic/Payment.hs
+++ Backend/app/rider-platform/rider-app/Main/src/SharedLogic/Payment.hs
@@ -169,8 +169,8 @@ orderStatusHandler ::
   m DPayment.PaymentStatusResp
 orderStatusHandler merchantOpCityId fulfillmentHandler paymentService paymentOrder orderStatusCall = do
   Redis.withWaitAndLockMasterCloudCrossAppRedis
-    "Redis"
-    "paymentOrderStatusLockWait"
+    "payment"
+    "waitForOrderStatusHandlerLock"
     makePaymentOrderStatusHandlerLockKey
     60
     100
